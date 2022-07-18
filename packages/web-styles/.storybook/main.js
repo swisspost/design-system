0b010194b3c64bed8b0f3fdf7015a9f53f19732b
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2022-07-18 06:27:07 +0200

    feat(web-styles): set up storybook docs (#141)
    Adds a storybook to the web styles for easier to test, more standard and easier to reuse documentation.
    
    Co-authored-by: Oliver Schürch <oli@skaos.ch>

---
+++ packages/web-styles/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "framework": "@storybook/html",
+  "stories": [
+    "../**/*.stories.@(js|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        sassLoaderOptions: {
+          includePaths: './node_modules'
+        }
+      }
+    }
+  ],
+}
