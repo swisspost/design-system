9771ef83e4da6c6a2d6e1027ccd10a0d96ee5132
debraya <alize.debray@post.ch>
2022-07-19 18:06:41 +0200

    Reduce algorithm complexity

--- packages/web-styles-migrations/src/utils/css/css-migration.ts
+++ packages/web-styles-migrations/src/utils/css/css-migration.ts
@@ -3,9 +3,12 @@ import {CssClassesUpdate} from "./css-classes-update";
 import {getCssMigrationRule} from "./css-migration-rule";
 
 export class CssMigration {
-    currentTag: string | null;
     updates: CssClassesUpdate[]
     rule: Rule;
+
+    set currentTag(value: string | null) {
+        this.updates.filter(update => !update.tag || update.tag === value);
+    }
 
     constructor(...updates: CssClassesUpdate[]) {
         this.updates = updates;
@@ -14,30 +17,23 @@ export class CssMigration {
 
     evaluate(classes: string = ''): boolean {
         const classList = classes.split(' ');
-        return this.updates.some(update => {
-            const incorrectTag = update.tag && update.tag !== this.currentTag;
-            const incorrectClasses = update.selector && !classList.includes(update.selector);
-            if (incorrectTag || incorrectClasses) {
-                return false;
-            }
+        const applicableUpdate = this.getApplicableUpdates(classList);
 
-            return classList.some(cssClass => update.searcher.test(cssClass));
+        return classList.some(cssClass => {
+            return applicableUpdate.some(update => update.searcher.test(cssClass));
         });
     }
 
     apply(classes: string = ''): string {
         const classList = classes.split(' ');
-        return this.updates.reduce((updatedClasses, update) => {
-            const incorrectTag = update.tag && update.tag !== this.currentTag;
-            const incorrectClasses = update.selector && !classList.includes(update.selector);
+        const applicableUpdate = this.getApplicableUpdates(classList);
 
-            if (incorrectTag || incorrectClasses) {
-                return updatedClasses;
-            }
+        return classList.map(cssClass => applicableUpdate.reduce((updatedClass, update) => {
+            return updatedClass.replace(update.searcher, update.replacer);
+        }, cssClass)).join(' ');
+    }
 
-            return updatedClasses
-                .map(currentClass => currentClass.replace(update.searcher, update.replacer))
-                .filter(updatedClass => !!updatedClass);
-        }, classList).join(' ');
+    private getApplicableUpdates(classList: string[]) {
+        return this.updates.filter(update => !!update.selector || classList.includes(update.selector));
     }
 }
