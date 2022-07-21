b2c145bcc85611d1e32047800d20284580e8f910
Philipp Gfeller <tuelsch@gmail.com>
2022-07-21 11:11:40 +0200

    build(storybook): clean up and fix dependencies
    Ignore storybook peer dependencies.

--- packages/web-styles/.storybook/main.js
+++ packages/web-styles/.storybook/main.js
@@ -1,19 +1,13 @@
 module.exports = {
   "framework": "@storybook/html",
   "stories": [
-    "../**/*.stories.@(js|mdx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/preset-scss",
-      options: {
-        sassLoaderOptions: {
-          implementation: require('sass')
-        }
-      }
-    }
+    "@storybook/preset-scss"
   ],
 }
