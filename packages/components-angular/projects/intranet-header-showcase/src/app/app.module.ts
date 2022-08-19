f93ef4c9a9c548866f218b1a69ec7ed3a2f30992
Philipp Gfeller <tuelsch@gmail.com>
2022-08-19 10:30:29 +0200

    fix: import header directly

--- packages/components-angular/projects/intranet-header-showcase/src/app/app.module.ts
+++ packages/components-angular/projects/intranet-header-showcase/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { SwissPostIntranetHeaderModule } from 'swisspost-intranet-header';
+import { SwissPostIntranetHeaderModule } from '../../../intranet-header/src/lib/swisspost-intranet-header.module';
 
 import { AppComponent } from './app.component';
 
