f1496008a2009b3ce48be2fc72d36c0f0b915eed
Philipp Gfeller <tuelsch@gmail.com>
2022-08-22 11:42:20 +0200

    fix(collapsible): update demo code and fix margin collapse

--- packages/demo/src/app/ng-bootstrap/components/collapse/collapse-demo/collapse-demo.component.ts
+++ packages/demo/src/app/ng-bootstrap/components/collapse/collapse-demo/collapse-demo.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ngb-collapse-demo',
-  templateUrl: './collapse-demo.component.html',
+  templateUrl: './collapse-demo.component.html'
 })
 export class NgbCollapseDemoComponent {
-  public isCollapsed : boolean;
-}
-
-
-export class NgbdCollapseBasic {
   public isCollapsed = false;
 }
-
