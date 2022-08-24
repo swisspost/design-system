84ba13a1304f16a370e9bc8d6c73c2caf1e33801
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2022-08-24 09:40:39 +0200

    feat: added Oli to the list of exalted Design System Devs (#407)
    Co-authored-by: Oliver Schürch <oli@skaos.ch>

--- packages/demo/src/app/common/footer/footer.component.ts
+++ packages/demo/src/app/common/footer/footer.component.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
-  constructor() { }
+  private devs = [
+    {
+      name: 'Philipp Gfeller',
+      title: 'Lead UI Developer',
+      avatar: 'https://avatars.githubusercontent.com/u/1659006?v=4'
+    },
+    {
+      name: 'Alizé Debray',
+      title: 'UI Developer',
+      avatar: 'https://avatars.githubusercontent.com/u/33580481?v=4'
+    },
+    {
+      name: 'Oliver Schürch',
+      title: 'UI Developer',
+      avatar: 'https://avatars.githubusercontent.com/u/9716662?v=4'
+    },
+    {
+      name: 'Jan Werren',
+      title: 'ICT Software Engineer',
+      avatar: 'https://avatars.githubusercontent.com/u/16400253?v=4'
+    }
+  ]
 
+  public shuffledDevs;
+
+  constructor() {
+    this.shuffledDevs = this.shuffleDevs();
+  }
+
+  shuffleDevs() {
+    return this.devs.sort(() => Math.random() > 0.5 ? 1 : -1);
+  }
 }
