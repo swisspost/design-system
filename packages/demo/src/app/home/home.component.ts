8239ccb138224466c0839ea446e5ef1ace603873
Oliver Schürch <oli@skaos.ch>
2022-09-02 14:12:06 +0200

    feat: add localStorage logic to migrationType Checkboxes

--- packages/demo/src/app/home/home.component.ts
+++ packages/demo/src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { environment } from './../../environments/environment';
   styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
+  public static MIGRATION_TYPE_INTRANET_KEY: string = 'post:migration_type_intranet';
+  public static MIGRATION_TYPE_ANGULAR_KEY: string = 'post:migration_type_angular';
   public version: string = environment.VERSION;
   public stylesVersion: string = environment.STYLES_VERSION;
   public angularVersion: string = environment.ANGULAR_VERSION;
@@ -15,14 +17,21 @@ export class HomeComponent {
   public ngBootstrapVersion: string = environment.NG_BOOTSTRAP_VERSION;
   public ngxToasterVersion: string = environment.NGX_TOASTER;
   public isIE11 = false;
-  public isMigratingIntranet = false;
-  public isMigratingAngular = true;
+  public isMigratingIntranet = this.getLocaleStorage(this.migrationTypeIntranetKey) ?? false;
+  public isMigratingAngular = this.getLocaleStorage(this.migrationTypeAngularKey) ?? true;
 
   constructor() {
     // Show deprecation warning if anybody still uses IE11
     this.isIE11 = window.navigator.userAgent.includes('Trident/7.0');
   }
 
+  get migrationTypeIntranetKey () {
+    return HomeComponent.MIGRATION_TYPE_INTRANET_KEY;
+  }
+
+  get migrationTypeAngularKey () {
+    return HomeComponent.MIGRATION_TYPE_ANGULAR_KEY;
+  }
 
   private getCleanVersion (version: string) {
     return version
@@ -54,5 +63,14 @@ export class HomeComponent {
     } else {
       return version;
     }
+  }
+
+  private getLocaleStorage (key: string) {
+    return JSON.parse(window.localStorage.getItem(key));
+  }
+
+  public setLocaleStorage (key: string, value: any) {
+    console.log(key, value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   }
 }
