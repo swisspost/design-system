c8edaccec1eb8fbfb82d9e853e23ef6a99040bb9
Oliver Schürch <oli@skaos.ch>
2022-09-22 10:59:18 +0200

    refactor: change pre regex, because of sonar security hotspot reporting

--- packages/demo/src/app/home/home.component.ts
+++ packages/demo/src/app/home/home.component.ts
@@ -144,7 +144,7 @@ export class HomeComponent {
     } else if (filter === 'patch' || filter === 'p') {
       matchArray = cleanVersion.match(/^(?:\d+\.\d+\.(\d+))/);
     } else if (filter === 'pre') {
-      matchArray = cleanVersion.match(/^(?:\d+\.\d+\.(?:\d+[^a-zA-Z0-9]|\d+)(.*))$/);
+      matchArray = cleanVersion.match(/^(?:\d+\.\d+\.\d+[\W_]?)(.*)/);
     } else if (filter === 'majorminor' || filter === 'Mm') {
       matchArray = cleanVersion.match(/^(?:(\d+\.\d+)\.\d+)/)
     } else if (filter === 'majorminorpatch' || filter === 'Mmp') {
