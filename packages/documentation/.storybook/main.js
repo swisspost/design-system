ebfa804571a052aa584173bf730f2fd94e47d5bf
Philipp Gfeller <philipp.gfeller@post.ch>
2022-10-05 08:45:51 +0200

    feat: set up storybook composition

--- packages/documentation/.storybook/main.js
+++ packages/documentation/.storybook/main.js
@@ -7,5 +7,15 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/web-components"
+  "framework": "@storybook/web-components",
+  "refs": {
+    styles: {
+      title: "Basics",
+      url: "http://localhost:6007"
+    },
+    components: {
+      title: "Components",
+      url: "http://localhost:6008"
+    }
+  }
 }
