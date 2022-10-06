f6f0b93b32f86fdcdfe77c6f7e1c55229ef9ceca
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-06 11:35:16 +0200

    fix(styles-percy): revert button-overview test spec

--- packages/styles/cypress/integration/button-overview.spec.js
+++ packages/styles/cypress/integration/button-overview.spec.js
@@ -1,62 +1,55 @@
 import snapshotConfig from '../percy/snapshot-configs/lg.json'
 
-describe('Home Page', () => {
+describe('Snapshot Button States', () => {
+  beforeEach(() => {
+    cy.visit('/iframe.html?args=&id=design-system-button--page&viewMode=story')
+  })
+
   it('successfully loads', () => {
     cy.visit('/')
     cy.percySnapshot('Home Page', snapshotConfig)
   })
+
+  it('displays focussed buttons on dark background', function () {
+    cy
+      .get('.sbdocs-preview')
+      .invoke('addClass', 'bg-dark')
+      .find('.btn')
+      .invoke('addClass', 'pretend-focus')
+    cy.percySnapshot('@dark: Button State: focus', snapshotConfig)
+  })
+
+  it('displays hovered buttons on dark background', function () {
+    cy
+      .get('.sbdocs-preview')
+      .invoke('addClass', 'bg-dark')
+      .find('.btn')
+      .invoke('addClass', 'pretend-hover')
+    cy.percySnapshot('@dark: Button State: hover', snapshotConfig)
+  })
+
+  it('displays default buttons on dark background', function () {
+    cy
+      .get('.sbdocs-preview')
+      .invoke('addClass', 'bg-dark')
+    cy.percySnapshot('@dark: Button State: default', snapshotConfig)
+  })
+
+  it('displays focussed buttons on light background', function () {
+    cy
+      .get('.btn')
+      .invoke('addClass', 'pretend-focus')
+    cy.percySnapshot('@light: Button State: focus', snapshotConfig)
+  })
+
+  it('displays hovered buttons on light background', function () {
+    cy
+      .get('.btn')
+      .invoke('addClass', 'pretend-hover')
+    cy.percySnapshot('@light: Button State: hover', snapshotConfig)
+  })
+
+  it('displays default buttons on light background', function () {
+    cy.percySnapshot('@light: Button State: default', snapshotConfig)
+  })
 })
-
-// describe('Snapshot Button States', () => {
-//   beforeEach(() => {
-//     cy.visit('/iframe.html?args=&id=design-system-button--page&viewMode=story')
-//   })
-
-//   it('successfully loads', () => {
-//     cy.visit('/')
-//     cy.percySnapshot('Home Page', snapshotConfig)
-//   })
-
-//   it('displays focussed buttons on dark background', function () {
-//     cy
-//       .get('.sbdocs-preview')
-//       .invoke('addClass', 'bg-dark')
-//       .find('.btn')
-//       .invoke('addClass', 'pretend-focus')
-//     cy.percySnapshot('@dark: Button State: focus', snapshotConfig)
-//   })
-
-//   it('displays hovered buttons on dark background', function () {
-//     cy
-//       .get('.sbdocs-preview')
-//       .invoke('addClass', 'bg-dark')
-//       .find('.btn')
-//       .invoke('addClass', 'pretend-hover')
-//     cy.percySnapshot('@dark: Button State: hover', snapshotConfig)
-//   })
-
-//   it('displays default buttons on dark background', function () {
-//     cy
-//       .get('.sbdocs-preview')
-//       .invoke('addClass', 'bg-dark')
-//     cy.percySnapshot('@dark: Button State: default', snapshotConfig)
-//   })
-
-//   it('displays focussed buttons on light background', function () {
-//     cy
-//       .get('.btn')
-//       .invoke('addClass', 'pretend-focus')
-//     cy.percySnapshot('@light: Button State: focus', snapshotConfig)
-//   })
-
-//   it('displays hovered buttons on light background', function () {
-//     cy
-//       .get('.btn')
-//       .invoke('addClass', 'pretend-hover')
-//     cy.percySnapshot('@light: Button State: hover', snapshotConfig)
-//   })
-
-//   it('displays default buttons on light background', function () {
-//     cy.percySnapshot('@light: Button State: default', snapshotConfig)
-//   })
-// })
