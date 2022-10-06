763375e91b264523287b56485e2f06674b4fb4eb
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-06 11:13:45 +0200

    fix(styles-percy): remove e2e example test

--- packages/styles/cypress/e2e/load.cy.js
+++
@@ -1,9 +0,0 @@
-import snapshotConfig from '../percy/snapshot-configs/lg.json'
-
-
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/')
-    cy.percySnapshot('Home Page', snapshotConfig)
-  })
-})
