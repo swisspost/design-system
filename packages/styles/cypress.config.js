39c93562a3016e74636d1276d136977e2dddb657
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-06 14:00:28 +0200

    fix(styles-percy): revert remove specPattern in e2e config

--- packages/styles/cypress.config.js
+++ packages/styles/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9301'
+    baseUrl: 'http://localhost:9301',
+    specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}'
   },
   video: false
 })
