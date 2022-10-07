af7e38da86aafa2819347ed0d049ce1569a45fc0
Philipp Gfeller <philipp.gfeller@post.ch>
2022-10-07 16:18:13 +0200

    chore: update storybook config ref urls

--- packages/documentation/.storybook/main.js
+++ packages/documentation/.storybook/main.js
@@ -1,21 +1,30 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components",
-  "refs": {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/web-components',
+  refs: {
     styles: {
-      title: "Basics",
-      url: "http://localhost:6007"
+      title: 'Basics',
+      url: 'http://localhost:6007',
     },
     components: {
-      title: "Components",
-      url: "http://localhost:6008"
-    }
-  }
-}
+      title: 'Components',
+      url: 'http://localhost:6008',
+    },
+  },
+  // Storybook composition
+  // https://storybook.js.org/docs/react/sharing/storybook-composition#compose-storybooks-per-environment
+  refs: (_config, { configType }) => {
+    const dev = configType === 'DEVELOPMENT';
+    return {
+      styles: {
+        title: 'Foundation',
+        url: dev ? 'http://localhost:9201' : 'https://styles.design-system.post.ch',
+      },
+      components: {
+        title: 'Components',
+        url: dev ? 'http://localhost:9203' : 'https://components.design-system.post.ch',
+      },
+    };
+  },
+};
