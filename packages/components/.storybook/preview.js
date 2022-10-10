c5f1d40ca71c0d00fae2025057b338b6a573c341
Philipp Gfeller <philipp.gfeller@post.ch>
2022-10-10 14:15:15 +0200

    fix(components): import basic styles

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,6 +1,7 @@
 import { defineCustomElements } from '../loader';
 import { setStencilDocJson } from '@pxtrn/storybook-addon-docs-stencil';
 import docJson from '../dist/docs.json';
+import '@swisspost/design-system-styles/basics.css';
 
 if(docJson) setStencilDocJson(docJson);
 defineCustomElements();
