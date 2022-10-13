3d7226c879154bae5c713fecf3b7dbce811e6409
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-13 12:13:51 +0200

    feat(components): add util formatPropValueList

--- packages/components/src/utils/utils.ts
+++ packages/components/src/utils/utils.ts
@@ -1,3 +1,14 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export function formatPropValueList (values: string[], allowAllValues: boolean = false) {
+  let valueList = values;
+  
+  if (!allowAllValues) valueList = valueList.filter(v => typeof v === 'string' && v !== '');
+
+  return valueList
+    .map(v => `<code>${v}</code>`)
+    .join(', ')
+    .replace(/,(?=[^,]*$)/, ' or')
+}
