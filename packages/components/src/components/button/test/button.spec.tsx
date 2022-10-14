8fbd58164ab5378591cb63734f2deee27d64b259
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-14 15:47:20 +0200

    test(components): add basic tests for component button

--- packages/components/src/components/button/test/button.spec.tsx
+++ packages/components/src/components/button/test/button.spec.tsx
@@ -1,17 +1,55 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PostButton } from '../button';
 
-describe('p-button', () => {
+describe('post-button', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [PostButton],
-      html: `<post-button></post-button>`,
+      html: `<post-button>Button</post-button>`,
     });
+
     expect(page.root).toEqualHtml(`
       <post-button>
         <mock:shadow-root>
-          <slot></slot>
+          <button class="btn" type="button">
+            <slot></slot>
+          </button>
         </mock:shadow-root>
+        Button
+      </post-button>
+    `);
+  });
+
+  it('renders as link', async () => {
+    const page = await newSpecPage({
+      components: [PostButton],
+      html: `<post-button tag="a" href="#" target="_self">Button</post-button>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <post-button href="#" tag="a" target="_self">
+        <mock:shadow-root>
+          <a class="btn" href="#" target="_self">
+            <slot></slot>
+          </a>
+        </mock:shadow-root>
+        Button
+      </post-button>
+    `);
+  });
+
+  it('renders as input', async () => {
+    const page = await newSpecPage({
+      components: [PostButton],
+      html: `<post-button tag="input">Button</post-button>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <post-button tag="input">
+        <mock:shadow-root>
+          <input class="btn" type="button" value="Button">
+        </mock:shadow-root>
+        Button
       </post-button>
     `);
   });
