bec3a5ddbee51a351f8fe7b097fef91546844a49
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-19 14:55:42 +0200

    feat(migrations): remove create-commits flag

--- packages/migrations/src/.bin/migrate.js
+++ packages/migrations/src/.bin/migrate.js
@@ -2,7 +2,7 @@
 
 const { exec } = require('child_process');
 
-exec('npx ng update @swisspost/design-system-styles --from=4 --to=5 --migrate-only --create-commits', (error, stdout, stderr) => {
+exec('npx ng update @swisspost/design-system-styles --from=4 --to=5 --migrate-only', (error, stdout, stderr) => {
   if (error) {
     console.error(`ERROR: ${error.message}`);
     return;
