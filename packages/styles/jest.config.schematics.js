3541c338469f68d65bf3dc0d41bf83006c28c59b
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-20 16:29:12 +0200

    refactor(migration-styles): update jest config

---
+++ packages/styles/jest.config.schematics.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        "tsconfig": "tsconfig.schematics.json"
+      }
+    ]
+  }
+}
