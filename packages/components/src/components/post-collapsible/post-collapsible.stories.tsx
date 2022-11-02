c1ec2fadcb93ecf75f029cece65b1402235437dc
debraya <alize.debray@gmail.com>
2022-11-02 11:47:54 +0100

    feat(components): add nested collapsible example

--- packages/components/src/components/post-collapsible/post-collapsible.stories.tsx
+++ packages/components/src/components/post-collapsible/post-collapsible.stories.tsx
@@ -1,40 +1,64 @@
 export default {
   title: 'Components/post-collapsible',
   component: 'post-collapsible',
+  argTypes: {
+    nested: {
+      name: 'nested collapsible',
+      table: { category: 'Contents' },
+      type: { name: 'boolean', required: false },
+      defaultValue: false,
+      description: 'If `true`, displays a post-collapsible element nested inside the main one.',
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
 };
 
 const defaultArgs = {
   collapsed: false,
 };
 
-const DefaultTemplate = ({ collapsed }) =>
+const DefaultTemplate = ({ collapsed, nested }) =>
   `<post-collapsible${collapsed ? ' collapsed="true"' : ''}>
     <div slot="header">Collapsible header</div>
-    <div slot="body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</div>
+    <div slot="body">
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>${nested ? `
+      <post-collapsible>
+        <div slot="header">Nested collapsible header</div>
+        <div slot="body">Nested collapsible body.</div>
+      </post-collapsible>` : ''}
+    </div>
   </post-collapsible>`;
 
 export const Default = DefaultTemplate.bind({});
 Default.args = { ...defaultArgs };
 
-const NoHeaderTemplate = ({ collapsed }) =>
+const NoHeaderTemplate = ({ collapsed, nested }) =>
   `<script>
-    function triggerCollapse(open) {
-        const collapsible = document.querySelector('post-collapsible');
+    function triggerCollapse(collapsibleControl, open) {
+        const collapsibleId = collapsibleControl.getAttribute('aria-controls');
+        const collapsible = document.querySelector('#' + collapsibleId);
         collapsible.toggle(open).then(() => {
-            const controls = document.querySelectorAll('[aria-controls="collapsibleExample"]');
+            const controls = document.querySelectorAll('[aria-controls=' + collapsibleId + ']');
             controls.forEach(control => control.setAttribute('aria-expanded', String(!collapsible.collapsed)));
         });
     }
   </script>
 
-  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse()">Toggle</button>
-  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(true)">Show</button>
-  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(false)">Hide</button>
+  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this)">Toggle</button>
+  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this, true)">Show</button>
+  <button aria-controls="collapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this, false)">Hide</button>${nested ? `
+
+  <button aria-controls="nestedCollapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this)">Toggle nested</button>
+  <button aria-controls="nestedCollapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this, true)">Show nested</button>
+  <button aria-controls="nestedCollapsibleExample" aria-expanded="${!collapsed}" onclick="triggerCollapse(this, false)">Hide nested</button>` : ''}
 
   <hr/>
 
   <post-collapsible id="collapsibleExample"${collapsed ? ' collapsed="true"' : ''}>
-    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>${nested ? `
+    <post-collapsible id="nestedCollapsibleExample">Nested collapsible content</post-collapsible>` : ''}
   </post-collapsible>`;
 
 export const NoHeader = NoHeaderTemplate.bind({});
