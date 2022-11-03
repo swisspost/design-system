537a0d438d6fe14f29741faff516cd51478bc6f2
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 16:45:06 +0100

    feat(styles-migration): add automatic migration for form-control component

--- packages/styles/schematics/migrations/bootstrap/form-group/index.ts
+++ packages/styles/schematics/migrations/bootstrap/form-group/index.ts
@@ -1,7 +1,7 @@
 import { Rule } from '@angular-devkit/schematics';
 import DomMigration from '../../../utils/dom/migration';
 import IDomUpdate from '../../../utils/dom/update';
-import { Cheerio } from 'cheerio';
+import { Cheerio, CheerioAPI } from 'cheerio';
 
 export default function (): Rule {
   return new DomMigration(
@@ -13,10 +13,19 @@ export default function (): Rule {
 class FormGroupClassUpdate implements IDomUpdate {
   selector = '.form-group';
 
-  update ($elements: Cheerio<any>) {
+  update ($elements: Cheerio<any>, $: CheerioAPI) {
     $elements
-      .removeClass('form-group')
-      .addClass('mb-regular');
+      // @ts-ignore
+      .each((i, element) => {
+        const $element = $(element);
+        const isFloatingLabelGroup = $element.find('input.form-control.form-control-lg').length > 0;
+
+        if (!isFloatingLabelGroup) {
+          $element
+            .removeClass('form-group')
+            .addClass('mb-regular');
+        }
+      })
   }
 }
 
