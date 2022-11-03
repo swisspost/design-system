a0950b03a4112faf8574773d4d38b7ed1cd90df2
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 15:55:28 +0100

    feat(styles-migrations): add automatic migration for text-auto

---
+++ packages/styles/schematics/migrations/general/classes/text-auto.ts
@@ -0,0 +1,18 @@
+import { Rule } from '@angular-devkit/schematics';
+import DomMigration from '../../../utils/dom/migration';
+import IDomUpdate from '../../../utils/dom/update';
+import { Cheerio } from 'cheerio';
+
+export default function (): Rule {
+  return new DomMigration(
+    new TextAutoClassUpdate
+  ).rule;
+}
+
+class TextAutoClassUpdate implements IDomUpdate {
+  selector = '.text-auto';
+
+  update ($elements: Cheerio<any>) {
+    $elements.removeClass('text-auto');
+  }
+}
