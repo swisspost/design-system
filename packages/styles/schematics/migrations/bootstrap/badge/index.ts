e39be02cc8f682c6fd3029c23b30d6eb691d69d4
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 11:31:37 +0100

    feat(styles-migrations): add automatic migration for bootstrap badge

---
+++ packages/styles/schematics/migrations/bootstrap/badge/index.ts
@@ -0,0 +1,92 @@
+import { Rule } from '@angular-devkit/schematics';
+import DomMigration from '../../../utils/dom/migration';
+import IDomUpdate from '../../../utils/dom/update';
+import { Cheerio } from 'cheerio';
+
+import { themeColors } from '../../../utils/constants';
+
+export default function (): Rule {
+  return new DomMigration(
+    new BadgePillClassUpdate,
+    new BadgeBGClassUpdate,
+    new BadgeOutlineClassUpdate,
+    new BadgeCararraClassUpdate,
+    new BadgeCararraThickClassUpdate
+  ).rule;
+}
+
+class BadgePillClassUpdate implements IDomUpdate {
+  selector = '.badge-pill';
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .removeClass('badge-pill')
+      .addClass('rounded-pill');
+  }
+}
+
+class BadgeBGClassUpdate implements IDomUpdate {
+  cssClassRegex: RegExp = new RegExp(`^badge-(${themeColors.join('|')})$`);
+
+  selector = themeColors.map(colorname => `.badge-${colorname}`).join(', ');
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .attr('class')
+      ?.split(' ')
+      .forEach(cssClass => {
+        const match = cssClass.match(this.cssClassRegex);
+        
+        if (match) {
+          const colorname = match[1];
+          
+          $elements
+            .removeClass(cssClass)
+            .addClass(`bg-${colorname}`);
+        }
+      });
+  }
+}
+
+class BadgeOutlineClassUpdate implements IDomUpdate {
+  cssClassRegex: RegExp = new RegExp(`^badge-outline-(${themeColors.join('|')})$`);
+
+  selector = themeColors.map(colorname => `.badge-outline-${colorname}`).join(', ');
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .attr('class')
+      ?.split(' ')
+      .forEach(cssClass => {
+        const match = cssClass.match(this.cssClassRegex);
+        
+        if (match) {
+          const colorname = match[1];
+          
+          $elements
+            .removeClass(cssClass)
+            .addClass(`border-${colorname}`);
+        }
+      });
+  }
+}
+
+class BadgeCararraClassUpdate implements IDomUpdate {
+  selector = '.badge-gray-cararra';
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .removeClass('badge-gray-cararra')
+      .addClass('bg-light');
+  }
+}
+
+class BadgeCararraThickClassUpdate implements IDomUpdate {
+  selector = '.badge-outline-gray-cararra-thick';
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .removeClass('badge-outline-gray-cararra-thick')
+      .addClass('border-light border-2');
+  }
+}
