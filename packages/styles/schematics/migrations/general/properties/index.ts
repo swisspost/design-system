8455d7ef1e19e9d89cbc4719763d0f28cc30895b
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 14:59:59 +0100

    feat(styles-migrations): add automatic migration for rtl

---
+++ packages/styles/schematics/migrations/general/properties/index.ts
@@ -0,0 +1,97 @@
+import { Rule } from '@angular-devkit/schematics';
+import DomMigration from '../../../utils/dom/migration';
+import IDomUpdate from '../../../utils/dom/update';
+import { Cheerio, CheerioAPI } from 'cheerio';
+
+import { breakpoints, sizes } from "../../../utils/constants";
+
+export default function (): Rule {
+  return new DomMigration(
+    new SpacingClassesUpdate,
+    new AlignmentClassesUpdate
+  ).rule;
+}
+
+class SpacingClassesUpdate implements IDomUpdate {
+  cssClassRegex = new RegExp(`^(m|p)(l|r)(?:-(${breakpoints.join('|')}))?-(${sizes.join('|')})$`);
+  sideUpdate = new Map([['l', 's'], ['r', 'e']]);
+
+  selector = '[class*="ml-"], [class*="mr-"], [class*="pl-"], [class*="pr-"]';
+
+  update ($elements: Cheerio<any>, $: CheerioAPI) {
+    $elements
+      // @ts-ignore
+      .each((i, element) => {
+        const $element = $(element);
+
+        $element
+          .attr('class')
+          ?.split(' ')
+          .forEach(cssClass => {
+            const match = cssClass.match(this.cssClassRegex);
+    
+            if (match) {
+              const property = match[1];
+              const side = match[2];
+              const breakpoint = match[3];
+              const size = match[4];
+              
+              $element
+                .removeClass(cssClass)
+                .addClass(`${property}${this.sideUpdate.get(side)}${breakpoint ? `-${breakpoint}` : ''}-${size}`);
+            }
+          });
+      })
+  }
+}
+
+class AlignmentClassesUpdate implements IDomUpdate {
+    cssClassRegex = new RegExp(`^(float|text)(?:-(${breakpoints.join('|')}))?-(left|right)$`);
+    sideUpdate = new Map([['left', 'start'], ['right', 'end']]);
+  
+    selector = '[class*="float-"], [class*="text-"]';
+  
+    update ($elements: Cheerio<any>, $: CheerioAPI) {
+      $elements
+        // @ts-ignore
+        .each((i, element) => {
+          const $element = $(element);
+  
+          $element
+            .attr('class')
+            ?.split(' ')
+            .forEach(cssClass => {
+              const match = cssClass.match(this.cssClassRegex);
+      
+              if (match) {
+                const property = match[1];
+                const breakpoint = match[2];
+                const side = match[3];
+                
+                $element
+                  .removeClass(cssClass)
+                  .addClass(`${property}${breakpoint ? `-${breakpoint}` : ''}-${this.sideUpdate.get(side)}`);
+              }
+            });
+        })
+    }
+  }
+
+// //////////////////////////
+// // Horizontal direction have been renamed to use start and end in lieu of left and right
+
+
+// class TextAlignmentAndFloatClassesUpdate extends CssClassesUpdate {
+//     properties = ['text', 'float'];
+//     sides = ['left', 'right'];
+
+//     searchValue = oneOf(this.properties) + optional('-' + oneOf(breakpoints)) + '-' + oneOf(this.sides);
+
+//     replaceValue = (property: string, breakpoint: string, side: string) => {
+//         return property + (breakpoint ? '-' + breakpoint : '') + '-' + update(side);
+//     };
+// }
+
+// function update(side: string): string | undefined {
+//     return new Map([['l', 's'], ['r', 'e'], ['left', 'start'], ['right', 'end']]).get(side);
+// }
