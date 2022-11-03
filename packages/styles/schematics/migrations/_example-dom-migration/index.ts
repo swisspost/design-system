64609ce374534f7669b657bfc07e3fc1e19592b7
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 16:06:34 +0100

    feat(styles-migration): add cheerio to integrate more flexible migration schematics (#651)

---
+++ packages/styles/schematics/migrations/_example-dom-migration/index.ts
@@ -0,0 +1,87 @@
+/*
+ * This is a dom-migration example
+ * 
+ * You can write more than just one update in a migration schematic
+ * by defining multiple DomUpdates and pass them all to the DomMigration Class.
+ * 
+ * Important note:
+ * The order of the DomUpdates is important, because updates are additive.
+ * This means, every update you register will receive the updated DOM from
+ * the previous update, and can now make it's own changes.
+ * 
+ * We use cheerio thogether with htmlparser2 under the hood which makes it possible
+ * to write updates by defining only two simple parameters:
+ * 
+ * selector: string
+ * This is the jQuery like selector which is used to find the elements to be updated.
+ * 
+ * update: Function
+ * This is the update function, which receives the cheerio-element as the first argument
+ * and the cheerio-instance as the second.
+ * On cheerio-elements you can directly run jQuery like functions to mutate them.
+ * The cheerio-instance is sometimes helpfull, for example to convert elements in loops
+ * into a new cheerio-element:
+ * $elements.filter((i, element) => $(element).hasClass('bla')).addClass('bla-2');
+ * 
+ * Note:
+ * It is not possible to remove the selected elements itself.
+ * Instead you can select the parent elements and then search the elements to remove
+ * and call the remove function on this elements directly.
+ * See RemoveElementUpdate example.
+ * 
+ * To try this example, you must add it as a schematic in your collection.json file
+ * and you need to add the following code somewhere in your html that should get updated:
+ * 
+ * <div id="example-dom-element"></div>
+ * 
+*/
+
+import { Rule } from '@angular-devkit/schematics';
+import DomMigration from '../../utils/dom/migration';
+import IDomUpdate from '../../utils/dom/update';
+import type { Cheerio } from 'cheerio';
+
+export default function (): Rule {
+  return new DomMigration(
+    new AddElementUpdate,
+    new AddClassUpdate,
+    new AddAttributeUpdate,
+    new AddTextUpdate,
+    new RemoveElementUpdate
+  ).rule;
+}
+
+class AddElementUpdate implements IDomUpdate {
+  selector = '#example-dom-element';
+  update = function ($elements: Cheerio<any>) {
+    $elements.append('<div>It\'s working...</div><div class="remove-example"></div>');
+  }
+}
+
+class AddClassUpdate implements IDomUpdate {
+  selector = '#example-dom-element > div:not([class])';
+  update = function ($elements: Cheerio<any>) {
+    $elements.addClass('inner');
+  }
+}
+
+class AddAttributeUpdate implements IDomUpdate {
+  selector = '#example-dom-element .inner';
+  update = function ($elements: Cheerio<any>) {
+    $elements.attr('style', 'padding: 10px; background-color: white;');
+  }
+}
+
+class AddTextUpdate implements IDomUpdate {
+  selector = '#example-dom-element .inner';
+  update = function ($elements: Cheerio<any>) {
+    $elements.text(`${$elements.text()} cheerio!`);
+  }
+}
+
+class RemoveElementUpdate implements IDomUpdate {
+  selector = '#example-dom-element';
+  update = function ($elements: Cheerio<any>) {
+    $elements.find('.remove-example').remove();
+  }
+}
---
+++ packages/styles/schematics/utils/dom/migration-rule.ts
@@ -0,0 +1,85 @@
+import { relative } from 'path';
+import { normalize } from '@angular-devkit/core';
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { getProjectTsConfigPaths } from "@angular/core/schematics/utils/project_tsconfig_paths";
+import { canMigrateFile, createMigrationProgram } from "@angular/core/schematics/utils/typescript/compiler_host";
+import { NgComponentTemplateVisitor } from "@angular/core/schematics/utils/ng_component_template";
+import { SourceFile } from "typescript";
+import * as cheerio from 'cheerio';
+import DomMigration from './migration';
+
+export default function DomMigrationRule (migration: DomMigration): Rule {
+  return async (tree: Tree, _context: SchematicContext) => {
+    const { buildPaths, testPaths } = await getProjectTsConfigPaths(tree);
+    const basePath = process.cwd();
+    
+    if (!buildPaths.length && !testPaths.length) {
+      throw new SchematicsException('Could not find any tsconfig file. Cannot check templates for empty routerLinks.');
+    }
+
+    for (const tsconfigPath of [...buildPaths, ...testPaths]) {
+      const { program } = createMigrationProgram(tree, tsconfigPath, basePath);
+      const sourceFiles = program.getSourceFiles().filter((sourceFile: SourceFile) => canMigrateFile(basePath, sourceFile, program));
+      const templateVisitor = new NgComponentTemplateVisitor(program.getTypeChecker(), basePath, tree);
+
+      sourceFiles.forEach((sourceFile: SourceFile) => templateVisitor.visitNode(sourceFile));
+
+      for (const template of templateVisitor.resolvedTemplates) {
+        const treeFilePath = relative(normalize(basePath), normalize(template.filePath));
+        let sourceCode = tree.read(treeFilePath)?.toString() ?? '';
+
+        if (!sourceCode) continue;
+
+        for (const [updateKey, { update, selector }] of Object.entries(migration.updates)) {
+          const context = migration.updates[Number(updateKey)];
+
+          // create cheerio dom tree
+          const $ = cheerio.load(sourceCode, {
+            xmlMode: true,
+            decodeEntities: false,
+            lowerCaseTags: false,
+            lowerCaseAttributeNames: false,
+            recognizeSelfClosing: true,
+            withStartIndices: true,
+            withEndIndices: true
+          }, false);
+
+          // get "cheerioElement[]" to mutate in "update" method
+          const $elements = $(selector);
+          
+          // continue to next migration.update if no elements were found
+          if ($elements.length <= 0) continue;
+          
+          // create "cheerioElement[]" out of cheerio elements
+          const elementsArray = Array.from($elements);
+          // map source elements as "string[]" for later comparison
+          const sourceElements = elementsArray.map(element => $(element).toString());
+          // start tree file recorder to update tree file later
+          const treeUpdateRecorder = tree.beginUpdate(treeFilePath);
+          
+          // send "cheerioElement[]" and cheerio instance to the "update" method
+          // after this "cheerioElement[]" in "$elements" are updated
+          update.bind(context)($elements, $);
+          
+          elementsArray
+            .forEach((element, index) => {
+              const $element = $(element);
+              
+              // continue to next "element", if eighter "element" has not been updated or "element" has no indices
+              if (sourceElements[index] === $element.toString() || element.startIndex === null || element.endIndex === null) return;
+              
+              // remove old "element" out of tree file
+              treeUpdateRecorder.remove(element.startIndex, element.endIndex- element.startIndex + 1);
+              // write new "element" into the tree file
+              treeUpdateRecorder.insertLeft(element.startIndex, $element.toString());
+            });
+          
+          // commit changes in tree file to tree
+          tree.commitUpdate(treeUpdateRecorder);
+          // update "sourceCode" for chained "DomUpdates" in same migration schematic
+          sourceCode = tree.read(treeFilePath)?.toString() ?? '';
+        }
+      }
+    }
+  };
+}
