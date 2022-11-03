8ca86135c79e4aef419ceafb1c110217f68c796d
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-03 14:00:27 +0100

    feat(styles-migrations): add automatic migration for topic-teaser

---
+++ packages/styles/schematics/migrations/post/topic-teaser/index.ts
@@ -0,0 +1,47 @@
+import { Rule } from '@angular-devkit/schematics';
+import DomMigration from '../../../utils/dom/migration';
+import IDomUpdate from '../../../utils/dom/update';
+import { Cheerio } from 'cheerio';
+
+export default function (): Rule {
+  return new DomMigration(
+    new TopicTeaserImageWidthAndHeightUpdate,
+    new TopicTeaserImageClassesUpdate,
+    new TopicTeaserContentClassesUpdate,
+    new TopicTeaserLinkListClassesUpdate
+  ).rule;
+}
+
+class TopicTeaserImageWidthAndHeightUpdate implements IDomUpdate {
+  selector = '.topic-teaser-image';
+
+  update ($elements: Cheerio<any>) {
+    $elements
+      .attr('width', '100%')
+      .attr('height', '100%');
+  }
+}
+
+class TopicTeaserImageClassesUpdate implements IDomUpdate {
+  selector = '.topic-teaser-image-container';
+
+  update ($elements: Cheerio<any>) {
+    $elements.removeClass('col-10 col-rg-8 col-lg-4');
+  }
+}
+
+class TopicTeaserContentClassesUpdate implements IDomUpdate {
+  selector = '.topic-teaser-content';
+    
+  update ($elements: Cheerio<any>) {
+    $elements.removeClass('col-12 col-lg-8');
+  }
+}
+
+class TopicTeaserLinkListClassesUpdate implements IDomUpdate {
+  selector = '.topic-teaser ul.link-list';
+      
+  update ($elements: Cheerio<any>) {
+    $elements.removeClass('font-curve-regular');
+  }
+}
