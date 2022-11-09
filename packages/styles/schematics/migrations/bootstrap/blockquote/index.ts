d3e980d907bd385359aa424a332333e9bd318784
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-09 16:30:54 +0100

    feat(styles-migration): add automatic migration for bootstrap blockquote component (#735)

--- packages/styles/schematics/migrations/bootstrap/blockquote/index.ts
+++ packages/styles/schematics/migrations/bootstrap/blockquote/index.ts
@@ -1,20 +1,46 @@
 import { Rule } from '@angular-devkit/schematics';
 import DomMigration from '../../../utils/dom/migration';
 import IDomUpdate from '../../../utils/dom/update';
-import { Cheerio } from 'cheerio';
+import { Cheerio, AnyNode, CheerioAPI } from 'cheerio';
 
 export default function (): Rule {
   return new DomMigration(
+    new BlockquoteFigureWrapperUpdate,
     new BlockquotePClassUpdate
   ).rule;
 }
 
-// TODO: Refactored blockquote with footer. Such blockquotes now need to be nested in a figure tag. In addition, the tag footer.blockquote-footer became figcaption.blockquote-footer.
+class BlockquoteFigureWrapperUpdate implements IDomUpdate {
+  selector = 'blockquote.blockquote';
+
+  update ($elements: Cheerio<AnyNode>, $: CheerioAPI) {
+    $elements
+      .each((_i, element) => {
+        const $element = $(element);
+        const $footer = $element.find('footer.blockquote-footer');
+        const hasFooter = $footer.length > 0;
+
+        if (hasFooter) {
+          const $wrapper = $('<figure/>').data($element.data());
+          const $replacer = $(`<figcaption>${$footer.prop('innerHTML')}</figcaption>`);
+
+          $footer
+            .prop('attributes')
+            .forEach(({ name, value }) => {
+              $replacer.attr(name, value);
+            });
+
+          $footer.replaceWith($replacer);
+          $element.wrap($wrapper);
+        }
+      });
+  }
+}
 
 class BlockquotePClassUpdate implements IDomUpdate {
   selector = 'blockquote.blockquote > p.mb-0';
 
-  update ($elements: Cheerio<any>) {
+  update ($elements: Cheerio<AnyNode>) {
     $elements.removeClass('mb-0');
   }
 }
