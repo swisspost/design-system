77966fd51b9a0f95a1bbd7f2b0dcfed1849e0476
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-18 17:03:15 +0100

    feat(documentation): add example component story

---
+++ packages/documentation/stories/components/example.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// import custom files
+import '../../../styles/src/basics.scss';
+// import '../../../styles/src/components/example.scss';
+
+// define component config
+export default {
+  title: 'Components/Example',
+  args: {
+    state: true
+  },
+  argTypes: {
+    state: {
+      name: 'State',
+      description: 'Example argument.',
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
+};
+
+// define template
+const Template = (args, story) => <div data-story-id={story.id} class="border-primary">{args.state.toString()}</div>;
+
+// define story
+export const Default = Template.bind({});
+
+// define other story
+export const Falsy = Template.bind({});
+
+// override component parameters, args, argTypes for specific story
+Falsy.args = {
+  state: false
+};
