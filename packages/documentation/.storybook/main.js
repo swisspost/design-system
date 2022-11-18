6a3b054d31ad958f671b46f5c295ab77e32a96e9
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-18 16:43:55 +0100

    refactor(documentation): setup storybook/react

---
+++ packages/documentation/.storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  framework: '@storybook/react',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        sassLoaderOptions: {
+          implementation: require('sass')
+        }
+      }
+    }
+  ],
+  stories: ['../stories/**/*.stories.@(ts|tsx|mdx)'],
+
+  // workaround, to prevent storybook from crashing, because of a EBUSY error, which occures on a npm cache file on storybook startup and when saving new content
+  managerWebpack: (config, options) => {
+    options.cache.set = () => Promise.resolve();
+    return config;
+  }
+};
