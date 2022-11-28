305b04a48c7e6a2c772541301d5b289dec4dde62
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-28 15:06:05 +0100

    feat(documentation): add alert buttons and remove icon-variant

--- packages/documentation/stories/components/alert.stories.tsx
+++ packages/documentation/stories/components/alert.stories.tsx
@@ -6,22 +6,12 @@ import docsPage from './alert.docs.mdx';
 import '@swisspost/design-system-styles/basics.scss';
 import '@swisspost/design-system-styles/components/alert.scss';
 import '@swisspost/design-system-styles/components/icons.scss';
-import './alert.stories.scss';
 
 export default {
   title: 'Components/Alert',
   parameters: {
     docs: {
       page: docsPage
-    },
-    controls: {
-      exclude: [
-        'Icon',
-        'Icon Color',
-        'No Icon',
-        'Dismissible',
-        'Fixed'
-      ]
     }
   },
   args: {
@@ -29,11 +19,10 @@ export default {
     content: '<p>Contentus momentus vero siteos et accusam iretea et justo.</p>',
     variant: 'alert-primary',
     icon: '',
-    iconVariant: '',
     noIcon: false,
     dismissible: false,
-    dismiss: false,
     fixed: false,
+    buttons: false,
     show: false
   },
   argTypes: {
@@ -93,32 +82,6 @@ export default {
       ],
       table: { category: 'Icon' }
     },
-    iconVariant: {
-      name: 'Icon Color',
-      control: {
-        type: 'select',
-        labels: {
-          '': 'Default',
-          'white': 'White',
-          'primary': 'primary',
-          'success': 'success',
-          'info': 'info',
-          'warning': 'warning',
-          'danger': 'danger',
-        }
-      },
-      options: [
-        '',
-        'white',
-        'primary',
-        'success',
-        'info',
-        'warning',
-        'danger'
-      ],
-      if: { arg: 'icon', neq: '' },
-      table: { category: 'Icon' }
-    },
     noIcon: {
       name: 'No Icon',
       control: {
@@ -129,91 +92,154 @@ export default {
     dismissible: {
       name: 'Dismissible',
       control: { type: 'boolean' },
-      if: { arg: 'fixed', truthy: false },
-      table: { category: 'Dismissible' }
-    },
-    dismiss: {
-      name: 'Dismiss',
-      control: { type: 'boolean' },
-      if: { arg: 'dismissible' },
       table: { category: 'Dismissible' }
     },
     fixed: {
       name: 'Fixed',
       control: { type: 'boolean' },
-      if: { arg: 'dismissible', truthy: false },
       table: { category: 'Fixed' }
+    },
+    buttons: {
+      name: 'Buttons',
+      control: { type: 'boolean' },
+      table: { category: 'Buttons' }
     },
     show: {
       name: 'Show',
-      control: { type: 'boolean' },
-      if: { arg: 'fixed' },
-      table: { category: 'Fixed' }
+      control: { type: 'boolean' }
     }
   }
 };
 
 const Template = args => {
   const [_, updateArgs] = useArgs();
-  
-  const dismissPropName = args.dismissible ? 'dismiss' : 'show';
-  const dismissButton = <button className="btn-close" data-dismiss="alert" aria-label="Close" onClick={ () => updateArgs({ [dismissPropName]: !args[dismissPropName] }) }></button>;
-  const toggleButton = <button className="btn btn-secondary" onClick={ () => updateArgs({ show: !args.show }) }>Toggle alert</button>;
-
-  const alertClasses = [
-    'alert',
-    args.variant,
-    `${args.icon ? `pi-${args.icon}${args.iconVariant ? `--${args.iconVariant}` : ''}` : ''}`,
-    args.noIcon ? 'no-icon' : '',
-    args.dismissible ? 'alert-dismissible' : '',
-    args.fixed ? 'alert-fixed-bottom' : ''
-  ].filter(c => c).join(' ');
-
-  const alert = <div className={ alertClasses } role="alert">
-    { args.dismissible || args.fixed ? dismissButton : null }
-    <h4 className="alert-heading">{ args.title }</h4>
-    <p>{ parse(args.content) }</p>
-  </div>;
 
   return <div>
-    { args.fixed ? toggleButton : null }
-    { (args.dismissible && !args.dismiss) || (args.fixed && args.show) || (!args.dismissible && !args.fixed) ? alert : null }
+    { getToggleButton() }
+    { getAlert() }
   </div>;
+
+  function getAlert() {
+    const isOnlyDismissibleAndDismissed = !args.fixed && !args.buttons && args.dismissible && args.show;
+    const isFixedAndVisible = args.fixed && args.show;
+    const isNotDismissibleNorFixed = !args.dismissible && !args.fixed
+
+    const classes = [
+      'alert',
+      args.variant,
+      `${args.icon ? `pi-${args.icon}` : ''}`,
+      args.noIcon ? 'no-icon' : '',
+      args.dismissible ? 'alert-dismissible' : '',
+      args.fixed ? 'alert-fixed-bottom' : '',
+      args.buttons ? 'alert-action' : ''
+    ].filter(c => c).join(' ');
+
+    return isOnlyDismissibleAndDismissed || isFixedAndVisible || isNotDismissibleNorFixed ? <div className={ classes } role="alert">
+      { getDismissButton() }
+      { getContent() }
+      { getButtons() }
+    </div> : getResetButton();
+  }
+
+  function getContent () {
+    const content = [
+      <h4 className="alert-heading">{ args.title }</h4>,
+      <p>{ parse(args.content) }</p>
+    ];
+
+    return args.buttons ? <div class="alert-content">{ content }</div> : content;
+  }
+
+  function getDismissButton () {
+    return args.dismissible ? <button className="btn-close" data-dismiss="alert" aria-label="Close" onClick={ toggle }></button> : null;
+  }
+
+  function getToggleButton () {
+    return args.fixed ? <button className="btn btn-secondary" onClick={ toggle }>Toggle alert</button> : null;
+  }
+
+  function getButtons () {
+    return args.buttons ? <div className="alert-buttons">
+      <button class="btn btn-primary btn-animated" onClick={ toggle }><span>Akcepti</span></button>
+      <button class="btn btn-secondary btn-animated" onClick={ toggle }><span>Aborti</span></button>
+    </div> : null;
+  }
+
+  function getResetButton () {
+    return args.dismissible && !args.fixed ? <a href="#" onClick={ toggle }>reset</a> : null;
+  }
+
+  function toggle (e) {
+    e.preventDefault();
+    updateArgs({ show: !args.show });
+  }
 };
 
 export const Variant = Template.bind({});
+Variant.parameters = {
+  controls: {
+    exclude: [
+      'Icon',
+      'Icon Color',
+      'No Icon',
+      'Dismissible',
+      'Fixed',
+      'Buttons',
+      'Show'
+    ]
+  }
+};
 
 export const CustomIcon = Template.bind({});
 CustomIcon.parameters = {
   controls: {
     exclude: [
+      'Variant',
       'No Icon',
       'Dismissible',
-      'Fixed'
+      'Fixed',
+      'Buttons',
+      'Show'
     ]
   }
-}
+};
 CustomIcon.args = {
-  variant: 'alert-danger',
-  icon: '2023',
-  iconVariant: 'white'
+  variant: 'alert-info',
+  icon: '2023'
 };
 
 export const WithoutIcon = Template.bind({});
 WithoutIcon.parameters = {
   controls: {
     exclude: [
+      'Variant',
+      'Icon',
       'Dismissible',
-      'Fixed'
+      'Fixed',
+      'Buttons',
+      'Show'
     ]
   }
-}
+};
 WithoutIcon.args = {
   variant: 'alert-info',
   noIcon: true
 };
 
 export const Content = Template.bind({});
+Content.parameters = {
+  controls: {
+    exclude: [
+      'Variant',
+      'Icon',
+      'No Icon',
+      'Dismissible',
+      'Fixed',
+      'Buttons',
+      'Show'
+    ]
+  }
+};
 Content.args = { 
   content: `<p>Contentum momentum ipsum tipsum sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
   <ul>
@@ -227,11 +253,56 @@ Content.args = {
 };
 
 export const Dismissible = Template.bind({});
+Dismissible.parameters = {
+  controls: {
+    exclude: [
+      'Variant',
+      'Icon',
+      'No Icon',
+      'Fixed',
+      'Buttons',
+      'Show'
+    ]
+  }
+};
 Dismissible.args = {
-  dismissible: true
+  dismissible: true,
+  show: true
 };
 
 export const Fixed = Template.bind({});
+Fixed.parameters = {
+  controls: {
+    exclude: [
+      'Variant',
+      'Icon',
+      'No Icon',
+      'Dismissible',
+      'Buttons',
+      'Show'
+    ]
+  }
+};
 Fixed.args = {
+  dismissible: true,
   fixed: true
+};
+
+export const Buttons = Template.bind({});
+Buttons.parameters = {
+  controls: {
+    exclude: [
+      'Variant',
+      'Icon',
+      'No Icon',
+      'Dismissible',
+      'Fixed',
+      'Show'
+    ]
+  }
+};
+Buttons.args = {
+  variant: 'alert-info',
+  fixed: true,
+  buttons: true
 };
