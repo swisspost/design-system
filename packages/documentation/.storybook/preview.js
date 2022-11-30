6ba4d68e62dc98168f27c1ae81409aa4e8a9acfb
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-30 17:26:01 +0100

    feat(documentation): add source transformation, to get rid of the react like html-examples

--- packages/documentation/.storybook/preview.js
+++ packages/documentation/.storybook/preview.js
@@ -17,6 +17,19 @@ export const parameters = {
       order: ['Welcome', 'Foundations', 'Components', 'Utilities', 'Misc'],
     },
   },
+  docs: {
+    transformSource(snippet) {
+      return snippet
+        // remove "key" attributes
+        .replace(/(\t+|\s+)?key=".*"/g, '')
+        // remove brackets from "{value}" attribute-values 
+        .replace(/={([^}]+)}/g, (match, g1) => `="${g1}"`)
+        // replace "className" attributes with "class"
+        .replace(/className/g, 'class')
+        // replace "htmlFor" attributes with "for"
+        .replace(/htmlFor/g, 'for');
+    }
+  },
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     hideNoControlsWarning: true,
