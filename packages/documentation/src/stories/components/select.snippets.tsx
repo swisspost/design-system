e293703416c8467c64996ee039577708ea285293
Oliver Schürch <oliver.schuerch@post.ch>
2022-11-30 17:26:49 +0100

    feat(documentation): add bootstrap/form-select stories

---
+++ packages/documentation/src/stories/components/select.snippets.tsx
@@ -0,0 +1,10 @@
+export const PackageImports = `@import "@swisspost/design-system-styles/index.scss";`;
+
+export const ComponentImports = `@import "@swisspost/design-system-styles/basis.scss";
+@import '@swisspost/design-system-styles/components/form-select.scss';
+
+// required if you use floating-labels
+@import '@swisspost/design-system-styles/components/floating-label.scss';
+
+// required if you use validation feedbacks
+@import '@swisspost/design-system-styles/components/form-feedback.scss';`;
---
+++ packages/documentation/src/stories/components/select.stories.tsx
@@ -0,0 +1,437 @@
+import React from 'react';
+import { Meta, Story, Args } from "@storybook/react";
+import docsPage from './select.docs.mdx';
+
+import '@swisspost/design-system-styles/basics.scss';
+import '@swisspost/design-system-styles/components/form-select.scss';
+import '@swisspost/design-system-styles/components/floating-label.scss';
+import '@swisspost/design-system-styles/components/form-feedback.scss';
+
+interface ISelectOption {
+  text: string,
+  label: string,
+  value?: string | number,
+  selected?: boolean,
+  disabled?: boolean
+};
+
+export default {
+  title: 'Components/Form/Select',
+  parameters: {
+    docs: {
+      page: docsPage
+    }
+  },
+  args: {
+    label: 'Label',
+    options: [
+      { text: 'Elektu opcion...' },
+      { text: 'Opcion 1', value: 1 },
+      { text: 'Opcion 2', value: 2 },
+      { text: 'Opcion 3', value: 3 },
+      { text: 'Opcion 4', value: 4 },
+      { text: 'Opcion 5', value: 5 },
+    ] as ISelectOption[],
+    floatingLabel: false,
+    size: 'null',
+    multiple: false,
+    multipleSize: 4,
+    hint: 'Hintus textus elare volare cantare hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.',
+    disabled: false,
+    validation: 'null',
+    validFeedback: 'Ggranda sukceso!',
+    invalidFeedback: 'Eraro okazis!'
+  },
+  argTypes: {
+    label: {
+      name: 'Label',
+      description: 'Describes the content/topic of the component.',
+      control: {
+        type: 'text'
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlElement | HtmlAttribute'
+        }
+      }
+    },
+    options: {
+      name: 'Options',
+      description: `Array of items to render in the component.<br>Single option example:<div><code>{<br/>&nbsp;&nbsp;text: string,<br/>&nbsp;&nbsp;label: string,<br/>&nbsp;&nbsp;value?: string | number,<br/>&nbsp;&nbsp;selected?: boolean,<br/>&nbsp;&nbsp;disabled?: boolean<br/>}</code></div>`,
+      control: {
+        type: 'object',
+        raw: false
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlElement[]'
+        }
+      }
+    },
+    floatingLabel: {
+      name: 'Floating Label',
+      description: 'Defines how the components label is rendered.',
+      control: {
+        type: 'boolean'
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlElement'
+        }
+      }
+    },
+    size: {
+      name: 'Size',
+      description: 'Sets the size of the component\'s appearance.',
+      if: {
+        arg: 'floatingLabel',
+        truthy: false
+      },
+      control: {
+        type: 'select',
+        labels: {
+          'form-select-sm': 'Small',
+          'form-select-rg': 'Regular',
+          'null': 'Middle',
+          'form-select-lg': 'Large'
+        }
+      },
+      options: [
+        'form-select-sm',
+        'form-select-rg',
+        'null',
+        'form-select-lg'
+      ],
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlAttribute'
+        }
+      }
+    },
+    multiple: {
+      name: 'Multiple',
+      description: 'When set, allows multiple options to be selected (multi-select).',
+      control: {
+        type: 'boolean'
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlAttribute'
+        }
+      }
+    },
+    multipleSize: {
+      name: 'Multiple Size',
+      description: 'When set to a number larger than 0, will set the number of display option rows.<div className="text-danger">Note: not all browser will respect this setting.</div>',
+      if: {
+        arg: 'multiple'
+      },
+      control: {
+        type: 'number',
+        min: 0,
+        max: 10,
+        step: 1
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlAttribute'
+        }
+      }
+    },
+    hint: {
+      name: 'Helper Text',
+      description: 'Text to place in the help text area of the component.',
+      control: {
+        type: 'text'
+      },
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HtmlElement'
+        }
+      }
+    },
+    disabled: {
+      name: 'Disabled',
+      description: 'When set to `true`, disables the component\'s functionality and places it in a disabled state.',
+      control: {
+        type: 'boolean'
+      },
+      table: {
+        category: 'States',
+        type: {
+          summary: 'HtmlAttribute'
+        }
+      }
+    },
+    validation: {
+      name: 'Validation',
+      description: 'Controls the validation state appearance of the component.',
+      control: {
+        type: 'select',
+        labels: {
+          'null': 'Default',
+          'is-valid': 'Valid',
+          'is-invalid': 'Invalid'
+        }
+      },
+      options: [
+        'null',
+        'is-valid',
+        'is-invalid'
+      ],
+      table: {
+        category: 'States',
+        type: {
+          summary: 'HtmlAttribute'
+        }
+      }
+    },
+    validFeedback: {
+      name: 'Valid Feedback',
+      description: 'Text to show when the component is in an valid state.',
+      control: {
+        type: 'text'
+      },
+      table: {
+        category: 'States',
+        type: {
+          summary: 'HtmlElement'
+        }
+      }
+    },
+    invalidFeedback: {
+      name: 'Invalid Feedback',
+      description: 'Text to show when the component is in an invalid state.',
+      control: {
+        type: 'text'
+      },
+      table: {
+        category: 'States',
+        type: {
+          summary: 'HtmlElement'
+        }
+      }
+    }
+  }
+} as Meta;
+
+const Template = (args: Args, story: Story) => {
+  const id = `SelectExample-${story.name}`;
+  const classes = [
+    'form-select',
+    args.size,
+    args.validation
+  ].filter(c => c && c !== 'null').join(' ');
+
+  const options = args.options.map((option: ISelectOption, index: number) => <option key={ index } label={ option.label } value={ option.value } selected={ option.selected } disabled={ option.disabled }>{ option.text }</option>);
+
+  const select = <select
+    key="key"
+    id={ id }
+    className={ classes }
+    multiple= { args.multiple }
+    size={ args.multipleSize }
+    disabled={ args.disabled }
+    aria-label={ args.label }
+    aria-invalid={ args.validation === 'is-valid' ? 'false' : args.validation === 'is-invalid' ? 'true' : undefined }
+  >{ options }</select>;
+
+  const contextuals: (JSX.Element | null)[] = [
+    args.validation === 'is-valid' ? <p key="valid" className="valid-feedback">{ args.validFeedback }</p> : null,
+    args.validation === 'is-invalid' ? <p key="invalid" className="invalid-feedback">{ args.invalidFeedback }</p> : null,
+    args.hint !== '' ? <div key="hint" className="form-text">{ args.hint }</div> : null
+  ].filter(f => f !== null);
+
+  if (args.floatingLabel) {
+    return <div className="form-floating">
+      { select }
+      <label htmlFor={ id } className="form-label">{ args.label }</label>
+      { contextuals }
+    </div>;
+  } else { 
+    return [select, contextuals];
+  }
+};
+
+export const Default: Story = Template.bind({});
+
+export const SelectedOption: Story = Template.bind({});
+SelectedOption.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Floating Label',
+      'Multiple',
+      'Size',
+      'Helper Text',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+SelectedOption.args = {
+  options: [
+    { text: 'Elektu opcion...' },
+    { text: 'Opcion 1', value: 1 },
+    { text: 'Opcion 2', value: 2 },
+    { text: 'Opcion 3', value: 3, selected: true },
+    { text: 'Opcion 4', value: 4 },
+    { text: 'Opcion 5', value: 5 },
+  ] as ISelectOption[],
+  hint: ''
+};
+
+export const DisabledOption: Story = Template.bind({});
+DisabledOption.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Floating Label',
+      'Multiple',
+      'Size',
+      'Helper Text',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+DisabledOption.args = {
+  options: [
+    { text: 'Elektu opcion...', selected: true, disabled: true },
+    { text: 'Opcion 1', value: 1 },
+    { text: 'Opcion 2', value: 2 },
+    { text: 'Opcion 3', value: 3, disabled: true },
+    { text: 'Opcion 4', value: 4, disabled: true },
+    { text: 'Opcion 5', value: 5 },
+  ] as ISelectOption[],
+  hint: ''
+};
+
+export const FloatingLabel: Story = Template.bind({});
+FloatingLabel.parameters = {
+  controls: {
+    exclude: [
+      'Options',
+      'Multiple',
+      'Size',
+      'Helper Text',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+FloatingLabel.args = {
+  floatingLabel: true,
+  hint: ''
+};
+
+export const Size: Story = Template.bind({});
+Size.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Options',
+      'Floating Label',
+      'Multiple',
+      'Helper Text',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+Size.args = {
+  hint: ''
+};
+
+export const Multiple: Story = Template.bind({});
+Multiple.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Options',
+      'Floating Label',
+      'Size',
+      'Helper Text',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+Multiple.args = {
+  multiple: true,
+  hint: ''
+};
+
+export const HelperText: Story = Template.bind({});
+HelperText.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Options',
+      'Floating Label',
+      'Size',
+      'Multiple',
+      'Disabled',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+
+export const Disabled: Story = Template.bind({});
+Disabled.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Options',
+      'Floating Label',
+      'Size',
+      'Multiple',
+      'Helper Text',
+      'Validation',
+      'Valid Feedback',
+      'Invalid Feedback'
+    ]
+  }
+};
+Disabled.args = {
+  disabled: true,
+  hint: ''
+};
+
+export const Validation: Story = Template.bind({});
+Validation.parameters = {
+  controls: {
+    exclude: [
+      'Label',
+      'Options',
+      'Floating Label',
+      'Size',
+      'Multiple',
+      'Helper Text',
+      'Disabled'
+    ]
+  }
+};
+Validation.args = {
+  validation: 'is-invalid',
+  hint: ''
+};
