12de598aca68eb245cb4d44cb6588640cc3e8e9a
Oliver Schürch <oliver.schuerch@post.ch>
2022-12-01 09:59:31 +0100

    chore(documentation): update scss import statments of component select

--- packages/documentation/src/stories/components/select/select.snippets.tsx
+++ packages/documentation/src/stories/components/select/select.snippets.tsx
@@ -1,10 +1,10 @@
-export const PackageImports = `@import "@swisspost/design-system-styles/index.scss";`;
+export const PackageImports = `@use "@swisspost/design-system-styles/index.scss";`;
 
-export const ComponentImports = `@import "@swisspost/design-system-styles/basis.scss";
-@import '@swisspost/design-system-styles/components/form-select.scss';
+export const ComponentImports = `@use "@swisspost/design-system-styles/basis.scss";
+@use '@swisspost/design-system-styles/components/form-select.scss';
 
 // required if you use floating-labels
-@import '@swisspost/design-system-styles/components/floating-label.scss';
+@use '@swisspost/design-system-styles/components/floating-label.scss';
 
 // required if you use validation feedbacks
-@import '@swisspost/design-system-styles/components/form-feedback.scss';`;
+@use '@swisspost/design-system-styles/components/form-feedback.scss';`;
