a85a54b28dab1f9207ba6ef42c02837c9fc18949
Oliver Schürch <oliver.schuerch@post.ch>
2022-12-14 12:38:29 +0100

    chore(documentation): fix typos

--- packages/documentation/src/stories/components/radio/radio.stories.tsx
+++ packages/documentation/src/stories/components/radio/radio.stories.tsx
@@ -72,7 +72,7 @@ export default {
     },
     disabled: {
       name: 'Disabled',
-      description: 'When set to `true`, disables the component\'s functionality and places it in a disabled state.<span className="mt-mini alert alert-info alert-sm">There are accessibility issues with the disabled state.<br/>Please read our <a href="/?path=/docs/foundations-accessibility--page#disabled-state">disabled state accessibility guide</a>.</span>',
+      description: 'When set to `true`, disables the component\'s functionality and places it in a disabled state.<span className="mt-mini alert alert-info alert-sm">There are accessibility concerns with the disabled state.<br/>Please read our <a href="/?path=/docs/foundations-accessibility--page#disabled-state">disabled state accessibility guide</a>.</span>',
       control: {
         type: 'boolean'
       },
