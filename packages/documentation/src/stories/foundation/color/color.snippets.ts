7f99cbcf9100728b5b9f391468707096d8d7469b
Philipp Gfeller <philipp.gfeller@post.ch>
2022-12-15 08:07:52 +0100

    refactor: inline snippets

--- packages/documentation/src/stories/foundation/color/color.snippets.ts
+++
@@ -1,9 +0,0 @@
-export const cssBackground = `.background {
-  background-color: var(--post-yellow);
-}`;
-
-export const scssBackground = `@use '@swisspost/design-system-styles/core' as post;
-
-.background {
-  background-color: post.$yellow;
-}`;
