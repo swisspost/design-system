f8f674d3c49ac927cad4e8c0f0463c288d1f0d5d
Philipp Gfeller <philipp.gfeller@post.ch>
2022-12-15 10:30:35 +0100

    style: lint preview.js

--- packages/documentation/.storybook/preview.js
+++ packages/documentation/.storybook/preview.js
@@ -51,24 +51,26 @@ export const parameters = {
       excludeDecorators: true,
     },
     transformSource(snippet) {
-      return snippet
-        // remove react fragments
-        .replace(/(<>|<\/>)/g, '')
+      return (
+        snippet
+          // remove react fragments
+          .replace(/(<>|<\/>)/g, '')
 
-        // remove "key" attributes
-        .replace(/(\t+|\s+)?key=".*"/g, '')
+          // remove "key" attributes
+          .replace(/(\t+|\s+)?key=".*"/g, '')
 
-        // repalce noRefCheck functions
-        .replace(/function noRefCheck\(\){}/g, '() => {}')
+          // repalce noRefCheck functions
+          .replace(/function noRefCheck\(\){}/g, '() => {}')
 
-        // remove brackets from "{value}" attribute-values
-        .replace(/([a-zA-Z][a-zA-Z0-9-_:.]+)={([^}]*}?)}/g, (_m, g1, g2) => `${g1}="${g2}"`)
+          // remove brackets from "{value}" attribute-values
+          .replace(/([a-zA-Z][a-zA-Z0-9-_:.]+)={([^}]*}?)}/g, (_m, g1, g2) => `${g1}="${g2}"`)
 
-        // replace "className" attributes with "class"
-        .replace(/className/g, 'class')
+          // replace "className" attributes with "class"
+          .replace(/className/g, 'class')
 
-        // replace "htmlFor" attributes with "for"
-        .replace(/htmlFor/g, 'for')
+          // replace "htmlFor" attributes with "for"
+          .replace(/htmlFor/g, 'for')
+      );
     },
   },
   actions: { argTypesRegex: '^on[A-Z].*' },
