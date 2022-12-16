0e478764f4542f01ee2657e2c15b621917c619c8
Philipp Gfeller <philipp.gfeller@post.ch>
2022-12-16 14:29:07 +0100

    chore: add specpattern

--- packages/documentation/cypress.config.js
+++ packages/documentation/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9300'
+    baseUrl: 'http://localhost:9300',
+    specPattern: ['./cypress/e2e/**/*.spec.ts']
   },
   includeShadowDom: true,
   retries: {
