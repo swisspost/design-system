a49d209e96135f729de51863d246e5bed7c64d79
Philipp Gfeller <philipp.gfeller@post.ch>
2023-01-09 23:45:57 +0100

    feat: added the post-icon component

---
+++ packages/components/src/components/post-icon/post-icon.tsx
@@ -0,0 +1,38 @@
+import { Component, Host, h, Prop, State } from '@stencil/core';
+
+@Component({
+  tag: 'post-icon',
+  styleUrl: 'post-icon.scss',
+  shadow: true,
+})
+export class PostIcon {
+  @Prop() name: number;
+  @Prop() base?: string;
+  @State() path: string;
+
+  constructor() {
+    // Construct icon path from different possible sources
+    let basePath: string;
+    const metaBase = document.head.querySelector(
+      'meta[name="design-system-settings"][data-post-icon-base]',
+    );
+    if (metaBase) basePath = metaBase.getAttribute('data-post-icon-base');
+    if (this.base) basePath = this.base;
+    if (!basePath) basePath = `https://unpkg.com/@swisspost/design-system-icons/public/svg`;
+
+    this.path = new URL(
+      [...basePath.split('/'), `${this.name}.svg#icon`].join('/'),
+      window.location.origin,
+    ).toString();
+  }
+
+  render() {
+    return (
+      <Host>
+        <svg viewBox="0 0 32 32">
+          <use href={this.path} />
+        </svg>
+      </Host>
+    );
+  }
+}
