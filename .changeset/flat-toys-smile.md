bd044a78eb1c714d240d19904972716c0f0ce534
Oliver Schürch <oliver.schuerch@post.ch>
2023-01-17 16:02:18 +0100

    feat(documentation): add typography documentation (#901)

---
+++ .changeset/flat-toys-smile.md
@@ -0,0 +1,5 @@
+---
+'@swisspost/design-system-documentation': minor
+---
+
+Added utilities/text docs page.
--- packages/documentation/.storybook/components/docs/header.js
+++ packages/documentation/.storybook/components/docs/header.js
@@ -1,24 +1,16 @@
 import React from "react";
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <header className="docs-header">
-        <div className="container py-5">
-          <h1 className="mt-0 bold font-curve-large">Swiss Post<br/>Design System</h1>
-          <p className="font-monospace small">
-            v{ this.props.getVersion('styles') ?? '' }
-          </p>
-          <p>
-            The Swiss Post Design System pattern library for a unified and accessible user experience
-            across the web platform.
-          </p>
-        </div>
-      </header>
-    );
-  }
-};
+export default (props) => (
+  <header className="docs-header bg-yellow">
+    <div className="container py-5">
+      <h1 className="mt-0 bold font-curve-large">Swiss Post<br/>Design System</h1>
+      <p className="font-monospace small">
+        v{ props?.getVersion('styles') ?? '' }
+      </p>
+      <p>
+        The Swiss Post Design System pattern library for a unified and accessible user experience
+        across the web platform.
+      </p>
+    </div>
+  </header>
+);
