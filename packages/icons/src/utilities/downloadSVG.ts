d77d8ca016435be1912cf15aae3e924e404529e1
Oliver Schürch <oliver.schuerch@post.ch>
2023-01-26 10:11:52 +0100

    chore(icons): include symbol in icons to be able to use it with the use tag

--- packages/icons/src/utilities/downloadSVG.ts
+++ packages/icons/src/utilities/downloadSVG.ts
@@ -28,7 +28,12 @@ export const downloadSVG = async (icon: IIcon, output: string) => {
       throw new Error(optimizedSvg.error);
     }
 
-    const optimizedSvgString = (optimizedSvg as OptimizedSvg).data;
+    // This wraps the content of an svg with a symbol tag to make the svg usable
+    // with an <use href="<url>#<id>" /> pattern
+    const optimizedSvgString = (optimizedSvg as OptimizedSvg).data.replace(
+      /^(<svg[^>]*>)([\S\s]*)(<\/svg>)$/gim,
+      '$1$2<symbol id="icon">$2</symbol>$3',
+    );
 
     fs.writeFileSync(path.join(output, icon.file.name), optimizedSvgString);
     return optimizedSvgString;
