e445b13c2a86ed9897c54d0c292d8e860f8a05bf
Philipp Gfeller <philipp.gfeller@post.ch>
2023-02-21 08:32:52 +0100

    test(header): add visual regression tests

--- packages/internet-header/cypress/support/prepare-story.ts
+++ packages/internet-header/cypress/support/prepare-story.ts
@@ -1,14 +1,18 @@
 import testConfiguration from '../fixtures/internet-header/test-configuration.json';
 import mockAuth from '../fixtures/internet-header/auth.json';
 
+export const installInterceptors = (config: Object = testConfiguration) => {
+  cy.intercept('**/api/headerjs/Json?serviceid=*', config).as('getConfig');
+  cy.intercept('/assets/config/test-configuration.json', config).as('getTestConfig');
+  cy.intercept('**/v1/session/subscribe', mockAuth).as('auth');
+};
+
 export const prepare = (
   storyTitle: string = 'Header',
   storyName: string = 'Default',
   config: Object = testConfiguration,
 ) => {
-  cy.intercept('**/api/headerjs/Json?serviceid=*', config).as('getConfig');
-  cy.intercept('/assets/config/test-configuration.json', config).as('getTestConfig');
-  cy.intercept('**/v1/session/subscribe', mockAuth).as('auth');
+  installInterceptors(config);
   cy.visitStorybook({
     onBeforeLoad(win: { navigator: any }) {
       // Set default browser language explicitly to English
