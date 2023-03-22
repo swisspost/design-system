039d0d92f283672d708b26a89d464f5786849cb2
Philipp Gfeller <philipp.gfeller@post.ch>
2023-03-22 07:45:45 +0100

    fix: use local version for dependency-links
    While refactoring the versions.json, the remote versions were used in the DependencyLink component by mistake. Refactored the component to use the local package.json information instead of the remote versions.json information.

--- packages/demo/src/app/common/dependency-link/dependency-link.component.ts
+++ packages/demo/src/app/common/dependency-link/dependency-link.component.ts
@@ -22,23 +22,22 @@ export class DependencyLinkComponent implements OnDestroy {
 
   constructor(private route: ActivatedRoute, private versionService: VersionService) {
     this.urlChangeSubscription = this.route.url.subscribe(() => {
-      this.currentVersion$ = this.versionService.currentVersion.subscribe(({ dependencies }) => {
-        if (route.parent.snapshot.url[0].path === 'ng-bootstrap-samples') {
-          this.dependency = 'ng-bootstrap';
-          this.dependencyVersion = dependencies.get('@ng-bootstrap/ng-bootstrap').format('x');
-          this.documentationBaseUrl = `https://ng-bootstrap.github.io${
-            this.dependencyVersion === '14' ? '' : `/releases/${this.dependencyVersion}.x`
-          }/#/`;
-        } else {
-          this.dependency = 'Bootstrap';
-          this.dependencyVersion = dependencies.get('bootstrap').format('x.x');
-          this.documentationBaseUrl = `https://getbootstrap.com/docs/${this.dependencyVersion}/`;
-        }
+      const { dependencies } = this.versionService.localVersion;
+      if (route.parent.snapshot.url[0].path === 'ng-bootstrap-samples') {
+        this.dependency = 'ng-bootstrap';
+        this.dependencyVersion = dependencies.get('@ng-bootstrap/ng-bootstrap').format('x');
+        this.documentationBaseUrl = `https://ng-bootstrap.github.io${
+          this.dependencyVersion === '14' ? '' : `/releases/${this.dependencyVersion}.x`
+        }/#/`;
+      } else {
+        this.dependency = 'Bootstrap';
+        this.dependencyVersion = dependencies.get('bootstrap').format('x.x');
+        this.documentationBaseUrl = `https://getbootstrap.com/docs/${this.dependencyVersion}/`;
+      }
 
-        if (!this.documentationPath) {
-          this.documentationPath = `components/${route.snapshot.url[0].path}`;
-        }
-      });
+      if (!this.documentationPath) {
+        this.documentationPath = `components/${route.snapshot.url[0].path}`;
+      }
     });
   }
 
