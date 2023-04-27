d1f33d48e92e2ae0955593889b77992123dbb834
debraya <alize.debray@gmail.com>
2023-04-27 18:02:06 +0200

    feat(internet-header): add the name of the active menu item to the flyout close button

--- packages/internet-header/src/components/post-main-navigation/post-main-navigation.tsx
+++ packages/internet-header/src/components/post-main-navigation/post-main-navigation.tsx
@@ -286,7 +286,7 @@ export class PostMainNavigation implements HasDropdown, IsFocusable {
                           onClick={() => this.closeFlyout(levelOne.id)}
                         >
                           <span class="visually-hidden">
-                            {headerConfig.translations.mobileNavToggleClose}
+                            {levelOne.text} - {headerConfig.translations.mobileNavToggleClose}
                           </span>
                           <SvgIcon name="pi-close" />
                         </button>
