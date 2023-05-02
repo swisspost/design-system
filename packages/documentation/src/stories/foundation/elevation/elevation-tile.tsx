a9829917d57ef801c8e60d96ab8f84a2d1fead82
Alizé Debray <33580481+alizedebray@users.noreply.github.com>
2023-05-02 15:07:34 +0200

    Update packages/documentation/src/stories/foundation/elevation/elevation-tile.tsx
    Co-authored-by: Oliver Schürch <oliver.schuerch@post.ch>

--- packages/documentation/src/stories/foundation/elevation/elevation-tile.tsx
+++ packages/documentation/src/stories/foundation/elevation/elevation-tile.tsx
@@ -2,18 +2,16 @@ export const ElevationTile = (props: { level: number }) => {
   return (
     <div className={`elevation-tile elevation-${props.level}`}>
       <h2 className="h3">Elevation {props.level}</h2>
-      <p>
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>.elevation-{props.level}</code>
-          </dd>
-          <dt>Sass</dt>
-          <dd>
-            <code>post.$elevation-{props.level}</code>
-          </dd>
-        </dl>
-      </p>
+      <dl>
+        <dt>Class</dt>
+        <dd>
+          <code>.elevation-{props.level}</code>
+        </dd>
+        <dt>Sass</dt>
+        <dd>
+          <code>post.$elevation-{props.level}</code>
+        </dd>
+      </dl>
     </div>
   );
 };
