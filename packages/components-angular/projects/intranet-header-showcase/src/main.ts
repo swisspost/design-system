2188887fa6262695056e914d848384cedd119d06
debraya <alize.debray@gmail.com>
2023-06-07 16:08:18 +0200

    feat(intranet-header): set-up post-icon component

--- packages/components-angular/projects/intranet-header-showcase/src/main.ts
+++ packages/components-angular/projects/intranet-header-showcase/src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@swisspost/design-system-components/loader';
+
+defineCustomElements();
 
 if (environment.production) {
   enableProdMode();
