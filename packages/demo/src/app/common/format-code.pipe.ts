53c621b9ea4631073263f747a88087c0f859ec94
Loïc Fürhoff <{ID}+{username}@users.noreply.github.com>
2023-07-10 18:15:53 +0200

    Fix most of the module not found

--- packages/demo/src/app/common/format-code.pipe.ts
+++ packages/demo/src/app/common/format-code.pipe.ts
@@ -1,16 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import prettier from 'prettier';
-import htmlParser from 'prettier/parser-html';
 import babelParser from 'prettier/parser-babel';
+import htmlParser from 'prettier/parser-html';
 
 @Pipe({
   name: 'formatCode'
 })
 export class FormatCodePipe implements PipeTransform {
 
-  transform(value: string, parser: prettier.BuiltInParserName | prettier.CustomParser = 'html'): string {
-    if (!value || !value.length) { return value; }
-    return prettier.format(value, { parser, plugins: [htmlParser, babelParser], htmlWhitespaceSensitivity: 'ignore' });
+  transform(value: string, parser: prettier.BuiltInParserName | prettier.CustomParser = 'html'): Promise<string> {
+    if (!value || !value.length) { return Promise.resolve(value); }
+    return prettier.format(value, { parser, plugins: [htmlParser as any, babelParser as any], htmlWhitespaceSensitivity: 'ignore' });
   }
 
 }
