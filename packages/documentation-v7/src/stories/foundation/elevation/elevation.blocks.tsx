c9cb760c9f05d50b1ceffded0f2d7f9847f9b8c0
debraya <alize.debray@gmail.com>
2023-07-14 10:45:37 +0200

    use SASS :export to show elevation tiles

--- packages/documentation-v7/src/stories/foundation/elevation/elevation.blocks.tsx
+++ packages/documentation-v7/src/stories/foundation/elevation/elevation.blocks.tsx
@@ -1,15 +1,20 @@
-export const ElevationTile = (props: { level: number }) => {
+import { parse } from '../../../utils/sass-export';
+import scss from './elevation.module.scss';
+
+export const SCSS_VARIABLES = parse(scss);
+
+export const ElevationTile = (props: { level: string }) => {
   return (
-    <div className={`elevation-tile elevation-${props.level}`}>
-      <h2 className="h3">Elevation {props.level}</h2>
+    <div className={`elevation-tile ${props.level}`}>
+      <h2 className="h3">{props.level.replace('-', ' ')}</h2>
       <dl>
         <dt>Class</dt>
         <dd>
-          <code>.elevation-{props.level}</code>
+          <code>.{props.level}</code>
         </dd>
         <dt>Sass</dt>
         <dd>
-          <code>post.$elevation-{props.level}</code>
+          <code>post.${props.level}</code>
         </dd>
       </dl>
     </div>
