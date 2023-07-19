ff24b9ebf0b7fc617a352bfd865f4286d85e1279
Oliver Schürch <oliver.schuerch@post.ch>
2023-07-19 15:41:07 +0200

    chore(internet-header): reverting stenciljs config

--- packages/internet-header/stencil.config.ts
+++ packages/internet-header/stencil.config.ts
@@ -14,6 +14,7 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'dist',
+      esmLoaderPath: '../loader',
       copy: [
         {
           src: 'assets/config/test-configuration.json',
