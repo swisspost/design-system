02870b48041299f3c41658e0956bab7d1c9d29aa
Oliver Schürch <oliver.schuerch@post.ch>
2023-07-26 17:45:39 +0200

    feat(documentation-v7): add heading stories

---
+++ packages/documentation-v7/src/stories/components/heading/heading.stories.ts
@@ -0,0 +1,79 @@
+import type { Args, Meta, StoryObj } from '@storybook/web-components';
+import { html, unsafeStatic } from 'lit/static-html.js';
+
+const meta: Meta = {
+  title: 'Components/Heading',
+  args: {
+    title: 'Heading',
+    level: 'h1',
+    showSubtitle: false,
+    subtitle: 'Subheading',
+  },
+  argTypes: {
+    title: {
+      name: 'Title',
+      description: 'Defines the text within the Heading.',
+      control: {
+        type: 'text',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+    level: {
+      name: 'Heading Level',
+      description: 'Defines the level of the Heading (e.g. `<h1>`, `<h2>`, ...).',
+      control: {
+        type: 'select',
+      },
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      table: {
+        category: 'General',
+      },
+    },
+    showSubtitle: {
+      name: 'Show Subheading',
+      description: 'When set to `true`, component renders a subheading.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+    subtitle: {
+      name: 'Subtitle',
+      description: 'Defines the text within the Subheading.',
+      if: {
+        arg: 'showSubtitle',
+      },
+      control: {
+        type: 'text',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+  render: (args: Args) => {
+    const tagName = unsafeStatic(args.level);
+    const content = args.showSubtitle
+      ? html`
+          <span>${args.title}</span>
+          <br />
+          <span class="fw-light">${args.subtitle}</span>
+        `
+      : args.title;
+
+    return html`
+      <${tagName} class="h3">${content}</${tagName}>
+    `;
+  },
+};
