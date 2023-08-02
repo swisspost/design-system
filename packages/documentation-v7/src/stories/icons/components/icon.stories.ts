de4d76c210b4c4b6dceb994bf2ba0744609d9b0c
debraya <alize.debray@gmail.com>
2023-08-02 10:58:08 +0200

    chore(documentation-v7): update all links

--- packages/documentation-v7/src/stories/icons/components/icon.stories.ts
+++ packages/documentation-v7/src/stories/icons/components/icon.stories.ts
@@ -6,6 +6,7 @@ import { ifDefined } from 'lit/directives/if-defined.js';
 import { BADGE } from '../../../../.storybook/constants';
 
 const meta: Meta = {
+  title: 'Icons/Components/Icon',
   component: 'post-icon',
   parameters: {
     badges: [BADGE.NEEDS_REVISION],
