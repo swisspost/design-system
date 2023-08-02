620c553e87e4436a0dde3601ec59e8f269175043
Philipp Gfeller <philipp.gfeller@post.ch>
2023-08-02 10:55:52 +0200

    tests: fix docsv7 e2e test timeout

--- packages/documentation-v7/cypress/e2e/home.cy.ts
+++ packages/documentation-v7/cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe('home', () => {
   before(() => {
-    cy.visit('/iframe.html?id=home--docs', { timeout: 5000 });
+    cy.visit('/iframe.html?id=home--docs');
   });
 
   it.skip('should show the home page', () => {
