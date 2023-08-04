f17f0d94613b926fb038a2a22c87fd459391f683
debraya <alize.debray@gmail.com>
2023-08-04 13:24:59 +0200

    chore(documentation-v7): update `mapClasses`

--- packages/documentation-v7/src/utils/map-classes.ts
+++ packages/documentation-v7/src/utils/map-classes.ts
@@ -1,8 +1,4 @@
-/*
- * Copyright 2023 by Swiss Post, Information Technology
- */
-
-export function mapClasses(classObject: {[cssClass: string]: boolean}): string {
+export function mapClasses(classObject: Record<string, boolean>): string {
   return Object.entries(classObject)
     .filter(([_newClass, shouldAddClass]) => shouldAddClass)
     .map(([newClass]) => newClass)
