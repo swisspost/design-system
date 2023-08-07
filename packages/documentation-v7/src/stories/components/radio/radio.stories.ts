a44c15299f59638dbf0922e94f5951575e605bdb
imagoiq <12294151+imagoiq@users.noreply.github.com>
2023-08-07 11:46:03 +0200

    chore(documentation-v7): add radio stories

---
+++ packages/documentation-v7/src/stories/components/radio/radio.stories.ts
@@ -0,0 +1,273 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { Args, Meta, StoryContext, StoryObj } from '@storybook/web-components';
+import { useArgs } from '@storybook/preview-api';
+import { BADGE } from '../../../../.storybook/constants';
+import { html, nothing, TemplateResult } from 'lit';
+
+const VALIDATION_STATE_MAP: Record<string, undefined | boolean> = {
+  'null': undefined,
+  'is-valid': false,
+  'is-invalid': true,
+};
+
+const meta: Meta = {
+  title: 'Components/Radio button',
+  parameters: {
+    controls: {
+      exclude: ['Hidden Legend'],
+    },
+    badges: [BADGE.NEEDS_REVISION],
+  },
+  args: {
+    hiddenLegend: false,
+    label: 'Label',
+    hiddenLabel: false,
+    checked: false,
+    disabled: false,
+    validation: 'null',
+  },
+  argTypes: {
+    hiddenLegend: {
+      name: 'Hidden Legend',
+      description: 'Render the group with or without a visible legend.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+    label: {
+      name: 'Label',
+      description: 'Describes the content/topic of the component.',
+      control: {
+        type: 'text',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+    hiddenLabel: {
+      name: 'Hidden Label',
+      description:
+        'Renders the component with or without a visible label.<span class="mt-mini alert alert-info alert-sm">There are accessibility concerns with hidden labels.<br/>Please read our <a href="/?path=/story/foundations-accessibility--page#labels">label accessibility guide</a>.</span>',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        category: 'General',
+      },
+    },
+    checked: {
+      name: 'Checked',
+      description: 'When set to `true`, places the component in the checked state.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        category: 'States',
+      },
+    },
+    disabled: {
+      name: 'Disabled',
+      description:
+        'When set to `true`, disables the component\'s functionality and places it in a disabled state.<span class="mt-mini alert alert-info alert-sm">There are accessibility concerns with the disabled state.<br/>Please read our <a href="/?path=/docs/foundations-accessibility--page#disabled-state">disabled state accessibility guide</a>.</span>',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        category: 'States',
+      },
+    },
+    validation: {
+      name: 'Validation',
+      description: "Controls the display of the component's validation state.",
+      control: {
+        type: 'radio',
+        labels: {
+          'null': 'Default',
+          'is-valid': 'Valid',
+          'is-invalid': 'Invalid',
+        },
+      },
+      options: ['null', 'is-valid', 'is-invalid'],
+      table: {
+        category: 'States',
+      },
+    },
+  },
+  render: render,
+};
+
+function render(args: Args, context: StoryContext) {
+  const [_, updateArgs] = useArgs();
+
+  const id = `${context.viewMode}_${context.name.replace(/\s/g, '-')}_ExampleRadio`;
+  const classes = ['form-check-input', args.validation].filter(c => c && c !== 'null').join(' ');
+
+  const useAriaLabel = args.hiddenLabel;
+  const label: TemplateResult | null = !useAriaLabel
+    ? html`
+        <label for="${id}" class="form-check-label">${args.label}</label>
+      `
+    : null;
+
+  const contextual: (TemplateResult | null)[] = [
+    args.validation === 'is-valid'
+      ? html`
+          <p class="valid-feedback">Ggranda sukceso!</p>
+        `
+      : null,
+    args.validation === 'is-invalid'
+      ? html`
+          <p class="invalid-feedback">Eraro okazis!</p>
+        `
+      : null,
+  ];
+
+  const control = html`
+    <input
+      id="${id}"
+      class="${classes}"
+      type="radio"
+      checked="${args.checked || nothing}"
+      disabled="${args.disabled || nothing}"
+      aria-label="${useAriaLabel && args.label}"
+      aria-invalid="${VALIDATION_STATE_MAP[args.validation]}"
+      @change="${(e: Event) => {
+        updateArgs({ checked: !args.checked });
+
+        if (document.activeElement === e.target) {
+          setTimeout(() => {
+            const element: HTMLInputElement | null = document.querySelector(`#${id}`);
+            if (element) element.focus();
+          }, 25);
+        }
+      }}"
+    />
+  `;
+
+  return html`
+    <div class="form-check">${[control, label, ...contextual].filter(el => el !== null)}</div>
+  `;
+}
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+  decorators: [
+    story => html`
+      <div class="pt-3">${story()}</div>
+    `,
+  ],
+};
+
+function renderInline(args: Args, context: StoryContext) {
+  const [_, updateArgs] = useArgs();
+  const baseId = `${context.viewMode}_${context.name.replace(/\s/g, '-')}_ExampleRadio`;
+  const id1 = baseId + '1';
+  const id2 = baseId + '2';
+  const id3 = baseId + '3';
+  const id4 = baseId + '4';
+
+  function onChange(e: Event, value: number) {
+    updateArgs({ checkedRadio: value });
+
+    if (document.activeElement === e.target) {
+      setTimeout(() => {
+        const element: HTMLInputElement | null = document.querySelector(
+          `#${(<HTMLElement>e.target).id}`,
+        );
+        if (element) element.focus();
+      }, 25);
+    }
+  }
+
+  return html`
+    <fieldset>
+      <legend class="${args.hiddenLegend ? 'visually-hidden' : undefined}">Legend</legend>
+      <div class="form-check form-check-inline">
+        <input
+          id="${id1}"
+          name="Inline_ExampleRadio_Group"
+          class="form-check-input"
+          type="radio"
+          checked="${args.checkedRadio === 1 || nothing}"
+          @change="${(e: Event) => onChange(e, 1)}"
+        />
+        <label for="${id1}" class="form-check-label">${args.label}</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          id="${id2}"
+          name="Inline_ExampleRadio_Group"
+          class="form-check-input"
+          type="radio"
+          checked="${args.checkedRadio === 2 || nothing}"
+          @change="${(e: Event) => onChange(e, 2)}"
+        />
+        <label for="${id2}" class="form-check-label">${args.label}</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          id="${id3}"
+          name="Inline_ExampleRadio_Group"
+          class="form-check-input"
+          type="radio"
+          checked="${args.checkedRadio === 3 || nothing}"
+          @change="${(e: Event) => onChange(e, 3)}"
+        />
+        <label for="${id3}" class="form-check-label">${args.label}</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          id="${id4}"
+          name="Inline_ExampleRadio_Group"
+          class="form-check-input"
+          type="radio"
+          checked="${args.checkedRadio === 4 || nothing}"
+          @change="${(e: Event) => onChange(e, 4)}"
+        />
+        <label for="${id4}" class="form-check-label">${args.label}</label>
+      </div>
+    </fieldset>
+  `;
+}
+
+export const Inline: Story = {
+  render: renderInline,
+  decorators: [
+    story => html`
+      <div class="pt-3">${story()}</div>
+    `,
+  ],
+  parameters: {
+    controls: {
+      exclude: ['Hidden Label', 'Checked', 'Disabled', 'Validation'],
+    },
+  },
+  args: {
+    checkedRadio: null,
+  },
+  argTypes: {
+    checkedRadio: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const Validation: Story = {
+  parameters: {
+    controls: {
+      exclude: ['Hidden Legend', 'Label', 'Hidden Label', 'Disabled'],
+    },
+  },
+  args: {
+    validation: 'is-invalid',
+  },
+};
