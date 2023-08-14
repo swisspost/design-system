d669541fda313b0b02e472768fd4e275d83baea3
Philipp Gfeller <philipp.gfeller@post.ch>
2023-08-14 11:00:41 +0200

    chore: ensure only one URL goes to prod

--- .github/actions/update-versions/update-versions.mjs
+++ .github/actions/update-versions/update-versions.mjs
@@ -1,11 +1,13 @@
 import fs from 'fs';
+import { get } from 'http';
 import path from 'path';
 
 const args = process.argv.splice(2);
 const versionsPath = path.resolve(process.cwd(), args[1]);
 const packagePath = path.resolve(process.cwd(), args[3]);
 
-const getMajorVersion = version => version.split('.')[0];
+const getMajorVersion = version =>
+  version === 'next' ? version : parseInt(version.split('.')[0].replace('workspace:', ''));
 
 if (!fs.existsSync(versionsPath)) {
   throw new Error(`versions.json could not be found at ${versionsPath}`);
@@ -15,53 +17,65 @@ if (!fs.existsSync(packagePath)) {
   throw new Error(`package.json could not be found at ${packagePath}`);
 }
 
+// Load source files
 const packageJSON = JSON.parse(fs.readFileSync(packagePath));
 const versionsJSON = JSON.parse(fs.readFileSync(versionsPath));
 
+// Get some constants
 const currentMajorVersion = getMajorVersion(packageJSON.version);
-const allDependencies = { ...packageJSON.dependencies, ...packageJSON.devDependencies };
-const hasVersionEntry = versionsJSON.find(
-  v => 'version' in v && getMajorVersion(v.version) === currentMajorVersion,
-);
 const interestingKeywords = ['@swisspost', '@angular/core', 'react', 'bootstrap'];
+const interestingDependencies = Object.entries({
+  ...packageJSON.dependencies,
+  ...packageJSON.devDependencies,
+})
+  .filter(([key]) => interestingKeywords.some(i => key.includes(i)))
+  .reduce((acc, [key, value]) => {
+    acc[key] = value.replace('workspace:', '');
+    return acc;
+  });
+const hasVersionEntry = versionsJSON.find(v => getMajorVersion(v.version) === currentMajorVersion);
+const allMajors = versionsJSON
+  .map(v => getMajorVersion(v.version))
+  .filter(n => typeof n !== 'number')
+  .sort((a, b) => b - a);
+const latestMajor = allMajors[0];
+// Update an url. If it's from the current prod release, don't append a /vX path
+const getURL = majorVersion =>
+  majorVersion === latestMajor
+    ? 'https://design-system.post.ch'
+    : `https://design-system.post.ch/v${majorVersion}`;
 
+// Update or add an entry
 let updatedVersions;
 if (hasVersionEntry) {
   // Update the existing entry
   updatedVersions = versionsJSON.map(version => {
-    if (!('version' in version) || getMajorVersion(version.version) !== currentMajorVersion)
-      return version;
+    const localMajorVersion = getMajorVersion(version.version);
 
-    const updatedVersion = {
+    // Not the current release
+    if (localMajorVersion !== currentMajorVersion) return version;
+
+    return {
       ...version,
       version: packageJSON.version,
-      dependencies:
-        'dependencies' in version
-          ? Object.keys(version.dependencies).reduce((acc, key) => {
-              acc[key] = allDependencies[key].replace('workspace:', '');
-              return acc;
-            }, {})
-          : null,
+      url: getURL(localMajorVersion),
+      lastUpdated: Date.now(),
+      dependencies: interestingDependencies,
     };
-
-    return updatedVersion;
   });
 } else {
-  console.log(allDependencies);
   // Add a new entry and sort based on version number
   updatedVersions = [
-    ...versionsJSON,
+    // Update URL on past releases to prevent two URLs going to prod
+    ...versionsJSON.map(v => ({ ...v, url: getURL(getMajorVersion(v.version)) })),
     {
       title: `Version ${currentMajorVersion}`,
       version: packageJSON.version,
       description: packageJSON.description,
-      url: `https://design-system.post.ch/v${currentMajorVersion}`,
-      dependencies: Object.keys(allDependencies)
-        .filter(k => interestingKeywords.some(i => k.includes(i)))
-        .reduce((acc, key) => {
-          acc[key] = allDependencies[key];
-          return acc;
-        }, {}),
+      created: Date.now(),
+      lastUpdated: Date.now(),
+      url: getURL(currentMajorVersion),
+      dependencies: interestingDependencies,
     },
   ].sort((a, b) => (a.version > b.version ? -1 : 1));
 }
