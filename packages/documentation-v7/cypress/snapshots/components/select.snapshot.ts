8aadb38537c30bbb16263d6a23042da0cdada9a1
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-08-17 10:00:56 +0200

    feat(documentation-v7): add select snapshot test

--- packages/documentation-v7/cypress/snapshots/components/select.snapshot.ts
+++ packages/documentation-v7/cypress/snapshots/components/select.snapshot.ts
@@ -1,6 +1,6 @@
 describe('Alert', () => {
   it('default', () => {
     cy.visit('./iframe.html?id=snapshots--select');
-    cy.percySnapshot('Alerts', { widths: [400] });
+    cy.percySnapshot('Selects', { widths: [400] });
   });
 });
