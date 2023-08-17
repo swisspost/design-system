353e73b254f44d7e5eb2b974e04cf6f1302c254c
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-08-17 10:18:14 +0200

    feat(documentation-v7): add select snapshot test

--- packages/documentation-v7/cypress/snapshots/components/select.snapshot.ts
+++ packages/documentation-v7/cypress/snapshots/components/select.snapshot.ts
@@ -1,4 +1,4 @@
-describe('Alert', () => {
+describe('Select', () => {
   it('default', () => {
     cy.visit('./iframe.html?id=snapshots--select');
     cy.percySnapshot('Selects', { widths: [400] });
