e9e8b72285ded1e6710368a11d9e45ce35a002fa
Alizé Debray <alize.debray@post.ch>
2023-08-23 13:14:28 +0200

    feat(components): update tabs component to make asynchronously added tab work

--- packages/components/src/components/post-tabs/post-tabs.tsx
+++ packages/components/src/components/post-tabs/post-tabs.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Element, Method, Event, EventEmitter, Prop } from '@stencil/core';
+import { Component, Host, h, Element, Method, Event, EventEmitter, Prop, Listen } from '@stencil/core';
 import { version } from '../../../package.json';
 import { fadeIn, fadeOut } from '../../animations';
 
@@ -8,18 +8,13 @@ import { fadeIn, fadeOut } from '../../animations';
   shadow: true,
 })
 export class PostTabs {
-  private panelsByName = new Map<HTMLPostTabPanelElement['name'], HTMLPostTabPanelElement>;
   private activeTab: HTMLPostTabHeaderElement;
   private showing: Animation;
   private hiding: Animation;
   private isLoaded = false;
 
-  private get tabs(): NodeListOf<HTMLPostTabHeaderElement> {
-    return this.host.querySelectorAll('post-tab-header');
-  }
-
-  private get panels(): NodeListOf<HTMLPostTabPanelElement> {
-    return this.host.querySelectorAll('post-tab-panel');
+  private get firstTab(): HTMLPostTabHeaderElement {
+    return this.host.querySelector('post-tab-header:first-child');
   }
 
   @Element() host: HTMLPostTabsElement;
@@ -36,34 +31,21 @@ export class PostTabs {
    */
   @Event() tabChange: EventEmitter<HTMLPostTabPanelElement['name']>;
 
-  async componentWillLoad() {
-    this.panels.forEach(panel => {
-      // save the panels by name to easily retrieve them later
-      this.panelsByName.set(panel.name, panel);
-
-      // remove all panels from the view
-      panel.remove();
-    });
-
-    this.tabs.forEach(tab => {
-      // add an event listener on the tabs so that they are activated on click
-      tab.addEventListener('click', async e => {
-        e.preventDefault();
-        await this.show(tab.panel);
-      });
-
-      // move each post-tab-header element to the "tabs" slot if it's not already there
-      if (tab.getAttribute('slot') !== 'tabs') {
-        tab.setAttribute('slot', 'tabs');
-      }
-    });
-  }
-
   async componentDidLoad() {
-    const initiallyActivePanel = this.activePanel || this.tabs.item(0).panel;
+    const initiallyActivePanel = this.activePanel || this.firstTab.panel;
     await this.show(initiallyActivePanel);
 
     this.isLoaded = true;
+  }
+
+  @Listen('click', {})
+  async onTabClick(event: Event) {
+    if (event.target['tagName'] !== 'POST-TAB-HEADER') return;
+
+    event.preventDefault();
+
+    const tab = event.target as HTMLPostTabHeaderElement;
+    await this.show(tab.panel);
   }
 
   /**
@@ -83,7 +65,7 @@ export class PostTabs {
 
     // if a panel is currently being displayed, remove it from the view and complete the associated animation
     if (this.showing) {
-      this.showing.effect['target'].remove();
+      this.showing.effect['target'].style.display = 'none';
       this.showing.finish();
     }
 
@@ -122,18 +104,18 @@ export class PostTabs {
   }
 
   private hidePanel(panelName: HTMLPostTabPanelElement['name']) {
-    const previousPanel = this.panelsByName.get(panelName);
+    const previousPanel = this.getPanel(panelName);
 
     this.hiding = fadeOut(previousPanel);
     this.hiding.onfinish = () => {
-      previousPanel.remove();
+      previousPanel.style.display = 'none';
       this.hiding = null;
     };
   }
 
   private showSelectedPanel() {
-    const panel = this.panelsByName.get(this.activeTab.panel);
-    this.host.appendChild(panel);
+    const panel = this.getPanel(this.activeTab.panel);
+    panel.style.display = 'block';
 
     // prevent the initially selected panel from fading in
     if (!this.isLoaded) return;
@@ -142,6 +124,12 @@ export class PostTabs {
     this.showing.onfinish = () => {
       this.showing = null;
     };
+  }
+
+  private getPanel(name: string): HTMLPostTabPanelElement {
+    return this.host.querySelector(
+      `post-tab-panel[name=${name}]`
+    );
   }
 
   render() {
