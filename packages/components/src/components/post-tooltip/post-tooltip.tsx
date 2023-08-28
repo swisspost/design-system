b009b030b7d865285812d7f600d34894b2dc96c2
Philipp Gfeller <philipp.gfeller@post.ch>
2023-08-28 10:24:00 +0200

    add default background color for tooltips

--- packages/components/src/components/post-tooltip/post-tooltip.tsx
+++ packages/components/src/components/post-tooltip/post-tooltip.tsx
@@ -1,8 +1,5 @@
 import { Component, h, Element, Prop, Host } from '@stencil/core';
 import { version } from '../../../package.json';
-import '@oddbird/popover-polyfill';
-import 'long-press-event';
-
 import {
   computePosition,
   Placement,
@@ -10,9 +7,22 @@ import {
   shift,
   autoUpdate,
   offset,
+  inline,
   arrow,
 } from '@floating-ui/dom';
 
+// Polyfill for popovers, can be removed when https://caniuse.com/?search=popover is green
+import '@oddbird/popover-polyfill';
+
+// Patch for long press on touch devices
+import 'long-press-event';
+
+const sidemap = {
+  top: 'bottom',
+  right: 'left',
+  bottom: 'top',
+  left: 'right',
+};
 @Component({
   tag: 'post-tooltip',
   styleUrl: 'post-tooltip.scss',
@@ -25,32 +35,34 @@ export class PostTooltip {
 
   private arrowRef: HTMLElement;
   private trigger: HTMLElement;
+  private clearAutoupdate: () => void;
 
   componentWillLoad() {
-    let clearAutoupdate: () => void;
     this.trigger = document.querySelector(`[data-tooltip-target="${this.host.id}"]`);
 
     // Patch popovertargetaction="interest" until it's implemented
     // https://github.com/openui/open-ui/issues/767#issuecomment-1654177227
-    ['mouseenter', 'focus', 'long-press'].forEach(e =>
-      this.trigger.addEventListener(e, () => {
-        clearAutoupdate = autoUpdate(this.trigger, this.host, this.positionTooltip.bind(this), {
-          ancestorScroll: false,
-        });
-        this.host.showPopover();
-      }),
-    );
-    ['mouseleave', 'blur'].forEach(e =>
-      this.trigger.addEventListener(e, () => {
-        clearAutoupdate();
-        this.host.hidePopover();
-      }),
-    );
+    this.trigger.addEventListener('mouseenter', this.showTooltip.bind(this));
+    this.trigger.addEventListener('mouseleave', this.hideTooltip.bind(this));
+    this.trigger.addEventListener('focus', this.showTooltip.bind(this));
+    this.trigger.addEventListener('blur', this.hideTooltip.bind(this));
+    this.trigger.addEventListener('long-press', this.showTooltip.bind(this));
+  }
+
+  showTooltip() {
+    this.clearAutoupdate = autoUpdate(this.trigger, this.host, this.positionTooltip.bind(this), {
+      ancestorScroll: false,
+    });
+    this.host.showPopover();
+  }
+
+  hideTooltip() {
+    this.clearAutoupdate();
+    this.host.hidePopover();
   }
 
   // Tooltip and arrow positioning with floating-ui
   // Docs: https://floating-ui.com/docs/computePosition
-  // Tutorial: https://codesandbox.io/s/mystifying-kare-ee3hmh?file=/src/index.js
   private async positionTooltip() {
     const {
       x,
@@ -61,23 +73,23 @@ export class PostTooltip {
       placement: this.placement || 'top',
       middleware: [
         flip(),
-        shift({ padding: 5 }),
-        offset(13),
-        arrow({ element: this.arrowRef, padding: 10 }),
+        inline(),
+        shift({ padding: 8 }),
+        offset(13), // 4px outside of element to account for focus outline + ~arrow size
+        arrow({ element: this.arrowRef, padding: 8 }),
       ],
     });
+
+    // Tooltip
     this.host.style.left = `${x}px`;
     this.host.style.top = `${y}px`;
 
+    // Arrow
+    // Tutorial: https://codesandbox.io/s/mystifying-kare-ee3hmh?file=/src/index.js
     const side = currentPlacement.split('-')[0];
-    const staticSide = {
-      top: 'bottom',
-      right: 'left',
-      bottom: 'top',
-      left: 'right',
-    }[side];
-
     const { x: arrowX, y: arrowY } = middlewareData.arrow;
+    const staticSide = sidemap[side];
+
     Object.assign(this.arrowRef.style, {
       top: arrowY ? `${arrowY}px` : '',
       left: arrowX ? `${arrowX}px` : '',
@@ -95,7 +107,7 @@ export class PostTooltip {
           }}
         ></span>
         <p>
-          <slot />
+          <slot>Enter a short message here</slot>
         </p>
       </Host>
     );
