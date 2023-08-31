580719e5134c37d1e310518a7f42ebd0c6888efa
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-08-31 15:16:36 +0200

    feat(documentation): adds documentation for Grid

--- packages/documentation/src/stories/foundation/layout/Grid/grid.stories.ts
+++ packages/documentation/src/stories/foundation/layout/Grid/grid.stories.ts
@@ -9,9 +9,30 @@ const meta: Meta = {
     badges: [BADGE.NEEDS_REVISION],
   },
   args: {
-    gutters: 'vertical-gutters',
+    gutters: '',
+    size: 2,
+    container: false,
   },
   argTypes: {
+    gap: {
+      name: 'Gap',
+      description: 'Sets the margin inbetween the columns',
+
+      control: {
+        type: 'select',
+        labels: {
+          '': 'none',
+          'gap-3': 'horizontal & vertical',
+          'row-gap-3': 'vertical',
+          'column-gap-3': 'horizontal',
+        },
+      },
+      options: ['', 'gap-3', 'row-gap-3', 'column-gap-3'],
+      table: {
+        category: 'General',
+      },
+    },
+
     gutters: {
       name: 'Gutter',
       description: 'Sets the padding inbetween the columns',
@@ -19,11 +40,12 @@ const meta: Meta = {
       control: {
         type: 'select',
         labels: {
+          '': 'none',
           'border-gutters': 'border',
           'vertical-gutters': 'vertical',
         },
       },
-      options: ['border-gutters', 'vertical-gutters'],
+      options: ['', 'border-gutters', 'vertical-gutters'],
       table: {
         category: 'General',
       },
@@ -37,31 +59,50 @@ type Story = StoryObj;
 
 export const Default: Story = {
   render: (args: Args) => {
-    const color: string = 'orange';
-    const classes = mapClasses({ 'row': true, [args.gutters]: args.gutters, 'text-center': true });
-    return html`
-      <div class=${classes}>
-        <div class="col-sm">
-          <div class="container" style="background-color:${color}">col-sm 400px</div>
-        </div>
-        <div class="col-rg">
-          <div class="container" style="background-color:${color}">col-rg 600px</div>
-        </div>
-        <div class="col-md">
-          <div class="container" style="background-color:${color}">col-md 780px</div>
-        </div>
-        <div class="col-lg">
-          <div class="container" style="background-color:${color}">col-lg 1024px</div>
-        </div>
-        <div class="col-xl">
-          <div class="container" style="background-color:${color}">col-xl 1280px</div>
-        </div>
-        <div class="col-xxl">
-          <div class="container" style="background-color:${color}">col-xxl 1441px</div>
-        </div>
-      </div>
+    const size: string = args.size ? '-' + args.size : '';
 
-    
+    const colorElement: string = 'orange';
+    const colorPadding: string = 'grey';
+    const colorMargin: string = 'black';
+    const classes = mapClasses({ row: true, [args.gutters]: args.gutters, [args.gap]: args.gap });
+    return html`
+      <div class=${classes} style="background-color:${colorMargin}">
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-4 col-rg-2" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-4 col-rg-2</div>
+        </div>
+        <div class="col-6 col-rg-3" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-6 col-rg-3</div>
+        </div>
+        <div class="col-6 col-rg-3" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-6 col-rg-3</div>
+        </div>
+        <div class="col-6 col-rg-3" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-6 col-rg-3</div>
+        </div>
+        <div class="col-6 col-rg-3" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-6 col-rg-3</div>
+        </div>
+        <div class="col-12 col-rg-6" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-12 col-rg-6</div>
+        </div>
+        <div class="col-12 col-rg-6" style="background-color:${colorPadding}">
+          <div style="background-color:${colorElement}">col-12 col-rg-6</div>
+        </div>
       </div>
     `;
   },
