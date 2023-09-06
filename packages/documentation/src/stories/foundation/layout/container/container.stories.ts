637ce021f738027777561c30aa2e6828516f8c75
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-09-06 15:14:45 +0200

    feat(documentation): cleans up container stories

--- packages/documentation/src/stories/foundation/layout/container/container.stories.ts
+++ packages/documentation/src/stories/foundation/layout/container/container.stories.ts
@@ -1,5 +1,5 @@
-import type { Args, Meta, StoryContext, StoryObj } from '@storybook/web-components';
-import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { html, nothing } from 'lit';
 import { BADGE } from '../../../../../.storybook/constants';
 
 const meta: Meta = {
@@ -14,29 +14,27 @@ export default meta;
 type Story = StoryObj;
 
 export const Default: Story = {
-  render: (args: Args, context: StoryContext) => {
+  render: () => {
+    function genericContent(classNames: string, withReset: boolean) {
+      return html`
+        <div class=${classNames} style="background-color:grey">
+          <h2>${classNames}</h2>
+          <div style="background-color:orange">
+            <h3>Generic Content</h3>
+          </div>
+          ${withReset
+            ? html`
+                <div class="container-reset" style="background-color:orange">
+                  <h3>Reset Content</h3>
+                </div>
+              `
+            : nothing}
+        </div>
+      `;
+    }
     return html`
-      <div class="container" style="background-color:grey">
-        <h2>Container</h2>
-        <div style="background-color:orange">
-          <h3>Generic Content</h3>
-        </div>
-        <div class="container-reset" style="background-color:orange">
-          <h3>Reset Content</h3>
-        </div>
-      </div>
-      <div class="container-fluid" style="background-color:grey">
-        <h2>Container-Fluid</h2>
-        <div style="background-color:orange">
-          <h3>Generic Content</h3>
-        </div>
-      </div>
-      <div class="container-fluid-rg" style="background-color:grey">
-        <h2>Container-Fluid-rg</h2>
-        <div style="background-color:orange">
-          <h3>Generic Content</h3>
-        </div>
-      </div>
+      ${genericContent('container', true)} ${genericContent('container-fluid', false)}
+      ${genericContent('container-fluid-rg', false)}
     `;
   },
 };
