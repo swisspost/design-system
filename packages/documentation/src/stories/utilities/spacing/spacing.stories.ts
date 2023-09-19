987befa9d61c9e03d41c71d270e42d2d26b27701
davidritter-dotcom <david.ritter.1@post.ch>
2023-09-19 16:43:21 +0200

    feat(documentation): add example for margin and padding

--- packages/documentation/src/stories/utilities/spacing/spacing.stories.ts
+++ packages/documentation/src/stories/utilities/spacing/spacing.stories.ts
@@ -1,10 +1,99 @@
-import { Meta, StoryObj } from '@storybook/web-components';
+import type { Args, Meta, StoryObj, StoryContext } from '@storybook/web-components';
 import { BADGE } from '../../../../.storybook/constants';
+import { html } from 'lit/static-html.js';
+
+const sizingOptions = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  'auto',
+  'hair',
+  'line',
+  'micro',
+  'mini',
+  'small-regular',
+  'regular',
+  'small-large',
+  'large',
+  'big',
+  'bigger-big',
+  'small-huge',
+  'huge',
+  'giant',
+  'bigger-giant',
+];
+
+const positionOptions = {
+  '': 'All around',
+  'x': 'Along the horizontal axis',
+  'y': 'Along the vertical axis',
+  't': 'At the top',
+  'b': 'At the bottom',
+  'r': 'To the right',
+  'l': 'To the left',
+};
 
 const meta: Meta = {
   title: 'Utilities/Spacing',
   parameters: {
-    badges: [BADGE.TODO],
+    badges: [BADGE.NEEDS_REVISION],
+  },
+  args: {
+    marginSize: 'regular',
+    marginPosition: '',
+    paddingSize: 'regular',
+    paddingPosition: '',
+  },
+  argTypes: {
+    marginSize: {
+      name: 'Margin size',
+      description: 'Sets the size of the Margin.',
+      control: {
+        type: 'select',
+      },
+      options: sizingOptions,
+      table: {
+        category: 'General',
+      },
+    },
+    marginPosition: {
+      name: 'Margin Position',
+      description: 'Sets the position of the Margin.',
+      control: {
+        type: 'select',
+        labels: positionOptions,
+      },
+      options: Object.keys(positionOptions),
+      table: {
+        category: 'General',
+      },
+    },
+    paddingSize: {
+      name: 'Padding size',
+      description: 'Sets the size of the Padding.',
+      control: {
+        type: 'select',
+      },
+      options: sizingOptions,
+      table: {
+        category: 'General',
+      },
+    },
+    paddingPosition: {
+      name: 'Pading Position',
+      description: 'Sets the position of the Padding.',
+      control: {
+        type: 'select',
+        labels: positionOptions,
+      },
+      options: Object.keys(positionOptions),
+      table: {
+        category: 'General',
+      },
+    },
   },
 };
 
@@ -12,4 +101,37 @@ export default meta;
 
 type Story = StoryObj;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args: Args) => {
+    return html`
+      <!-- Demo -->
+      <div class="bg-primary p-regular d-flex align-items-start">
+        <div class="flex-fill d-flex">
+          <div class="bg-petrol">
+            <div
+              class="bg-petrol-bright border border-dark h-bigger-giant m${args.marginPosition}-${args.marginSize} p${args.paddingPosition}-${args.paddingSize} w-bigger-giant"
+            >
+            <div class="bg-light h-100"></div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Legend -->
+        <ul class="ps-regular text-white">
+          <li class="d-flex align-items-center mb-regular">
+            <div class="bg-petrol h-regular w-regular me-mini"></div>
+            <span>margin</span>
+          </li>
+          <li class="d-flex align-items-center mb-regular">
+            <div class="bg-petrol-bright h-regular w-regular me-mini"></div>
+            <span>padding</span>
+          </li>
+          <li class="d-flex align-items-center mb-regular">
+            <div class="bg-light h-regular w-regular me-mini"></div>
+            <span>content</span>
+          </li>
+        </ul>
+      </div>
+    `;
+  },
+};
