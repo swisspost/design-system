2e87eefabb604702f201147632826ccc13af8d58
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-09-19 17:04:31 +0200

    feat(documentation): adds visual regression tests for the text input
    adds a new utils file with a function to help generate snapshot-test for InputComponent-Elements
    
    TODO
     update cypres snapshot file for input
    create a new test-case with a value in the input field

--- packages/documentation/src/stories/components/input/input.snapshot.stories.ts
+++ packages/documentation/src/stories/components/input/input.snapshot.stories.ts
@@ -1,61 +1,94 @@
 import type { Args, StoryContext, StoryObj } from '@storybook/web-components';
 import meta from './input.stories';
 import { html } from 'lit';
-import { bombArgs } from '../../../utils/bombArgs';
+import { getCombinations, COMBINATIONS } from '../../../utils/inputComponentsGetCombinations';
 
 export default {
   ...meta,
   title: 'Snapshots',
+  render: renderInputSnapshot,
 };
+
+function renderInputSnapshot(_args: Args, context: StoryContext) {
+  const combinations = [
+    ...COMBINATIONS,
+    {
+      label: `Label - no Placeholder`,
+      placeholder: null,
+    },
+  ];
+  return html`
+    <div class="d-flex flex-wrap align-items-start gap-regular">
+      ${['bg-white', 'bg-dark'].map(
+        bg => html`
+          <div class="${bg} d-flex gap-3 flex-column p-3">
+            <h3>Sizes</h3>
+            ${getCombinations('size', context.argTypes.size.options, combinations).map(
+              (args: Args) =>
+                html`
+                  <div>
+                    ${args.title !== undefined && args.title
+                      ? html`
+                          <h4>${args.size}</h4>
+                        `
+                      : ''}
+                    <div>${meta.render?.({ ...context.args, ...args }, context)}</div>
+                  </div>
+                `,
+            )}
+            <h3>Floating Label</h3>
+            ${getCombinations('floatingLabel', [true], combinations).map(
+              (args: Args) =>
+                html`
+                  <div>${meta.render?.({ ...context.args, ...args }, context)}</div>
+                `,
+            )}
+          </div>
+        `,
+      )}
+    </div>
+  `;
+}
 
 type Story = StoryObj;
 
-export const Input: Story = {
-  render: (_args: Args, context: StoryContext) => {
-    const shortText =
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero mollitia magnam quo quam saepe. Aliquam tempore non deleniti culpa reprehenderit.';
-    const longText =
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem maxime eius aut quae ducimus dignissimos pariatur suscipit distinctio, accusamus laudantium, sint quibusdam nisi optio? Ut quae obcaecati, harum ullam quos beatae, ipsam enim, placeat eligendi dolores excepturi. Quia quod eligendi ab voluptas modi id distinctio iure vel possimus deserunt, amet, dolores laboriosam quas qui aut laborum? Et numquam esse laboriosam totam quod sapiente recusandae consectetur optio, quaerat quia. Sequi excepturi quia voluptate nesciunt cum veritatis? Quas molestias nostrum temporibus saepe porro facilis tempora natus non modi fugiat, vitae facere quos placeat maiores incidunt illo itaque sequi dolore! Temporibus recusandae, veritatis eos vitae optio porro magni rem culpa enim provident sed. Libero blanditiis delectus voluptatibus, temporibus alias laudantium ad tempora iure, sunt minima maiores qui ut? Aliquam quis obcaecati id, officiis accusamus voluptas rerum magnam, est a culpa voluptatum tenetur ab, asperiores vel dolor ipsum alias tempore sint aliquid? Eum architecto laboriosam dolor inventore deleniti? Repellat perferendis ratione dolorem, amet deleniti minima repudiandae eos iure maiores, dicta sequi architecto ipsa sit ab laudantium praesentium maxime asperiores molestiae ad nulla ullam est saepe vero. Qui ratione vero dicta nisi molestiae rem consectetur, natus ipsam facilis repellendus animi ullam debitis temporibus sapiente quam.';
-
-    return html`
-      <div class="d-flex flex-wrap align-items-start gap-regular">
-        ${['bg-white', 'bg-dark'].map(
-          bg => html`
-            <div class="${bg} d-flex gap-3 flex-column p-3">
-              ${[
-                ...bombArgs({
-                  label: [shortText, longText],
-                  hint: [shortText, longText],
-                  floatingLabel: [false, true],
-                  hiddenLabel: [false, true],
-                })
-                  .filter(
-                    (args: Args) =>
-                      !args.hiddenLabel ||
-                      (args.label === shortText && !args.floatingLabel && !args.disabled),
-                  )
-                  .filter((args: Args) => args.label == args.hint),
-                ...bombArgs({
-                  type: ['text', 'date', 'datetime-local', 'month', 'week', 'time', 'color'],
-                  validation: context.argTypes.validation.options,
-                  size: context.argTypes.size.options,
-                  disabled: [false, true],
-                })
-                  .filter(
-                    (args: Args) =>
-                      !args.disabled || (args.size === 'null' && args.validation === 'null'),
-                  )
-                  .filter(
-                    (args: Args) =>
-                      (args.validation === 'null' || args.size === 'null') &&
-                      (args.type === 'text' || args.size === 'null') &&
-                      (args.validation === 'null' || args.type === 'text'),
-                  ),
-              ].map((args: Args) => meta.render?.({ ...context.args, ...args }, context))}
-            </div>
-          `,
-        )}
-      </div>
-    `;
+export const Inputtext: Story = {
+  args: {
+    type: 'text',
+  },
+};
+export const InputPassword: Story = {
+  args: {
+    type: 'password',
+  },
+};
+export const Inputdate: Story = {
+  args: {
+    type: 'date',
+  },
+};
+export const Inputdatetimelocal: Story = {
+  args: {
+    type: 'local',
+  },
+};
+export const Inputmonth: Story = {
+  args: {
+    type: 'month',
+  },
+};
+export const Inputweek: Story = {
+  args: {
+    type: 'week',
+  },
+};
+export const Inputtime: Story = {
+  args: {
+    type: 'time',
+  },
+};
+export const Inputcolor: Story = {
+  args: {
+    type: 'color',
   },
 };
---
+++ packages/documentation/src/utils/inputComponentsGetCombinations.ts
@@ -0,0 +1,58 @@
+const SHORTHINT =
+  'Hintus ipsum dolor sit amet consectetur adipisicing elit. Vero mollitia magnam quo quam saepe. Aliquam tempore non deleniti culpa reprehenderit.';
+const LONGHINT =
+  'Hintus ipsum dolor sit amet consectetur adipisicing elit. Voluptatem maxime eius aut quae ducimus dignissimos pariatur suscipit distinctio, accusamus laudantium, sint quibusdam nisi optio? Ut quae obcaecati, harum ullam quos beatae, ipsam enim, placeat eligendi dolores excepturi. Quia quod eligendi ab voluptas modi id distinctio iure vel possimus deserunt, amet, dolores laboriosam quas qui aut laborum? Et numquam esse laboriosam totam quod sapiente recusandae consectetur optio, quaerat quia.';
+const SHORTLABEL = 'Label';
+const LONGTEXT =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem maxime eius aut quae ducimus dignissimos pariatur suscipit distinctio, accusamus laudantium, sint quibusdam nisi optio? Ut quae obcaecati, harum ullam quos beatae, ipsam enim, placeat eligendi dolores excepturi';
+
+export const COMBINATIONS = [
+  {
+    title: true, // This property is true when a heading should be rendered above the story
+    label: `${SHORTLABEL} - no Hint`,
+    hint: null,
+  },
+  {
+    label: `${SHORTLABEL} - short Hint`,
+    hint: SHORTHINT,
+  },
+  {
+    label: `$Label - long - ${LONGTEXT}`,
+    hint: LONGHINT,
+  },
+  {
+    label: `${SHORTLABEL} - Disabled`,
+    disabled: true,
+  },
+  {
+    label: `${SHORTLABEL} - Valid`,
+    validation: 'is-valid',
+  },
+  {
+    label: `${SHORTLABEL} - Invalid`,
+    validation: 'is-invalid',
+  },
+];
+
+export function getCombinations(
+  argumentName: string,
+  argumentValues: Array<unknown>,
+  combinations: Array<{ label: string; [propName: string]: any }>,
+) {
+  console.log(combinations);
+
+  let result: Array<Object> = [];
+  for (const argumentValue of argumentValues) {
+    const labelExtension: string = `[ ${argumentName} : ${argumentValue} ] `;
+
+    result = [
+      ...result,
+      ...combinations.map(c => ({
+        ...c,
+        [argumentName]: argumentValue,
+        label: `${labelExtension}  ${c.label}`,
+      })),
+    ];
+  }
+  return result;
+}
