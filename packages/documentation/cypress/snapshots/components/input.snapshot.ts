7601d29ec770bda50efa9c857a7d2458411a4620
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-09-20 10:55:46 +0200

    feat(documentation): adds visual regression tests for the text input
    changes the cypress intsructions to avoid duplication

--- packages/documentation/cypress/snapshots/components/input.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/input.snapshot.ts
@@ -1,20 +1,26 @@
+const BASEURL = '/iframe.html?id=snapshots--input';
+const types = [
+  'text',
+  'number',
+  'email',
+  'tel',
+  'url',
+  'password',
+  'date',
+  'datetime-local',
+  'month',
+  'week',
+  'time',
+  'color',
+];
+
 describe('Input', () => {
-  it('default', () => {
-    cy.visit('/iframe.html?id=snapshots--inputtext');
-    cy.percySnapshot('Inputs-Text', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputpassword');
-    cy.percySnapshot('Inputs-Password', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputdate');
-    cy.percySnapshot('Inputs-Date', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputdatetimelocal');
-    cy.percySnapshot('Inputs-Datetime-local', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputmonth');
-    cy.percySnapshot('Inputs-Month', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputweek');
-    cy.percySnapshot('Inputs-Week', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputtime');
-    cy.percySnapshot('Inputs-Time', { widths: [320, 1024] });
-    cy.visit('/iframe.html?id=snapshots--inputcolor');
-    cy.percySnapshot('Inputs-Color', { widths: [320, 1024] });
+  describe('types', () => {
+    types.forEach(type => {
+      it(type, () => {
+        cy.visit(`${BASEURL}${type}`);
+        cy.percySnapshot(`Inputs-${type}`, { widths: [320, 1024] });
+      });
+    });
   });
 });
