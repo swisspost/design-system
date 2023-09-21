02369d1b135b08987c2bf05df2baa2892e97eec5
imagoiq <12294151+imagoiq@users.noreply.github.com>
2023-09-21 16:03:13 +0200

    Fix e2e tests

--- packages/components/cypress/e2e/tooltip.cy.ts
+++ packages/components/cypress/e2e/tooltip.cy.ts
@@ -3,26 +3,28 @@ describe('tooltips', { baseUrl: null }, () => {
     cy.visit('cypress/fixtures/tooltip/tooltip-non-focusable.html');
     cy.get('p[data-tooltip-target="t1"]').as('target1');
     cy.get('button[data-tooltip-target="t2"]').as('target2');
-    cy.get('#t1').shadow().get('p[popover]').as('tooltip1');
-    cy.get('#t2').shadow().get('p[popover]').as('tooltip2');
+    cy.get('#t1').shadow().find('div[popover]').as('tooltip1');
+    cy.get('#t2').shadow().find('div[popover]').as('tooltip2');
   });
 
   describe('default', () => {
     it('should display a tooltip', () => {
       cy.get('@tooltip2').should('not.be.visible');
-      cy.get('@target2').focus();
+      cy.get('button[data-tooltip-target="t2"]').focus();
       cy.get('@tooltip2').should('be.visible');
       cy.get('@target2').blur();
+      cy.get('@target1').focus();
       cy.get('@tooltip2').should('not.be.visible');
     });
 
     it('tooltip placement right', () => {
-      cy.get('@tooltip2').invoke('attr', 'placement', 'right');
+      cy.get('#t2').invoke('attr', 'placement', 'right');
       cy.get('@target2').focus();
+      cy.wait(10);
       cy.get('@tooltip2')
         .should('have.css', 'left')
         .then((v: any) => {
-          expect(parseInt(v)).to.be.greaterThan(500);
+          expect(parseInt(v)).to.be.greaterThan(150);
         });
     });
   });
