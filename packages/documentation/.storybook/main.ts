42f15f581dc82cd5c227832ac78c44d6f0069f63
Schuerch Oliver, IT16.12 <oliver.schuerch@post.ch>
2023-09-21 15:54:38 +0200

    feat(documentation): add gtm script

--- packages/documentation/.storybook/main.ts
+++ packages/documentation/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
+import pkg from '../package.json';
 
 const config: StorybookConfig = {
   logLevel: 'info',
@@ -35,28 +36,13 @@ const config: StorybookConfig = {
     '../public',
     '../node_modules/@swisspost/design-system-icons/public',
   ],
-  managerHead: head => `
-    <!-- Google Tag Manager -->
-    <script>
-      (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({ 'gtm.start': new Date().getTime(), 'event': 'gtm.js' });
-    
-        let f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
-    
-        j.async = true;
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-WKSKHGJ');
-    </script>
-    <!-- End Google Tag Manager -->
-    ${head}
-  `,
   docs: {
     autodocs: 'tag',
   },
+  env: config => ({
+    ...config,
+    STORYBOOK_GTM_KEY: 'GTM-WKSKHGJ',
+  }),
 };
 
 export default config;
