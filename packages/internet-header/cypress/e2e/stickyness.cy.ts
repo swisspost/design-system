d5d19572609257c3c80a1d19017d5e24620fde07
imagoiq <12294151+imagoiq@users.noreply.github.com>
2023-09-21 10:35:45 +0200

    Try to fix e2e not working in ci

--- packages/internet-header/cypress/e2e/stickyness.cy.ts
+++ packages/internet-header/cypress/e2e/stickyness.cy.ts
@@ -9,7 +9,7 @@ describe('stickyness', () => {
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.changeArg('stickyness', 'none');
     cy.wait(10);
-    cy.scrollTo('bottom');
+    cy.scrollTo('bottom', { ensureScrollable: false });
     cy.get('swisspost-internet-header').should('not.be.inViewport');
   });
 
@@ -20,11 +20,11 @@ describe('stickyness', () => {
     cy.get('.fake-content:last-of-type').should('be.inViewport');
     cy.get('swisspost-internet-header').should('not.be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
-    cy.scrollTo('center');
+    cy.scrollTo('center', { ensureScrollable: false });
     cy.scrollTo(0, -20);
     cy.get('post-main-navigation').should('be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
-    cy.scrollTo('top');
+    cy.scrollTo('top', { ensureScrollable: false });
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
   });
@@ -33,7 +33,7 @@ describe('stickyness', () => {
     cy.changeArg('stickyness', 'main');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
-    cy.scrollTo('bottom');
+    cy.scrollTo('bottom', { ensureScrollable: false });
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
   });
@@ -42,7 +42,7 @@ describe('stickyness', () => {
     cy.changeArg('stickyness', 'full');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
-    cy.scrollTo('bottom');
+    cy.scrollTo('bottom', { ensureScrollable: false });
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
   });
