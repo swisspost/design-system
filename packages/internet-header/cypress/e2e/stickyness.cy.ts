f8e7d565fbca4c6f0c46f546dc29d7a3464c34ce
imagoiq <12294151+imagoiq@users.noreply.github.com>
2023-09-21 13:45:51 +0200

    Fix e2e stickyness tests

--- packages/internet-header/cypress/e2e/stickyness.cy.ts
+++ packages/internet-header/cypress/e2e/stickyness.cy.ts
@@ -9,22 +9,26 @@ describe('stickyness', () => {
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.changeArg('stickyness', 'none');
     cy.wait(10);
-    cy.scrollTo('bottom', { ensureScrollable: false });
+    cy.get('.header-story-wrapper').scrollTo('bottom');
     cy.get('swisspost-internet-header').should('not.be.inViewport');
   });
 
   it('should not show header when scrolling down with stickyness minimal, should show header without meta when scrolling up a little', () => {
     cy.changeArg('stickyness', 'minimal');
     cy.get('post-meta-navigation').should('be.inViewport');
-    cy.scrollTo('bottom');
+    cy.get('.header-story-wrapper').scrollTo('bottom');
     cy.get('.fake-content:last-of-type').should('be.inViewport');
     cy.get('swisspost-internet-header').should('not.be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
-    cy.scrollTo('center', { ensureScrollable: false });
-    cy.scrollTo(0, -20);
+    cy.get('.header-story-wrapper').scrollTo('center', { duration: 10 });
+    cy.get('.header-story-wrapper').then($el => {
+      const el = $el.get(0); //native DOM element
+      el.scrollTo(0, el.scrollTop - 20);
+    });
+    cy.get('post-main-navigation').should('be.visible');
     cy.get('post-main-navigation').should('be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
-    cy.scrollTo('top', { ensureScrollable: false });
+    cy.get('.header-story-wrapper').scrollTo('top');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
   });
@@ -33,7 +37,7 @@ describe('stickyness', () => {
     cy.changeArg('stickyness', 'main');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
-    cy.scrollTo('bottom', { ensureScrollable: false });
+    cy.get('.header-story-wrapper').scrollTo('bottom');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('not.be.inViewport');
   });
@@ -42,7 +46,7 @@ describe('stickyness', () => {
     cy.changeArg('stickyness', 'full');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
-    cy.scrollTo('bottom', { ensureScrollable: false });
+    cy.get('.header-story-wrapper').scrollTo('bottom');
     cy.get('swisspost-internet-header').should('be.inViewport');
     cy.get('post-meta-navigation').should('be.inViewport');
   });
