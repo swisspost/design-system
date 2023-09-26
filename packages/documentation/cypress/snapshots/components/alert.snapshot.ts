7c7e4be7c049f0925921ff8490eb2050d2c3d364
Alizé Debray <alize.debray@post.ch>
2023-09-26 17:32:45 +0200

    Update snapshot tests

--- packages/documentation/cypress/snapshots/components/alert.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/alert.snapshot.ts
@@ -1,6 +1,11 @@
 describe('Alert', () => {
   it('default', () => {
     cy.visit('./iframe.html?id=snapshots--alert');
-    cy.percySnapshot('Alerts', { widths: [320, 1440] });
+    cy.percySnapshot('Alerts (Standard HTML)', { widths: [320, 1440] });
+  });
+
+  it('post-alert', () => {
+    cy.visit('./iframe.html?id=snapshots--post-alert');
+    cy.percySnapshot('Alerts (Web Component)', { widths: [320, 1440] });
   });
 });
--- packages/documentation/src/stories/components/alert/alert.snapshot.stories.ts
+++ packages/documentation/src/stories/components/alert/alert.snapshot.stories.ts
@@ -1,5 +1,6 @@
 import { Args, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import { spreadArgs } from '../../../utils';
 import { bombArgs } from '../../utilities/bombArgs';
 import alertMeta from './standard-html/alert.stories';
 import { getAlertClasses } from './standard-html/getAlertClasses';
@@ -16,15 +17,12 @@ export const Alert: Story = {
     <div class="d-flex gap-3 flex-wrap">
       ${['bg-white', 'bg-dark'].map(
     bg => html`
-          <div class=${bg + ' d-flex gap-3 flex-wrap p-3'}>
+          <div class=${bg + ' d-flex flex-column gap-3 flex-wrap p-3'}>
             ${bombArgs({
-      variant: alertMeta?.argTypes?.variant?.options,
-      noIcon: [true, false],
-      icon: ['null', '1001'],
-      dismissible: [true, false],
+      type: alertMeta?.argTypes?.type?.options,
+      icon: ['no-icon', undefined, 'pi-1001'],
       action: [true, false],
     })
-      .filter(args => !(args.noIcon && args.icon === 'null'))
       .map(args => ({ ...args, show: true } as Args))
       .map(
         args => html`
@@ -75,4 +73,44 @@ export const Alert: Story = {
   )}
     </div>
   `
+};
+
+export const PostAlert: Story = {
+  render: () => {
+    const textContent =
+      '<h4 slot="heading">post-alert</h4>' +
+      '<p>' +
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis' +
+      'temporibus blanditiis expedita inventore atque. Numquam velit aut' +
+      'eveniet cumque non?' +
+      '</p>';
+
+    const actionButton =
+      '<button class="btn btn-primary btn-animated" slot="actions" >' +
+      '<span>Akcepti</span>' +
+      '</button>' +
+      '<button class="btn btn-default btn-animated" slot="actions" >' +
+      '<span>Aborti</span>' +
+      '</button>';
+
+    return html`
+    <div class='d-flex gap-3 flex-wrap'>
+      ${['bg-white', 'bg-dark'].map(bg => html`
+        <div class='${bg + ' d-flex flex-column gap-3 flex-wrap p-3'}'>
+          ${bombArgs({
+      type: ['primary', 'success', 'danger', 'warning', 'info', 'gray'],
+      icon: ['none', undefined, '1001'],
+      dismissible: [true, false],
+      innerHTML: [textContent + actionButton, textContent],
+    }).map(args => html`
+            <post-alert
+              ${spreadArgs(args)}
+              dismiss-label=${args.dismissible ? 'Dismiss' : undefined}
+            ></post-alert>
+          `)}
+        </div>
+      `)}
+    </div>
+  `
+  },
 };
