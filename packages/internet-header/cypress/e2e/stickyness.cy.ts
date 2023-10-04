d0d8a45031878325fd2800935c516439fb791452
Philipp Gfeller <philipp.gfeller@post.ch>
2023-10-04 17:09:15 +0200

    chore: fix e2e

--- packages/internet-header/cypress/e2e/stickyness.cy.ts
+++ packages/internet-header/cypress/e2e/stickyness.cy.ts
@@ -6,7 +6,7 @@ describe('stickyness', () => {
   });
 
   it('should not show header when scrolling when stickyness is none', () => {
-    cy.get('swisspost-internet-header').should('be.inViewport');
+    cy.get('post-main-navigation').should('be.inViewport');
     cy.changeArg('stickyness', 'none');
     cy.wait(10);
     cy.get('.header-story-wrapper').scrollTo('bottom');
@@ -15,22 +15,17 @@ describe('stickyness', () => {
 
   it('should not show header when scrolling down with stickyness minimal, should show header without meta when scrolling up a little', () => {
     cy.changeArg('stickyness', 'minimal');
-    cy.get('post-meta-navigation').should('be.inViewport');
+    cy.get('post-meta-navigation').should('be.visible');
     cy.get('.header-story-wrapper').scrollTo('bottom');
-    cy.get('.fake-content:last-of-type').should('be.inViewport');
-    cy.get('swisspost-internet-header').should('not.be.inViewport');
-    cy.get('post-meta-navigation').should('not.be.inViewport');
-    cy.get('.header-story-wrapper').scrollTo('center', { duration: 10 });
-    cy.get('.header-story-wrapper').then($el => {
-      const el = $el.get(0); //native DOM element
-      el.scrollTo(0, el.scrollTop - 20);
-    });
+    cy.get('.fake-content:last-of-type').should('be.visible');
+    cy.get('swisspost-internet-header').should('not.be.visible');
+    cy.get('post-meta-navigation').should('not.be.visible');
+    cy.get('.header-story-wrapper').scrollTo('center').trigger('scroll');
     cy.get('post-main-navigation').should('be.visible');
-    cy.get('post-main-navigation').should('be.inViewport');
-    cy.get('post-meta-navigation').should('not.be.inViewport');
+    cy.get('post-meta-navigation').should('not.be.visible');
     cy.get('.header-story-wrapper').scrollTo('top');
-    cy.get('swisspost-internet-header').should('be.inViewport');
-    cy.get('post-meta-navigation').should('be.inViewport');
+    cy.get('swisspost-internet-header').should('be.visible');
+    cy.get('post-meta-navigation').should('be.visible');
   });
 
   it('should show main header when scrolling down', () => {
