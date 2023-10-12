8068df6d8645b18267ad4667ad09c37d117a9ac9
oliverschuerch <oliver.schuerch@post.ch>
2023-10-12 16:29:29 +0200

    chore: update bad example

--- packages/documentation/src/stories/foundation/layout/grid/grid.stories.ts
+++ packages/documentation/src/stories/foundation/layout/grid/grid.stories.ts
@@ -36,10 +36,11 @@ export const SingleColumnOnly: Story = {
       <div class="row">
         <div class="col">Don't do this!</div>
       </div>
+      <br/>
       <div class="row">
         <div class="col-12">Nor this!</div>
       </div>
-      <br />
+      <br/>
       <p>Instead, your content should go here!</p>
     </div>
   `,
