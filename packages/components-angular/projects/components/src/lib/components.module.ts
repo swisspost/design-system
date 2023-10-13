da6b490655c732febadfcc10444b1224e8342726
oliverschuerch <oliver.schuerch@post.ch>
2023-10-13 15:39:57 +0200

    fix(components-angular): add custom-element definition in components.module

--- packages/components-angular/projects/components/src/lib/components.module.ts
+++ packages/components-angular/projects/components/src/lib/components.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { DIRECTIVES } from './stencil-generated';
-
-
+import { defineCustomElements } from '@swisspost/design-system-components';
 
 @NgModule({
   declarations: [
     ...DIRECTIVES
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => defineCustomElements,
+      multi: true,
+    }
   ],
   exports: [
     ...DIRECTIVES
