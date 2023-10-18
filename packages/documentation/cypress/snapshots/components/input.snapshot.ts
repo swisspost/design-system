44e267d05c888ce2e842b3a717aac1e300c52f2d
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-10-18 15:20:07 +0200

    fix(documentation): prevents snapshot for input from being blank

--- packages/documentation/cypress/snapshots/components/input.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/input.snapshot.ts
@@ -6,7 +6,7 @@ describe('Input', () => {
     types.forEach(type => {
       it(type, () => {
         cy.visit(`${BASEURL}${type}`);
-        cy.get(`ExampleTextarea_Input${type}`, { timeout: 30000 }).should('be.visible');
+        cy.get('.form-control', { timeout: 30000 }).should('be.visible');
         cy.percySnapshot(`Inputs-${type}`, { widths: [320, 1024] });
       });
     });
