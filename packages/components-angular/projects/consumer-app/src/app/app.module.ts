f303b86aacbd719d1fd3fdb298bea623e6fa373b
oliverschuerch <oliver.schuerch@post.ch>
2023-10-26 13:05:05 +0200

    chore(components-angular): add consumer-app

---
+++ packages/components-angular/projects/consumer-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from 'components';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
