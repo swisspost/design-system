5e673d1c86b432e814a0e411d0e4cde6e729bff4
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2023-11-02 22:45:55 +0100

    Update packages/components/cypress/e2e/popover.cy.ts
    Co-authored-by: Loïc Fürhoff <12294151+imagoiq@users.noreply.github.com>

--- packages/components/cypress/e2e/popover.cy.ts
+++ packages/components/cypress/e2e/popover.cy.ts
@@ -2,7 +2,7 @@ describe('popover', () => {
   describe('default', () => {
     beforeEach(() => {
       cy.getComponent('popover');
-      // Aria-expanded is set by the web component, therefor it's a good measure to indicate the component is ready
+      // Aria-expanded is set by the web component, therefore it's a good measure to indicate the component is ready
       cy.get('[data-popover-target="popover-one"][aria-expanded]').as('trigger');
       cy.get('#popover-one.hydrated').as('popover');
     });
