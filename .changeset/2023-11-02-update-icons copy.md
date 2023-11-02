fb8878e6bf7ed1b3e96becd6c3512d69ef54eb57
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2023-11-02 12:04:27 +0100

    chore: revert internet header output targets (#2208)

---
+++ .changeset/2023-11-02-update-icons copy.md
@@ -0,0 +1,6 @@
+---
+'@swisspost/internet-header': patch
+'@swisspost/design-system-components': patch
+---
+
+Reverted #2152 because of an issue with importing the internet headers `defineCustomElements`.
--- packages/internet-header/stencil.config.ts
+++ packages/internet-header/stencil.config.ts
@@ -30,6 +30,10 @@ export const config: Config = {
       type: 'docs-readme',
     },
     {
+      type: 'www',
+      serviceWorker: null, // disable service workers,
+    },
+    {
       type: 'docs-json',
       file: 'dist/docs.json',
     },
