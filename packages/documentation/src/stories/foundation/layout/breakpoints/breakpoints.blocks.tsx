dbe89ed966b5f17762df0bce2b043375e74fa679
oliverschuerch <oliver.schuerch@post.ch>
2023-11-03 12:46:28 +0100

    refactor(documentation): reducing code duplication in layout docs

--- packages/documentation/src/stories/foundation/layout/breakpoints/breakpoints.blocks.tsx
+++ packages/documentation/src/stories/foundation/layout/breakpoints/breakpoints.blocks.tsx
@@ -1,54 +1,36 @@
 import { forEach } from '../../../../utils/react';
 import { parse } from '../../../../utils/sass-export';
+import { SpecTable } from '../shared.blocks';
 import scss from './breakpoints.module.scss';
 
 export const SCSS_VARIABLES = parse(scss);
 
-export const SpecTable = () => (
-  <div className="table-responsive my-big">
-    <table className="table">
-      <thead>
-        <tr>
-          <th></th>
-          {forEach(
-            SCSS_VARIABLES.breakpoint,
-            (data: { key: string; value: { dimensions: string } }) => (
-              <th key={data.key}>
-                {data.key}
-                <br />
-                <small className="fw-normal">{data.value.dimensions}</small>
-              </th>
-            ),
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { name: string } }) => (
-            <td key={data.key}>
-              <small>{data.value.name}</small>
-            </td>
-          ))}
-        </tr>
+export const BreakpointTable = () => (
+  <SpecTable>
+    <tr>
+      <th>Name</th>
+      {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { name: string } }) => (
+        <td key={data.key}>
+          <small>{data.value.name}</small>
+        </td>
+      ))}
+    </tr>
 
-        <tr>
-          <th>Code name</th>
-          {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string }) => (
-            <td key={data.key}>
-              <code>{data.key}</code>
-            </td>
-          ))}
-        </tr>
-        <tr>
-          <th>Class infix</th>
-          {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { infix: string } }) => (
-            <td key={data.key}>
-              {data.value.infix === 'none' ? 'none' : <code>-{data.value.infix}-</code>}
-            </td>
-          ))}
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <tr>
+      <th>Code name</th>
+      {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string }) => (
+        <td key={data.key}>
+          <code>{data.key}</code>
+        </td>
+      ))}
+    </tr>
+    <tr>
+      <th>Class infix</th>
+      {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { infix: string } }) => (
+        <td key={data.key}>
+          {data.value.infix === 'none' ? 'none' : <code>-{data.value.infix}-</code>}
+        </td>
+      ))}
+    </tr>
+  </SpecTable>
 );
--- packages/documentation/src/stories/foundation/layout/grid/grid.blocks.tsx
+++ packages/documentation/src/stories/foundation/layout/grid/grid.blocks.tsx
@@ -1,78 +1,60 @@
 import LinkTo from '@storybook/addon-links/react';
 import { parse } from '../../../../utils/sass-export';
 import { forEach } from '../../../../utils/react';
+import { SpecTable } from '../shared.blocks';
 import scss from './grid.module.scss';
 
 export const SCSS_VARIABLES = parse(scss);
 
-export const SpecTable = () => (
-  <div className="table-responsive my-big">
-    <table className="table">
-      <thead>
-        <tr>
-          <th></th>
-          {forEach(
-            SCSS_VARIABLES.breakpoint,
-            (data: { key: string; value: { dimensions: string } }) => (
-              <th key={data.key}>
-                {data.key}
-                <br />
-                <small className="fw-normal">{data.value.dimensions}</small>
-              </th>
-            ),
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Class prefix</th>
-          {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { infix: string } }) => (
-            <td key={data.key}>
-              <code>.col-{data.value.infix === 'none' ? '' : `${data.value.infix}-`}</code>
-            </td>
-          ))}
-        </tr>
+export const GridTable = () => (
+  <SpecTable>
+    <tr>
+      <th>Class prefix</th>
+      {forEach(SCSS_VARIABLES.breakpoint, (data: { key: string; value: { infix: string } }) => (
+        <td key={data.key}>
+          <code>.col-{data.value.infix === 'none' ? '' : `${data.value.infix}-`}</code>
+        </td>
+      ))}
+    </tr>
 
-        <tr>
-          <th>Gutter width</th>
-          {forEach(
-            SCSS_VARIABLES.grid,
-            (data: { key: string; value: { ['gutter-width']: string } }) => (
-              <td key={data.key}>{data.value['gutter-width']}</td>
-            ),
-          )}
-        </tr>
+    <tr>
+      <th>Gutter width</th>
+      {forEach(
+        SCSS_VARIABLES.grid,
+        (data: { key: string; value: { ['gutter-width']: string } }) => (
+          <td key={data.key}>{data.value['gutter-width']}</td>
+        ),
+      )}
+    </tr>
 
-        <tr>
-          <th>Amount of columns</th>
-          <td colSpan={SCSS_VARIABLES['breakpoint-count']}>{SCSS_VARIABLES['grid-columns']}</td>
-        </tr>
+    <tr>
+      <th>Amount of columns</th>
+      <td colSpan={SCSS_VARIABLES['breakpoint-count']}>{SCSS_VARIABLES['grid-columns']}</td>
+    </tr>
 
-        <tr>
-          <th>Custom gutters</th>
-          <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
-            <LinkTo kind="foundations-layout-gutters" story="docs">
-              yes
-            </LinkTo>
-          </td>
-        </tr>
-        <tr>
-          <th>Nestable</th>
-          <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
-            <a href="#nesting" target="_self">
-              yes
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th>Column ordering</th>
-          <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
-            <LinkTo kind="foundations-layout-columns" story="docs">
-              yes
-            </LinkTo>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <tr>
+      <th>Custom gutters</th>
+      <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
+        <LinkTo kind="foundations-layout-gutters" story="docs">
+          yes
+        </LinkTo>
+      </td>
+    </tr>
+    <tr>
+      <th>Nestable</th>
+      <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
+        <a href="#nesting" target="_self">
+          yes
+        </a>
+      </td>
+    </tr>
+    <tr>
+      <th>Column ordering</th>
+      <td colSpan={SCSS_VARIABLES['breakpoint-count']}>
+        <LinkTo kind="foundations-layout-columns" story="docs">
+          yes
+        </LinkTo>
+      </td>
+    </tr>
+  </SpecTable>
 );
