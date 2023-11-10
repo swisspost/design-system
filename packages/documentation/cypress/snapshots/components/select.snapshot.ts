89a125851cf30af842390e239d9663f3546c887c
David Ritter <141235163+davidritter-dotcom@users.noreply.github.com>
2023-11-10 10:23:44 +0100

    fix(documentation): add timeout in the select snapshot (#2253)

--- packages/documentation/cypress/snapshots/components/select.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/select.snapshot.ts
@@ -6,9 +6,9 @@ describe('Select', () => {
     multiple.forEach(multiple => {
       it(multiple, () => {
         cy.visit(`${SELECTBASEURL}${multiple}`);
+        cy.get('.form-select', { timeout: 30000 }).should('be.visible');
         cy.percySnapshot(`Selects-${multiple}`, { widths: [400] });
       });
     });
-
   });
 });
