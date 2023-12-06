48f48537d871c6576bf2b75b7a3a073a8fe8db5c
Lukas Blaser <141234680+b1aserlu@users.noreply.github.com>
2023-12-06 09:06:57 +0100

    feat(documentation): restructurs components folder now contains subfolders Forms, Ions, Internet and Intranet Header (#2175)
    Alternative Version to #2172 .
    Was chosen and as discussed the headers and the icons documentation are
    now also subfolders of components.
    
    
    ![image](https://github.com/swisspost/design-system/assets/141234680/32d0d194-7535-4fa6-bb72-96226a253680)

---
+++ packages/documentation/src/stories/components/internet-header/components/header/overrides-stories/header-os-flyout-overrides.stories.ts
@@ -0,0 +1,8 @@
+import * as HeaderStories from '../header.stories';
+
+export default {
+  ...HeaderStories.default,
+  title: 'Components/Internet Header/Header/Os Flyout Overrides',
+};
+
+export const Default = HeaderStories.OSFlyoutOverrides;
