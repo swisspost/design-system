d9f0c7d9dcc81123995b1b25a17b19a9c81d5683
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-12-07 11:09:28 +0100

    feat(documentation): adds contrastValues and an example text for colors story

--- packages/documentation/src/stories/foundation/color/color.blocks.tsx
+++ packages/documentation/src/stories/foundation/color/color.blocks.tsx
@@ -10,7 +10,16 @@ export const ColorSwatch = (props: { name: string; color: string; noCSS?: boolea
         <h3 className="description__title h6">{props.name}</h3>
       </div>
       <div className="color-swatch__color">
-        <div className="color__tile" style={{ backgroundColor: props.color }} />
+        <div className="color__tile" style={{ backgroundColor: props.color }}>
+          <ul className="list-unstyled ps-1 d-flex flex-column align-content-end">
+            <li>
+              <span className="white">white</span>
+            </li>
+            <li>
+              <span className="black">black</span>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="color-swatch__props">
         <dl>
@@ -23,6 +32,14 @@ export const ColorSwatch = (props: { name: string; color: string; noCSS?: boolea
           <dt>Sass</dt>
           <dd>
             <code>post.${props.name}</code>
+          </dd>
+          <dt>contrast to black</dt>
+          <dd>
+            <code>{SCSS_VARIABLES.contrast.black[props.name]}</code>
+          </dd>
+          <dt>contrast to white</dt>
+          <dd>
+            <code>{SCSS_VARIABLES.contrast.white[props.name]}</code>
           </dd>
         </dl>
       </div>
