2138132875e41458abc590aa9f090682000a846b
Debray Alize, IT16.12 <alize.debray@post.ch>
2023-12-11 17:32:43 +0100

    update story title

--- packages/documentation/src/stories/components/internet-header/components/header/overrides-stories/header-custom-content.stories.ts
+++ packages/documentation/src/stories/components/internet-header/components/header/overrides-stories/header-custom-content.stories.ts
@@ -1,5 +1,8 @@
 import * as HeaderStories from '../header.stories';
 
-export default { ...HeaderStories.default, title: 'Internet Header/Header/Custom Content' };
+export default {
+  ...HeaderStories.default,
+  title: 'Components/Internet Header/Header/Custom Content',
+};
 
 export const Default = HeaderStories.CustomContent;
