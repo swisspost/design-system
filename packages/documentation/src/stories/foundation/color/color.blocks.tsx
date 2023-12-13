1a8eb0395559dd7f2f2f1e2c2f7c4a84f1837f24
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2023-12-13 15:16:53 +0100

    beautifies the example and changes the infobox on the color story docs

--- packages/documentation/src/stories/foundation/color/color.blocks.tsx
+++ packages/documentation/src/stories/foundation/color/color.blocks.tsx
@@ -4,21 +4,34 @@ import scss from './color.module.scss';
 export const SCSS_VARIABLES = parse(scss);
 
 export const ColorSwatch = (props: { name: string; color: string; noCSS?: boolean }) => {
+  let contrastWhite: Number = SCSS_VARIABLES.contrast.white[props.name];
+  let contrastBlack: Number = SCSS_VARIABLES.contrast.black[props.name];
   return (
     <article className="color-swatch">
       <div className="color-swatch__description">
         <h3 className="description__title h6">{props.name}</h3>
       </div>
       <div className="color-swatch__color">
-        <div className="color__tile" style={{ backgroundColor: props.color }}>
-          <ul className="list-unstyled ps-1 d-flex flex-column align-content-end">
-            <li>
-              <span className="white">white</span>
-            </li>
-            <li>
-              <span className="black">black</span>
-            </li>
-          </ul>
+        <div
+          className="color__tile"
+          style={{
+            backgroundColor: props.color,
+            color:
+              contrastBlack >= contrastWhite
+                ? SCSS_VARIABLES.brand.black
+                : SCSS_VARIABLES.brand.white,
+          }}
+        >
+          <div className="h-100 px-2 d-flex flex-column align-items-end justify-content-center">
+            <div className="d-flex align-items-center">
+              <span className="fs-tiny">{Number(contrastWhite).toFixed(2)}</span>
+              <div className="h-mini w-mini ms-mini  white"></div>
+            </div>
+            <div className="d-flex align-items-center">
+              <span className="fs-tiny">{Number(contrastBlack).toFixed(2)}</span>
+              <div className="h-mini w-mini ms-mini black"></div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="color-swatch__props">
@@ -32,14 +45,6 @@ export const ColorSwatch = (props: { name: string; color: string; noCSS?: boolea
           <dt>Sass</dt>
           <dd>
             <code>post.${props.name}</code>
-          </dd>
-          <dt>contrast to black</dt>
-          <dd>
-            <code>{SCSS_VARIABLES.contrast.black[props.name]}</code>
-          </dd>
-          <dt>contrast to white</dt>
-          <dd>
-            <code>{SCSS_VARIABLES.contrast.white[props.name]}</code>
           </dd>
         </dl>
       </div>
