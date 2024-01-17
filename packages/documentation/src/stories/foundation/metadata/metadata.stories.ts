1ac462cbe22e0586d10197657c3eb8a1c16a3c97
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2024-01-17 08:48:11 +0100

    feat(documentation): adds metadata story
    feat(documentation): adds metadata story
    
    feat(documentation): adds metadata story
    
    morning commit

---
+++ packages/documentation/src/stories/foundation/metadata/metadata.stories.ts
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+
+const meta: Meta = {
+  title: 'Foundations/Metadata',
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {};
