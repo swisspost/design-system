50bd0a7ba0654404c95bcaf791a2434aaf3921f9
b1aserlu <141234680+b1aserlu@users.noreply.github.com>
2024-01-18 12:59:24 +0100

    removes unecessary tags adds env variable for url
    checked the documentation for twitter cards and saw that they had fallbacks on the respective open graph tags.

--- packages/documentation/.storybook/main.ts
+++ packages/documentation/.storybook/main.ts
@@ -48,6 +48,7 @@ const config: StorybookConfig = {
     STORYBOOK_GTM_PAGE_CONTEXT_ENVIRONMENT_INT: 'preview-',
     STORYBOOK_GTM_PAGE_CONTEXT_ENVIRONMENT_PROD: 'design-system.post.ch,next.design-system.post.ch',
     STORYBOOK_GTM_PAGE_CONTEXT_ENVIRONMENT_FALLBACK: 'dev',
+    STORYBOOK_BASE_URL: 'https://next.design-system-post.ch',
   }),
   async viteFinal(config, options) {
     return mergeConfig(config, {
