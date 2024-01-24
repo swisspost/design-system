cd541fa0cf8823b4ae12abae6707b8513ab87807
davidritter-dotcom <david.ritter.1@post.ch>
2024-01-24 16:20:13 +0100

    feat(documentation): samples and doc for dropdown

---
+++ packages/documentation/src/stories/components/dropdown/dropdown.stories.ts
@@ -0,0 +1,13 @@
+import { Meta } from '@storybook/web-components';
+import { BADGE } from '../../../../.storybook/constants';
+
+const meta: Meta = {
+  title: 'Components/Dropdown',
+  parameters: {
+    badges: [BADGE.NEEDS_REVISION],
+  },
+};
+
+export default meta;
+
+export const Default = {};
