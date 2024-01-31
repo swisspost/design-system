94796ecc3d9b0cb8eec12fef43848fc0a201ef64
Oliver Schürch <oliver.schuerch@post.ch>
2024-01-31 11:37:47 +0100

    feat: add components-angular as output-target from components package (#2071)
    Co-authored-by: Loïc Fürhoff <12294151+imagoiq@users.noreply.github.com>
    Co-authored-by: Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>

---
+++ packages/components/.config/bindings.angular.ts
@@ -0,0 +1,4 @@
+import { ValueAccessorConfig } from '@stencil/angular-output-target';
+
+// https://stenciljs.com/docs/v4/angular#valueaccessorconfigs
+export const angularValueAccessorBindings: ValueAccessorConfig[] = [];
