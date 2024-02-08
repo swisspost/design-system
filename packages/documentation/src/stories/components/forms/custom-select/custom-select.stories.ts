f693a99f92fd5eb085a2e059393f4b48d37ec784
Loïc Fürhoff <12294151+imagoiq@users.noreply.github.com>
2024-02-08 10:03:27 +0100

    Update packages/documentation/src/stories/components/forms/custom-select/custom-select.stories.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/documentation/src/stories/components/forms/custom-select/custom-select.stories.ts
+++ packages/documentation/src/stories/components/forms/custom-select/custom-select.stories.ts
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/web-components';
 import { BADGE } from '../../../../../.storybook/constants';
 
 const meta: Meta = {
-  id: 'Components/Forms/Custom-Select',
+  id: 'Components/Forms/Custom Select',
   title: 'Components/Forms/Custom-Select',
   parameters: {
     badges: [BADGE.DEPRECATED],
