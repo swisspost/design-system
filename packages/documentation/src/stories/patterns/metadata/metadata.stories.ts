89b2cb088915772a994fb966841cd7bbf44adba7
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2024-02-16 11:35:39 +0100

    Update packages/documentation/src/stories/patterns/metadata/metadata.stories.ts

--- packages/documentation/src/stories/patterns/metadata/metadata.stories.ts
+++ packages/documentation/src/stories/patterns/metadata/metadata.stories.ts
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/web-components';
 
 const meta: Meta = {
   id: 'a9543ea3-044d-4e86-bbb9-985c3f74f270',
-  title: 'Foundations/Metadata',
+  title: 'Patterns/Metadata',
 };
 
 export default meta;
