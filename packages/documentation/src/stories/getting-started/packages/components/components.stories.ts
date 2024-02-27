e7c3701f5a7b07012e1c51924a5b3a494016545c
imagoiq <12294151+imagoiq@users.noreply.github.com>
2024-02-27 16:19:46 +0100

    Remove badge to ensure, that we'll not add them again after the PR to remove all badges

--- packages/documentation/src/stories/getting-started/packages/components/components.stories.ts
+++ packages/documentation/src/stories/getting-started/packages/components/components.stories.ts
@@ -1,11 +1,10 @@
 import { Meta, StoryObj } from '@storybook/web-components';
-import { BADGE } from '../../../../../.storybook/constants';
 
 const meta: Meta = {
   id: 'edfb619b-fda1-4570-bf25-20830303d483',
   title: 'Getting Started/Packages/Components',
   parameters: {
-    badges: [BADGE.NEEDS_REVISION],
+    badges: [],
   },
 };
 
--- packages/documentation/src/stories/getting-started/packages/internet-header/internet-header.stories.ts
+++ packages/documentation/src/stories/getting-started/packages/internet-header/internet-header.stories.ts
@@ -1,11 +1,10 @@
 import { Meta, StoryObj } from '@storybook/web-components';
-import { BADGE } from '../../../../../.storybook/constants';
 
 const meta: Meta = {
   id: 'cfa21cbc-5f8d-4a42-8165-7f6ed20bf133',
   title: 'Getting Started/Packages/Internet-header',
   parameters: {
-    badges: [BADGE.NEEDS_REVISION],
+    badges: [],
   },
 };
 
