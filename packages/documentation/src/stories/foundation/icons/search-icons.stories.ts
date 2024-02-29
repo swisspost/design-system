5cbe3e0571afca0b75682cd650bbe61b9cfcaf36
imagoiq <12294151+imagoiq@users.noreply.github.com>
2024-02-29 10:14:20 +0100

    Change icon to Search for Icons

--- packages/documentation/src/stories/foundation/icons/search-icons.stories.ts
+++ packages/documentation/src/stories/foundation/icons/search-icons.stories.ts
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/web-components';
 
 const meta: Meta = {
   id: '5704bdc4-c5b5-45e6-b123-c54d01fce2f1',
-  title: 'Foundations/Icons',
+  title: 'Foundations/Search for Icons',
 };
 
 export default meta;
