e074b766412767c7a9e66c8efc3abe94b862b02d
Loïc Fürhoff <12294151+imagoiq@users.noreply.github.com>
2024-03-05 12:02:16 +0100

    chore(documentation): Snapshot tests failing (#2754)

--- packages/documentation/cypress/snapshots/components/rating.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/rating.snapshot.ts
@@ -1,6 +1,6 @@
 describe('Post-Rating', () => {
   it('default', () => {
-    cy.visit('/iframe.html?id=snapshots--post-rating');
+    cy.visit('/iframe.html?id=snapshots--rating');
     cy.get('post-rating', { timeout: 30000 }).should('be.visible');
     cy.percySnapshot('Post-Rating', { widths: [600] });
   });
