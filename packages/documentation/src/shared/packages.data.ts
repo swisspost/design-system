e53f0c11f6dea1dd2a1eae09cd9fe474d8292350
imagoiq <12294151+imagoiq@users.noreply.github.com>
2024-03-05 14:51:55 +0100

    Extract package data and types

--- packages/documentation/src/shared/packages.data.ts
+++ packages/documentation/src/shared/packages.data.ts
@@ -1,18 +1,11 @@
-import { DEPENDENCIES, getVersion } from '../../../utils/version';
-import metaStyles from './styles/styles.stories';
-import metaComponents from './components/components.stories';
-import metaComponentsAngular from './components-angular/components-angular.stories';
-import metaInternetHeader from './internet-header/internet-header.stories';
-import metaIntranetHeader from './intranet-header/intranet-header.stories';
-import metaIcons from './icons/package-icons.stories';
-
-export enum PackageType {
-  Html = 'HTML',
-  Wc = 'WebComponents',
-  Angular = 'Angular',
-  React = 'React',
-  Assets = 'Assets',
-}
+import { DEPENDENCIES, getVersion } from '../utils/version';
+import metaStyles from '../stories/getting-started/packages/styles/styles.stories';
+import metaComponents from '../stories/getting-started/packages/components/components.stories';
+import metaComponentsAngular from '../stories/getting-started/packages/components-angular/components-angular.stories';
+import metaInternetHeader from '../stories/getting-started/packages/internet-header/internet-header.stories';
+import metaIntranetHeader from '../stories/getting-started/packages/intranet-header/intranet-header.stories';
+import metaIcons from '../stories/getting-started/packages/icons/package-icons.stories';
+import { PackageType } from '../../types/package';
 
 interface IPackage {
   name: string;
---
+++ packages/documentation/types/package.ts
@@ -0,0 +1,7 @@
+export enum PackageType {
+  Html = 'HTML',
+  Wc = 'WebComponents',
+  Angular = 'Angular',
+  React = 'React',
+  Assets = 'Assets',
+}
