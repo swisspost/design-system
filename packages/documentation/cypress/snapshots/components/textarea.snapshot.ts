48184d4c9d541f798eafaff442dfb18c1c12e3da
imagoiq <12294151+imagoiq@users.noreply.github.com>
2024-03-05 10:50:52 +0100

    Add timeout for textarea

--- packages/documentation/cypress/snapshots/components/textarea.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/textarea.snapshot.ts
@@ -1,6 +1,7 @@
 describe('Textarea', () => {
   it('default', () => {
     cy.visit('/iframe.html?id=snapshots--textarea');
+    cy.get('textarea', { timeout: 30000 }).should('be.visible');
     cy.percySnapshot('Textareas', { widths: [400] });
   });
 });
