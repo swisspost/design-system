f17eff60ab7f52ede8412c7af8c97c53ad94f95b
imagoiq <12294151+imagoiq@users.noreply.github.com>
2024-04-03 10:05:34 +0200

    Revert heading url

--- packages/documentation/src/stories/components/heading/heading.stories.ts
+++ packages/documentation/src/stories/components/heading/heading.stories.ts
@@ -10,7 +10,7 @@ const meta: MetaComponent = {
     badges: [],
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/xZ0IW0MJO0vnFicmrHiKaY/Components-Post?type=design&node-id=20729-18799&mode=design&t=3lniLiZhl7q9Gqgn-4',
+      url: 'https://www.figma.com/file/xZ0IW0MJO0vnFicmrHiKaY/Components-Post?type=design&node-id=6424-29869&mode=design&t=ErO7GVSw8DfHlx0L-4',
     },
   },
   args: {
