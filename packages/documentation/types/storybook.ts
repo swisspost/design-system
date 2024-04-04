736366fa44d50860e1e8f7e8cd3e6bf23a9bdc3d
oliverschuerch <oliver.schuerch@post.ch>
2024-04-04 17:08:00 +0200

    chore: revert unnecessary change

--- packages/documentation/types/storybook.ts
+++ packages/documentation/types/storybook.ts
@@ -1,12 +1,12 @@
 import { Args, Meta } from '@storybook/web-components';
 import { PackageType } from './package';
 
-export type TagPackagePrefix<K> = K extends string ? `package:${K}` : K;
-
 export interface MetaExtended<T = Args> extends Meta<T> {
   id: string; // Make id required
   title: string; // Make title required
 }
+
+export type TagPackagePrefix<K> = K extends string ? `package:${K}` : K;
 
 export interface MetaComponent<T = Args> extends MetaExtended<T> {
   tags: [TagPackagePrefix<PackageType>, ...string[]];
