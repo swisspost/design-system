6e17494c84f72738aeeee18f7c95488241d1d4d7
Alizé Debray <33580481+alizedebray@users.noreply.github.com>
2024-04-11 09:27:22 +0200

    fix: fix snapshot tests (#2924)

--- packages/documentation/src/stories/components/subnavigation/subnavigation.snapshot.stories.ts
+++ packages/documentation/src/stories/components/subnavigation/subnavigation.snapshot.stories.ts
@@ -3,8 +3,10 @@ import meta from './subnavigation.stories';
 import { html } from 'lit';
 import { bombArgs } from '../../../utils';
 
+const { id, ...metaWithoutId } = meta;
+
 export default {
-  ...meta,
+  ...metaWithoutId,
   title: 'Snapshots',
 };
 
