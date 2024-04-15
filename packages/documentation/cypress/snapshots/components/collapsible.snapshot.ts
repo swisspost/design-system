d0b7d89f33f006314c28c95ba988309f5e6be0ad
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2024-04-15 15:36:07 +0200

    test(collapsible): temporary fix for initial animation (#2946)
    Updated #2929 with additional info to fix the real bug.

--- packages/documentation/cypress/snapshots/components/collapsible.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/collapsible.snapshot.ts
@@ -2,6 +2,7 @@ describe('Collapsible', () => {
   it('default', () => {
     cy.visit('/iframe.html?id=snapshots--collapsible');
     cy.get('post-collapsible.hydrated', { timeout: 30000 }).should('be.visible');
+    cy.wait(500); // Wait for collapse animation to run
     cy.percySnapshot('Collapsible');
   });
 });
