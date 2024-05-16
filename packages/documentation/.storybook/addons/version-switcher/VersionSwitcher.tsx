b3299746fc148def947766e1a3c11a0acee0d676
Oliver Schürch <oliver.schuerch@post.ch>
2024-05-16 09:30:27 +0200

    refactor(documentation): version-switcher structure and styles

--- packages/documentation/.storybook/addons/version-switcher/VersionSwitcher.tsx
+++ packages/documentation/.storybook/addons/version-switcher/VersionSwitcher.tsx
@@ -1,11 +1,10 @@
 import { IconButton, WithTooltip } from '@storybook/components';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getVersion } from '../../../src/utils/version';
 import * as packageJson from '../../../package.json';
 
 const DESIGN_SYSTEM_URL = 'https://design-system.post.ch/assets/versions.json';
-
-const currentVersionMajorMinor = getVersion(
+const BROWSED_VERSION = getVersion(
   packageJson.dependencies['@swisspost/design-system-styles'] || '',
   'majorminor',
 );
@@ -36,81 +35,74 @@ function VersionSwitcher() {
         setVersions(versionsJSON);
         setLoading(false);
       } catch (error) {
-        console.log(`failed to fetch versions file. Errormessage: ${error}`);
+        console.log(`Failed to fetch versions file. Errormessage: ${error}`);
       }
     }
     void fetchVersions();
   }, []);
 
-  if (loading) {
-    return <div className="version-switcher-loading">Loading...</div>;
-  }
+  if (loading) return <div className="version-switcher__loading">Loading...</div>;
 
   return (
-    <Fragment>
-      <WithTooltip
-        placement="bottom"
-        trigger="click"
-        closeOnOutsideClick
-        tooltip={() => {
-          return (
-            <div className="version-switcher-dropdown">
-              {versions.map(version => (
+    <WithTooltip
+      placement="bottom"
+      trigger="click"
+      closeOnOutsideClick
+      tooltip={() => (
+        <>
+          <IconButton
+            placeholder="Versions"
+            className="version_switcher__sizing_placeholder"
+            aria-hidden="true"
+          >
+            v{BROWSED_VERSION}
+            <post-icon name="2052"></post-icon>
+          </IconButton>
+          <div className="version-switcher__dropdown">
+            {versions.map(version => {
+              const isActive =
+                getVersion(version.version || '', 'majorminor') === BROWSED_VERSION ? 'active' : '';
+              const deps = Object.entries(version.dependencies || [])
+                .filter(([k]) => !/^@swisspost\//.test(k))
+                .map(([k, v]) => ({
+                  key: k,
+                  name: k.match(/(?:@?)([^/]+)/)?.[1],
+                  version: v,
+                }));
+
+              return (
                 <a
-                  className={`version-switcher-dropdown-item${
-                    getVersion(version.version || '', 'majorminor') === currentVersionMajorMinor
-                      ? ' active'
-                      : ''
-                  }`}
+                  className={['dropdown__item', isActive].filter(c => c).join(' ')}
                   key={version.title}
                   href={version.url}
                 >
-                  <span>{version.title}</span>
-                  {version.dependencies ? (
-                    <table className="version-switcher-dependencies">
-                      <caption className="visually-hidden">Compatibility</caption>
-                      <thead className="visually-hidden">
-                        <tr>
-                          <th scope="col">Angular</th>
-                          <th scope="col">Bootstrap</th>
-                          <th scope="col">Ng-bootstrap</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>
-                            <span>
-                              <img src="/assets/images/technologies/logo-angular.svg" alt="" />
-                              {version.dependencies['@angular/core']}
-                            </span>
-                          </td>
-                          <td>
-                            <span>
-                              <img src="/assets/images/technologies/logo-bootstrap.svg" alt="" />
-                              {version.dependencies['bootstrap']}
-                            </span>
-                          </td>
-                          <td>
-                            <span>
-                              <img src="/assets/images/technologies/logo-ng-bootstrap.svg" alt="" />
-                              {version.dependencies['@ng-bootstrap/ng-bootstrap']}
-                            </span>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  ) : (
-                    ''
-                  )}
+                  <span className="item__title">v{version.version}</span>
+                  <span className="item__deps">
+                    {deps.map(d => (
+                      <span key={d.key} className="deps_dep" title={d.key}>
+                        {d.name !== null ? (
+                          <img
+                            className="dep__icon"
+                            src={`/assets/images/technologies/logo-${d.name}.svg`}
+                            alt={`${d.key} logo`}
+                          />
+                        ) : null}
+                        <span className="dep__version">{d.version}</span>
+                      </span>
+                    ))}
+                  </span>
                 </a>
-              ))}
-            </div>
-          );
-        }}
-      >
-        <IconButton placeholder="Versions">Version {currentVersionMajorMinor}</IconButton>
-      </WithTooltip>
-    </Fragment>
+              );
+            })}
+          </div>
+        </>
+      )}
+    >
+      <IconButton placeholder="Versions">
+        v{BROWSED_VERSION}
+        <post-icon name="2052"></post-icon>
+      </IconButton>
+    </WithTooltip>
   );
 }
 
