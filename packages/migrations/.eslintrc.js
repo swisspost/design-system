3f1dcf88fc7063e9f7e66c030eeab212f2c1a901
Oliver Schürch <oliver.schuerch@post.ch>
2024-05-31 10:21:58 +0200

    chore(migrations): add linting

---
+++ packages/migrations/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.eslint.json',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js', 'dist'],
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
