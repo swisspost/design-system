f11c3d3ae0a5968e24cfaa7bf2af6b544d6a5eb8
Oliver Schürch <oliver.schuerch@post.ch>
2024-06-19 15:30:23 +0200

    chore(documentation): move layout block files together

--- packages/documentation/.storybook/preview.ts
+++ packages/documentation/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/web-components';
 import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
 import { format } from 'prettier';
-import DocsLayout from './blocks/layout';
+import DocsLayout from './blocks/layout/layout';
 import { badgesConfig, openFullScreenDemo, prettierOptions, resetComponents } from './helpers';
 import './helpers/register-web-components';
 import './addons/cypress-storybook/client';
