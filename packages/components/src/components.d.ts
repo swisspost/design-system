55f3a60893b02f36edd117fc06dadb66e1663d4d
Alizé Debray <alize.debray@post.ch>
2024-07-30 17:01:51 +0200

    fix(components): remove scss module import

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -80,7 +80,7 @@ export namespace Components {
      */
     interface PostCardControl {
         /**
-          * Defines the `checked` attribute of the control. If `true`, the control is selected at its value will be included in the forms data.
+          * Defines the `checked` attribute of the control. If `true`, the control is selected at its value will be included in the forms' data.
          */
         "checked": boolean;
         /**
@@ -88,7 +88,7 @@ export namespace Components {
          */
         "description": string;
         /**
-          * Defines the `disabled` attribute of the control. If `true`, the user can not interact with the control and the controls value will not be included in the forms data.
+          * Defines the `disabled` attribute of the control. If `true`, the user can not interact with the control and the controls value will not be included in the forms' data.
          */
         "disabled": boolean;
         /**
@@ -96,7 +96,7 @@ export namespace Components {
          */
         "groupReset": () => Promise<void>;
         /**
-          * Defines the icon `name` inside of the card. <span className="alert alert-sm alert-info">If not set the icon will not show up.</span>
+          * Defines the icon `name` inside the card. <span className="alert alert-sm alert-info">If not set the icon will not show up.</span>
          */
         "icon": string;
         /**
@@ -116,7 +116,7 @@ export namespace Components {
          */
         "type": 'checkbox' | 'radio';
         /**
-          * Defines the validation `validity` of the control. To reset validity to an undefiend state, simply remove the attribute from the control.
+          * Defines the validation `validity` of the control. To reset validity to an undefined state, simply remove the attribute from the control.
          */
         "validity": null | 'true' | 'false';
         /**
@@ -575,7 +575,7 @@ declare namespace LocalJSX {
      */
     interface PostCardControl {
         /**
-          * Defines the `checked` attribute of the control. If `true`, the control is selected at its value will be included in the forms data.
+          * Defines the `checked` attribute of the control. If `true`, the control is selected at its value will be included in the forms' data.
          */
         "checked"?: boolean;
         /**
@@ -583,11 +583,11 @@ declare namespace LocalJSX {
          */
         "description"?: string;
         /**
-          * Defines the `disabled` attribute of the control. If `true`, the user can not interact with the control and the controls value will not be included in the forms data.
+          * Defines the `disabled` attribute of the control. If `true`, the user can not interact with the control and the controls value will not be included in the forms' data.
          */
         "disabled"?: boolean;
         /**
-          * Defines the icon `name` inside of the card. <span className="alert alert-sm alert-info">If not set the icon will not show up.</span>
+          * Defines the icon `name` inside the card. <span className="alert alert-sm alert-info">If not set the icon will not show up.</span>
          */
         "icon"?: string;
         /**
@@ -611,7 +611,7 @@ declare namespace LocalJSX {
          */
         "type": 'checkbox' | 'radio';
         /**
-          * Defines the validation `validity` of the control. To reset validity to an undefiend state, simply remove the attribute from the control.
+          * Defines the validation `validity` of the control. To reset validity to an undefined state, simply remove the attribute from the control.
          */
         "validity"?: null | 'true' | 'false';
         /**
