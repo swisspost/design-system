1b39dfb0c2cbf683fbbd573207c457b05c9c2254
Oliver Schürch <oliver.schuerch@post.ch>
2024-08-13 16:53:27 +0200

    test(components): fix avatar-picture e2e tests

--- packages/components/cypress/e2e/avatar-picture.cy.ts
+++ packages/components/cypress/e2e/avatar-picture.cy.ts
@@ -13,11 +13,11 @@ describe('Avatar-Picture', () => {
       cy.get('@consoleError').should('not.be.called');
     });
 
-    it('should have no attributes but class "large" by default', () => {
+    it('should have only attribute firstname and class "large" by default', () => {
       cy.get('@avatar-picture').should('exist');
       cy.get('@avatar-picture').should('not.have.attr', 'size');
       cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
+      cy.get('@avatar-picture').should('have.attr', 'firstname');
       cy.get('@avatar-picture').should('not.have.attr', 'lastname');
       cy.get('@avatar-picture').should('have.class', 'large');
       cy.get('@avatar-picture').should('not.have.class', 'small');
@@ -29,112 +29,48 @@ describe('Avatar-Picture', () => {
       cy.get('@avatar-picture').should('not.have.class', 'large');
     });
 
-    it('should show fallback, when no attribute is defined', () => {
-      cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
-      cy.get('@avatar-picture').should('not.have.attr', 'lastname');
-
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
+    it('should have a console error, when attribute "firstname" is not defined', () => {
+      cy.get('@avatar-picture').invoke('removeAttr', 'firstname');
+      cy.get('@consoleError').should('have.been.calledOnce');
     });
 
-    it('should show initials when, firstname and/or lastname is defined', () => {
-      cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
-      cy.get('@avatar-picture').should('not.have.attr', 'lastname');
+    it('should show initials when, firstname and lastname is defined', () => {
+      cy.get('@avatar-picture').invoke('attr', 'firstname', 'Open');
+      cy.get('@avatar-picture').invoke('attr', 'lastname', 'Source');
       cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-
-      cy.get('@avatar-picture').invoke('attr', 'firstname', 'o');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('exist').and('have.text', 'o');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
-
-      cy.get('@avatar-picture').invoke('attr', 'lastname', 's');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('exist').and('have.text', 'os');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
-
-      cy.get('@avatar-picture').invoke('removeAttr', 'firstname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('exist').and('have.text', 's');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
+      cy.get('@avatar-picture').find('div').should('exist').and('have.text', 'OS');
+      cy.get('@consoleError').should('not.have.been.called');
 
       cy.get('@avatar-picture').invoke('removeAttr', 'lastname');
       cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
+      cy.get('@avatar-picture').find('div').should('exist').and('have.text', 'O');
+      cy.get('@consoleError').should('not.have.been.called');
+
+      cy.get('@avatar-picture').invoke('removeAttr', 'firstname');
+      cy.get('@avatar-picture').find('img').should('not.exist');
+      cy.get('@consoleError').should('have.been.calledOnce');
     });
 
     it('should show image when, email with gravatar account is defined', () => {
-      cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
-      cy.get('@avatar-picture').should('not.have.attr', 'lastname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-
       cy.get('@avatar-picture').invoke('attr', 'email', 'oss@post.ch');
+      cy.get('@avatar-picture').should('have.attr', 'email');
+      cy.get('@avatar-picture').should('have.attr', 'firstname');
       cy.get('@avatar-picture').find('img').should('exist');
       cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
 
       cy.get('@avatar-picture').invoke('removeAttr', 'email');
       cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
+      cy.get('@avatar-picture').find('div').should('exist');
+      cy.get('@consoleError').should('not.have.been.called');
     });
 
-    it('should show fallback when, email with no gravatar account and no firstname and lastname is defined', () => {
-      cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
-      cy.get('@avatar-picture').should('not.have.attr', 'lastname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-
-      cy.get('@avatar-picture').invoke('attr', 'email', 'no.gravatar-account@post.ch');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-
-      cy.get('@avatar-picture').invoke('removeAttr', 'email');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-    });
-
-    it('should show initials when, email with no gravatar account, but first and/or lastname is defined', () => {
-      cy.get('@avatar-picture').should('not.have.attr', 'email');
-      cy.get('@avatar-picture').should('not.have.attr', 'firstname');
-      cy.get('@avatar-picture').should('not.have.attr', 'lastname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
-
-      cy.get('@avatar-picture').invoke('attr', 'email', 'no.gravatar-account@post.ch');
-      cy.get('@avatar-picture').invoke('attr', 'firstname', 'o');
+    it('should show initials when, email with no gravatar account is defined', () => {
+      cy.get('@avatar-picture').invoke('attr', 'email', 'no-gravatar-account@post.ch');
+      cy.get('@avatar-picture').should('have.attr', 'email');
+      cy.get('@avatar-picture').should('have.attr', 'firstname');
       cy.get('@avatar-picture').find('img').should('not.exist');
       cy.get('@avatar-picture').find('div').should('exist');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
-
-      cy.get('@avatar-picture').invoke('attr', 'lastname', 's');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('exist');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
-
-      cy.get('@avatar-picture').invoke('removeAttr', 'firstname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('exist');
-      cy.get('@avatar-picture').find('post-icon').should('not.exist');
-
-      cy.get('@avatar-picture').invoke('removeAttr', 'email');
-      cy.get('@avatar-picture').invoke('removeAttr', 'lastname');
-      cy.get('@avatar-picture').find('img').should('not.exist');
-      cy.get('@avatar-picture').find('div').should('not.exist');
-      cy.get('@avatar-picture').find('post-icon').should('exist');
+      cy.get('@consoleError').should('not.have.been.called');
     });
   });
 
