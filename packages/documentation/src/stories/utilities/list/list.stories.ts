f90299a2e111ba2fab8ecc27cee998dfd9ee5a02
karimim <muhammad.karimi@post.ch>
2024-09-06 10:21:13 +0200

    chore: add nested unordered list

--- packages/documentation/src/stories/utilities/list/list.stories.ts
+++ packages/documentation/src/stories/utilities/list/list.stories.ts
@@ -85,11 +85,21 @@ export const OrderedList: Story = {
 
 export const CheckList: Story = {
   render: () => html`
+    <p>Text to show alignment of check mark with text</p>
     <ul class="list-checks">
       <li>This is a check list item.</li>
       <li>And another one.</li>
-      <li>And one more.</li>
+      <li>
+        Nested list:
+        <ul class="list-checks">
+          <li>This is a check list item.</li>
+          <li>And another one.</li>
+          <li>And one more.</li>
+        </ul>
+      </li>
+      <li>And another one.</li>
     </ul>
+    <p>Text to show alignment of check mark with text</p>
   `,
 };
 
