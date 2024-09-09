b30c7f3378da952bd88446f573c7dc0064f2c2f4
Pedroso Munoz Yohandris, IT10.3 extern <yohandris.pedrosomunoz@post.ch>
2024-09-09 12:25:49 +0200

    fix: search responsivness and clean up

--- packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/samples-navigation-condensed-header/samples-navigation-condensed-header.component.spec.ts
+++ packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/samples-navigation-condensed-header/samples-navigation-condensed-header.component.spec.ts
@@ -1,23 +1,22 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { SamplesNavigationCondensedHeaderComponent } from './samples-navigation-condensed-header.component';
+import { SamplesNavigationCondensedHeaderComponent } from './samples-navigation-condensed-header.component';
 
-// describe('SamplesNavigationCondensedHeaderComponent', () => {
-//   let component: SamplesNavigationCondensedHeaderComponent;
-//   let fixture: ComponentFixture<SamplesNavigationCondensedHeaderComponent>;
+describe('SamplesNavigationCondensedHeaderComponent', () => {
+  let component: SamplesNavigationCondensedHeaderComponent;
+  let fixture: ComponentFixture<SamplesNavigationCondensedHeaderComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [SamplesNavigationCondensedHeaderComponent]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SamplesNavigationCondensedHeaderComponent],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(SamplesNavigationCondensedHeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(SamplesNavigationCondensedHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
