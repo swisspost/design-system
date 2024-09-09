a9c1194568a34a09af8c8b892db97c2d388b4d91
yopemu <44609906+yopemu@users.noreply.github.com>
2024-09-09 15:00:29 +0200

    feat: allow to include the navigation bar in a single line as a condensed header

---
+++ packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/samples-navigation-condensed-header/samples-navigation-condensed-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-samples-navigation-condensed-header',
+  templateUrl: './samples-navigation-condensed-header.component.html',
+  styleUrl: './samples-navigation-condensed-header.component.scss',
+})
+export class SamplesNavigationCondensedHeaderComponent {}
