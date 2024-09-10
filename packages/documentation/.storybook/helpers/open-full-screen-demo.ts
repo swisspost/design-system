79d191fc0035b1e9a5ce655113efb399120b793f
Alizé Debray <alize.debray@post.ch>
2024-09-10 19:10:57 +0200

    feat(docs): add toolbar to switch theme, channel, and mode

--- packages/documentation/.storybook/helpers/open-full-screen-demo.ts
+++ packages/documentation/.storybook/helpers/open-full-screen-demo.ts
@@ -1,11 +1,36 @@
+import { StoryContext, StoryFn } from '@storybook/web-components';
+import { html } from 'lit';
+
+export const fullScreenUrlDecorator = (story: StoryFn, context: StoryContext) => {
+  const { allArgs, initialArgs, id } = context;
+
+  const args = Object.entries(allArgs)
+    .map(([argKey, argValue]) => {
+      const initialValue = initialArgs[argKey];
+      if (argValue === initialValue) return null;
+
+      const separator = typeof argValue === 'string' || typeof argValue === 'number' ? ':' : ':!';
+      const value = typeof argValue === 'string' ? encodeURI(argValue) : argValue;
+      return `${argKey}${separator}${value}`;
+    })
+    .filter(arg => !!arg)
+    .join(';');
+
+  const storyURL = `/?path=/story/${id}&args=${args}&full=true`;
+
+  return html`
+    <p class="storyURL" hidden>${storyURL}</p>
+    ${story(context.args, context)}
+  `;
+};
+
 export const openFullScreenDemo = (e: Event) => {
   const target = e.target as HTMLButtonElement;
   const canvas = target.closest('.docs-story');
-  const story = canvas?.querySelector('.sb-story');
-  const iframeId = story?.id?.replace('story--', '');
+  const storyURL = canvas && canvas.querySelector('.storyURL');
 
-  if (iframeId) {
-    window.open(`/iframe.html?id=${iframeId}`, '_blank');
+  if (storyURL) {
+    window.open(storyURL.textContent, '_blank');
   } else {
     alert('The full screen demo is not available.');
   }
