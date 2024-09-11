f858d70f6829357335e7413f2389721a0062e287
Pedroso Munoz Yohandris, IT10.3 extern <yohandris.pedrosomunoz@post.ch>
2024-09-11 15:20:18 +0200

    fix: fix sonar issues

--- packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/samples-navigation-condensed-header/samples-navigation-condensed-header.component.ts
+++ packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/samples-navigation-condensed-header/samples-navigation-condensed-header.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-samples-navigation-condensed-header',
   templateUrl: './samples-navigation-condensed-header.component.html',
-  styleUrl: './samples-navigation-condensed-header.component.scss',
 })
 export class SamplesNavigationCondensedHeaderComponent {}
