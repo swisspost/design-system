0202395cbfb0221e8b7a3e0900caa9b18bf76a71
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-09-20 09:56:32 +0200

    feat(docs): create principles interface and data and list the principles

---
+++ packages/documentation/src/stories/getting-started/design-principles/design-principles.data.ts
@@ -0,0 +1,32 @@
+interface IPrinciples {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export const principles: IPrinciples[] = [
+  {
+    icon: '2525',
+    title: 'Easy to use',
+    description:
+      'Create easy-to-use design patterns that consider the five elements of usability: Effectiveness, Efficiency, Engagement, Error Tolerance, and Ease of learning. This helps us to promote industry standards amongst our community and lowers the barrier of entry for new employees.',
+  },
+  {
+    icon: '2526',
+    title: 'Trustworthy',
+    description:
+      'We establish mutual trust with our stakeholders by creating robust and consistent patterns, maintaining comprehensive documentation, communicating transparently, and actively engaging with our community. Through these efforts, we ensure that our stakeholders can rely on our work. As a result, we can scale adoption across the organization, unlocking our full efficiency potential.',
+  },
+  {
+    icon: '2527',
+    title: 'Cordial',
+    description:
+      'We approach our customers on equal footing. By aligning our communication patterns with the Posts’ guidelines, we ensure that our interactions mirror how Post employees engage with clients: cordially, courteously, anticipatively, and empathically. Our goal is to foster the same strong bond between customers and the Post that exists beyond digital channels.',
+  },
+  {
+    icon: '2524',
+    title: 'Inclusive',
+    description:
+      'We promote inclusivity. We’re designing for diverse abilities, access to technology, literacy, economic circumstances, educational backgrounds, geographic locations, ages, and languages. With this approach, we ensure that everyone can use the public services we provide.',
+  },
+];
