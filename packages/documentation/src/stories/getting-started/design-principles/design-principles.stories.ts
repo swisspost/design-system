9d08ef922f2783f20161cd16b2ca461f9b13836f
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-09-23 14:23:39 +0200

    fix(spelling): capitalize the title for consistency

--- packages/documentation/src/stories/getting-started/design-principles/design-principles.stories.ts
+++ packages/documentation/src/stories/getting-started/design-principles/design-principles.stories.ts
@@ -3,7 +3,7 @@ import { StoryObj } from '@storybook/web-components';
 
 const meta: MetaExtended = {
   id: '43f5818c-98a0-48ba-84f1-bf4d8fe1e70d',
-  title: 'Getting started/Design principles',
+  title: 'Getting Started/Design Principles',
   parameters: {
     badges: [],
   },
