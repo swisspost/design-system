55acad1091a7a85cec13cef0fc6187d703eee5a1
karimim <muhammad.karimi@post.ch>
2024-09-24 15:42:16 +0200

    feat: add text-highlight component

---
+++ packages/documentation/src/stories/components/text-highlight/text-highlight.stories.ts
@@ -0,0 +1,34 @@
+import type { StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { MetaExtended } from '@root/types';
+
+const meta: MetaExtended = {
+  id: '88318ccf-be8d-4bdf-b8dc-06b0b78e5e3d',
+  title: 'Components/Text-highlight',
+  parameters: {
+    badges: [],
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/JIT5AdGYqv6bDRpfBPV8XR/Foundations-%26-Components-Next-Level?node-id=1481-12917',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+  render: () => html`
+    <p class="text-highlight">
+      Lead text nullam quis risus eget urna mollis ornare veleu leo. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies
+      vehicula.
+    </p>
+    <p class="text-highlight">
+      Lead text nullam quis risus eget urna mollis ornare veleu leo. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies
+      vehicula.
+    </p>
+  `,
+};
