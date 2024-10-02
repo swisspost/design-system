cd8b74ed73db42e2981b1202c1670102fbd80d31
karimim <muhammad.karimi@post.ch>
2024-10-02 10:59:49 +0200

    feat: add legend story

---
+++ packages/documentation/src/stories/elements/legend/legend.stories.ts
@@ -0,0 +1,37 @@
+import type { StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { MetaExtended } from '@root/types';
+
+const meta: MetaExtended = {
+  id: '61faccd2-de2c-48f0-9a06-c051a56580ef',
+  title: 'Elements/Legend',
+  parameters: {
+    badges: [],
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/JIT5AdGYqv6bDRpfBPV8XR/Foundations-%26-Components-Next-Level?node-id=26-49',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+  render: () => html`
+    <fieldset>
+      <fieldset>
+        <legend>Default legend</legend>
+      </fieldset>
+    </fieldset>
+  `,
+};
+
+export const Large: Story = {
+  render: () => html`
+    <fieldset>
+      <legend post-size="large">Large legend</legend>
+    </fieldset>
+  `,
+};
