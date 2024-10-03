d7d908bb1b2cec7195351b3de01bc86fc8d87f57
Tim Schär <59233938+schaertim@users.noreply.github.com>
2024-10-03 09:49:18 +0200

    chore(demo): removed demo app and workflows

--- packages/demo/src/app/post-sample/components/subnavigation/subnavigation-demo/subnavigation-demo.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subnavigation-demo',
-  templateUrl: './subnavigation-demo.component.html',
-})
-export class SubnavigationDemoComponent {}
