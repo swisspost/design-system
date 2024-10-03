cda79ca0eb11d645faa9fda72b19cee365ab4eb6
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-10-03 12:30:57 +0200

    chore(docs): add random UUID to the link stories

--- packages/documentation/src/stories/components/link/link.stories.ts
+++ packages/documentation/src/stories/components/link/link.stories.ts
@@ -3,7 +3,7 @@ import { html } from 'lit/static-html.js';
 import { MetaComponent } from '@root/types';
 
 const meta: MetaComponent = {
-  id: 'link-component',
+  id: '6f359d06-bca5-4983-b588-c8c790531642',
   title: 'Components/Link',
   tags: ['package:HTML'],
   parameters: {
