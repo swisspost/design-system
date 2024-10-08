d2eec4f70f897ab0856302a5eaa9eb53e47eae95
leagrdv <lea.gardavaud@post.ch>
2024-10-08 09:57:48 +0200

    update documentation

--- packages/documentation/src/stories/utilities/flex/flex.blocks.tsx
+++ packages/documentation/src/stories/utilities/flex/flex.blocks.tsx
@@ -1,127 +1,161 @@
+import { breakpoints } from '../spacing/spacing.docs.mdx';
+
+export const FlexDocContainer = (props: {
+  children: string | JSX.Element | JSX.Element[];
+  name: string;
+  prefix: string;
+}) => (
+  <article className="d-flex flex-column flex-lg-row align-items-start flex-doc-container">
+    <div className="flex-doc-container__description order-2 order-lg-1">
+      <h3 className="h6 text-capitalize">{props.name}</h3>
+    </div>
+    <div className="flex-doc-container__demo order-3 order-lg-2">{props.children}</div>
+    <div className="flex-doc-container__props order-1 order-lg-3">
+      <dl>
+        <dt>Class</dt>
+        <dd>
+          <code>
+            .{props.prefix}-{props.name}
+          </code>
+        </dd>
+      </dl>
+    </div>
+  </article>
+);
+
+export function FlexBreakpointBlock(props: { display: string }) {
+  return (
+    <>
+      If you want to set it from a specific breakpoint: (
+      {breakpoints.map(([b, isLast]) => (
+        <span key={b}>
+          <code>{b}</code>
+          {isLast ? ', ' : ''}
+        </span>
+      ))}
+      ): <code>{props.display}</code>
+    </>
+  );
+}
+
+export function FlexValuesBlock(props: { name: string; values: string[] }) {
+  return (
+    <>
+      The available values for {props.name} are the following:{' '}
+      {props.values.map((val, i) => (
+        <span key={val}>
+          <code>{val}</code>
+          {i < props.values.length - 1 ? ', ' : ''}
+        </span>
+      ))}
+      .
+    </>
+  );
+}
+
 export function FlexDirectionContainer(props: { name: string }) {
   return (
-    <article className="d-flex align-items-start flex-container">
-      <div className="flex-container__description">
-        <h3 className="description__title h6">{props.name}</h3>
+    <FlexDocContainer name={props.name} prefix="flex">
+      <div className={`d-flex flex-${props.name}`}>
+        <div className="bg-yellow p-12">First content</div>
+        <div className="bg-gray p-12">Second content</div>
       </div>
-      <div className="flex-container__flex">
-        <div className={`d-flex flex-${props.name}`}>
-          <div className="bg-yellow p-8">First content</div>
-          <div className="bg-black p-8">Second content</div>
-        </div>
-      </div>
-      <div className="flex-container__props">
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>.flex-{props.name}</code>
-          </dd>
-        </dl>
-      </div>
-    </article>
+    </FlexDocContainer>
   );
 }
 
 export function AlignItemsContainer(props: { name: string }) {
   return (
-    <article className="d-flex align-items-start flex-container">
-      <div className="flex-container__description">
-        <h3 className="description__title h6">{props.name}</h3>
+    <FlexDocContainer name={props.name} prefix="align-items">
+      <div className={`d-flex align-items-${props.name}`}>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray px-12 py-48">Content</div>
       </div>
-      <div className="flex-container__flex">
-        <div className={`d-flex align-items-${props.name}`}>
-          <div className="bg-yellow p-8">Content</div>
-          <div className="bg-black px-8 py-32">Content</div>
-        </div>
-      </div>
-      <div className="flex-container__props">
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>.align-items-{props.name}</code>
-          </dd>
-        </dl>
-      </div>
-    </article>
+    </FlexDocContainer>
   );
 }
 
 export function JustifyContentContainer(props: { name: string }) {
   return (
-    <article className="d-flex align-items-start flex-container">
-      <div className="flex-container__description">
-        <h3 className="description__title h6">{props.name}</h3>
+    <FlexDocContainer name={props.name} prefix="justify-content">
+      <div className={`d-flex justify-content-${props.name}`}>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
       </div>
-      <div className="flex-container__flex">
-        <div className={`d-flex justify-content-${props.name}`}>
-          <div className="bg-yellow p-8">Content</div>
-          <div className="bg-black p-8">Content</div>
-        </div>
-      </div>
-      <div className="flex-container__props">
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>.justify-content-{props.name}</code>
-          </dd>
-        </dl>
-      </div>
-    </article>
+    </FlexDocContainer>
   );
 }
 
 export function AlignSelfContainer(props: { name: string }) {
   return (
-    <article className="d-flex align-items-start flex-container">
-      <div className="flex-container__description">
-        <h3 className="description__title h6">{props.name}</h3>
+    <FlexDocContainer name={props.name} prefix="align-self">
+      <div className="d-flex align-items-start">
+        <div className="bg-gray px-12 py-48">Content</div>
+        <div className={`bg-yellow p-12 align-self-${props.name}`}>Content with align-self</div>
+        <div className="bg-gray px-12 py-48">Content</div>
       </div>
-      <div className="flex-container__flex">
-        <div className="d-flex align-items-start">
-          <div className="bg-black px-8 py-32">Content</div>
-          <div className={`bg-yellow p-8 align-self-${props.name}`}>Content with align-self</div>
-          <div className="bg-black px-8 py-32">Content</div>
-        </div>
-      </div>
-      <div className="flex-container__props">
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>.align-self-{props.name}</code>
-          </dd>
-        </dl>
-      </div>
-    </article>
+    </FlexDocContainer>
   );
 }
 
-export function GrowShrinkContainer(props: { name: string; value: string }) {
+export function GrowContainer(props: { name: number }) {
   return (
-    <article className="d-flex align-items-start flex-container">
-      <div className="flex-container__description">
-        <h3 className="description__title h6">
-          {props.name} {props.value}
-        </h3>
-      </div>
-      <div className="flex-container__flex">
-        <div className="d-flex align-items-start">
-          <div className="bg-black px-8 py-32">Content</div>
-          <div className={`bg-yellow p-8 flex-${props.name}-${props.value}`}>
-            Content with custom {props.name}
-          </div>
-          <div className="bg-black px-8 py-32">Content</div>
+    <FlexDocContainer name={props.name.toString()} prefix="flex-grow">
+      <div className="d-flex align-items-start grow-shrink-container">
+        <div className={`bg-yellow p-12 flex-grow-${props.name}`}>
+          I'm {props.name === 0 ? 'not' : ''} growing
         </div>
+        <div className="bg-gray p-12">Content</div>
       </div>
-      <div className="flex-container__props">
-        <dl>
-          <dt>Class</dt>
-          <dd>
-            <code>
-              .flex-{props.name}-{props.value}
-            </code>
-          </dd>
-        </dl>
-      </div>
-    </article>
+    </FlexDocContainer>
   );
 }
+
+export function ShrinkContainer(props: { name: number }) {
+  return (
+    <FlexDocContainer name={props.name.toString()} prefix="flex-shrink">
+      <div className="d-flex align-items-start grow-shrink-container">
+        <div className="bg-gray p-12">Content with a lot more text than the yellow tile</div>
+        <div className={`bg-yellow p-12 flex-shrink-${props.name}`}>
+          I {props.name === 0 ? "don't" : ''} shrink
+        </div>
+      </div>
+    </FlexDocContainer>
+  );
+}
+
+export function AlignContentContainer(props: { name: string }) {
+  return (
+    <FlexDocContainer name={props.name} prefix="align-content">
+      <div className={`align-content-container d-flex align-content-${props.name} flex-wrap`}>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+      </div>
+    </FlexDocContainer>
+  );
+}
+
+export function WrapContainer(props: { name: string }) {
+  return (
+    <FlexDocContainer name={props.name} prefix="flex">
+      <div className={`d-flex flex-${props.name}`}>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+        <div className="bg-gray p-12">Content</div>
+        <div className="bg-yellow p-12">Content</div>
+      </div>
+    </FlexDocContainer>
+  );
+}
+
+// TODO: Test de non régression visuels (snapshots)
--- packages/documentation/src/stories/utilities/flex/flex.stories.ts
+++ packages/documentation/src/stories/utilities/flex/flex.stories.ts
@@ -19,7 +19,7 @@ export const Default: Story = {
     html`
       <div class="d-flex flex-row">
         <div class="bg-yellow p-8">Content</div>
-        <div class="bg-black p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
       </div>
     `,
 };
@@ -29,7 +29,7 @@ export const AlignItems: Story = {
     html`
       <div class="d-flex align-items-end">
         <div class="bg-yellow p-8">Content</div>
-        <div class="bg-black p-8">Content<br />that's a bit bigger</div>
+        <div class="bg-gray px-8 py-48">Content</div>
       </div>
     `,
 };
@@ -38,9 +38,9 @@ export const AlignSelf: Story = {
   render: () =>
     html`
       <div class="d-flex">
-        <div class="bg-black p-8">Content<br />that's a bit bigger</div>
+        <div class="bg-gray px-8 py-48">Content</div>
         <div class="bg-yellow align-self-end p-8">Content</div>
-        <div class="bg-black p-8">Content<br />that's a bit bigger</div>
+        <div class="bg-gray px-8 py-48">Content</div>
       </div>
     `,
 };
@@ -50,7 +50,90 @@ export const JustifyContent: Story = {
     html`
       <div class="d-flex justify-content-between">
         <div class="bg-yellow p-8">Content</div>
-        <div class="bg-black p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+      </div>
+    `,
+};
+
+export const GrowShrink: Story = {
+  render: () =>
+    html`
+      <div class="d-flex">
+        <div class="bg-yellow flex-grow-1 p-8">I'm growing</div>
+        <div class="bg-gray flex-shrink-1 p-8">I'm shrinking</div>
+      </div>
+    `,
+};
+
+export const AlignContent: Story = {
+  render: () =>
+    html`
+      <div style="height: 14rem" class="d-flex flex-wrap align-content-around">
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+      </div>
+    `,
+};
+
+export const Order: Story = {
+  render: () =>
+    html`
+      <div class="d-flex">
+        <div class="bg-yellow p-8 order-3">First element, positioned last</div>
+        <div class="bg-gray p-8 order-2">Second element</div>
+        <div class="bg-yellow p-8 order-1">Last element, positioned first</div>
+      </div>
+    `,
+};
+
+export const Wrap: Story = {
+  render: () =>
+    html`
+      <div class="d-flex flex-wrap">
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+        <div class="bg-yellow p-8">Content</div>
+        <div class="bg-gray p-8">Content</div>
+      </div>
+    `,
+};
+
+export const FillEqual: Story = {
+  render: () =>
+    html`
+      <div class="d-flex">
+        <div class="bg-yellow p-8 flex-fill">Same content, same width</div>
+        <div class="bg-gray p-8 flex-fill">Same content, same width</div>
+        <div class="bg-yellow p-8 flex-fill">Same content, same width</div>
+      </div>
+    `,
+};
+
+export const Fill: Story = {
+  render: () =>
+    html`
+      <div class="d-flex">
+        <div class="bg-yellow p-8 flex-fill">Content</div>
+        <div class="bg-gray p-8 flex-fill">Element with more content than the others</div>
+        <div class="bg-yellow p-8 flex-fill">Content</div>
       </div>
     `,
 };
