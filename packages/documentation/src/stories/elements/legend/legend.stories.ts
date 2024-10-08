9b3d18b7bf8580c27bf3ccb90323d9420eff1445
karimim <muhammad.karimi@post.ch>
2024-10-08 08:23:27 +0200

    chore: combine stories and add controls

--- packages/documentation/src/stories/elements/legend/legend.stories.ts
+++ packages/documentation/src/stories/elements/legend/legend.stories.ts
@@ -1,5 +1,5 @@
-import type { StoryObj } from '@storybook/web-components';
-import { html } from 'lit';
+import type { Args, StoryObj } from '@storybook/web-components';
+import { html, unsafeStatic } from 'lit/static-html.js';
 import { MetaExtended } from '@root/types';
 
 const meta: MetaExtended = {
@@ -9,7 +9,31 @@ const meta: MetaExtended = {
     badges: [],
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/JIT5AdGYqv6bDRpfBPV8XR/Foundations-%26-Components-Next-Level?node-id=26-49',
+      url: 'https://www.figma.com/design/JIT5AdGYqv6bDRpfBPV8XR/Foundations-%26-Components-Next-Level?node-id=1495-27801&node-type=instance&m=dev',
+    },
+  },
+  args: {
+    type: 'small',
+  },
+  argTypes: {
+    type: {
+      name: 'Type',
+      description:
+        'Large: Used for the title of a section in a form. Small: Used for the title of group of selection options.',
+      control: {
+        type: 'radio',
+        labels: {
+          large: 'Large',
+          small: 'Small',
+        },
+      },
+      options: ['large', 'small'],
+      table: {
+        category: 'General',
+        type: {
+          summary: 'HTML class attribute',
+        },
+      },
     },
   },
 };
@@ -19,17 +43,11 @@ export default meta;
 type Story = StoryObj;
 
 export const Default: Story = {
-  render: () => html`
+  render: (args: Args) => html`
     <fieldset>
-      <legend>Default legend</legend>
-    </fieldset>
-  `,
-};
-
-export const Large: Story = {
-  render: () => html`
-    <fieldset>
-      <legend class="legend-large">Large legend</legend>
+      <legend ${args.type !== 'small' && args.type ? unsafeStatic(`class=${args.type}`) : ''}>
+        Default legend
+      </legend>
     </fieldset>
   `,
 };
