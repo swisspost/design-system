cefb5e51fe4aa6bf731b79fe0655ad997c9df391
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-10-15 14:32:54 +0200

    chore(docs): implemented the initial documentation of the menu button component

---
+++ packages/documentation/src/stories/components/menu-button/menu-button.stories.ts
@@ -0,0 +1,54 @@
+import { Args, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { MetaComponent } from '@root/types';
+
+const meta: MetaComponent = {
+  id: '8ca2bd70-56e6-4da9-b1fd-4e55388dca88',
+  title: 'Components/Menu Button',
+  tags: ['package:WebComponents'],
+  component: 'post-menu',
+  parameters: {
+    badges: [],
+    docs: {
+      argTypes: {
+        sort: 'requiredFirst',
+      },
+    },
+    design: {},
+  },
+  render,
+  args: {
+    id: 'menu-one',
+    placement: 'top',
+  },
+  argTypes: {
+    id: {
+      name: 'Id',
+      description:
+        'The id is used to connect a trigger element with the menu. <div className="mt-8 alert alert-info alert-sm">`<button data-menu-target="...">` is the only valid trigger element for `post-menu`.</div>',
+      table: {
+        category: 'General',
+      },
+    },
+  },
+  placement: {
+    name: 'Placement',
+  },
+};
+
+function render(args: Args) {
+  return html`
+    <post-menu-toggle class="d-flex justify-content-start">
+      <button class="btn btn-secondary btn-large" data-menu-target="${args.id}">Menu button</button>
+    </post-menu-toggle>
+    <post-menu class="hydrated" id="${args.id}" placement="${args.placement}">
+      <post-menu-item><button>Example 1</button></post-menu-item>
+      <post-menu-item><button>Example 2</button></post-menu-item>
+      <post-menu-item><button>Example 3</button></post-menu-item>
+      <post-menu-item>Hello there</post-menu-item>
+    </post-menu>
+  `;
+}
+
+export default meta;
+export const Default: StoryObj = {};
