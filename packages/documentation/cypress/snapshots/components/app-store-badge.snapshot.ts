e8f05556d106c67cfd48581c561d386219c474c7
Alona Zherdetska <138328641+alionazherdetska@users.noreply.github.com>
2024-10-18 11:19:39 +0200

    Update packages/documentation/cypress/snapshots/components/app-store-badge.snapshot.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/documentation/cypress/snapshots/components/app-store-badge.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/app-store-badge.snapshot.ts
@@ -2,6 +2,6 @@ describe('AppStoreBadge', () => {
   it('default', () => {
     cy.visit('/iframe.html?id=snapshots--app-store-badge');
     cy.get('.app-store-badge', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Badges', { widths: [400] });
+    cy.percySnapshot('App Store Badges', { widths: [400] });
   });
 });
