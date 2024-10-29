e8ab78fd0cfc6ef59cd6db41420d96614f83d617
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-10-29 20:23:38 +0100

    fix(function): checkUrl function was refactored to the initial state

--- packages/components/src/utils/property-checkers/check-url.ts
+++ packages/components/src/utils/property-checkers/check-url.ts
@@ -2,7 +2,7 @@ export function checkUrl(value: unknown, error: string) {
   if (typeof value !== 'string' && !(value instanceof URL)) throw new Error(error);
 
   try {
-    URL.canParse(value, window.location.origin);
+    new URL(value);
   } catch (e) {
     throw new Error(error);
   }
