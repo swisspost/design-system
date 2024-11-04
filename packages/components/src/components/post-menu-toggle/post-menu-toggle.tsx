c3abaa2927c5a2e3c283d2e9e1fa063687139046
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-11-04 12:56:30 +0100

    chore(aria-atrributes): added aria-atributes to post-menu-toggle

--- packages/components/src/components/post-menu-toggle/post-menu-toggle.tsx
+++ packages/components/src/components/post-menu-toggle/post-menu-toggle.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host } from '@stencil/core';
+import { Component, Element, Prop, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'post-menu-toggle',
@@ -6,9 +6,25 @@ import { Component, h, Host } from '@stencil/core';
   shadow: true,
 })
 export class PostMenuToggle {
+  @Element() host: HTMLPostMenuToggleElement;
+
+  /**
+   * Links the toggle to a `post-menu` with this ID.
+   */
+  @Prop() for: string;
+
+  private toggleMenu() {
+    const menu = document.getElementById(this.for) as HTMLPostMenuElement;
+    if (menu) {
+      menu.toggle(this.host);
+    } else {
+      console.warn(`No post-menu found with ID: ${this.for}`);
+    }
+  }
+
   render() {
     return (
-      <Host aria-haspopup="true">
+      <Host aria-haspopup="true" onClick={() => this.toggleMenu()}>
         <slot></slot>
       </Host>
     );
