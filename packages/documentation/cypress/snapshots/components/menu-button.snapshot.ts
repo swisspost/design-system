7af38245185c266340d45b05cd079b0ba7350889
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-11-04 15:02:05 +0100

    fix(cypress): removed redundant snapshot

--- packages/documentation/cypress/snapshots/components/menu-button.snapshot.ts
+++
@@ -1,8 +0,0 @@
-describe('MenuButton', () => {
-    it('default', () => {
-      cy.visit('/iframe.html?id=snapshots--menu-button');
-      cy.get('post-menu.hydrated', { timeout: 30000 }).should('be.visible');
-      cy.percySnapshot('Menu-Button', { widths: [1440] });
-    });
-  });
-  
