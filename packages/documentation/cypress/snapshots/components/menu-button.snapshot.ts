d4cea6bc0bd15ade0d74c5e45dcf6a4e8e7f600b
Zherdetska Alona, IT21.1 <alona.zherdetska@post.ch>
2024-11-04 12:58:19 +0100

    chore(cypress): updated cypress test selector for menuButton to hydrated post-men

--- packages/documentation/cypress/snapshots/components/menu-button.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/menu-button.snapshot.ts
@@ -1,7 +1,7 @@
 describe('MenuButton', () => {
     it('default', () => {
       cy.visit('/iframe.html?id=snapshots--menu-button');
-      cy.get('.menuButton', { timeout: 30000 }).should('be.visible');
+      cy.get('post-menu.hydrated', { timeout: 30000 }).should('be.visible');
       cy.percySnapshot('Menu-Button', { widths: [1440] });
     });
   });
