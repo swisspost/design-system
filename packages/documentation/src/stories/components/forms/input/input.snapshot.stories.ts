f809d71da9c211bf7edc74ecf185acfcab039f00
leagrdv <lea.gardavaud@post.ch>
2024-11-07 13:07:57 +0100

    remove floating label + form control coming from bootstrap

--- packages/documentation/src/stories/components/forms/input/input.snapshot.stories.ts
+++ packages/documentation/src/stories/components/forms/input/input.snapshot.stories.ts
@@ -32,6 +32,26 @@ function renderInputSnapshot(_args: Args, context: StoryContext) {
       ${['bg-white', 'bg-dark'].map(
         bg => html`
           <div class="${bg} d-flex gap-16 flex-column p-16">
+            <div class="mb-3">
+              <label for="formFile" class="form-label">Default file input example</label>
+              <input class="form-control" type="file" id="formFile" />
+            </div>
+            <div class="mb-3">
+              <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+              <input class="form-control" type="file" id="formFileMultiple" multiple />
+            </div>
+            <div class="mb-3">
+              <label for="formFileDisabled" class="form-label">Disabled file input example</label>
+              <input class="form-control" type="file" id="formFileDisabled" disabled />
+            </div>
+            <div class="mb-3">
+              <label for="formFileSm" class="form-label">Small file input example</label>
+              <input class="form-control form-control-sm" id="formFileSm" type="file" />
+            </div>
+            <div>
+              <label for="formFileLg" class="form-label">Large file input example</label>
+              <input class="form-control form-control-lg" id="formFileLg" type="file" />
+            </div>
             <h3>Standard</h3>
             ${getCombinations('size', [true], combinations)
               .filter(
