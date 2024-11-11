a5fa924fb928c868950001a38910c310b889770c
Alona Zherdetska <alionazhv@gmail.com>
2024-11-11 14:34:00 +0100

    fix(lint): fixed indentation in is-Focusable

--- packages/components/src/utils/is-focusable.ts
+++ packages/components/src/utils/is-focusable.ts
@@ -1,28 +1,28 @@
 const focusableSelector = `:where(${[
-    'button',
-    'input:not([type="hidden"])',
-    '[tabindex]',
-    'select',
-    'textarea',
-    '[contenteditable]',
-    'a[href]',
-    'iframe',
-    'audio[controls]',
-    'video[controls]',
-    'area[href]',
-    'details > summary:first-of-type',
-  ].join(',')})`;
-  
-  const focusDisablingSelector = `:where(${[
-    '[inert]',
-    '[inert] *',
-    ':disabled',
-    'dialog:not([open]) *',
-    '[popover]:not(:popover-open) *',
-    'details:not([open]) > *:not(details > summary:first-of-type)',
-    'details:not([open]) > *:not(details > summary:first-of-type) *',
-  ].join(',')})`;
-  
-  export const isFocusable = (element: Element) => {
-    return element?.matches(focusableSelector) && !element?.matches(focusDisablingSelector);
-  };
+  'button',
+  'input:not([type="hidden"])',
+  '[tabindex]',
+  'select',
+  'textarea',
+  '[contenteditable]',
+  'a[href]',
+  'iframe',
+  'audio[controls]',
+  'video[controls]',
+  'area[href]',
+  'details > summary:first-of-type',
+].join(',')})`;
+
+const focusDisablingSelector = `:where(${[
+  '[inert]',
+  '[inert] *',
+  ':disabled',
+  'dialog:not([open]) *',
+  '[popover]:not(:popover-open) *',
+  'details:not([open]) > *:not(details > summary:first-of-type)',
+  'details:not([open]) > *:not(details > summary:first-of-type) *',
+].join(',')})`;
+
+export const isFocusable = (element: Element) => {
+  return element?.matches(focusableSelector) && !element?.matches(focusDisablingSelector);
+};
