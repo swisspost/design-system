bedc088865a309b53deefb3a19f73ad45ecf9a4f
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2024-11-12 11:54:00 +0100

    feat(components): implement back-to-top button

--- packages/components/src/components/post-back-to-top/post-back-to-top.tsx
+++ packages/components/src/components/post-back-to-top/post-back-to-top.tsx
@@ -11,39 +11,19 @@ import { version } from '@root/package.json';
   shadow: true,
 })
 export class PostBackToTop {
-  @Element() host: HTMLPostListElement;
+  @Element() el: HTMLElement;
 
-  /**
-   *
-   */
   @State() buttonId: string;
 
-  /**
-   *
-   */
-
-  @State() isBelowFold: boolean = false;
+  @State() scrollPositionVH: number = null;
 
   handleScroll = () => {
-    // Get the current scroll position in pixels
     const scrollY = window.pageYOffset;
-
-    // Calculate the scroll position in viewport height units (vh)
-    const scrollPositionVh = (scrollY / window.innerHeight) * 100;
-
-    // Set CSS custom property for other potential uses
-    document.documentElement.style.setProperty('--scroll', scrollPositionVh.toString());
-    console.log(scrollPositionVh);
-
-    // Update state when the scroll position reaches or exceeds 80vh
-    this.isBelowFold = scrollPositionVh >= 80;
-    console.log(this.isBelowFold); // Logs true when past 80vh
+    this.scrollPositionVH = (scrollY / window.innerHeight) * 100;
+    document.body.style.setProperty('--scroll', this.scrollPositionVH.toString());
   };
 
   componentWillLoad() {
-    /**
-     * Use a random id by default
-     */
     this.buttonId = `back-to-top-${crypto.randomUUID()}`;
   }
 
@@ -58,7 +38,7 @@ export class PostBackToTop {
   render() {
     return (
       <Host data-version={version}>
-        <button role="button" class={`back-to-top ${this.isBelowFold ? 'show' : ''}`}>
+        <button role="button" class="back-to-top">
           <post-icon aria-hidden="true" name="3026"></post-icon>
           <span id={this.buttonId} class="visually-hidden">
             Back To Top Button
