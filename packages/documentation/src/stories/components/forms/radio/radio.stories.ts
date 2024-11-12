596a245d418cf6f83db20bef99d6a9b2d84f6f7b
Tim Schär <59233938+schaertim@users.noreply.github.com>
2024-11-12 09:22:41 +0100

    fix: linter error

--- packages/documentation/src/stories/components/forms/radio/radio.stories.ts
+++ packages/documentation/src/stories/components/forms/radio/radio.stories.ts
@@ -2,7 +2,6 @@ import { Args, StoryContext, StoryObj } from '@storybook/web-components';
 import { useArgs } from '@storybook/preview-api';
 import { html, nothing, TemplateResult } from 'lit';
 import { MetaComponent } from '@root/types';
-import { ifDefined } from 'lit/directives/if-defined.js';
 
 const VALIDATION_STATE_MAP: Record<string, undefined | boolean> = {
   'null': undefined,
