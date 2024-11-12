8eeb4e48f78917dbc3ab8325b47d2b9e390ffff7
Romain Veya <veya.romain@gmail.com>
2024-11-12 11:50:39 +0100

    feat(styles): fixed conflicts

--- packages/components/cypress/e2e/list.cy.ts
+++ packages/components/cypress/e2e/list.cy.ts
@@ -57,4 +57,16 @@ describe('PostList Component', { baseUrl: null, includeShadowDom: false }, () =>
       });
     });
   });
+
+  describe('Accessibility', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?id=snapshots--post-list');
+      cy.get('list-example', { timeout: 30000 }).should('be.visible');
+      cy.injectAxe();
+    });
+
+    it('Has no detectable a11y violations on load for all variants', () => {
+      cy.checkA11y('#root-inner');
+    });
+  });
 });
