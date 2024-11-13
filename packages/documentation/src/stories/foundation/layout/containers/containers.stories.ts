337f62307537b9a1766a48b24b0c77b3d7bcd718
Alizé Debray <alize.debray@post.ch>
2024-11-13 13:23:07 +0100

    Update containers doc

--- packages/documentation/src/stories/foundation/layout/containers/containers.stories.ts
+++ packages/documentation/src/stories/foundation/layout/containers/containers.stories.ts
@@ -1,16 +1,36 @@
-import type { StoryObj } from '@storybook/web-components';
+import type { Args, StoryObj } from '@storybook/web-components';
+import { StoryContext, StoryFn } from '@storybook/web-components';
 import { MetaExtended } from '@root/types';
+import { html } from 'lit';
+import './containers.styles.scss';
 
 const meta: MetaExtended = {
   id: 'a4ca9660-bb4a-4cc7-adfd-84767382ac03',
   title: 'Foundations/Layout/Containers',
-  parameters: {
-    badges: [],
-  },
+  render: (args: Args) => html`
+    <div class=${args.containerClass}>
+      <!-- Content goes here -->
+    </div>
+  `,
+  decorators: [
+    (story: StoryFn, context: StoryContext) => html`
+      <div class="containers-example">${story(context.args, context)}</div>
+    `,
+  ],
 };
 
 export default meta;
 
 type Story = StoryObj;
 
-export const Default: Story = {};
+export const Container: Story = {
+  args: {
+    containerClass: 'container',
+  },
+};
+
+export const ContainerFluid: Story = {
+  args: {
+    containerClass: 'container-fluid',
+  },
+};
