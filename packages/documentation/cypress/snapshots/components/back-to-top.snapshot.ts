32fe78ffb4aea50c15742582d9aa246f836f69d4
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2024-11-14 12:04:18 +0100

    tests added

--- packages/documentation/cypress/snapshots/components/back-to-top.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/back-to-top.snapshot.ts
@@ -1,7 +1,13 @@
-describe('Button', () => {
-  it('default', () => {
-    cy.visit('/iframe.html?id=snapshots--back-to-top');
-    cy.get('button post-icon', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Buttons', { widths: [1440] });
+describe('Back To Top', () => {
+  it('back-to-top-light', () => {
+    cy.visit('/iframe.html?id=snapshots--back-to-top-light');
+    cy.get('.back-to-top-example', { timeout: 30000 }).should('be.visible');
+    cy.percySnapshot('BackToTopLight', { widths: [320, 1440] });
+  });
+
+  it('back-to-top-dark', () => {
+    cy.visit('/iframe.html?id=snapshots--back-to-top-dark');
+    cy.get('.back-to-top-example', { timeout: 30000 }).should('be.visible');
+    cy.percySnapshot('BackToTopDark', { widths: [320, 1440] });
   });
 });
--- packages/documentation/src/stories/components/back-to-top/back-to-top.snapshot.stories.ts
+++ packages/documentation/src/stories/components/back-to-top/back-to-top.snapshot.stories.ts
@@ -1,7 +1,6 @@
-import type { Args, StoryContext, StoryObj, StoryFn } from '@storybook/web-components';
+import type { StoryObj } from '@storybook/web-components';
 import meta from './back-to-top.stories';
 import { html } from 'lit';
-import { bombArgs } from '@/utils';
 
 const { id, ...metaWithoutId } = meta;
 
@@ -12,25 +11,10 @@ export default {
 
 type Story = StoryObj;
 
-export const BackToTop: Story = {
+export const BackToTopLight: Story = {
   render: () => {
-    return html`
-      ${['bg-white', 'bg-dark'].map(bg => {
-        const id = `${bg}-${crypto.randomUUID()}`; // Unique ID for each background
-        return html`
-          ${[80, 50, 0].map(
-            threshold => html`<post-back-to-top threshold="${threshold}"></post-back-to-top> `,
-          )}
-        `;
-      })}
-    `;
-  },
-  decorators: [
-    () => {
-      const storyTemplate = html`<div
-        class="header-story-wrapper"
-        style="--header-z-index: 1;overflow: auto; "
-      >
+    return html`<div data-color-scheme="light" class="back-to-top-example bg-white">
+      <div class="header-story-wrapper" style="--header-z-index: 1;overflow: auto; ">
         <swisspost-internet-header
           project="test"
           environment="int01"
@@ -46,9 +30,35 @@ export const BackToTop: Story = {
           <p class="fake-content my-32"></p>
           <p class="fake-content my-32"></p>
           <p class="fake-content my-32"></p>
+          <post-back-to-top threshold="80"></post-back-to-top>
         </div>
-      </div>`;
-      return storyTemplate;
-    },
-  ],
+      </div>
+    </div>`;
+  },
+};
+
+export const BackToTopDark: Story = {
+  render: () => {
+    return html`<div data-color-scheme="dark" class="back-to-top-example bg-dark">
+      <div class="header-story-wrapper" style="--header-z-index: 1;overflow: auto; ">
+        <swisspost-internet-header
+          project="test"
+          environment="int01"
+          language="en"
+        ></swisspost-internet-header>
+        <div class="container">
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <p class="fake-content my-32"></p>
+          <post-back-to-top threshold="80"></post-back-to-top>
+        </div>
+      </div>
+    </div>`;
+  },
 };
