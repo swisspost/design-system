bfce2d2f14216d62c04a8865c6ad91ecebbff0af
leagrdv <lea.gardavaud@post.ch>
2024-11-19 10:52:16 +0100

    refacto slider for less complexity

--- packages/documentation/src/stories/components/forms/slider/slider.stories.ts
+++ packages/documentation/src/stories/components/forms/slider/slider.stories.ts
@@ -200,43 +200,37 @@ function render(args: Args, context: StoryContext) {
     />
   `;
 
-  let valueElement: TemplateResult | TemplateResult[] | null = null;
+  let valueElement: TemplateResult | null = null;
 
-  if (args.showValue === 'text') {
-    valueElement = html` <p class="form-hint" id="slider-form-hint">${args.value}</p> `;
-  } else if (args.showValue === 'input') {
+  if (args.showValue === 'input') {
     const inputId = context.id
       ? `${context.id}_input`
       : `${context.viewMode}_${context.name.replace(/\s/g, '-')}_ExampleRangeInput`;
 
-    valueElement = [
-      html` <label class="form-label visually-hidden" for="${inputId}">Range controller</label> `,
-      html`
-        <input
-          id="${inputId}"
-          class="form-control mw-giant"
-          type="text"
-          inputmode="decimal"
-          value="${args.value}"
-          .value="${args.value}"
-          ?disabled="${args.disabled}"
-          aria-describedby="${args.showValue === 'text' ? 'slider-form-hint' : ''}"
-          @input="${(e: Event) => updateArgs({ value: (e.target as HTMLInputElement).value })}"
-        />
-      `,
-    ];
-  }
-
-  if (args.showValue === 'input') {
     return html`
       <div class="row align-items-end">
         <div class="col">${[label, control, ...contextual].filter(el => el !== null)}</div>
-        <div class="col-auto">${valueElement}</div>
+        <div class="col-auto">
+          <label class="form-label visually-hidden" for="${inputId}">Range controller</label>
+          <input
+            id="${inputId}"
+            class="form-control mw-giant"
+            type="text"
+            inputmode="decimal"
+            value="${args.value}"
+            .value="${args.value}"
+            ?disabled="${args.disabled}"
+            aria-describedby="${args.showValue === 'text' ? 'slider-form-hint' : ''}"
+            @input="${(e: Event) => updateArgs({ value: (e.target as HTMLInputElement).value })}"
+          />
+        </div>
       </div>
     `;
-  } else {
-    return html`${[label, control, valueElement, ...contextual].filter(el => el !== null)}`;
+  } else if (args.showValue === 'text') {
+    valueElement = html`<p class="form-hint" id="slider-form-hint">${args.value}</p> `;
   }
+
+  return html`${[label, control, valueElement, ...contextual].filter(el => el !== null)}`;
 }
 
 export const Default: Story = {};
