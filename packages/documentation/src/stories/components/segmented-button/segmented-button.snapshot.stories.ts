c217db285850764e9ed9ddd5c1c0c921061ca192
Alona Zherdetska <alionazhv@gmail.com>
2024-11-20 15:39:43 +0100

    fixed: linting snapshot.stories

--- packages/documentation/src/stories/components/segmented-button/segmented-button.snapshot.stories.ts
+++ packages/documentation/src/stories/components/segmented-button/segmented-button.snapshot.stories.ts
@@ -1,4 +1,4 @@
-import type { Args, StoryContext, StoryObj } from '@storybook/web-components';
+import type { StoryObj } from '@storybook/web-components';
 import meta from './segmented-button.stories';
 import { html } from 'lit';
 
