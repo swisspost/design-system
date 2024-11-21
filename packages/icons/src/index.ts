655221ba3ec920c9a665558499ae8c6516605378
Oliver Schürch <oliver.schuerch@post.ch>
2024-11-21 16:03:19 +0100

    fix(icons): make the package buildable

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -3,16 +3,21 @@ import { IIcon, IJSONReport } from './models/icon.model';
 import { downloadSVG } from './utilities/downloadSVG';
 import { formatResponse } from './utilities/mapResponse';
 import { fetchPage } from './utilities/fetchPage';
-import createUIIcons from './utilities/uiIcons';
+import buildSVGs from './utilities/buildSVGs';
 import { url } from './utilities/environment';
 import path from 'path';
 import packageJSON from '../package.json';
 
 import { SOURCE_PATH, OUTPUT_PATH } from './constants';
 
-const iconDownloadPath = path.join(SOURCE_PATH, 'v1');
-const iconOutputPath = path.join(OUTPUT_PATH, 'post-icons');
-const reportOutputPath = path.join(OUTPUT_PATH, 'report.json');
+// v2 icons will be loaded from a different icon set (url) from censhare
+const downloadUrls: (string | undefined)[] = [url];
+// v2 icons will need to be saved under {SOURCE_PATH}/v2
+const iconDownloadDirectories: string[] = [path.join(SOURCE_PATH, 'v1')];
+// v2 icons will end up in the same output folder
+// icon names of v1 icons are number-based (e.g. 1000.svg, etc.)
+// while v2 icon names are letter-based (e.g. accessibility.svg, etc.)
+const iconBuildDirectory: string = path.join(OUTPUT_PATH, 'post-icons');
 
 const jsonReport: IJSONReport = {
   icons: [],
@@ -29,39 +34,54 @@ const jsonReport: IJSONReport = {
   version: packageJSON.version,
 };
 
-export const main = async () => {
+export const fetchAndBuildSVGs = async () => {
   setup();
 
-  // Start recursively downloading pages of icons
-  if (url !== undefined) {
-    console.log('Starting to download icons');
+  let i = 0;
 
-    const report = await downloadAllIcons(url);
-    fs.writeFileSync(path.join(OUTPUT_PATH, 'report.json'), JSON.stringify(report, null, 2));
+  await fetchSVGs();
+  buildSVGs();
 
-    console.log(
-      `\x1b[32mDownload finished.\x1b[0m Saved \x1b[32m${report.stats.success}\x1b[0m icons, \x1b[31m${report.stats.errors}\x1b[0m icons errored and \x1b[31m${report.stats.notFound}\x1b[0m where not found.`,
-    );
+  async function fetchSVGs() {
+    for (const directory of iconDownloadDirectories) {
+      const downloadUrl = downloadUrls[i++];
+
+      // Start recursively downloading pages of icons
+      if (downloadUrl !== undefined) {
+        console.log('Starting to download icons');
+
+        const report = await downloadAllIcons(downloadUrl, directory);
+        fs.writeFileSync(path.join(directory, 'report.json'), JSON.stringify(report, null, 2));
+
+        console.log(
+          `\x1b[32mDownload finished.\x1b[0m Saved \x1b[32m${report.stats.success}\x1b[0m icons, \x1b[31m${report.stats.errors}\x1b[0m icons errored and \x1b[31m${report.stats.notFound}\x1b[0m where not found.`,
+        );
+      }
+    }
   }
-
-  createUIIcons();
 };
 
 function setup() {
   // remove generated files & folders
-  if (fs.existsSync(iconDownloadPath)) fs.rmSync(iconDownloadPath, { recursive: true });
-  if (fs.existsSync(iconOutputPath)) fs.rmSync(iconOutputPath, { recursive: true });
-  if (fs.existsSync(reportOutputPath)) fs.unlinkSync(reportOutputPath);
+  iconDownloadDirectories.forEach(directory => {
+    if (fs.existsSync(directory)) fs.rmSync(directory, { recursive: true });
+  });
+  if (fs.existsSync(iconBuildDirectory)) fs.rmSync(iconBuildDirectory, { recursive: true });
 
   // ensure used folders exist
-  if (!fs.existsSync(iconDownloadPath)) fs.mkdirSync(iconDownloadPath, { recursive: true });
-  if (!fs.existsSync(iconOutputPath)) fs.mkdirSync(iconOutputPath, { recursive: true });
+  iconDownloadDirectories.forEach(directory => {
+    if (!fs.existsSync(directory)) fs.mkdirSync(directory, { recursive: true });
+  });
+  if (!fs.existsSync(iconBuildDirectory)) fs.mkdirSync(iconBuildDirectory, { recursive: true });
 }
 
 const sortIcons = (a: IIcon, b: IIcon) => (a.file.name < b.file.name ? -1 : 1);
 
-const downloadAllIcons = async (currentUrl: string): Promise<IJSONReport> => {
-  const body = await fetchPage(currentUrl);
+const downloadAllIcons = async (
+  downloadUrl: string,
+  downloadFolder: string,
+): Promise<IJSONReport> => {
+  const body = await fetchPage(downloadUrl);
 
   if (body === undefined) {
     throw new Error(`Fetch icons failed, response was ${body}`);
@@ -80,7 +100,7 @@ const downloadAllIcons = async (currentUrl: string): Promise<IJSONReport> => {
   await Promise.all(
     formattedResponse.map(async icon => {
       try {
-        const svg = await downloadSVG(icon, iconDownloadPath);
+        const svg = await downloadSVG(icon, downloadFolder);
 
         if (svg === false) {
           jsonReport.noSVG.push(icon);
@@ -101,7 +121,7 @@ const downloadAllIcons = async (currentUrl: string): Promise<IJSONReport> => {
 
   if (body.page.next) {
     // Recursively fetch more pages
-    return downloadAllIcons(body.page.next);
+    return downloadAllIcons(body.page.next, downloadFolder);
   } else {
     // Write JSON
     jsonReport.icons = [...jsonReport.icons].sort(sortIcons);
@@ -116,4 +136,4 @@ const downloadAllIcons = async (currentUrl: string): Promise<IJSONReport> => {
 };
 
 // Run Forest, run
-main();
+fetchAndBuildSVGs();
--- packages/icons/src/utilities/buildSVGs.ts
+++ packages/icons/src/utilities/buildSVGs.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 import { HTMLElement, parse } from 'node-html-parser';
 import { optimize } from 'svgo';
 import { version } from '../../package.json';
 import svgoOptions from '../../svgo.config.v2';
+import { IJSONReport } from './models/icon.model';
 
 import {
   SOURCE_PATH,
@@ -19,9 +21,6 @@ import {
   ICON_V2_TEMPLATE,
 } from '../constants';
 
-const iconOutputPath = path.join(OUTPUT_PATH, 'post-icons');
-const reportOutputPath = path.join(OUTPUT_PATH, 'report.v2.json');
-
 type File = {
   size: number | null;
   filePath: string;
@@ -32,34 +31,48 @@ type ReportIcon = {
   sizes: (number | null)[];
 };
 
-export default function main() {
+const iconSourcePath = SOURCE_PATH;
+const iconOutputPath = path.join(OUTPUT_PATH, 'post-icons');
+const buildReportOutputPath = path.join(OUTPUT_PATH, 'report.json');
+const jsonReport: IJSONReport = {
+  icons: [],
+  wrongViewBox: [],
+  noKeywords: [],
+  noSVG: [],
+  errored: [],
+  created: null,
+  stats: {
+    errors: 0,
+    notFound: 0,
+    success: 0,
+  },
+  version: version,
+};
+
+export default function build() {
   console.log('\nCreating UI icons...');
 
   setup();
 
-  const report = createFiles(getFileGroups());
-  createReport(report);
+  createFiles(getFileGroups());
+  const report = createReport();
 
   console.log(
-    `\x1b[32mUI icons created.\x1b[0m Saved \x1b[32m${report.length}\x1b[0m icons to use with the <post-icon> component.`,
+    `\x1b[32mUI icons created.\x1b[0m Saved \x1b[32m${report.stats.success}\x1b[0m icons, \x1b[31m${report.stats.errors}\x1b[0m icons errored and \x1b[31m${report.stats.notFound}\x1b[0m where not found.`,
   );
 }
 
 function setup() {
   // remove generated files & folders
   if (fs.existsSync(iconOutputPath)) fs.rmSync(iconOutputPath, { recursive: true });
-  if (fs.existsSync(reportOutputPath)) fs.unlinkSync(reportOutputPath);
+  if (fs.existsSync(buildReportOutputPath)) fs.unlinkSync(buildReportOutputPath);
 
   // // ensure used folders exist
   if (!fs.existsSync(iconOutputPath)) fs.mkdirSync(iconOutputPath, { recursive: true });
 }
 
-function sortIcons(a: ReportIcon, b: ReportIcon): number {
-  return a.id < b.id ? -1 : 1;
-}
-
 function getFileGroups(): Record<string, File[]> {
-  const filePaths = fs.readdirSync(SOURCE_PATH, { recursive: true });
+  const filePaths = fs.readdirSync(iconSourcePath, { recursive: true });
 
   return filePaths
     .filter(p => p.toString().endsWith('.svg'))
@@ -99,31 +112,24 @@ function getFileGroups(): Record<string, File[]> {
   }
 }
 
-function createFiles(groupedFilePaths: Record<string, File[]>): ReportIcon[] {
-  const report: ReportIcon[] = [];
-
+function createFiles(groupedFilePaths: Record<string, File[]>) {
   Object.entries(groupedFilePaths).forEach(([id, files]) => {
+    const isSingleFile = files.length === 1;
+
     const svgs = files.map(({ size, filePath }) => ({
       size,
-      svg: fs.readFileSync(path.join(SOURCE_PATH, filePath), 'utf-8'),
+      svg: fs.readFileSync(path.join(iconSourcePath, filePath), 'utf-8'),
     }));
 
-    const symbolId = files.length === 1 ? [ID_PREFIX, id].join(ID_SEPERATOR) : ID_SYMBOL_PREFIX;
-    const template = files.length === 1 ? ICON_V1_TEMPLATE : ICON_V2_TEMPLATE;
+    const symbolId = isSingleFile ? [ID_PREFIX, id].join(ID_SEPERATOR) : ID_SYMBOL_PREFIX;
+    const template = isSingleFile ? ICON_V1_TEMPLATE : ICON_V2_TEMPLATE;
 
     const symbols = svgs.map(({ size, svg }) => getSymbol(svg, symbolId, size));
     const uses = svgs.map(({ size }) => getUse(symbolId, size));
     const file = createSvg(id, template, symbols, uses);
 
     fs.writeFileSync(path.join(iconOutputPath, `${id}.svg`), file);
-
-    report.push({
-      id,
-      sizes: svgs.map(({ size }) => size),
-    });
   });
-
-  return report;
 
   function getSymbol(svg: string, symbolId: string, size: number | null): string {
     svg = optimize(svg, svgoOptions).data;
@@ -170,36 +176,97 @@ function createFiles(groupedFilePaths: Record<string, File[]>): ReportIcon[] {
   }
 }
 
-function createReport(report: ReportIcon[]) {
-  const lightIcons: ReportIcon[] = report.filter(({ id }) => !id.endsWith('-solid'));
-  const solidIcons: ReportIcon[] = report.filter(({ id }) => id.endsWith('-solid'));
-  const allIcons: ReportIcon[] = [...lightIcons, ...solidIcons];
-  const allNormalizedIconIds = allIcons.map(icon => icon.id.replace(/-solid$/, ''));
+function createReport(): IJSONReport {
+  // TODO: remove as soon as v2 icons are fetched from censhare
+  createV2Report();
 
-  fs.writeFileSync(
-    reportOutputPath,
-    JSON.stringify(
-      {
-        icons: report.toSorted(sortIcons),
-        count: {
-          light: lightIcons.length,
-          solid: solidIcons.length,
-          total: report.length,
+  const filePaths = fs.readdirSync(iconSourcePath, { recursive: true });
+
+  const reports = filePaths
+    .filter((p: string | Buffer) => path.basename(p as string) === 'report.json')
+    .reduce((report, filePath) => {
+      const file = JSON.parse(
+        fs.readFileSync(path.join(iconSourcePath, filePath as string), 'utf-8'),
+      );
+
+      return {
+        icons: [...report.icons, ...(file.icons ?? [])],
+        wrongViewBox: [...report.wrongViewBox, ...(file.wrongViewBox ?? [])],
+        noKeywords: [...report.noKeywords, ...(file.noKeywords ?? [])],
+        errored: [...report.errored, ...(file.errored ?? [])],
+        stats: {
+          errors: report.stats.errors + file.stats.errors,
+          notFound: report.stats.notFound + file.stats.notFound,
+          success: report.stats.success + file.stats.success,
         },
-        missmatches: {
-          id: allIcons.filter((icon: ReportIcon) => {
-            const normalizedId = icon.id.replace(/-solid$/, '');
-            return allNormalizedIconIds.filter(id => id === normalizedId).length !== 2;
-          }),
-          sizes: allIcons.filter(
-            (icon: ReportIcon) => !ICON_V2_SIZES.every(size => icon.sizes.includes(size)),
-          ),
+      };
+    }, jsonReport);
+
+  reports.created = new Date();
+  reports.version = version;
+
+  fs.writeFileSync(buildReportOutputPath, JSON.stringify(reports, null, 2));
+
+  return reports;
+}
+
+// TODO: remove as soon as v2 icons are fetched from censhare
+function createV2Report() {
+  const filePaths = fs.readdirSync(iconOutputPath, { recursive: true });
+
+  const report = filePaths
+    .filter((p: string | Buffer) => {
+      const basename = path.basename(p as string);
+      return basename.endsWith('.svg') && !/^(\d){4}\.svg$/.test(basename);
+    })
+    .reduce((report, filePath) => {
+      const name = path.basename(filePath as string);
+      const ext = path.extname(name);
+      const basename = name.replace(ext, '');
+      const now = new Date();
+
+      return {
+        ...report,
+        icons: [
+          ...report.icons,
+          {
+            uuid: crypto.randomUUID(),
+            id: crypto.randomInt(100000, 999999),
+            type: 'picture.pictogram.',
+            typeFilter: 'pictograms',
+            meta: {
+              downloadLink: '',
+              businessfield: [],
+              keywords: ['ui', name],
+              year: ['2024'],
+            },
+            file: {
+              mime: 'image/svg+xml',
+              name,
+              basename,
+              ext,
+              size: {
+                width: 0,
+                dpi: 72,
+                height: 0,
+              },
+            },
+            createdAt: now,
+            modifiedAt: now,
+          },
+        ],
+        stats: {
+          errors: report.stats.errors,
+          notFound: report.stats.notFound,
+          success: report.stats.success + 1,
         },
-        created: new Date(),
-        version,
-      },
-      null,
-      2,
-    ),
-  );
+      };
+    }, jsonReport);
+
+  report.created = new Date();
+  report.version = version;
+
+  fs.writeFileSync(path.join(iconSourcePath, 'v2', 'report.json'), JSON.stringify(report, null, 2));
+
+  return report;
 }
