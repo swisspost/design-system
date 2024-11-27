421c4e5dba7627c4e0a2e8941bed74441c64bcf8
Philipp Gfeller <philipp.gfeller@post.ch>
2024-11-27 12:51:18 +0100

    remove demo content

--- packages/demo/src/app/intranet-layout/components/intranet-header-demo-condensed/intranet-header-demo-condensedomponent.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-intranet-header',
-  templateUrl: './intranet-header-demo-condensed.component.html',
-  styleUrls: ['../../intranet-layout.component.css'],
-})
-export class IntranetHeaderDemoCondensedComponent {}
