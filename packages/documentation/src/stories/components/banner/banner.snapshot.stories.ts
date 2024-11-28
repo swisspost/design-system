60fbfacdb5397ebf5c43b16f65fa5d27e158e429
Oliver Schürch <oliver.schuerch@post.ch>
2024-11-28 17:21:02 +0100

    chore(documentation): fix linting error

--- packages/documentation/src/stories/components/banner/banner.snapshot.stories.ts
+++ packages/documentation/src/stories/components/banner/banner.snapshot.stories.ts
@@ -1,4 +1,4 @@
-import { Args, StoryContext, StoryObj } from '@storybook/web-components';
+import { Args, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { schemes } from '@/shared/snapshots/schemes';
 import { bombArgs } from '@/utils';
