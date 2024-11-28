8e3caf27cdc3cc71bd8ec4d555f189f8ac6bae90
Philipp Gfeller <philipp.gfeller@post.ch>
2024-11-28 15:13:39 +0100

    Delete accent.snapshot.ts

--- packages/documentation/cypress/snapshots/utilities/accent.snapshot.ts
+++
@@ -1,7 +0,0 @@
-describe('Accent', () => {
-  it('accent', () => {
-    cy.visit('/iframe.html?id=snapshots--accent');
-    cy.get('.accent-default', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Accents', { widths: [1440] });
-  });
-});
