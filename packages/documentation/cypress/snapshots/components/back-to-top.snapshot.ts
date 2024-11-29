bffcd8bdec5d35c37155897f111d5c4c4bd214e7
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2024-11-29 19:01:44 +0100

    scheme wrapper

--- packages/documentation/cypress/snapshots/components/back-to-top.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/back-to-top.snapshot.ts
@@ -1,6 +1,6 @@
 describe('Back To Top', () => {
   it('back-to-top', () => {
-    cy.visit('/iframe.html?id=snapshots--back-to-top');
+    cy.visit('/iframe.html?id=snapshots--post-back-to-top');
     cy.get('.back-to-top-example', { timeout: 30000 }).should('be.visible');
     cy.percySnapshot('BackToTopLight', { widths: [320, 1440] });
   });
--- packages/documentation/src/stories/components/back-to-top/back-to-top.snapshot.stories.ts
+++ packages/documentation/src/stories/components/back-to-top/back-to-top.snapshot.stories.ts
@@ -1,6 +1,7 @@
 import type { StoryObj, StoryFn, StoryContext } from '@storybook/web-components';
 import meta from './back-to-top.stories';
 import { html } from 'lit';
+import { schemes } from '@/shared/snapshots/schemes';
 
 const { id, ...metaWithoutId } = meta;
 
@@ -13,7 +14,8 @@ type Story = StoryObj;
 
 export const PostBackToTop: Story = {
   decorators: [
-    (story: StoryFn, { args, context }: StoryContext) =>
-      html`<div data-color-scheme="light" class="bg-white">${story(args, context)}</div>`,
+    (story: StoryFn, { args, context }: StoryContext) => {
+      return schemes(() => html`${story(args, context)}`);
+    },
   ],
 };
