9744dfa727b628169146614b6574c77b09b22e9c
Oliver Schürch <oliver.schuerch@post.ch>
2024-12-02 14:16:54 +0100

    Update packages/documentation/cypress/snapshots/components/list.snapshot.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/documentation/cypress/snapshots/components/list.snapshot.ts
+++ packages/documentation/cypress/snapshots/components/list.snapshot.ts
@@ -1,7 +1,7 @@
 describe('List', () => {
   it('default', () => {
     cy.visit('/iframe.html?id=snapshots--lists');
-    cy.get('#storybook-root', { timeout: 30000 }).should('be.visible');
+    cy.get('ol', { timeout: 30000 }).should('be.visible');
     cy.percySnapshot('Lists', { widths: [1440] });
   });
 });
