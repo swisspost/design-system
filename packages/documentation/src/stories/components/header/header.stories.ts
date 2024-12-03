62b3bcbfbaada17c968eec068db02344896a33b2
Philipp Gfeller <philipp.gfeller@post.ch>
2024-12-03 13:18:54 +0100

    feat(docs): added basic header story
    This initial story only contains markup for the header. Options and other controls can be added later.

---
+++ packages/documentation/src/stories/components/header/header.stories.ts
@@ -0,0 +1,30 @@
+import type { Args, StoryObj } from '@storybook/web-components';
+import { html } from 'lit/static-html.js';
+import { MetaComponent } from '@root/types';
+import HeaderMarkup from './components/header.markup';
+
+const meta: MetaComponent = {
+  id: 'header',
+  title: 'Components/Header',
+  tags: ['package:HTML'],
+  parameters: {
+    layout: 'fullscreen',
+    badges: [],
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/JIT5AdGYqv6bDRpfBPV8XR/Foundations-%26-Components-Next-Level?node-id=558-7012&t=ywmfJhyvd2euoiGI-1',
+    },
+  },
+  args: {},
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+  render: (_args: Args) => {
+    return HeaderMarkup;
+  },
+};
