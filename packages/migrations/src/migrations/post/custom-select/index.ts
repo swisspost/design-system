e07061ee2f548ce3f234fab43878b74397fcf2b3
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2024-12-03 15:32:47 +0100

    revert file

--- packages/migrations/src/migrations/post/custom-select/index.ts
+++ packages/migrations/src/migrations/post/custom-select/index.ts
@@ -4,9 +4,9 @@ import { DomUpdate, getDomMigrationRule } from '../../../utils/dom-migration';
 
 export default function (): Rule {
   return getDomMigrationRule(
-    new CustomSelectFloatingLabelWrapperUpdate(),
-    new CustomSelectClassesUpdate(),
-    new CustomSelectMenuClassesUpdate(),
+    new CustomSelectFloatingLabelWrapperUpdate,
+    new CustomSelectClassesUpdate,
+    new CustomSelectMenuClassesUpdate,
   );
 }
 
@@ -14,19 +14,22 @@ class CustomSelectFloatingLabelWrapperUpdate implements DomUpdate {
   selector = '.form-group';
 
   update($elements: Cheerio<AnyNode>, $: CheerioAPI) {
-    $elements.each((_i, element) => {
-      const $element = $(element);
-      const $control = $element.find('> button.form-control-lg');
-      const $label = $control.next('label');
-      const isNgbDropdown = $element.attr('ngbDropdown') !== undefined;
-      const isFloatingLabel = $control.length > 0 && $label.length > 0;
+    $elements
+      .each((_i, element) => {
+        const $element = $(element);
+        const $control = $element.find('> button.form-control-lg');
+        const $label = $control.next('label');
+        const isNgbDropdown = $element.attr('ngbDropdown') !== undefined;
+        const isFloatingLabel = $control.length > 0 && $label.length > 0;
 
-      if (isNgbDropdown && isFloatingLabel) {
-        $element.removeClass('form-group').addClass('form-floating');
+        if (isNgbDropdown && isFloatingLabel) {
+          $element
+            .removeClass('form-group')
+            .addClass('form-floating');
 
-        $control.removeClass('form-control-lg');
-      }
-    });
+          $control.removeClass('form-control-lg');
+        }
+      });
   }
 }
 
@@ -34,14 +37,17 @@ class CustomSelectClassesUpdate implements DomUpdate {
   selector = 'button.form-control';
 
   update($elements: Cheerio<AnyNode>, $: CheerioAPI) {
-    $elements.each((_i, element) => {
-      const $element = $(element);
-      const isNgbDropdownToggle = $element.attr('ngbDropdownToggle') !== undefined;
+    $elements
+      .each((_i, element) => {
+        const $element = $(element);
+        const isNgbDropdownToggle = $element.attr('ngbDropdownToggle') !== undefined;
 
-      if (isNgbDropdownToggle) {
-        $element.removeClass('form-control custom-select').addClass('form-select');
-      }
-    });
+        if (isNgbDropdownToggle) {
+          $element
+            .removeClass('form-control custom-select')
+            .addClass('form-select');
+        }
+      });
   }
 }
 
@@ -49,13 +55,16 @@ class CustomSelectMenuClassesUpdate implements DomUpdate {
   selector = '.custom-select-menu';
 
   update($elements: Cheerio<AnyNode>, $: CheerioAPI) {
-    $elements.each((_i, element) => {
-      const $element = $(element);
-      const isNgbDropdownMenu = $element.attr('ngbDropdownMenu') !== undefined;
+    $elements
+      .each((_i, element) => {
+        const $element = $(element);
+        const isNgbDropdownMenu = $element.attr('ngbDropdownMenu') !== undefined;
 
-      if (isNgbDropdownMenu) {
-        $element.removeClass('custom-select-menu').addClass('w-100 max-w-100');
-      }
-    });
+        if (isNgbDropdownMenu) {
+          $element
+            .removeClass('custom-select-menu')
+            .addClass('w-100 mw-100');
+        }
+      });
   }
 }
