8949b29033827c295f4e724627bb29bc1806f08d
Tim Schär <59233938+schaertim@users.noreply.github.com>
2024-12-04 13:06:48 +0100

    chore(components): removed padding on tertiary button

--- packages/documentation/src/stories/components/form-footer/form-footer.stories.ts
+++ packages/documentation/src/stories/components/form-footer/form-footer.stories.ts
@@ -134,7 +134,7 @@ export function render(args: Args) {
         ? html` <div class="form-footer-primary-actions">${primaryButton} ${secondaryButton}</div> `
         : null}
       ${args.showTertiaryButton
-        ? html`<button class="btn btn-tertiary">
+        ? html`<button class="btn btn-tertiary btn-link">
             <post-icon aria-hidden="true" name="${args.tertiaryButtonIcon}"></post-icon
             >${args.tertiaryButtonText}
           </button>`
