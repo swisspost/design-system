2be3e27e0057c5ccee9a885ec5623fd8f0ac7277
Oliver Schürch <oliver.schuerch@post.ch>
2024-12-11 17:38:33 +0100

    chore: renameing variables to fix a lint issue

--- packages/components/src/utils/breakpoints.ts
+++ packages/components/src/utils/breakpoints.ts
@@ -8,22 +8,24 @@ type MapItem = {
 type ListenerType = 'key' | 'name';
 
 export class Breakpoint {
-  private breakpointMap: MapItem[];
-  private current_key: string;
-  private current_name: string;
+  private readonly breakpointMap: MapItem[];
+  private readonly current = {
+    key: '',
+    name: '',
+  };
 
   constructor() {
     if (!this.breakpointMap) {
       const styles = getComputedStyle(document.documentElement);
-      const keys = styles.getPropertyValue('--post-breakpoint-names').split(', ');
-      const names = styles.getPropertyValue('--post-viewport-names').split(', ');
+      const keys = styles.getPropertyValue('--post-breakpoint-keys').split(', ');
+      const names = styles.getPropertyValue('--post-breakpoint-names').split(', ');
 
       this.breakpointMap = styles
-        .getPropertyValue('--post-breakpoint-keys')
-        .split(' ')
-        .map((key, i) => {
+        .getPropertyValue('--post-breakpoint-widths')
+        .split(', ')
+        .map((width, i) => {
           return {
-            minWidth: Number(key),
+            minWidth: Number(width),
             key: keys[i],
             name: names[i],
           };
@@ -37,26 +39,24 @@ export class Breakpoint {
   private updateHandler(emitEvents = true) {
     const calculated = this.breakpointMap.find(({ minWidth }) => innerWidth >= minWidth);
 
-    if (this.current_key !== calculated.key) {
-      this.current_key = calculated.key;
+    if (this.current.key !== calculated.key) {
+      this.current.key = calculated.key;
       if (emitEvents) this.dispatchEvent('key');
     }
 
-    if (this.current_name !== calculated.name) {
-      this.current_name = calculated.name;
+    if (this.current.name !== calculated.name) {
+      this.current.name = calculated.name;
       if (emitEvents) this.dispatchEvent('name');
     }
   }
 
   private dispatchEvent(type: ListenerType) {
-    window.dispatchEvent(
-      new CustomEvent(`postBreakpoint:${type}`, { detail: this[`current_${type}`] }),
-    );
+    window.dispatchEvent(new CustomEvent(`postBreakpoint:${type}`, { detail: this.current[type] }));
   }
 
   public get(type: ListenerType) {
     this.updateHandler(false);
-    return this[`current_${type}`];
+    return this.current[type];
   }
 }
 
