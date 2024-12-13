a3ce33d0b72020afd8258d37d71f0d007adc6c30
Oliver Schürch <oliver.schuerch@post.ch>
2024-12-13 14:57:54 +0100

    fix(documentation): move internet-header, footer and breadcrumbs to raw-components and new header to modules (#4258)

--- packages/documentation/src/stories/raw-components/internet-header/header.stories.ts
+++ packages/documentation/src/stories/raw-components/internet-header/header.stories.ts
@@ -8,7 +8,7 @@ import { MetaComponent } from '@root/types';
 
 const meta: MetaComponent = {
   id: 'ebb11274-091b-4cb7-9a3f-3e0451c9a865',
-  title: 'Components/Internet Header',
+  title: 'Raw Components/Internet Header',
   tags: ['package:InternetHeader'],
   component: 'swisspost-internet-header',
   parameters: {
