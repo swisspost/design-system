a91d74692190f4cdbe810f540eac2c234aa8e584
Lea <lea.gardavaud@post.ch>
2024-12-31 16:01:25 +0100

    Update packages/documentation/src/utils/fake-content.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/documentation/src/utils/fake-content.ts
+++ packages/documentation/src/utils/fake-content.ts
@@ -2,6 +2,6 @@ import { html } from 'lit-html';
 
 export function fakeContent(paragraphs = 6) {
   return html`<div class="container">
-    ${Array.from({ length: paragraphs }).map(() => html`<p class="fake-content"></p>`)}
+    ${Array.from({ length: paragraphs }).map(() => html`<p aria-hidden="true" class="fake-content"></p>`)}
   </div>`;
 }
