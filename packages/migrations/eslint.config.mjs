42c1d4cd6580540de9a6ff9b9f3da4b5d6469c8e
Oliver Schürch <oliver.schuerch@post.ch>
2025-01-17 11:17:06 +0100

    deps(migrations): update eslint to v9

---
+++ packages/migrations/eslint.config.mjs
@@ -0,0 +1,57 @@
+// this config was created using https://eslint.org/blog/2024/04/eslint-config-inspector/
+
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import globals from 'globals';
+
+export default [
+  {
+    name: 'post/global/ignores',
+    ignores: ['dist/*'],
+  },
+  {
+    name: 'post/defaults',
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    name: 'eslint/recommended',
+    ...js.configs.recommended,
+  },
+  {
+    name: 'post/ts/defaults',
+    files: ['**/*.{ts,mts,cts}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+    },
+    rules: {
+      'indent': [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      'semi': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'none',
+          destructuredArrayIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  ...ts.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ...ts.configs.disableTypeChecked,
+  },
+];
