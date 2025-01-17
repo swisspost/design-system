136736a4e369dc4e9a3b262145907c4937ecf42d
Oliver Schürch <oliver.schuerch@post.ch>
2025-01-17 15:47:09 +0100

    chore(documentation): update eslint config

--- packages/documentation/eslint.config.mjs
+++ packages/documentation/eslint.config.mjs
@@ -4,7 +4,6 @@ import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import sb from 'eslint-plugin-storybook';
 import mdx from 'eslint-plugin-mdx';
-// import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import reactPlugin from 'eslint-plugin-react';
 
