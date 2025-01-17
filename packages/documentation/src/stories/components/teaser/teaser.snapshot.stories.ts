f1f7e598ff46472a322cb4a973f0ccf74bc9c69d
leagrdv <lea.gardavaud@post.ch>
2025-01-17 14:50:49 +0100

    add snapshots

--- packages/documentation/src/stories/components/teaser/teaser.snapshot.stories.ts
+++ packages/documentation/src/stories/components/teaser/teaser.snapshot.stories.ts
@@ -2,7 +2,7 @@ import { Args, StoryContext, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { schemes } from '@/shared/snapshots/schemes';
 
-import meta from './teaser.stories';
+import meta, { renderTeaserCard, renderTeaserSectionHeader } from './teaser.stories';
 
 const { id, ...metaWithoutId } = meta;
 
@@ -14,7 +14,30 @@ export default {
 type Story = StoryObj;
 
 export const Teaser: Story = {
-  render: (args: Args, context: StoryContext) => {
-    return schemes(() => html` ${meta.render?.(args, context)} `);
+  render: (_args: Args, context: StoryContext) => {
+    return schemes(
+      () => html`
+        <div class="d-flex flex-column gap-16">
+          ${['sm', 'lg'].map((size: string) => {
+            const args = {
+              ...context.args,
+              size,
+            };
+            return html`
+              <div class="teaser-container container py-56 d-flex flex-column gap-32">
+                ${renderTeaserSectionHeader(args.size)}
+                <div class="row gy-16 gy-lg-24">
+                  ${Array.from(
+                    { length: 4 },
+                    () => html` <div class="col-md-6 col-lg-3">${renderTeaserCard(args)}</div> `,
+                  )}
+                </div>
+              </div>
+              <div class="container py-56 d-flex flex-column gap-32"></div>
+            `;
+          })}
+        </div>
+      `,
+    );
   },
 };
--- packages/documentation/src/stories/components/teaser/teaser.stories.ts
+++ packages/documentation/src/stories/components/teaser/teaser.stories.ts
@@ -71,20 +71,38 @@ export default meta;
 
 type Story = StoryObj;
 
-export const Default: Story = {
-  render: args => html`<div class="teaser-card${args.size === 'lg' ? ' teaser-card-lg' : ''}">
-      <img src="https://picsum.photos/id/38/500/400" alt="My placeholder image">
+export function renderTeaserCard(args: Args) {
+  return html`<div class="teaser-card${args.size === 'lg' ? ' teaser-card-lg' : ''}">
+    <img src="https://picsum.photos/id/38/500/400" alt="My placeholder image">
+    <div>
       <div>
-        <div>
-          <h3>${args.title}</h3>
-          <p>${args.content}</p>
-        </div>
-        <a href="#" class="btn btn-tertiary px-0">${
-          args.buttonLabel
-        } <post-icon name="arrowright"></post-icon></post-icon></a>
+        <h3>${args.title}</h3>
+        <p>${args.content}</p>
       </div>
+      <a href="#" class="btn btn-tertiary px-0">${
+        args.buttonLabel
+      } <post-icon name="arrowright"></post-icon></post-icon></a>
     </div>
-  `,
+  </div>
+  `;
+}
+
+export function renderTeaserSectionHeader(size?: string) {
+  return html` <div class="d-flex align-items-end justify-content-between gap-24">
+    <div class="d-flex flex-column gap-8">
+      <h2 class="m-0">Teaser section ${size}</h2>
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo.
+      </p>
+    </div>
+    <a href="#" class="flex-shrink-0 btn btn-primary"
+      >Let's go <post-icon name="arrowright"></post-icon
+    ></a>
+  </div>`;
+}
+
+export const Default: Story = {
+  render: renderTeaserCard,
   decorators: [story => html`<div style="max-width: 400px;">${story()}</div>`],
 };
 
@@ -92,36 +110,13 @@ export const Section: Story = {
   parameters: {
     layout: 'fullscreen',
   },
-  render: () => html`
+  render: args => html`
     <div class="container py-56 d-flex flex-column gap-32">
-      <div class="d-flex align-items-end justify-content-between gap-24">
-        <div class="d-flex flex-column gap-8">
-          <h2 class="m-0">Teaser section</h2>
-          <p class="m-0">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo
-          </p>
-        </div>
-        <a href="#" class="flex-shrink-0 btn btn-primary"
-          >Let's go <post-icon name="arrowright"></post-icon
-        ></a>
-      </div>
+      ${renderTeaserSectionHeader()}
       <div class="row gy-16 gy-lg-24">
         ${Array.from(
           { length: 4 },
-          () => html`
-          <div class="col-md-6 col-lg-3">
-            <div class="teaser-card">
-              <img src="https://picsum.photos/id/38/500/400" alt="My placeholder">
-              <div>
-                <div>
-                  <h3>Title</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <a href="#" class="btn btn-tertiary px-0">Read more <post-icon name="arrowright"></post-icon></post-icon></a>
-              </div>
-            </div>
-          </div>
-        `,
+          () => html` <div class="col-md-6 col-lg-3">${renderTeaserCard(args)}</div> `,
         )}
       </div>
     </div>
