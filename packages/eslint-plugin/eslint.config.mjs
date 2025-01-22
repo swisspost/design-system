8da8123eab3bfd6ec9273450260c21c82e47414f
Alizé Debray <alize.debray@post.ch>
2025-01-22 09:31:06 +0100

    Update eslint config

---
+++ packages/eslint-plugin/eslint.config.mjs
@@ -0,0 +1,48 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    name: 'post/global/ignores',
+    ignores: ['dist/*'],
+  },
+  {
+    name: 'post/defaults',
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    name: 'eslint/recommended',
+    ...js.configs.recommended,
+  },
+  {
+    name: 'post/ts/defaults',
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'none',
+          destructuredArrayIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  ...ts.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ...ts.configs.disableTypeChecked,
+  },
+];
