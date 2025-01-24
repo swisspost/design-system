51acb004bc57575c07458f060ead10c61f12d73b
Oliver Schürch <oliver.schuerch@post.ch>
2025-01-24 11:14:01 +0100

    chore(icons): rename constants

--- packages/icons/src/constants.ts
+++ packages/icons/src/constants.ts
@@ -1,7 +1,7 @@
 export const SOURCE_PATH = 'src/icons';
 export const OUTPUT_PATH = 'public';
 
-export const ICON_V2_SIZES = [16, 24, 32, 40, 48, 64];
+export const UI_ICON_SIZES = [16, 24, 32, 40, 48, 64];
 
 export const ID_PREFIX = 'i';
 export const ID_SEPERATOR = '-';
@@ -9,19 +9,19 @@ export const ID_UNWANTED_PARTS = ['shape'];
 export const ID_SYMBOL_PREFIX = 's';
 export const ID_SYMBOL_SEPERATOR = '';
 
-export const ICON_V1_TEMPLATE = `<svg xmlns="http://www.w3.org/2000/svg">
+export const POST_ICON_TEMPLATE = `<svg xmlns="http://www.w3.org/2000/svg">
   <defs>
     {symbols}
   </defs>
   {uses}
 </svg>`;
 
-const ICON_V2_TEMPLATE_STYLES = `<style>
-  ${ICON_V2_SIZES.map((size, i) => {
+const UI_ICON_TEMPLATE_STYLES = `<style>
+  ${UI_ICON_SIZES.map((size, i) => {
     const query = [];
 
-    if (i > 0) query.push(`(min-width: ${ICON_V2_SIZES[i]}px)`);
-    if (i < ICON_V2_SIZES.length - 1) query.push(`(max-width: ${ICON_V2_SIZES[i + 1] - 0.02}px)`);
+    if (i > 0) query.push(`(min-width: ${UI_ICON_SIZES[i]}px)`);
+    if (i < UI_ICON_SIZES.length - 1) query.push(`(max-width: ${UI_ICON_SIZES[i + 1] - 0.02}px)`);
 
     return `@media ${query.join(' and ')} {
       g {
@@ -31,7 +31,7 @@ const ICON_V2_TEMPLATE_STYLES = `<style>
   }).join('\n\n')}
   </style>`;
 
-export const ICON_V2_TEMPLATE = `<svg xmlns="http://www.w3.org/2000/svg">
+export const UI_ICON_TEMPLATE = `<svg xmlns="http://www.w3.org/2000/svg">
   <defs>
     <symbol id="{id}">
       {symbols}
@@ -39,7 +39,7 @@ export const ICON_V2_TEMPLATE = `<svg xmlns="http://www.w3.org/2000/svg">
     </symbol>
   </defs>
   
-  ${ICON_V2_TEMPLATE_STYLES.split('\n')
+  ${UI_ICON_TEMPLATE_STYLES.split('\n')
     .map(line => line.trim())
     .map(line => (line.startsWith('@') ? line : line.replace(/(\s|\t|\n)/g, '')))
     .join('')}
