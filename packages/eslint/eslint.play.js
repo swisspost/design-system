a0a3ae643a77aabc64d8fe9b760021e2ca141939
Alizé Debray <alize.debray@post.ch>
2025-02-03 10:58:37 +0100

    Add play script

---
+++ packages/eslint/eslint.play.js
@@ -0,0 +1,8 @@
+/**
+ * This configuration is not for linting the package files.
+ * It is used only to test the ESLint configurations exported by the package.
+ */
+
+const designSystemESLint = require('./dist');
+
+module.exports = [designSystemESLint.configs.template];
