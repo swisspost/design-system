3246710d795e8b2b05c150aca8520185ca683aa1
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-02-04 10:15:59 +0100

    update snapshots

--- packages/documentation/src/stories/foundations/layout/align-container/align-container.snapshot.stories.ts
+++ packages/documentation/src/stories/foundations/layout/align-container/align-container.snapshot.stories.ts
@@ -1,5 +1,5 @@
 import type { Args, StoryContext, StoryObj } from '@storybook/web-components';
-import meta from './align-container.stories';
+import meta, { AlignContainerGrid } from './align-container.stories';
 import { html } from 'lit';
 import { schemes } from '@/shared/snapshots/schemes';
 const { id, ...metaWithoutId } = meta;
@@ -15,8 +15,12 @@ export const AlignContainer: Story = {
   render: (_args: Args, context: StoryContext) => {
     return schemes(
       () => html`
-        ${['none', 'align-container-start', 'align-container-end'].map(alignContainer =>
-          meta.render({ alignContainer }, context),
+        ${['none', 'align-container-start', 'align-container-end'].map(
+          alignContainer =>
+            html`
+              ${meta.render({ alignContainer }, context)}
+              ${AlignContainerGrid.render({ alignContainer }, context)}
+            `,
         )}
       `,
     );
