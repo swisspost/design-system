6c091cab624f743c0227e90b4ea557b2275b7bf3
Romain Veya <veya.romain@gmail.com>
2025-02-04 14:42:47 +0100

    feat(styles): fixed max-width property bug

--- packages/documentation/src/stories/utilities/text-truncation/text-truncation.stories.ts
+++ packages/documentation/src/stories/utilities/text-truncation/text-truncation.stories.ts
@@ -4,7 +4,7 @@ import { MetaExtended } from '@root/types';
 
 const meta: MetaExtended = {
   id: '83fd4b10-9010-435a-be42-c1e7236068cc',
-  title: 'Utilities/TextTruncation',
+  title: 'Utilities/Text Truncation',
   argTypes: {
     truncateWidth: {
       name: 'max-width',
@@ -20,9 +20,9 @@ const meta: MetaExtended = {
   args: {
     truncateWidth: 200,
   },
-  render: () => {
+  render: args => {
     return html`
-      <p class="text-truncate" style="max-width: ${meta.args?.truncateWidth}px">
+      <p class="text-truncate" style="max-width: ${args?.truncateWidth}px">
         This is a long text that should be truncated when it exceeds the defined max-width.
       </p>
     `;
