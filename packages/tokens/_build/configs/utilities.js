5b8c00df40b628fcd877fad263d8967f5fc5a039
Alizé Debray <alize.debray@post.ch>
2025-02-05 17:42:35 +0100

    Fix tests adn sonar issues

--- packages/tokens/_build/configs/utilities.js
+++ packages/tokens/_build/configs/utilities.js
@@ -1,5 +1,5 @@
 import { fileHeader } from 'style-dictionary/utils';
-import { TOKENSET_LAYERS, TOKENSET_NAMES, TOKENSET_PREFIX } from '../constants.js';
+import { TOKENSET_NAMES } from '../constants.js';
 import StyleDictionary from '../style-dictionary.js';
 import { registerConfigMethod, getTokenValue } from '../methods.js';
 
@@ -7,49 +7,34 @@ import { registerConfigMethod, getTokenValue } from '../methods.js';
  * Registers a config method to generate output files for utility tokens.
  */
 registerConfigMethod((tokenSets, { sourcePath, buildPath }) => {
-  return Object.entries(tokenSets.output)
-    .filter(
-      ([name, { layer }]) =>
-        layer === TOKENSET_LAYERS.component && name === TOKENSET_NAMES.Utilities,
-    )
-    .map(([_name, { type, layer, filePath, sets }]) => {
-      return {
-        meta: {
-          type,
-          layer,
-          filePath,
-          setNames: Object.keys(sets),
-        },
-        source: [`${sourcePath}_temp/output/${filePath}`],
-        include: [`${sourcePath}_temp/source/**/*.json`],
-        platforms: {
-          utilities: {
-            transforms: ['name/kebab'],
-            buildPath,
-            files: [
-              {
-                destination: `_utilities-formatted.scss`,
-                format: 'swisspost/utility-format',
-                filter: 'swisspost/utility-filter',
-                options: {
-                  outputReferences: true,
-                },
-              },
-            ],
-          },
-        },
-      };
-    });
-});
+  const { type, layer, filePath, sets } = tokenSets.output[TOKENSET_NAMES.Utilities];
 
-/**
- * Filters only the component layer tokens defined in the utility tokensets.
- */
-StyleDictionary.registerFilter({
-  name: 'swisspost/utility-filter',
-  filter: token => {
-    return token.filePath.includes('/output/');
-  },
+  return {
+    meta: {
+      type,
+      layer,
+      filePath,
+      setNames: Object.keys(sets),
+    },
+    source: [`${sourcePath}_temp/output/${filePath}`],
+    include: [`${sourcePath}_temp/source/**/*.json`],
+    platforms: {
+      utilities: {
+        transforms: ['name/kebab'],
+        buildPath,
+        files: [
+          {
+            destination: `_utilities-formatted.scss`,
+            filter: 'swisspost/source-tokens-filter',
+            format: 'swisspost/utility-format',
+            options: {
+              outputReferences: true,
+            },
+          },
+        ],
+      },
+    },
+  };
 });
 
 /**
@@ -60,22 +45,23 @@ StyleDictionary.registerFormat({
   format: async ({ dictionary, options, file }) => {
     const header = await fileHeader({ file, commentStyle: 'short' });
 
-    const utilityTokensObject = {};
+    const utilityTokens = new Map();
 
     dictionary.allTokens.forEach(token => {
-      const [utilityName, utilityKey] = token.path.slice(token.path.indexOf(TOKENSET_PREFIX) + 2);
+      const { subitem, state } = token.attributes;
 
-      const previousUtilityValues = utilityTokensObject[utilityName] ?? [];
-      utilityTokensObject[utilityName] = [
-        ...previousUtilityValues,
-        `\n  ${utilityKey}: ${getTokenValue(options, token)},`,
-      ];
+      const previousStates = utilityTokens.get(subitem) ?? [];
+      const newState = `\n  ${state}: ${getTokenValue(options, token)},`;
+
+      utilityTokens.set(subitem, [...previousStates, newState]);
     });
 
-    const textOutputs = Object.entries(utilityTokensObject).map(([utilityName, utilityValues]) => {
-      return `$${utilityName}: (${utilityValues.join('')}\n);\n`;
+    const utilityMaps = Array.from(utilityTokens.entries()).map(([subitem, values]) => {
+      return `$${subitem}: (${values.join('')}\n);\n`;
     });
 
-    return header + textOutputs.join('\n');
+    console.log(header + utilityMaps.join('\n'));
+
+    return header + utilityMaps.join('\n');
   },
 });
