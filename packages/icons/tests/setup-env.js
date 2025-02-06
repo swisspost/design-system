cd923667ffb7ce7d962cb57f81165d09592ff1d1
Oliver Schürch <oliver.schuerch@post.ch>
2025-02-06 10:13:24 +0100

    test(icons): fix spec files

--- packages/icons/tests/setup-env.js
+++ packages/icons/tests/setup-env.js
@@ -1,3 +1,4 @@
 process.env.CEN_USERNAME = '<Your username>';
 process.env.CEN_PASSWORD = '<Your password>';
-process.env.CEN_URL = '<Censhare URL>';
+process.env.CEN_URL_ICONSET_POST = '<Censhare URL>';
+process.env.CEN_URL_ICONSET_UI = '<Censhare URL>';
