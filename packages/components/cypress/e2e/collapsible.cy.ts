1f081647e13248bb9d37d198eb311541706b3710
Alona Zherdetska <alionazhv@gmail.com>
2025-02-10 14:23:11 +0100

    fix: possible fix for the tests

--- packages/components/cypress/e2e/collapsible.cy.ts
+++ packages/components/cypress/e2e/collapsible.cy.ts
@@ -4,20 +4,11 @@ describe('collapsible', () => {
   describe('default', () => {
     beforeEach(() => {
       cy.getComponents(COLLAPSIBLE_ID, 'default', 'post-collapsible', 'post-collapsible-trigger');
-
-      // Ensure Cypress uses a valid ID selector
-      cy.get('@collapsible')
-        .invoke('attr', 'id')
-        .then((id) => {
-          const safeId = Cypress.$.escapeSelector(id);
-          cy.wrap(`#${safeId}`).as('collapsibleSafeSelector');
-        });
-
       cy.get('@collapsible-trigger').find('.btn').as('trigger');
     });
 
     it('should have a collapsible', () => {
-      cy.get('@collapsibleSafeSelector').should('exist');
+      cy.get('@collapsible').should('exist');
     });
 
     it('should have a trigger', () => {
@@ -25,27 +16,21 @@ describe('collapsible', () => {
     });
 
     it('should show the collapsible', () => {
-      cy.get('@collapsibleSafeSelector').should('be.visible');
+      cy.get('@collapsible').should(`be.visible`);
     });
 
     it('should set the correct ARIA attribute on the trigger', () => {
-      cy.get('@collapsibleSafeSelector')
+      cy.get('@collapsible')
         .invoke('attr', 'id')
-        .then((collapsibleId) => {
+        .then(collapsibleId => {
           cy.get('@trigger').should('have.attr', 'aria-controls', collapsibleId);
         });
-
       cy.get('@trigger').should('have.attr', 'aria-expanded', 'true');
     });
 
     it('should hide the collapsible after clicking on the trigger once', () => {
       cy.get('@trigger').click();
-      
-      // Ensure aria-expanded updates first
-      cy.get('@trigger').invoke('attr', 'aria-expanded').should('equal', 'false');
-
-      // Then check visibility
-      cy.get('@collapsibleSafeSelector').should('be.hidden');
+      cy.get('@collapsible').should(`be.hidden`);
     });
 
     it('should update the "aria-expanded" attribute after hiding the collapsible', () => {
@@ -55,12 +40,7 @@ describe('collapsible', () => {
 
     it('should show the collapsible after clicking on the trigger twice', () => {
       cy.get('@trigger').dblclick();
-      
-      // Ensure aria-expanded updates first
-      cy.get('@trigger').invoke('attr', 'aria-expanded').should('equal', 'true');
-
-      // Then check visibility
-      cy.get('@collapsibleSafeSelector').should('be.visible');
+      cy.get('@collapsible').should(`be.visible`);
     });
 
     it('should update the "aria-expanded" attribute after showing the collapsible', () => {
@@ -75,37 +55,23 @@ describe('collapsible', () => {
         COLLAPSIBLE_ID,
         'initially-collapsed',
         'post-collapsible',
-        'post-collapsible-trigger'
+        'post-collapsible-trigger',
       );
-
-      cy.get('@collapsible')
-        .invoke('attr', 'id')
-        .then((id) => {
-          const safeId = Cypress.$.escapeSelector(id);
-          cy.wrap(`#${safeId}`).as('collapsibleSafeSelector');
-        });
-
       cy.get('@collapsible-trigger').find('.btn').as('trigger');
     });
 
     it('should hide the collapsible', () => {
-      cy.get('@collapsibleSafeSelector').should('be.hidden');
+      cy.get('@collapsible').should(`be.hidden`);
     });
 
     it('should show the collapsible after clicking on the trigger once', () => {
       cy.get('@trigger').click();
-      
-      cy.get('@trigger').invoke('attr', 'aria-expanded').should('equal', 'true');
-
-      cy.get('@collapsibleSafeSelector').should('be.visible');
+      cy.get('@collapsible').should(`be.visible`);
     });
 
     it('should hide the collapsible after clicking on the trigger twice', () => {
       cy.get('@trigger').dblclick();
-      
-      cy.get('@trigger').invoke('attr', 'aria-expanded').should('equal', 'false');
-
-      cy.get('@collapsibleSafeSelector').should('be.hidden');
+      cy.get('@collapsible').should(`be.hidden`);
     });
   });
 });
