c3224cb5c04f6c03cb99dd3930316c83bed466e6
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-02-11 10:01:17 +0100

    update type again

--- packages/components/src/utils/property-checkers/check-one-of.ts
+++ packages/components/src/utils/property-checkers/check-one-of.ts
@@ -1,19 +1,18 @@
-export function checkOneOf<T extends { host: HTMLElement }, K extends keyof T>(
+export function checkOneOf<T extends { host: HTMLElement }>(
   component: T,
-  prop: K,
-  possibleValues: readonly T[K][],
+  prop: keyof T,
+  possibleValues: readonly unknown[],
   customMessage?: string,
 ) {
   const componentName = component.host.localName;
   const value = component[prop];
-
+  console.log(value);
   const defaultMessage = `The prop \`${String(
     prop,
   )}\` of the \`${componentName}\` component must be one of the following values: \`
     ${possibleValues.join(', ')} \`.`;
 
   const message = customMessage || defaultMessage;
-
   if (!possibleValues.includes(value)) {
     throw new Error(message);
   }
