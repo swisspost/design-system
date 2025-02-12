ae75b727f31f8e19187784714ad44935cc21e04a
leagrdv <lea.gardavaud@post.ch>
2025-02-12 16:05:17 +0100

    Revert "chore(components, styles): remove core tokens from scss files"
    This reverts commit a6bde4ddcfb5a7cf9158978df45815f8367babf0.

--- .changeset/fuzzy-files-lie.md
+++
@@ -1,7 +0,0 @@
----
-'@swisspost/design-system-documentation': patch
-'@swisspost/design-system-components': patch
-'@swisspost/design-system-styles': patch
----
-
-Removed core tokens usage in scss.
--- packages/documentation/src/stories/utilities/float/float.stories.ts
+++ packages/documentation/src/stories/utilities/float/float.stories.ts
@@ -3,6 +3,8 @@ import { html } from 'lit/static-html.js';
 import { MetaExtended } from '@root/types';
 import { nothing } from 'lit';
 
+ 
+
 const meta: MetaExtended = {
   id: 'ccf092c1-f0ab-49a1-a612-87e5be23adde',
   title: 'Utilities/Float',
@@ -62,11 +64,11 @@ export const Float: Story = {};
 export const Clearfix: Story = {
   render: () => {
     return html`
-      <div class="clearfix" style="background-color: #e1e0dc;">
+      <div class="clearfix" style="background-color: var(--post-core-color-sandgrey-012);">
         <button type="button" class="btn btn-primary float-start">Button 1</button>
         <button type="button" class="btn btn-primary float-end">Button 2</button>
       </div>
-      <div style="background-color: #e1e0dc;">
+      <div style="background-color: var(--post-core-color-sandgrey-012);">
         <button type="button" class="btn btn-primary float-start">Button 1</button>
         <button type="button" class="btn btn-primary float-end">Button 2</button>
       </div>
