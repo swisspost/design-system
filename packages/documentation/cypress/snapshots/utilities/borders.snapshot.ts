37a3c1dc432745410c271988db769a93d227b09a
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-02-13 12:18:15 +0100

    minor

--- packages/documentation/cypress/snapshots/utilities/borders.snapshot.ts
+++ packages/documentation/cypress/snapshots/utilities/borders.snapshot.ts
@@ -1,13 +1,13 @@
 describe('Borders', () => {
   it('borders', () => {
     cy.visit('/iframe.html?id=snapshots--borders');
-    cy.get('.borders-example', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Borders', { widths: [1440] });
+    cy.get('.border-example', { timeout: 30000 }).should('be.visible');
+    cy.percySnapshot('Borders', { widths: [320, 1440] });
   });
 
   it('rounded', () => {
     cy.visit('/iframe.html?id=snapshots--rounded');
-    cy.get('.borders-example', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Rounded', { widths: [1440] });
+    cy.get('.border-example', { timeout: 30000 }).should('be.visible');
+    cy.percySnapshot('Rounded', { widths: [320, 1440] });
   });
 });
