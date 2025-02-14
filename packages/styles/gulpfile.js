0f2b481cbbbae8e411b7853e7c744fa1250663bb
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-02-14 18:27:34 +0100

    error test

--- packages/styles/gulpfile.js
+++ packages/styles/gulpfile.js
@@ -165,7 +165,7 @@ gulp.task(
  */
 
 gulp.task('extract-component-names', done => {
-  const filePath = path.join(__dirname, '../components/dist/collection/index.js');
+  const filePath = path.join(__dirname, '../components/src/index.js');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
