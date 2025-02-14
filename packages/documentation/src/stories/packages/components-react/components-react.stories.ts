76b24b6bf4f67b7f485802b9a31f96a4244a7852
Lea <lea.gardavaud@post.ch>
2025-02-14 15:32:17 +0100

    Update packages/documentation/src/stories/packages/components-react/components-react.stories.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/documentation/src/stories/packages/components-react/components-react.stories.ts
+++ packages/documentation/src/stories/packages/components-react/components-react.stories.ts
@@ -4,9 +4,6 @@ import { MetaExtended } from '@root/types';
 const meta: MetaExtended = {
   id: '13b9c7f1-993d-4348-a3b7-a7ceb92fd5c7',
   title: 'Packages/Components for React',
-  parameters: {
-    badges: [],
-  },
 };
 
 export default meta;
