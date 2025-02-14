f5ba68cf328687a3878cdca79617de9cec5bb0ed
Oliver Schürch <oliver.schuerch@post.ch>
2025-02-14 12:44:58 +0100

    fix(components): make it possible to use multiple language switches in the post header (#4674)

--- packages/components/src/components/post-language-switch/switch-variants.ts
+++ packages/components/src/components/post-language-switch/switch-variants.ts
@@ -1,3 +1,6 @@
 export const SWITCH_VARIANTS = ['list', 'menu'] as const;
+export const SWITCH_TYPES = ['language', 'region'] as const;
 
 export type SwitchVariant = (typeof SWITCH_VARIANTS)[number];
+
+export type SwitchType = 'language' | 'region';
