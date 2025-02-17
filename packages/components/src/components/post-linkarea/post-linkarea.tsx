48424a26ea81d62607cd826a56ef488f994c6868
Alizé Debray <alize.debray@post.ch>
2025-02-17 14:10:05 +0100

    chore(component): stop using the window object in the post-linkarea

--- packages/components/src/components/post-linkarea/post-linkarea.tsx
+++ packages/components/src/components/post-linkarea/post-linkarea.tsx
@@ -1,30 +1,49 @@
 import { Component, Element, h, Host } from '@stencil/core';
 import { version } from '@root/package.json';
 
+const INTERACTIVE_ELEMENTS = ['a'].join(',');
+const INTERACTIVE_ELEMENTS_SELECTOR = `:where(${INTERACTIVE_ELEMENTS})`;
+
+type InteractiveElement = HTMLAnchorElement;
+
 @Component({
   tag: 'post-linkarea',
   styleUrl: 'post-linkarea.scss',
 })
 export class PostLinkarea {
-  @Element() hostElement: HTMLPostLinkareaElement;
+  @Element() host: HTMLPostLinkareaElement;
 
-  private handleClick = () => {
-    const customSlottedLink: HTMLAnchorElement = this.hostElement.querySelector('a[data-link]');
-
-    const linkElement: HTMLAnchorElement = customSlottedLink
-      ? customSlottedLink
-      : this.hostElement.querySelector('a');
-
-    if (linkElement.href) {
-      window.location.href = linkElement.href;
-    } else {
-      console.error('<post-linkarea> : Your element must contain a child with a `href` property.');
+  private delegateClick() {
+    // get any child with a data-link attribute and check that it is interactive
+    const elementWithDataLink = this.host.querySelector('[data-link]');
+    if (!this.isInteractive(elementWithDataLink)) {
+      throw new Error(
+        `The \`data-link\` attribute must be used on a interactive element inside the \`post-linkarea\` component. Possible elements are: ${INTERACTIVE_ELEMENTS}`,
+      );
     }
-  };
+
+    // if no element with a data-link attribute was found then try and find the first interactive element
+    const interactiveElement: InteractiveElement = elementWithDataLink
+      ? elementWithDataLink
+      : this.host.querySelector(INTERACTIVE_ELEMENTS_SELECTOR);
+
+    if (!interactiveElement) {
+      throw new Error(
+        `The \`post-linkarea\` component must contain an interactive element. Possible elements are: ${INTERACTIVE_ELEMENTS}`,
+      );
+    }
+
+    // delegate the click to the interactive element
+    interactiveElement.click();
+  }
+
+  private isInteractive(element: Element): element is InteractiveElement {
+    return element && !element.matches(INTERACTIVE_ELEMENTS_SELECTOR);
+  }
 
   render() {
     return (
-      <Host data-version={version} onClick={this.handleClick} tabindex="0">
+      <Host data-version={version} onClick={() => this.delegateClick()} tabindex="0">
         <slot></slot>
       </Host>
     );
