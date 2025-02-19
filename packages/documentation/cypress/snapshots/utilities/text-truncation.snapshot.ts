86a59fbbd4932eb7e8eae668fff1e3f9ac8b1cc0
Veyar <romain.veya@post.ch>
2025-02-19 09:16:36 +0100

    feat(style): removed mobile size regretion test

--- packages/documentation/cypress/snapshots/utilities/text-truncation.snapshot.ts
+++ packages/documentation/cypress/snapshots/utilities/text-truncation.snapshot.ts
@@ -2,6 +2,6 @@ describe('Text Truncation', () => {
   it('text truncation', () => {
     cy.visit('/iframe.html?id=snapshots--text-truncation');
     cy.get('.text-truncation-example', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Text Truncation', { widths: [320, 1440] });
+    cy.percySnapshot('Text Truncation', { widths: [1440] });
   });
 });
