1b88e62041b280d958d939e29f8504c634232287
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-02-21 10:21:31 +0100

    apply overflow to scrollParent

--- packages/components/src/components/post-header/post-header.tsx
+++ packages/components/src/components/post-header/post-header.tsx
@@ -57,7 +57,7 @@ export class PostHeader {
   // Clean up possible side effects when post-header is disconnected
   disconnectedCallback() {
     this.mobileMenuExtended = false;
-    document.body.style.overflow = '';
+    this.scrollParent.style.overflow = '';
     this.host.removeEventListener('keydown', e => {
       this.keyboardHandler(e);
     });
@@ -70,7 +70,7 @@ export class PostHeader {
 
   @Watch('mobileMenuExtended')
   frozeBody(isMobileMenuExtended: boolean) {
-    this.host.parentElement.style.overflow = isMobileMenuExtended ? 'hidden' : '';
+    this.scrollParent.style.overflow = isMobileMenuExtended ? 'hidden' : '';
 
     if (isMobileMenuExtended) {
       this.host.addEventListener('keydown', e => {
