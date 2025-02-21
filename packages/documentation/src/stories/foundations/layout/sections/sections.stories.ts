9e531a0c96e0ca42d3a181e5f5324c0513c32855
Oliver Schürch <oliver.schuerch@post.ch>
2025-02-21 10:20:45 +0100

    feat(styles): add bleeding classes

--- packages/documentation/src/stories/foundations/layout/sections/sections.stories.ts
+++ packages/documentation/src/stories/foundations/layout/sections/sections.stories.ts
@@ -33,3 +33,105 @@ export const Default: Story = {
     </section>
   `,
 };
+
+export const Bleed: Story = {
+  render: () => html`
+    <section class="section palette-brand">
+      <div class="container">
+        <img class="section-bleed" src="https://picsum.photos/id/20/1920/640" alt="" />
+        <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+        <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+
+        <div class="row">
+          <div class="col">
+            <img class="section-bleed" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <p>Text next to a <code>.section-bleed-start</code> image.</p>
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <p>Text next to a <code>.section-bleed-end</code> image.</p>
+          </div>
+          <div class="col">
+            <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col-3">
+            <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <p>
+              Text between <code>.section-bleed-start</code> and
+              <code>.section-bleed-end</code> images.
+            </p>
+          </div>
+          <div class="col-3">
+            <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <p>Works also with more columns.</p>
+          </div>
+          <div class="col">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius sit eligendi
+              nulla.
+            </p>
+          </div>
+          <div class="col">
+            <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col-8">
+            <img class="section-bleed-start" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <p>Works also with custom with columns.</p>
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="col-8">
+            <img class="section-bleed-end" src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col">
+            <p class="section-bleed-start">
+              The bleeing classes do not need to be applied on an image tag, you can let bleed out,
+              whatever block element you want.
+            </p>
+          </div>
+          <div class="col-4">
+            <img src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="w-100"></div>
+          <div class="col-4">
+            <img src="https://picsum.photos/id/20/1920/640" alt="" />
+          </div>
+          <div class="col">
+            <p class="section-bleed-end">
+              The bleeing classes do not need to be applied on an image tag, you can let bleed out,
+              whatever block element you want.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  `,
+};
