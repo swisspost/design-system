8364cd519ac4a3accbd3456864de771125c4159e
Alona Zherdetska <alionazhv@gmail.com>
2025-02-28 22:01:55 +0100

    updated tests

--- packages/components/src/components/post-tooltip-trigger/post-tooltip-trigger.tsx
+++ packages/components/src/components/post-tooltip-trigger/post-tooltip-trigger.tsx
@@ -31,7 +31,7 @@ export class PostTooltipTrigger {
     return ref && ref.localName === 'post-tooltip' ? (ref as HTMLPostTooltipElement) : null;
   }
 
-  connectedCallback() {
+  componentDidLoad() {
     this.host.setAttribute('data-version', version);
     this.setupTrigger();
     this.attachListeners();
