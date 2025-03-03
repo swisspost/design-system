69d7f8f9ef1d94b1b7fea08b3379e8c615c46ea4
Alona Zherdetska <alionazhv@gmail.com>
2025-03-03 15:05:22 +0100

    refactored eventGuard for `post-accordion-item`

--- packages/components/src/components/post-accordion-item/post-accordion-item.tsx
+++ packages/components/src/components/post-accordion-item/post-accordion-item.tsx
@@ -62,7 +62,7 @@ export class PostAccordionItem {
     eventGuard(
       this.host,
       event,
-      { targetLocalName: 'post-accordion-item' },
+      { targetLocalName: 'post-accordion-item', delegatorSelector: 'post-accordion-item' },
       () => {
         this.collapsed = !event.detail;
       }
