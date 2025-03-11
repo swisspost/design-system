0eb070d4afd16a95004c3fd2f79a0a331afac001
leagrdv <lea.gardavaud@post.ch>
2025-03-11 08:35:02 +0100

    chore(eslint): add rounded migration rule

--- packages/eslint/src/rules/html/migrations/index.ts
+++ packages/eslint/src/rules/html/migrations/index.ts
@@ -1,5 +1,7 @@
 import noDeprecatedBtnRgRule, { name as noDeprecatedBtnRgRuleName } from './no-deprecated-btn-rg';
+import noRoundedBordersRule, { name as noRoundedBordersRuleName } from './no-rounded-borders';
 
 export const htmlMigrationRules = {
   [noDeprecatedBtnRgRuleName]: noDeprecatedBtnRgRule,
+  [noRoundedBordersRuleName]: noRoundedBordersRule,
 };
