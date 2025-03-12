4f49e942fc685b399047f4d7d559d5c013825cc6
leagrdv <lea.gardavaud@post.ch>
2025-03-12 13:05:58 +0100

    chore(documentation): add beta disclaimer

---
+++ packages/documentation/.storybook/blocks/beta-banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div className="docs-beta">
+    <div className="container text-center py-16">
+      <p>
+        This is an early <span className="fw-bold">beta version</span> and is not final. For the
+        current version documentation, please visit <a href="https://design-system.post.ch/">this page</a>.
+      </p>
+    </div>
+  </div>
+);
