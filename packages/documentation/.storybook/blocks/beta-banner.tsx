ab2700235332b66346ad4048a5f32fa119432ecd
Lea <lea.gardavaud@post.ch>
2025-03-12 14:09:28 +0100

    Update packages/documentation/.storybook/blocks/beta-banner.tsx
    Co-authored-by: Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>

--- packages/documentation/.storybook/blocks/beta-banner.tsx
+++ packages/documentation/.storybook/blocks/beta-banner.tsx
@@ -4,8 +4,7 @@ export default () => (
   <div className="docs-beta">
     <div className="container text-center py-16">
       <p>
-        This is an early <span className="fw-bold">beta version</span> and is not final. For the
-        current version documentation, please visit <a href="https://design-system.post.ch/">this page</a>.
+        This is a <span className="fw-bold">pre-release version</span> and is not production ready. For new and ongoing projects, please refer to the <a href="https://design-system.post.ch/">latest Design System version</a>.
       </p>
     </div>
   </div>
