9915fac3dabad4e4f76bc27855ca47734fd0370d
Oliver Schürch <oliver.schuerch@post.ch>
2025-03-12 09:15:46 +0100

    chore(components): implement event binding as mentioned by stencil in post-footer

--- packages/components/src/components/post-footer/post-footer.tsx
+++ packages/components/src/components/post-footer/post-footer.tsx
@@ -27,12 +27,16 @@ export class PostFooter {
   @State() isMobile: boolean = breakpoint.get('name') === 'mobile';
 
   connectedCallback() {
-    window.addEventListener('postBreakpoint:name', this.breakpointChange.bind(this));
+    window.addEventListener('postBreakpoint:name', this.breakpointChange);
   }
 
-  private breakpointChange(e: CustomEvent) {
-    this.isMobile = e.detail === 'mobile';
+  disconnectedCallback() {
+    window.removeEventListener('postBreakpoint:name', this.breakpointChange);
   }
+
+  private readonly breakpointChange = (e: CustomEvent) => {
+    this.isMobile = e.detail === 'mobile';
+  };
 
   private renderAccordion() {
     return (
@@ -120,9 +124,5 @@ export class PostFooter {
         </footer>
       </Host>
     );
-  }
-
-  disconnectedCallback() {
-    window.removeEventListener('postBreakpoint:name', this.breakpointChange.bind(this));
   }
 }
