43d6259fa9a1865a6ff9cb0ff8bc2af69283e74f
Alona Zherdetska <alionazhv@gmail.com>
2025-03-21 10:28:49 +0100

    possible fix

--- packages/components/cypress.config.js
+++ packages/components/cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress');
-const { version } = require('../../package.json');
+import { version } from '@root/package.json';
 
 module.exports = defineConfig({
   e2e: {
