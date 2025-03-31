767c6e542ba6beea8ab97919ce4cd57d5c60920f
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-03-31 12:37:09 +0200

    add page height and remove padding

--- packages/components-react/src/react-demo-app/src/components/home.tsx
+++ packages/components-react/src/react-demo-app/src/components/home.tsx
@@ -5,14 +5,17 @@ const Home: React.FC = () => {
 <Link to="/other" slot="post-logo">
   <PostLogo>Homepage</PostLogo>
 </Link>`;
-  const css = `a:has(post-logo) -> height: 100%;
-`;
+  const css = `a:has(post-logo) -> height: 100%;`;
+  const css2 = `post-logo -> min-height:24px`;
+
   return (
     <>
       <h4>HTML</h4>
       <p>{linkCode}</p>
       <h4>SCSS</h4>
       <p>{css}</p>
+      <p>{css2}</p>
+      <p id="content"></p>
     </>
   );
 };
