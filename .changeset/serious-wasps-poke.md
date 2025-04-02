19dc9dce87f9b874b15104c488d9a2068b270ac3
Oliver Schürch <oliver.schuerch@post.ch>
2025-04-02 07:40:37 +0200

    feat(components): make post-collapsible-trigger a shadow component

---
+++ .changeset/serious-wasps-poke.md
@@ -0,0 +1,5 @@
+---
+'@swisspost/design-system-components': minor
+---
+
+Made `post-collapsible-trigger` a shadow component to avoid hydration errors.
--- packages/components/src/components/post-collapsible-trigger/post-collapsible-trigger.tsx
+++ packages/components/src/components/post-collapsible-trigger/post-collapsible-trigger.tsx
@@ -1,14 +1,14 @@
-import { Component, Element, Method, Prop, Watch } from '@stencil/core';
+import { Component, Element, h, Host, Method, Prop, Watch } from '@stencil/core';
 import { version } from '@root/package.json';
-import { checkNonEmpty, checkType, debounce, getRoot } from '@/utils';
-import { eventGuard } from '@/utils/event-guard';
+import { checkNonEmpty, checkType, debounce, eventGuard, getRoot } from '@/utils';
 
 @Component({
   tag: 'post-collapsible-trigger',
+  shadow: true,
 })
 export class PostCollapsibleTrigger {
   private trigger?: HTMLButtonElement;
-  private observer = new MutationObserver(() => this.setTrigger());
+  private readonly observer = new MutationObserver(() => this.setTrigger());
   private root?: Document | ShadowRoot;
 
   @Element() host: HTMLPostCollapsibleTriggerElement;
@@ -16,7 +16,7 @@ export class PostCollapsibleTrigger {
   /**
    * Link the trigger to a post-collapsible with this id
    */
-  @Prop() for: string;
+  @Prop({ reflect: true }) for: string;
 
   /**
    * Set the "aria-controls" and "aria-expanded" attributes on the trigger to match the state of the controlled post-collapsible
@@ -38,27 +38,11 @@ export class PostCollapsibleTrigger {
     this.observer.observe(this.host, { childList: true, subtree: true });
   }
 
-  /**
-   * Attach a "postToggle" event listener to the root node
-   * to update the trigger's "aria-expanded" attribute whenever the controlled post-collapsible is toggled
-   */
-  componentWillLoad() {
-    this.root = getRoot(this.host);
-  }
-
-  /**
-   * Add the "data-version" to the host element and set the trigger
-   */
   componentDidLoad() {
-    this.host.setAttribute('data-version', version);
     this.setTrigger();
-
     if (!this.trigger) console.warn('The post-collapsible-trigger must contain a button.');
   }
 
-  /**
-   * Disconnect the mutation observer
-   */
   disconnectedCallback() {
     this.observer.disconnect();
     this.root.removeEventListener('postToggle', this.handlePostToggle);
@@ -77,33 +61,30 @@ export class PostCollapsibleTrigger {
    * This updates the trigger's "aria-expanded" attribute based on the event detail.
    */
   private handlePostToggle(e: CustomEvent): void {
-    eventGuard(
-      this.host,
-      e, 
-      { targetLocalName: 'post-collapsible' },
-      () => {
-        if (this.trigger) {
-          this.trigger.setAttribute('aria-expanded', `${e.detail}`);
-        }
+    eventGuard(this.host, e, { targetLocalName: 'post-collapsible' }, () => {
+      if (this.trigger) {
+        this.trigger.setAttribute('aria-expanded', `${e.detail}`);
       }
-    );
+    });
   }
 
-  private debouncedUpdate = debounce(() => {
-    if (!this.trigger) return;
+  private debouncedUpdate() {
+    debounce(() => {
+      if (!this.trigger) return;
 
-    // add the provided id to the aria-controls list
-    const ariaControls = this.trigger.getAttribute('aria-controls');
-    if (!ariaControls?.includes(this.for)) {
-      const newAriaControls = ariaControls ? `${ariaControls} ${this.for}` : this.for;
-      this.trigger.setAttribute('aria-controls', newAriaControls);
-    }
+      // add the provided id to the aria-controls list
+      const ariaControls = this.trigger.getAttribute('aria-controls');
+      if (!ariaControls?.includes(this.for)) {
+        const newAriaControls = ariaControls ? `${ariaControls} ${this.for}` : this.for;
+        this.trigger.setAttribute('aria-controls', newAriaControls);
+      }
 
-    // set the aria-expanded to `false` if the controlled collapsible is collapsed or undefined, set it to `true` otherwise
-    const isCollapsed = this.collapsible?.collapsed;
-    const newAriaExpanded = isCollapsed !== undefined ? !isCollapsed : undefined;
-    this.trigger.setAttribute('aria-expanded', `${newAriaExpanded}`);
-  });
+      // set the aria-expanded to `false` if the controlled collapsible is collapsed or undefined, set it to `true` otherwise
+      const isCollapsed = this.collapsible?.collapsed;
+      const newAriaExpanded = isCollapsed !== undefined ? !isCollapsed : undefined;
+      this.trigger.setAttribute('aria-expanded', `${newAriaExpanded}`);
+    });
+  }
 
   /**
    * Toggle the post-collapsible controlled by the trigger
@@ -136,5 +117,13 @@ export class PostCollapsibleTrigger {
 
     this.trigger.addEventListener('click', () => this.toggleCollapsible());
     this.setAriaAttributes();
+  }
+
+  render() {
+    return (
+      <Host data-version={version}>
+        <slot></slot>
+      </Host>
+    );
   }
 }
