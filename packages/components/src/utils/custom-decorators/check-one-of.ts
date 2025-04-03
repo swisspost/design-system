537286dc41b804efef8bcad2291fb7da532198a2
alona <alionazhv@gmail.com>
2025-04-03 18:31:48 +0200

    refactored checkOneOf

--- packages/components/src/utils/custom-decorators/check-one-of.ts
+++ packages/components/src/utils/custom-decorators/check-one-of.ts
@@ -1,21 +1,33 @@
-export function CheckOneOf(possibleValues: any[], customMessage?: string) {
-    return function (target: any, propertyKey: string) {
-      const backingField = `__${propertyKey}`;
-      Object.defineProperty(target, propertyKey, {
-        get: function () {
-          return this[backingField];
-        },
-        set: function (newVal: any) {
-          const defaultMessage = `The property ${propertyKey} must be one of the following values: ${possibleValues.join(', ')}.`;
-          const message = customMessage || defaultMessage;
-          if (!possibleValues.includes(newVal)) {
-            throw new Error(message);
-          }
-          this[backingField] = newVal;
-        },
-        enumerable: true,
-        configurable: true,
-      });
+export function CheckOneOf<T extends { host: HTMLElement }>(
+  prop: keyof T,
+  possibleValues: readonly unknown[],
+  customMessage?: string,
+) {
+  return function (
+    _target: T,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: unknown[]) {
+      const componentName = this.host?.localName;
+      console.log(componentName);
+      const value = this[prop];
+      const defaultMessage = `The prop \`${String(
+        prop,
+      )}\` of the \`${componentName}\` component must be one of the following values: ${possibleValues.join(', ')}.`;
+      console.log(`[CheckOneOf] Validating ${String(prop)} =`, value);
+      const message = customMessage || defaultMessage;
+
+      if (!possibleValues.includes(value)) {
+        throw new Error(message);
+      }
+
+      // Run original method
+      return originalMethod.apply(this, args);
     };
-  }
-  
+
+    return descriptor;
+  };
+}
