33c2eedc524db99e80d01209ce4b6fa56b1eefb0
alona <alionazhv@gmail.com>
2025-04-07 19:08:04 +0200

    clean: removed visual-test folder form git

--- packages/styles/visual-tests/accordion.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('accordion default visual regression', async ({ page }) => {
-  await page.goto('http://localhost:9000/iframe.html?viewMode=story&id=4d1b4185-e04d-494a-ab38-2b56c1778b0b--default');
-
-  const accordion = page.locator('post-accordion').first();
-
-  await expect(accordion).toHaveScreenshot('accordion.png');
-});
