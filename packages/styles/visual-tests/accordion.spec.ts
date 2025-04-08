a6add103309dce528d3559295d03170c7d1ee81e
alona <alionazhv@gmail.com>
2025-04-08 12:13:00 +0200

    possible fix

--- packages/styles/visual-tests/accordion.spec.ts
+++ packages/styles/visual-tests/accordion.spec.ts
@@ -8,8 +8,8 @@ test('accordion default visual regression', async ({ page }) => {
   await page.waitForTimeout(1000);
 
   const accordion = page.locator('post-accordion').first();
+
   await expect(accordion).toBeVisible({ timeout: 10000 });
 
-
-  await expect(accordion).toHaveScreenshot('accordion-default.png');
+  await expect(accordion).toHaveScreenshot('accordion.png');
 });
