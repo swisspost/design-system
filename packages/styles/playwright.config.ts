b2629898ff7e9bbe761e2463b8446008f05893a1
alona <alionazhv@gmail.com>
2025-04-09 08:53:40 +0200

    feat(config): refactored the config file

--- packages/styles/playwright.config.ts
+++ packages/styles/playwright.config.ts
@@ -45,9 +45,5 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    }
   ]
 });
