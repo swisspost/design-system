0ce394c23773d07719f7cedda50700b7daf7a907
alona <alionazhv@gmail.com>
2025-04-09 08:43:08 +0200

    feat(config): refactored the config file

--- packages/styles/playwright.config.ts
+++ packages/styles/playwright.config.ts
@@ -1,30 +1,53 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 const baseURL = process.env.BASE_URL || 'http://localhost:9000';
+const isCI = !!process.env.CI;
 
 export default defineConfig({
+  timeout: 60 * 1000,
   testDir: './visual-tests',
   outputDir: './visual-tests/test-results',
   snapshotPathTemplate: '{testDir}/__screenshots__/{testFilePath}/{arg}{ext}',
-  workers: 4,
-  retries: 1,
+  workers: isCI ? 4 : 2,
+  forbidOnly: isCI,
+
   reporter: [
-    ['html', {
-      outputFolder: './visual-tests/playwright-report'
-    }],
-    ['list']
+    ['html', { outputFolder: './visual-tests/playwright-report' }],
+    isCI ? ['github'] : ['list']
   ],
+
   use: {
     baseURL,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
+
   expect: {
     toHaveScreenshot: {
       threshold: 0.1,
-      animations: 'disabled',
+      maxDiffPixelRatio: 0.025,
+      animations: 'disabled'
     },
+    toMatchSnapshot: {
+      threshold: 0.1,
+      maxDiffPixelRatio: 0.025,
+    }
   },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
 });
