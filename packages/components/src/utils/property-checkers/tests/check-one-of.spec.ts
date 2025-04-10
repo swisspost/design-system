5652e433f04140a1a5b944b32941a0cc2c730649
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-04-10 09:54:00 +0200

    tests update

--- packages/components/src/utils/property-checkers/tests/check-one-of.spec.ts
+++ packages/components/src/utils/property-checkers/tests/check-one-of.spec.ts
@@ -4,16 +4,22 @@ describe('checkOneOf', () => {
   const possibleValues = ['A', 'B', 'C', 'D'];
   const error = 'Is not one of.';
 
-  const runCheckForValue = (value: string) => () => {
+  const runCheckForValue = (value: string) => {
     const component = { host: { localName: 'post-component' } as HTMLElement, prop: value };
     checkOneOf(component, 'prop', possibleValues, error);
   };
 
-  it('should not throw an error if the value is one of the possible values', () => {
-    expect(runCheckForValue('A')).not.toThrow();
+  it('should not log an error if the value is one of the possible values', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    runCheckForValue('A');
+    expect(consoleErrorSpy).not.toHaveBeenCalledWith(expect.stringContaining(error));
+    consoleErrorSpy.mockRestore();
   });
 
-  it('should throw the provided error if the value is not one of the possible values', () => {
-    expect(runCheckForValue('E')).toThrow(error);
+  it('should log the provided error if the value is not one of the possible values', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    runCheckForValue('E');
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.stringContaining(error));
+    consoleErrorSpy.mockRestore();
   });
 });
