6dc9852bdceefdd6e5e128295a8093e370d3039b
Alona Zherdetska <alionazhv@gmail.com>
2025-04-10 12:21:43 +0200

    removed screenshot folder

--- packages/styles/playwright.config.ts
+++ packages/styles/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   timeout: 60 * 1000,
   testDir: './visual-tests',
   outputDir: './visual-tests/test-results',
-  snapshotPathTemplate: '{testDir}/__screenshots__/{testFilePath}/{arg}{ext}',
+  snapshotPathTemplate: '{testDir}/__screenshots__/{projectName}/{testFilePath}/{arg}{ext}',
   workers: isCI ? 4 : 2,
   forbidOnly: isCI,
 
@@ -19,18 +19,29 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
   },
 
   expect: {
     toHaveScreenshot: {
       threshold: 0.1,
       maxDiffPixelRatio: 0.03,
-      animations: 'disabled'
+      animations: 'disabled',
     },
     toMatchSnapshot: {
       threshold: 0.1,
       maxDiffPixelRatio: 0.03,
-    }
+    },
   },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
 });
