9134a298d9579a1f16773b06e8d481eb74912787
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-04-11 15:12:11 +0200

    revert comments

--- packages/documentation/src/stories/guidelines/header with custom logo link/code.sample.ts
+++ packages/documentation/src/stories/guidelines/header with custom logo link/code.sample.ts
@@ -3,7 +3,7 @@ export const firstStep = `
 <!-- Angular -->
 <post-logo>Homepage</post-logo>
 
-{/* React & Next.js */}
+<!-- React & Next.js -->
 <PostLogo>Homepage</PostLogo>
 `;
 
@@ -11,19 +11,19 @@ export const secondStep = `
 <!-- Angular -->
 <a routerLink="#"><post-logo>Homepage</post-logo></a>
 
-{/* React */}
+<!-- React -->
 <Link to="#"><PostLogo>Homepage</PostLogo></Link>
 
-{/* Next.js */}
+<!-- Next.js -->
 <Link href="#"><PostLogo>Homepage</PostLogo></Link>
 `;
 export const thirdStep = `
 <!-- Angular -->
 <a routerLink="#" slot="post-logo"><post-logo>Homepage</post-logo></a>
 
-{/* React */}
+<!-- React -->
 <Link to="#" slot="post-logo"><PostLogo>Homepage</PostLogo></Link>
 
-{/* Next.js */}
+<!-- Next.js -->
 <Link href="#" slot="post-logo"><PostLogo>Homepage</PostLogo></Link>
 `;
