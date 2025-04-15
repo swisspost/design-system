d6937d64c4a2c81af478bca87540da5cb9ef5fd8
Alizé Debray <alize.debray@post.ch>
2025-04-15 17:56:18 +0200

    update scripts

--- .github/workflows/scripts/icons.js
+++ .github/workflows/scripts/icons.js
@@ -1,76 +1,86 @@
 const fs = require('fs');
 const path = require('path');
 
-function getIcons(...fileSet) {
-  const icons = new Map();
-  fileSet
-    .flatMap(files => files.split(' '))
-    .filter(str => str !== '')
-    .forEach(file => {
-      const chunks = path.basename(file, path.extname(file)).split('_');
-      const icon = chunks[0];
-      const size = chunks[chunks.length - 1];
-      const variant = chunks.includes('Solid') ? 'solid' : 'line';
-
-      const details = icons.get(icon) || { sizes: new Set(), variants: new Set() };
-      details.sizes.add(size);
-      details.variants.add(variant);
-
-      icons.set(icon, details);
-    });
-
-  return Array.from(icons.entries())
-    .map(([icon, { sizes, variants }]) => {
-      const allVariants = Array.from(variants).sort().join(' & ');
-      const allSizes = Array.from(sizes)
-        .sort()
-        .join(', ')
-        .replace(/, ([^,]*)$/, ', and $1');
-      return `- \`${icon}\` (${allVariants}): ${allSizes}px`;
-    })
-    .join('\n');
-}
-
-function getUiIconChanges({
+function getIconChanges({
   ADDED_FILES,
   MODIFIED_FILES,
   RENAMED_FILES,
   COPIED_FILES,
   DELETED_FILES,
 }) {
+  const getIcons = (...fileSet) => {
+    const icons = new Map();
+    fileSet
+      .flatMap(files => files.split(' '))
+      .filter(str => str !== '')
+      .forEach(file => {
+        const chunks = path.basename(file, path.extname(file)).split('_');
+        const icon = chunks[0];
+        const size = chunks[chunks.length - 1];
+        const variant = chunks.includes('Solid') ? 'solid' : 'line';
+
+        const details = icons.get(icon) || { sizes: new Set(), variants: new Set() };
+        details.sizes.add(size);
+        details.variants.add(variant);
+
+        icons.set(icon, details);
+      });
+
+    return Array.from(icons.entries())
+      .map(([icon, { sizes, variants }]) => {
+        const allVariants = Array.from(variants).sort().join(' & ');
+        const allSizes = Array.from(sizes)
+          .sort()
+          .join(', ')
+          .replace(/, ([^,]*)$/, ', and $1');
+        return `- \`${icon}\` (${allVariants}): ${allSizes}px`;
+      })
+      .join('\n');
+  };
+
   return {
-    deleted_icons: getIcons(DELETED_FILES),
-    added_icons: getIcons(ADDED_FILES),
-    updated_icons: getIcons(MODIFIED_FILES, RENAMED_FILES, COPIED_FILES),
+    major: `Deleted icons:\n\n${getIcons(DELETED_FILES)}`,
+    minor: `Added icons:\n\n${getIcons(ADDED_FILES)}`,
+    patch: `Added icons:\n\n${getIcons(MODIFIED_FILES, RENAMED_FILES, COPIED_FILES)}`,
   };
 }
 
-function writeChangeset(title, icons, bump) {
+function writeChangesets({ DATE, ICON_CHANGES }) {
+  const iconChanges = JSON.parse(ICON_CHANGES);
+
+  const writeChangeset = (icons, bump) => {
+    try {
+      fs.writeFileSync(
+        `./.changeset/${DATE}-${bump}-ui-icon-update.md`,
+        `---\n'@swisspost/design-system-icons': ${bump}\n---\n\n${icons}`,
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  Object.entries(iconChanges).forEach(([bump, icons]) => {
+    if (icons) writeChangeset(icons, bump);
+  });
+}
+
+function writePrBody({ ICON_CHANGES }) {
+  const iconChanges = JSON.parse(ICON_CHANGES);
+
+  let content = '# Design system icons are now up to date!';
+  Object.values(iconChanges).forEach(icons => {
+    if (icons) content += `\n\n##${icons}`;
+  });
+
   try {
-    const content = `---\n'@swisspost/design-system-icons': ${bump}\n---\n\n${title}:\n\n${icons}`;
-    fs.writeFileSync(`./.changeset/${DATE}-${bump}-ui-icon-update.md`, content);
+    fs.writeFileSync(`./pr-body.md`, content);
   } catch (err) {
     console.error(err);
   }
 }
 
-function writeChangesets({ DATE, ICON_CHANGES }) {
-  const { deleted_icons, added_icons, updated_icons } = JSON.parse(ICON_CHANGES);
-
-  if (deleted_icons) {
-    writeChangeset('Deleted icons', deleted_icons, 'major');
-  }
-
-  if (added_icons) {
-    writeChangeset('Added icons', added_icons, 'minor');
-  }
-
-  if (updated_icons) {
-    writeChangeset('Updated icons', updated_icons, 'patch');
-  }
-}
-
 module.exports = {
-  getUiIconChanges,
+  getIconChanges,
   writeChangesets,
+  writePrBody,
 };
