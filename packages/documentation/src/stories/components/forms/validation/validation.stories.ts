8a3d9228c9c590fd2269618b31b056dbaaff8d2f
Alona Zherdetska <alionazhv@gmail.com>
2025-04-23 17:54:19 +0200

    chore(clean): removed redundant code

--- packages/documentation/src/stories/components/forms/validation/validation.stories.ts
+++ packages/documentation/src/stories/components/forms/validation/validation.stories.ts
@@ -91,9 +91,10 @@ export const Checkbox: Story = {
         aria-invalid=${props.ariaInvalid}
         aria-describedby="${props.ariaDescribedBy}"
       />
-      <label class="form-check-label" for="Checkbox_1${props.validationState}${props.scheme}"
-        >Label</label
-      >
+      <label class="form-check-label" for="Checkbox_1${props.validationState}${props.scheme}">
+        Label
+      </label>
+      <span class="validation-icon"></span>
       ${feedbackTemplate}
     </div>`;
   },
@@ -110,15 +111,21 @@ export const Input: Story = {
     const component = context.name.replace(/\s+/g, '');
     const props = getValidationProps(component, args);
     const feedbackTemplate = renderFeedback(props.validFeedbackId, props.invalidFeedbackId);
-    return html`
-    <div class="form-floating">
-      <input type="text" placeholder="Placeholder" id="Input_1is-invalid" class="form-control form-control-lg is-invalid" aria-invalid="true" aria-describedby="is-invalid-id-" autocomplete="email" />
-      <label class="form-label" for="Input_1is-invalid">Label</label>
+    return html`<div class="form-floating">
+      <input
+        id="Input_1${props.validationState}${props.scheme}"
+        class="form-control form-control-lg ${props.isValidationSet ? props.validationState : ''}"
+        aria-invalid=${props.ariaInvalid}
+        aria-describedby="${props.ariaDescribedBy}"
+        type="text"
+        placeholder="Placeholder"
+      />
+      <label class="form-label" for="Input_1${props.validationState}${props.scheme}">Label</label>
       <span class="validation-icon"></span>
-      <p class="valid-feedback">Valid message.</p>
-      <p class="invalid-feedback" id="is-invalid-id-">Invalid message.</p>
+      ${feedbackTemplate}
       <p class="form-hint">
-      Hintus textus elare volare cantare hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.
+        Hintus textus elare volare cantare hendrerit in vulputate velit esse molestie consequat, vel
+        illum dolore eu feugiat nulla facilisis.
       </p>
     </div> `;
   },
