b596540a1b3a08fc282813f946592671312bed99
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-05-30 09:17:28 +0200

    type object-deepmerge

--- packages/tokens/src/_build/utils/object-deepmerge.ts
+++ packages/tokens/src/_build/utils/object-deepmerge.ts
@@ -1,9 +1,16 @@
-export default function deepmerge(obj1 = {}, obj2 = {}) {
+type PlainObject = { [key: string]: unknown };
+
+// Checks if object is of type PlainObject
+function isPlainObject(value: unknown): value is PlainObject {
+  return typeof value === 'object' && value !== null && !Array.isArray(value);
+}
+
+export default function deepmerge(obj1: PlainObject = {}, obj2: PlainObject = {}) {
   const result = { ...obj1 };
 
   for (let key in obj2) {
     result[key] =
-      obj2[key] instanceof Object && obj1[key] instanceof Object
+      isPlainObject(obj2[key]) && isPlainObject(obj1[key])
         ? deepmerge(obj1[key], obj2[key])
         : obj2[key];
   }
