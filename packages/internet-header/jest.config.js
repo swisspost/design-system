486708986b2460c0af5dcb9c5e8e60182a5f52ed
Alizé Debray <alize.debray@post.ch>
2025-06-18 11:13:19 +0200

    chore: remove internet header package

--- packages/internet-header/jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/src/tests/setup-tests.ts'],
-};
