90916c574092739109439582a60c138145e9e64f
Alizé Debray <alize.debray@post.ch>
2025-06-26 19:08:57 +0200

    Try and solve issue with viewport header tests

--- packages/components/cypress/e2e/header.cy.ts
+++ packages/components/cypress/e2e/header.cy.ts
@@ -1,49 +1,46 @@
 const HEADER_ID = '27a2e64d-55ba-492d-ab79-5f7c5e818498';
 const TEST_FILES = ['post-header', 'post-header-without-title'];
+const VIEWPORTS: Record<string, Cypress.ViewportPreset>= {
+  desktop: 'macbook-15',
+  tablet: 'ipad-2',
+  mobile: 'iphone-6',
+};
 
 describe('header', () => {
-  TEST_FILES.forEach(testFile => {
-    describe(testFile.replaceAll('-', ' '), { baseUrl: null }, () => {
-      beforeEach(() => {
-        cy.visit(`./cypress/fixtures/${testFile}.test.html`);
+  function getContentTop() {
+    return cy.get('main').then($main => Math.round($main.position().top));
+  }
 
-        cy.get('post-header').as('header');
+  function checkLayoutShift() {
+    cy.get('@header').should('exist');
+
+    // get the content position before the header is visible
+    let initialContentTop: number;
+    getContentTop().then(pos => {
+      initialContentTop = pos;
+      cy.get('@header').should('not.be.visible');
+    });
+
+    // check the content position did not change when the header is visible
+    cy.get('@header')
+      .should('be.visible')
+      .then(() => {
+        getContentTop().should('eq', initialContentTop);
       });
+  }
 
-      function getContentTop() {
-        return cy.get('main').then($main => Math.round($main.position().top));
-      }
-
-      function checkLayoutShift() {
-        cy.get('@header').should('exist');
-
-        // get the content position before the header is visible
-        let initialContentTop: number;
-        getContentTop().then(pos => {
-          initialContentTop = pos;
-          cy.get('@header').should('not.be.visible');
+  TEST_FILES.forEach(testFile => {
+    Object.entries(VIEWPORTS).forEach(([viewportName, viewportPreset]) => {
+      describe(testFile.replaceAll('-', ' '), { baseUrl: null }, () => {
+        beforeEach(() => {
+          cy.viewport(viewportPreset);
+          cy.visit(`./cypress/fixtures/${testFile}.test.html`);
+          cy.get('post-header').as('header');
         });
 
-        // check the content position did not change when the header is visible
-        cy.get('@header')
-          .should('be.visible')
-          .then(() => {
-            getContentTop().should('eq', initialContentTop);
-          });
-      }
-
-      it('should not shift layout on desktop', () => {
-        checkLayoutShift();
-      });
-
-      it('should not shift layout on tablet', () => {
-        cy.viewport('ipad-2');
-        checkLayoutShift();
-      });
-
-      it('should not shift layout on mobile', () => {
-        cy.viewport('iphone-6');
-        checkLayoutShift();
+        it(`should not shift layout on ${viewportName}`, () => {
+          checkLayoutShift();
+        });
       });
     });
   });
