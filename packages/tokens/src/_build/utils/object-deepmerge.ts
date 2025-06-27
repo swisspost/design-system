00ff30dd885966ad110b0e6260736d896a40f783
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-06-27 17:25:38 +0200

    update object-deepmerge.ts

--- packages/tokens/src/_build/utils/object-deepmerge.ts
+++ packages/tokens/src/_build/utils/object-deepmerge.ts
@@ -1,25 +1,27 @@
-type DeepMerged<T, U> = T extends object
-  ? U extends object
-    ? {
-        [K in keyof T | keyof U]: K extends keyof T
-          ? K extends keyof U
-            ? DeepMerged<T[K], U[K]>
-            : T[K]
-          : K extends keyof U
-          ? U[K]
-          : never;
-      }
-    : T & U
-  : T & U;
+type DeepMerged<T, U> = {
+  [K in keyof T | keyof U]: K extends keyof U
+    ? K extends keyof T
+      ? T[K] extends object
+        ? U[K] extends object
+          ? DeepMerged<T[K], U[K]>
+          : U[K]
+        : U[K]
+      : U[K]
+    : K extends keyof T
+    ? T[K]
+    : never;
+};
 
-export function deepmerge<T extends object, U extends object>(obj1: T, obj2: U): DeepMerged<T, U> {
-  const result = { ...obj1 } as DeepMerged<T, U>;
+export default function deepmerge<T extends object, U extends object>(
+  obj1: T,
+  obj2: U,
+): DeepMerged<T, U> {
+  const result = { ...obj1 } as Record<string, any>;
 
   for (const key in obj2) {
     if (Object.hasOwn(obj2, key)) {
-      const k = key as keyof (T | U);
-      const val1 = obj1[k as keyof T];
-      const val2 = obj2[k as keyof U];
+      const val1 = (obj1 as Record<string, any>)[key];
+      const val2 = obj2[key as keyof U];
 
       const shouldMerge =
         typeof val1 === 'object' &&
@@ -29,11 +31,13 @@ export function deepmerge<T extends object, U extends object>(obj1: T, obj2: U):
         !Array.isArray(val1) &&
         !Array.isArray(val2);
 
-      result[k as keyof DeepMerged<T, U>] = shouldMerge
-        ? (deepmerge(val1, val2) as DeepMerged<T, U>[typeof k])
-        : (val2 as DeepMerged<T, U>[typeof k]);
+      if (shouldMerge) {
+        result[key] = deepmerge(val1 as object, val2 as object);
+      } else {
+        result[key] = val2;
+      }
     }
   }
 
-  return result;
+  return result as DeepMerged<T, U>;
 }
