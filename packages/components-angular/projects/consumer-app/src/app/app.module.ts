1f1c0e58f72a3189067fa0a9ecea8cd86cb8ad70
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-06-30 17:34:04 +0200

    revert angular testing changes

--- packages/components-angular/projects/consumer-app/src/app/app.module.ts
+++ packages/components-angular/projects/consumer-app/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { PostComponentsModule } from '@swisspost/design-system-components-angular';
-import { TableModule } from 'primeng/table';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
 import { CardControlComponent } from './routes/card-control/card-control.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
@@ -18,9 +17,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     PostComponentsModule,
     CardControlComponent,
-    TableModule,
-    BrowserModule,
-    BrowserAnimationsModule,
   ],
   declarations: [AppComponent, HomeComponent],
   providers: [],
