2041450bb502b00aceea193087e0765a2bbe265f
alionazherdetska <alionazhv@gmail.com>
2025-07-14 12:05:03 +0200

    Revert "chore(docs): reorder story action buttons and add corresponding icons (#5598)"
    This reverts commit 2abf54a0b32779102ecc94adbbf37e92003975d8.

--- packages/documentation/.storybook/preview.ts
+++ packages/documentation/.storybook/preview.ts
@@ -83,7 +83,7 @@ const preview: Preview = {
             onClick: openFullScreenDemo,
           },
           {
-            title: 'Copy deep link',
+            title: 'Copy link',
             onClick: copyStoryConfigUrl,
           },
           {
