9a5a73fae361fad6b399f850afc29fc87dfa044c
alionazherdetska <alionazhv@gmail.com>
2025-07-31 10:17:54 +0200

    fix(components): reverted the changes to `breakpoint.ts` file

--- packages/components/src/utils/breakpoints.ts
+++ packages/components/src/utils/breakpoints.ts
@@ -1,68 +1,72 @@
-import { throttle } from 'throttle-debounce';
-import { IS_SERVER } from '@/utils/environment';
+import { IS_BROWSER } from '@/utils/environment';
 
-interface BreakpointDefinition {
+type MapItem = {
+  minWidth: number;
   key: string;
   device: string;
-  minWidth: number;
-}
+};
+type ListenerType = 'key' | 'device';
 
-type BreakpointProperty = keyof BreakpointDefinition;
-
-class Breakpoint {
-  private readonly breakpoints: BreakpointDefinition[];
-  private currentBreakpoint: BreakpointDefinition;
+export class Breakpoint {
+  private readonly breakpointMap: MapItem[];
+  private readonly current = {
+    key: '',
+    device: '',
+  };
 
   constructor() {
-    if (IS_SERVER || !!this.breakpoints) return;
+    if (IS_BROWSER && !this.breakpointMap) {
+      const keys = this.getStyles('--post-grid-breakpoint-keys');
+      const devices = this.getStyles('--post-grid-breakpoint-names');
+      const widths = this.getStyles('--post-grid-breakpoint-widths');
 
-    const keys = this.getValues('--post-grid-breakpoint-keys');
-    const devices = this.getValues('--post-grid-breakpoint-devices');
-    const widths = this.getValues('--post-grid-breakpoint-widths');
+      this.breakpointMap = widths
+        .map((width, i) => ({
+          minWidth: Number(width),
+          key: keys[i],
+          device: devices[i],
+        }))
+        .reverse();
 
-    this.breakpoints = widths
-      .map((width, i): BreakpointDefinition => ({
-        key: keys[i],
-        device: devices[i],
-        minWidth: Number(width),
-      }))
-      .sort((a, b) => b.minWidth - a.minWidth);
-
-    this.updateCurrentBreakpoint({ emitEvents: false });
-    window.addEventListener('resize', () => this.updateCurrentBreakpoint(), { passive: true });
-  }
-
-  private getValues(cssCustomProperty: string): string[] {
-    const values = getComputedStyle(document.documentElement).getPropertyValue(cssCustomProperty);
-    return values ? values.split(',').map(v => v.trim()) : [];
-  }
-
-  private updateCurrentBreakpoint = throttle(50,
-    (options: { emitEvents: boolean } = { emitEvents: true }) => {
-      const previousBreakpoint = this.currentBreakpoint;
-      this.currentBreakpoint = this.breakpoints.find(breakpoint => {
-        return breakpoint.minWidth <= innerWidth;
-      });
-
-      if (!this.currentBreakpoint || !options.emitEvents) return;
-
-      Object.keys(this.currentBreakpoint)
-        .filter(key => !previousBreakpoint || this.currentBreakpoint[key] !== previousBreakpoint[key])
-        .forEach((key: BreakpointProperty) => this.dispatchEvent(key));
+      window.addEventListener('resize', () => this.updateHandler(), { passive: true });
     }
-  );
+  }
 
-  private dispatchEvent(property: BreakpointProperty): void {
-    if (IS_SERVER) return;
-
-    window.dispatchEvent(
-      new CustomEvent(`postBreakpoint:${property}`, { detail: this.currentBreakpoint[property] }),
+  private getStyles(propertyName: string) {
+    const styles = getComputedStyle(document.documentElement);
+    return (
+      styles
+        .getPropertyValue(propertyName)
+        ?.split(',')
+        .map(w => w.trim()) ?? []
     );
   }
 
-  public get<T extends BreakpointProperty>(property: T): BreakpointDefinition[T] {
-    this.updateCurrentBreakpoint({ emitEvents: false });
-    return this.currentBreakpoint[property];
+  private updateHandler(emitEvents: boolean = true) {
+    const calculated = this.breakpointMap.find(({ minWidth }) => innerWidth >= minWidth);
+
+    if (this.current.key !== calculated.key) {
+      this.current.key = calculated.key;
+      if (emitEvents) this.dispatchEvent('key');
+    }
+
+    if (this.current.device !== calculated.device) {
+      this.current.device = calculated.device;
+      if (emitEvents) this.dispatchEvent('device');
+    }
+  }
+
+  private dispatchEvent(type: ListenerType) {
+    if (IS_BROWSER) {
+      window.dispatchEvent(
+        new CustomEvent(`postBreakpoint:${type}`, { detail: this.current[type] }),
+      );
+    }
+  }
+
+  public get(type: ListenerType) {
+    this.updateHandler(false);
+    return this.current[type];
   }
 }
 
