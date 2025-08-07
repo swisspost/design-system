009576e1cd1e188a70de98ebb224d40afb5a6cca
leagrdv <lea.gardavaud@post.ch>
2025-08-07 13:11:13 +0200

    add custom event `posttriggered` and updated documentation

--- packages/documentation/src/stories/components/tooltip/dynamic-tooltip-data.sample.ts
+++ packages/documentation/src/stories/components/tooltip/dynamic-tooltip-data.sample.ts
@@ -1,20 +1,19 @@
-const items = [
+items: Array<MyItem> = [
   {
     id: 1,
     title: 'Shipment A',
     shippingInfo: 'Sent and in transit, expected soon',
-    quantity: 5,
   },
   {
     id: 2,
     title: 'Shipment B',
     shippingInfo: 'Preparing for shipment',
-    quantity: 2,
   },
   {
     id: 3,
     title: 'Shipment C',
     shippingInfo: 'Delivered yesterday',
-    quantity: 8,
   },
 ];
+
+currentItem: MyItem;
