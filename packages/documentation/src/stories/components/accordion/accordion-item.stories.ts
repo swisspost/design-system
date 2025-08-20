55b3872040e8cf4770beb53c0cd66d17d3387a33
alionazherdetska <alionazhv@gmail.com>
2025-08-20 18:38:05 +0200

    exposed the part

--- packages/documentation/src/stories/components/accordion/accordion-item.stories.ts
+++ packages/documentation/src/stories/components/accordion/accordion-item.stories.ts
@@ -4,13 +4,6 @@ const meta: Meta = {
   id: '3b86bc9b-3dcd-4788-a436-88fd18a6312d',
   title: 'Components/Accordion Item',
   component: 'post-accordion-item',
-  parameters: {
-    controls: {
-      exclude: [
-        'accordion-item', // this CSS part is for internal use only
-      ],
-    },
-  },
   argTypes: {
     collapsed: {
       control: false, // disable the control since it is not usable on the story
