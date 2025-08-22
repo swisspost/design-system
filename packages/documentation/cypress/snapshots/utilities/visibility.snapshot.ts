4a193b4a0635f4ce41f8d621d8c6517e75b4c7e6
Machado da Silva Hugo <hugomslv@users.noreply.github.com>
2025-08-22 07:47:40 +0200

    fix bug

--- packages/documentation/cypress/snapshots/utilities/visibility.snapshot.ts
+++ packages/documentation/cypress/snapshots/utilities/visibility.snapshot.ts
@@ -1,5 +1,5 @@
 describe('Visibility', () => {
-  it('z-index', () => {
+  it('visibility', () => {
     cy.visit('/iframe.html?id=snapshots--visibility');
     cy.get('.container-examples', { timeout: 30000 }).should('be.visible');
     cy.percySnapshot('Visibility', { widths: [1440] });
