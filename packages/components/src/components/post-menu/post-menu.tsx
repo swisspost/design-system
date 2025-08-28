eb8db3e114271f4e440f1d03d3cf6f462e88dfba
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-08-28 16:09:05 +0200

    removed aria-label again as obsolete

--- packages/components/src/components/post-menu/post-menu.tsx
+++ packages/components/src/components/post-menu/post-menu.tsx
@@ -39,12 +39,6 @@ export class PostMenu {
   @Element() host: HTMLPostMenuElement;
 
   /**
-   * The caption describes the purpose of the menu.
-   * It is used as the content for the <post-menu> aria-label for accessibility.
-   **/
-  @Prop() caption?: string;
-
-  /**
    * Defines the position of the menu relative to its trigger.
    * Menus are automatically flipped to the opposite side if there is not enough available space and are shifted towards the viewport if they would overlap edge boundaries.
    * For supported values and behavior details, see the [Floating UI placement documentation](https://floating-ui.com/docs/computePosition#placement).
@@ -153,38 +147,38 @@ export class PostMenu {
 
   @EventFrom('post-popovercontainer')
   private handlePostToggle = (event: CustomEvent<boolean>) => {
-      this.isVisible = event.detail;
-      this.toggleMenu.emit(this.isVisible);
+    this.isVisible = event.detail;
+    this.toggleMenu.emit(this.isVisible);
 
-      requestAnimationFrame(() => {
-        if (this.isVisible) {
-          this.lastFocusedElement = this.root?.activeElement as HTMLElement;
+    requestAnimationFrame(() => {
+      if (this.isVisible) {
+        this.lastFocusedElement = this.root?.activeElement as HTMLElement;
 
-          const menuItems = this.getSlottedItems();
-          this.focusableChildren = menuItems;
+        const menuItems = this.getSlottedItems();
+        this.focusableChildren = menuItems;
 
-          if (!this.popovercontainerRendered) {
-            // Only for the first time that the popovercontainer is rendered
-            if (menuItems.length > 0) {
-              // Add role="menu" and "aria-label" to the popovercontainer
-              this.host.setAttribute('role', 'menu');
-              this.host.setAttribute('aria-label', this.caption);
+        if (!this.popovercontainerRendered) {
+          // Only for the first time that the popovercontainer is rendered
+          if (menuItems.length > 0) {
+            // Add role="menu" to the popovercontainer
+            this.host.setAttribute('role', 'menu');
 
-              // Add role="menuitem" to the focusable elements
-              menuItems.forEach(item => {
-                item.setAttribute('role', 'menuitem');
-              });
-              this.popovercontainerRendered = true;
-            }
+            // Add role="menuitem" to the focusable elements
+            menuItems.forEach(item => {
+              item.setAttribute('role', 'menuitem');
+            });
+            this.popovercontainerRendered = true;
           }
-          (menuItems[0] as HTMLElement).focus();
-        } else if (this.lastFocusedElement) {
-          setTimeout(() => { // This timeout is added for NVDA to announce the menu as collapsed
-            this.lastFocusedElement.focus();
-          }, 0);
         }
-      });
-    };
+        (menuItems[0] as HTMLElement).focus();
+      } else if (this.lastFocusedElement) {
+        setTimeout(() => {
+          // This timeout is added for NVDA to announce the menu as collapsed
+          this.lastFocusedElement.focus();
+        }, 0);
+      }
+    });
+  };
 
   private handleClick = (e: MouseEvent) => {
     const target = e.target as HTMLElement;
