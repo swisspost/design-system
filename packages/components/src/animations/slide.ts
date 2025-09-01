1d385bc68e8df96f0c0a9e19f45922e98e7f6745
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-09-01 13:37:29 +0200

    update animation

--- packages/components/src/animations/slide.ts
+++ packages/components/src/animations/slide.ts
@@ -2,7 +2,7 @@ const easing: string = 'ease';
 const duration: number = 500;
 const fill: FillMode = 'forwards';
 
-export function slideUp(el: HTMLElement, translateSize: string = '-200%'): Animation {
+export function slideUp(el: HTMLElement, translateSize: string = '-100%'): Animation {
   return el.animate(
     [
       { transform: 'translateY(0)' }, // Starting position (no translation)
