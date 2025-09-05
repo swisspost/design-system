ebf602862add854f3b145367d8d36b5e604f7b7c
Oliver Schürch <oli@skaos.ch>
2025-09-05 10:13:19 +0200

    chore(eslint): export new rules in rules/html/migrations

--- packages/eslint/src/rules/html/migrations/index.ts
+++ packages/eslint/src/rules/html/migrations/index.ts
@@ -2,8 +2,14 @@ import noDeprecatedBtnRgRule, { name as noDeprecatedBtnRgRuleName } from './no-d
 import noUnnumberedBorderRadiusRule, {
   name as noUnnumberedBorderRadiusRuleName,
 } from './no-unnumbered-border-radius';
+import noClassHClearfixRule, { name as noClassHClearfixRuleName } from './no-class-h-clearfix';
+import noClassHVisuallyhiddenRule, {
+  name as noClassHVisuallyhiddenRuleName,
+} from './no-class-h-visuallyhidden';
 
 export const htmlMigrationRules = {
   [noDeprecatedBtnRgRuleName]: noDeprecatedBtnRgRule,
   [noUnnumberedBorderRadiusRuleName]: noUnnumberedBorderRadiusRule,
+  [noClassHClearfixRuleName]: noClassHClearfixRule,
+  [noClassHVisuallyhiddenRuleName]: noClassHVisuallyhiddenRule,
 };
