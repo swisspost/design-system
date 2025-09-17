bff2071982261eaa8de57934a251f02c91e6fe97
Alizé Debray <33580481+alizedebray@users.noreply.github.com>
2025-09-17 11:04:01 +0200

    chore(eslint): propose common rule creator (#6146)
    This PR implements a rule common rule creator for all rules that aim to
    replace a class with another.
    The `createClassUpdateRule` could be used with simple properties: 
    ```ts
    {
      name: string; // name of the rule 
      description: string; // description of the rule 
      messages: Record<string, string>;  // error messages as { messageId: 'message text' }
      mutations: Record<string, [string] | [string, string]>; // actual class updates as { messageId: ['oldClass', 'newClass'] }
      type?: 'suggestion' | 'layout' | 'problem'; // rule type (defaults to 'problem')
    }
    ```
    
    **Advantages:**
    - Simpler development for the rule
    - Easy to update all rule at once
    
    **Disadvantages:**
    - Less flexible rules 
    
    **Vote:**
    🎉 to use the `createClassUpdateRule` creator
    🚀 to right individual rules

--- packages/eslint/src/rules/html/migrations/no-deprecated-btn-rg.ts
+++ packages/eslint/src/rules/html/migrations/no-deprecated-btn-rg.ts
@@ -1,43 +1,15 @@
-import { createRule } from '../../../utils/create-rule';
-import { HtmlNode } from '../../../parsers/html/html-node';
+import { createClassUpdateRule } from '../../../utils/create-class-update-rule';
 
 export const name = 'no-deprecated-btn-rg';
 
-// Type: RuleModule<"uppercase", ...>
-export default createRule({
+export default createClassUpdateRule({
   name,
-  meta: {
-    docs: {
-      dir: 'html',
-      description:
-        'Flags deprecated "btn-rg" class and suggests removal or replacement with "btn-sm".',
-    },
-    messages: {
-      deprecatedBtnRg:
-        'The "btn-rg" class is deprecated. Please remove it or replace it with "btn-sm".',
-    },
-    type: 'suggestion',
-    fixable: 'code',
-    schema: [],
+  type:'suggestion',
+  description: 'Flags deprecated "btn-rg" class and suggests removal or replacement with "btn-sm".',
+  messages: {
+    deprecatedBtnRg: 'The "btn-rg" class is deprecated. Please remove it or replace it with "btn-sm".',
   },
-  defaultOptions: [],
-  create(context) {
-    return {
-      tag(node: HtmlNode) {
-        if (node.name && ['button', 'input', 'a'].includes(node.name)) {
-          const $node = node.toCheerio();
-          if ($node.hasClass('btn-rg')) {
-            context.report({
-              messageId: 'deprecatedBtnRg',
-              loc: node.loc,
-              fix(fixer) {
-                const fixedNode = $node.removeClass('btn-rg').addClass('btn-sm');
-                return fixer.replaceTextRange(node.range, fixedNode.toString());
-              },
-            });
-          }
-        }
-      },
-    };
+  mutations: {
+    deprecatedBtnRg: ['btn-rg', 'btn-sm']
   },
 });
