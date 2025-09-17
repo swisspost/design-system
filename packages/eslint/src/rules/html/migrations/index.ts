8922499b6e8ec59dd928015670b8522c3065cd4a
Lea <lea.gardavaud@post.ch>
2025-09-17 14:43:57 +0200

    feat(eslint): add automigration rules to update spacing utilities (#6219)
    ## 📄 Description
    
    Add auto migration rules for the spacing utility classes.
    Migration is done in two phases to avoid errors due to classes that are
    named the same before and after the migration (e.g. deprecated class
    `mt-4` which was equal to `margin-top: 24px` and new class `mt-4` which
    is equal to `margin-top: 4px`)
    
    ---------
    
    Co-authored-by: Alizé Debray <alize.debray@post.ch>

--- packages/eslint/src/rules/html/migrations/index.ts
+++ packages/eslint/src/rules/html/migrations/index.ts
@@ -3,6 +3,12 @@ import noDeprecatedLoaderRule, { name as noDeprecatedLoaderRuleName } from './no
 import noUnnumberedBorderRadiusRule, {
   name as noUnnumberedBorderRadiusRuleName,
 } from './no-unnumbered-border-radius';
+import {
+  rulePhase1 as noDeprecatedSpacingUtilitiesRulePhase1,
+  rulePhase2 as noDeprecatedSpacingUtilitiesRulePhase2,
+  namePhase1 as noDeprecatedSpacingUtilitiesRulePhase1Name,
+  namePhase2 as noDeprecatedSpacingUtilitiesRulePhase2Name,
+} from './no-deprecated-spacing-utilities';
 import noFormTextRule, { name as noFormTextRuleName } from './no-form-text';
 import noDeprecatedFontWeightRule, {
   name as noDeprecatedFontWeightRuleName,
@@ -12,6 +18,8 @@ export const htmlMigrationRules = {
   [noDeprecatedBtnRgRuleName]: noDeprecatedBtnRgRule,
   [noDeprecatedLoaderRuleName]: noDeprecatedLoaderRule,
   [noUnnumberedBorderRadiusRuleName]: noUnnumberedBorderRadiusRule,
+  [noDeprecatedSpacingUtilitiesRulePhase1Name]: noDeprecatedSpacingUtilitiesRulePhase1,
+  [noDeprecatedSpacingUtilitiesRulePhase2Name]: noDeprecatedSpacingUtilitiesRulePhase2,
   [noFormTextRuleName]: noFormTextRule,
   [noDeprecatedFontWeightRuleName]: noDeprecatedFontWeightRule,
 };
