847e2be3562b8a70a157162e59ab7221accd7419
Oliver Schürch <oliver.schuerch@post.ch>
2025-09-18 16:06:05 +0200

    Update packages/components/prebuild.ts
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/components/prebuild.ts
+++ packages/components/prebuild.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { format } from 'prettier';
 
-interface IComponentNameOutputOptions {
+interface ComponentNameOutputOptions {
   type: 'json' | 'scss';
   template: string;
   lineSeparator: string;
