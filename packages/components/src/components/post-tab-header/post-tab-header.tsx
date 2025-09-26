4e3399af1ec442104d638730acefc71f9617e979
alionazherdetska <alionazhv@gmail.com>
2025-09-26 17:57:53 +0200

    test

--- packages/components/src/components/post-tab-header/post-tab-header.tsx
+++ packages/components/src/components/post-tab-header/post-tab-header.tsx
@@ -1,49 +1,28 @@
-import { Component, Element, h, Host, Prop, State, Watch } from '@stencil/core';
-import { version } from '@root/package.json';
-import { checkRequiredAndType } from '@/utils';
+import { Component, h, Host, Element, Prop, State } from '@stencil/core';
 import { nanoid } from 'nanoid';
-
-/**
- * @slot default - Slot for the content of the tab header.
- */
 
 @Component({
   tag: 'post-tab-header',
   styleUrl: 'post-tab-header.scss',
   shadow: true,
 })
+
 export class PostTabHeader {
   @Element() host: HTMLPostTabHeaderElement;
-
+  @Prop() panel!: string;
   @State() tabId: string;
 
-  /**
-   * The name of the panel controlled by the tab header.
-   */
-  @Prop({ reflect: true }) readonly panel!: string;
-
-  @Watch('panel')
-  validateFor() {
-    checkRequiredAndType(this, 'panel', 'string');
-  }
-
   componentWillLoad() {
-    this.tabId = `tab-${this.host.id || nanoid(6)}`;
+    this.tabId = `tab-${nanoid(6)}`
   }
 
   render() {
     return (
-      <Host
-        id={this.tabId}
-        role="tab"
-        data-version={version}
-        aria-selected="false"
-        tabindex="-1"
-        class="tab-title"
-        slot="tabs"
-      >
+      <Host slot="tabs" role="tab" aria-selected="false" tab-index="-1">
         <slot />
       </Host>
-    );
+    )
   }
 }
+
+
--- packages/components/src/components/post-tab-panel/post-tab-panel.tsx
+++ packages/components/src/components/post-tab-panel/post-tab-panel.tsx
@@ -1,42 +1,26 @@
-import { Component, Element, h, Host, Prop, State, Watch } from '@stencil/core';
-import { version } from '@root/package.json';
+import { Component, h, Host, Element, Prop, State } from '@stencil/core';
 import { nanoid } from 'nanoid';
-import { checkRequiredAndType } from '@/utils';
 
-/**
- * @slot default - Slot for placing the content of the tab panel.
- */
-
-@Component({
+@Component ({
   tag: 'post-tab-panel',
   styleUrl: 'post-tab-panel.scss',
   shadow: true,
 })
+
 export class PostTabPanel {
   @Element() host: HTMLPostTabPanelElement;
+  @Prop() name!: string;
+  @State() panelID: string;
 
-  @State() panelId: string;
-
-  /**
-   * The name of the panel, used to associate it with a tab header.
-   */
-  @Prop({ reflect: true }) readonly name!: string;
-
-  @Watch('name')
-  validateName() {
-    checkRequiredAndType(this, 'name', 'string');
-  }
   componentWillLoad() {
-    this.validateName();
-    // get the id set on the host element or use a random id by default
-    this.panelId = `panel-${this.host.id || nanoid(6)}`;
+    this.panelID = `panel-${nanoid(6)}`
   }
 
   render() {
     return (
-      <Host data-version={version} id={this.panelId} role="tabpanel">
+      <Host role="tabpanel" id={this.panelID}>
         <slot />
       </Host>
-    );
+    )
   }
 }
