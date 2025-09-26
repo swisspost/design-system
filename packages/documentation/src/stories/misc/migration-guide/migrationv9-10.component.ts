f8ef7d2aad02ec889fcf56985a6d158df40d6c23
Lea <lea.gardavaud@post.ch>
2025-09-26 14:17:49 +0200

    feat(eslint): automigration rules for the loader (#6249)
    ## 📄 Description
    
    Updated automigration for the loader to add the renaming from `loader`
    to `spinner`
    
    ---------
    
    Co-authored-by: Alizé Debray <alize.debray@post.ch>

--- packages/documentation/src/stories/misc/migration-guide/migrationv9-10.component.ts
+++ packages/documentation/src/stories/misc/migration-guide/migrationv9-10.component.ts
@@ -598,6 +598,7 @@ export class MigrationV99Component extends LitElement {
 
                 <li class="mb-16">
                   <p>
+                    <span data-info="automigration" class="tag tag-sm tag-info">🪄 migration rule</span>
                     The following spinner classes have been renamed
                   </p>
                   <ul>
