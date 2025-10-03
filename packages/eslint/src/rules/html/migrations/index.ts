1189ad5cc9b35dd7b4986b1b784a19ba937801ac
Lea <lea.gardavaud@post.ch>
2025-10-03 17:12:27 +0200

    feat(eslint): automigration rules for breakpoint classes (#6309)
    ## 📄 Description
    
    Migration rules for the breakpoint utilities
    
    ---------
    
    Co-authored-by: Alizé Debray <alize.debray@post.ch>

--- packages/eslint/src/rules/html/migrations/index.ts
+++ packages/eslint/src/rules/html/migrations/index.ts
@@ -27,7 +27,16 @@ import noDeprecatedChipFilter, {
   name as noDeprecatedChipFilterName,
 } from './no-deprecated-chip-filter';
 
+import {
+  rulePhase1 as noDeprecatedBreakpointsRulePhase1,
+  rulePhase2 as noDeprecatedBreakpointsRulePhase2,
+  namePhase1 as noDeprecatedBreakpointsRulePhase1Name,
+  namePhase2 as noDeprecatedBreakpointsRulePhase2Name,
+} from './no-deprecated-breakpoints';
+
 export const htmlMigrationRules = {
+  [noDeprecatedBreakpointsRulePhase1Name]: noDeprecatedBreakpointsRulePhase1,
+  [noDeprecatedBreakpointsRulePhase2Name]: noDeprecatedBreakpointsRulePhase2,
   [noDeprecatedBtnRgRuleName]: noDeprecatedBtnRgRule,
   [noDeprecatedLoaderRuleName]: noDeprecatedLoaderRule,
   [noUnnumberedBorderRadiusRuleName]: noUnnumberedBorderRadiusRule,
