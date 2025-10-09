3faedfc03c7b38dd1d6d69b6b613617b0ccc005b
Nattaya Buck <166115007+bucknatt@users.noreply.github.com>
2025-10-09 11:22:11 +0200

    chore(docs): moved the "Highlighted Text" documentation page to the component section and rename it "Text Highlighted"

--- packages/documentation/src/stories/components/text-highlighted/text-highlighted.stories.ts
+++ packages/documentation/src/stories/components/text-highlighted/text-highlighted.stories.ts
@@ -4,7 +4,7 @@ import { MetaExtended } from '@root/types';
 
 const meta: MetaExtended = {
   id: '88318ccf-be8d-4bdf-b8dc-06b0b78e5e3d',
-  title: 'Foundations/Typography/Highlighted Text',
+  title: 'Components/Text Highlighted',
   tags: ['package:Styles'],
   parameters: {
     badges: [],
@@ -22,7 +22,9 @@ type Story = StoryObj;
 export const Default: Story = {
   render: () => html`
     <p class="text-highlighted">
-      Use this component when you need to emphasize important information or make a section more noticeable to readers. The highlighted style helps the content break away from the normal text flow.
+      Use this component when you need to emphasize important information or make a section more
+      noticeable to readers. The highlighted style helps the content break away from the normal text
+      flow.
     </p>
   `,
 };
