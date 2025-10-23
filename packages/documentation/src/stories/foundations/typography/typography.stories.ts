be3899b81dd4939b3a0f61ea533a5f6b26f08246
Nattaya Buck <166115007+bucknatt@users.noreply.github.com>
2025-10-23 09:33:45 +0200

    chore(docs): renamed the 'Typography/Overview' page to simply be 'Typography'

--- packages/documentation/src/stories/foundations/typography/typography.stories.ts
+++ packages/documentation/src/stories/foundations/typography/typography.stories.ts
@@ -4,7 +4,7 @@ import { html, unsafeStatic } from 'lit/static-html.js';
 
 const meta: MetaExtended = {
   id: '677cfabf-dbf0-4de2-ad07-6d5bfb9e2375',
-  title: 'Foundations/Typography/Overview',
+  title: 'Foundations/Typography',
   tags: ['package:Styles'],
   parameters: {
     badges: [],
