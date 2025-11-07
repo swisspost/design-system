421cb802910e94a7ed8d47352a9ff2dc63494e5e
alionazherdetska <alionazhv@gmail.com>
2025-11-07 15:24:45 +0100

    fix linting

--- packages/components/src/components/post-menu/post-menu.tsx
+++ packages/components/src/components/post-menu/post-menu.tsx
@@ -93,7 +93,7 @@ export class PostMenu {
 
   componentDidLoad() {
     // Temporary workaround: defer validation to avoid timing issues with parent components.
-      this.validatePlacement();
+    this.validatePlacement();
   }
 
   /**
