e2b6e8328905abdf14edb007c3b45435fc4a90d1
Nattaya Buck <166115007+bucknatt@users.noreply.github.com>
2025-11-18 11:21:17 +0100

    fix(docs): removed `div`-tag for all stories on typography page

--- packages/documentation/src/stories/foundations/typography/typography.stories.ts
+++ packages/documentation/src/stories/foundations/typography/typography.stories.ts
@@ -19,113 +19,101 @@ export const Default: Story = {};
 
 export const Link: Story = {
   render: () => html`
-    <div>
-      <a href="post.ch">Example Link</a>
+    <a href="post.ch">Example Link</a>
 
-      <p>
-        This paragraph contains an inline
-        <a href="post.ch">clickable</a>
-        that demonstrates how hyperlinks will appear within body text.
-      </p>
-    </div>
+    <p>
+      This paragraph contains an inline
+      <a href="post.ch">clickable</a>
+      that demonstrates how hyperlinks will appear within body text.
+    </p>
   `,
 };
 
 export const Paragraph: Story = {
   render: () => html`
-    <div>
-      <p>
-        This is a sample paragraph showing how text will appear in your application. It demonstrates
-        the font style, size, and spacing that will be used throughout your content.
-      </p>
+    <p>
+      This is a sample paragraph showing how text will appear in your application. It demonstrates
+      the font style, size, and spacing that will be used throughout your content.
+    </p>
 
-      <p>
-        Another paragraph follows to illustrate how multiple paragraphs will look. Notice the
-        spacing between paragraphs and how the text flows naturally from one line to the next.
-      </p>
+    <p>
+      Another paragraph follows to illustrate how multiple paragraphs will look. Notice the spacing
+      between paragraphs and how the text flows naturally from one line to the next.
+    </p>
 
-      <p class="lead">
-        This is a lead paragraph that stands out from regular text, typically used for introductory
-        content.
-      </p>
-    </div>
+    <p class="lead">
+      This is a lead paragraph that stands out from regular text, typically used for introductory
+      content.
+    </p>
   `,
 };
 
 export const Legend: Story = {
   render: () => html`
-    <div>
-      <fieldset>
-        <legend>Default legend</legend>
-      </fieldset>
+    <fieldset>
+      <legend>Default legend</legend>
+    </fieldset>
 
-      <fieldset>
-        <legend class="large">Default legend</legend>
-      </fieldset>
-    </div>
+    <fieldset>
+      <legend class="large">Default legend</legend>
+    </fieldset>
   `,
 };
 
 export const Heading: Story = {
   render: () => html`
-    <div>
-      <h1>
-        <span class="d-block">h1 Heading</span>
-        <span class="fw-normal">h1 Subheading</span>
-      </h1>
+    <h1>
+      <span class="d-block">h1 Heading</span>
+      <span class="fw-normal">h1 Subheading</span>
+    </h1>
 
-      <h2>
-        <span class="d-block">h2 Heading</span>
-        <span class="fw-normal">h2 Subheading</span>
-      </h2>
+    <h2>
+      <span class="d-block">h2 Heading</span>
+      <span class="fw-normal">h2 Subheading</span>
+    </h2>
 
-      <h3>
-        <span class="d-block">h3 Heading</span>
-        <span class="fw-normal">h3 Subheading</span>
-      </h3>
+    <h3>
+      <span class="d-block">h3 Heading</span>
+      <span class="fw-normal">h3 Subheading</span>
+    </h3>
 
-      <h4>
-        <span class="d-block">h4 Heading</span>
-        <span class="fw-normal">h4 Subheading</span>
-      </h4>
+    <h4>
+      <span class="d-block">h4 Heading</span>
+      <span class="fw-normal">h4 Subheading</span>
+    </h4>
 
-      <h5>
-        <span class="d-block">h5 Heading</span>
-        <span class="fw-normal">h5 Subheading</span>
-      </h5>
+    <h5>
+      <span class="d-block">h5 Heading</span>
+      <span class="fw-normal">h5 Subheading</span>
+    </h5>
 
-      <h6>
-        <span class="d-block">h6 Heading</span>
-        <span class="fw-normal">h6 Subheading</span>
-      </h6>
-    </div>
+    <h6>
+      <span class="d-block">h6 Heading</span>
+      <span class="fw-normal">h6 Subheading</span>
+    </h6>
   `,
 };
 
 export const Inline: Story = {
   render: () => html`
-    <div>
-      <p>This text contains <small>small inline text</small> for fine print or disclaimers.</p>
+    <p>This text contains <small>small inline text</small> for fine print or disclaimers.</p>
 
-      <p>This text shows <strong>strong emphasis</strong> for important words or phrases.</p>
+    <p>This text shows <strong>strong emphasis</strong> for important words or phrases.</p>
 
-      <p>This text shows <em>emphasis through italics</em> for subtle highlighting.</p>
+    <p>This text shows <em>emphasis through italics</em> for subtle highlighting.</p>
 
-      <p>This text contains <sub>subscript text</sub> for chemical formulas or footnotes.</p>
+    <p>This text contains <sub>subscript text</sub> for chemical formulas or footnotes.</p>
 
-      <p>This text contains <sup>superscript text</sup> for exponents or references.</p>
+    <p>This text contains <sup>superscript text</sup> for exponents or references.</p>
 
-      <p>This text <mark>highlights important information</mark> for visual scanning.</p>
+    <p>This text <mark>highlights important information</mark> for visual scanning.</p>
 
-      <p>
-        This text contains an <abbr title="abbreviation">abbr</abbr> element for shortened forms.
-      </p>
+    <p>This text contains an <abbr title="abbreviation">abbr</abbr> element for shortened forms.</p>
 
-      <p>This text shows <code>inline code examples</code> for technical documentation.</p>
+    <p>This text shows <code>inline code examples</code> for technical documentation.</p>
 
-      <p>Press <kbd>Ctrl</kbd> + <kbd>C</kbd> to copy text to clipboard.</p>
+    <p>Press <kbd>Ctrl</kbd> + <kbd>C</kbd> to copy text to clipboard.</p>
 
-      <p>This shows <del>deleted text</del> for tracking changes or corrections.</p>
-    </div>
+    <p>This shows <del>deleted text</del> for tracking changes or corrections.</p>
   `,
 };
