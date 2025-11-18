d4b109b73c18446f39000f02a302dc3e0ae85b0b
alionazherdetska <alionazhv@gmail.com>
2025-11-18 18:09:49 +0100

    try

--- packages/nextjs-integration/playwright.config.ts
+++ packages/nextjs-integration/playwright.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  /* Number of workers. */
+  workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
