8f1bd159b1f572b10430f6d43b78e665cdcd3adf
Nattaya Buck <nattaya.buck@post.ch>
2025-11-19 13:49:05 +0100

    chore(docs): added 2e2 cy test for snapshot stories of `Typography`

---
+++ packages/documentation/cypress/e2e/components/typography.cy.ts
@@ -0,0 +1,24 @@
+describe('Typography', () => {
+  describe('Accessibility', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?id=snapshots--typography');
+      cy.get('h1', { timeout: 30000 }).should('be.visible');
+      cy.get('a', { timeout: 30000 }).should('be.visible');
+      cy.get('p', { timeout: 30000 }).should('be.visible');
+      cy.get('legend', { timeout: 30000 }).should('be.visible');
+      // Test Inline Elements
+      cy.get('small', { timeout: 30000 }).should('be.visible');
+      cy.get('strong', { timeout: 30000 }).should('be.visible');
+      cy.get('sub', { timeout: 30000 }).should('be.visible');
+      cy.get('mark', { timeout: 30000 }).should('be.visible');
+      cy.get('abbr', { timeout: 30000 }).should('be.visible');
+      cy.get('code', { timeout: 30000 }).should('be.visible');
+      cy.get('kbd', { timeout: 30000 }).should('be.visible');
+      cy.injectAxe();
+    });
+
+    it('Has no detectable a11y violations on load for all variants', () => {
+      cy.checkA11y('#root-inner');
+    });
+  });
+});
