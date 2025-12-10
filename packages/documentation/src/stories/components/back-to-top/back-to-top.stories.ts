83689a42f8499e8354f495c603039a1aea2f69cf
Alona Zherdetska <138328641+alionazherdetska@users.noreply.github.com>
2025-12-10 12:22:04 +0100

    chore(components): rename `post-header` slots to improve clarity and consistency (#6780)
    ## 📄 Description
    
    This PR renames slots in the `post-header` component to improve clarity
    and consistency with design specifications. The changes align slot names
    with their semantic purpose.
    
    ## 🚀 Demo
    
    [Header
    docs](https://preview-6780--swisspost-design-system-next.netlify.app/?path=/docs/introduction--docs&devModeEnabled=true)
    
    ---
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [ ] No design review needed
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes

--- packages/documentation/src/stories/components/back-to-top/back-to-top.stories.ts
+++ packages/documentation/src/stories/components/back-to-top/back-to-top.stories.ts
@@ -22,7 +22,7 @@ const meta: MetaComponent = {
         <post-logo slot="post-logo" url="/">Homepage</post-logo>
 
         <!-- Meta navigation -->
-        <ul slot="meta-navigation">
+        <ul slot="global-nav-secondary">
           <li><a href="">Jobs</a></li>
           <li><a href="">Über uns</a></li>
         </ul>
@@ -40,7 +40,7 @@ const meta: MetaComponent = {
           description="The currently selected language is English."
           variant="list"
           name="language-menu-example"
-          slot="post-language-switch"
+          slot="language-menu"
         >
           <post-language-menu-item active="false" code="de" name="Deutsch">de</post-language-menu-item>
           <post-language-menu-item active="false" code="fr" name="French">fr</post-language-menu-item>
@@ -68,7 +68,7 @@ const meta: MetaComponent = {
         </ul>
 
         <!-- Main navigation -->
-        <post-mainnavigation slot="post-mainnavigation" caption="Hauptnavigation">
+        <post-mainnavigation slot="main-nav" caption="Hauptnavigation">
           <ul>
             <!-- Link only level 1 -->
             <li><a href="/briefe">Briefe</a></li>
