cfc32d62769a71d6822f623e51a9f5e8e3304e33
alionazherdetska <alionazhv@gmail.com>
2025-12-12 16:37:10 +0100

    autogenerated docs

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -308,6 +308,9 @@ export namespace Components {
         "url"?: string | URL;
     }
     interface PostMainnavigation {
+        /**
+          * Defines the accessible label for the navigation element. This text is used as the `aria-label` attribute to provide screen reader users with a description of the navigation's purpose.
+         */
         "caption": string;
     }
     interface PostMegadropdown {
@@ -1327,6 +1330,9 @@ declare namespace LocalJSX {
         "url"?: string | URL;
     }
     interface PostMainnavigation {
+        /**
+          * Defines the accessible label for the navigation element. This text is used as the `aria-label` attribute to provide screen reader users with a description of the navigation's purpose.
+         */
         "caption": string;
     }
     interface PostMegadropdown {
