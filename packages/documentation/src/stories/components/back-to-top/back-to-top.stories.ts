25a1b8fc2941ae2042c1332e70605c8c685448fc
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2025-12-23 17:54:38 +0100

    markup update

--- packages/documentation/src/stories/components/back-to-top/back-to-top.stories.ts
+++ packages/documentation/src/stories/components/back-to-top/back-to-top.stories.ts
@@ -34,7 +34,7 @@ const meta: MetaComponent = {
         <!-- Language menu -->
         <post-language-menu
           text-change-language="Change the language"
-          text-current-language="The currently selected language is English."
+          text-current-language="The currently selected language is #name."
           variant="list"
           name="language-menu-example"
           slot="language-menu"
