d947353c43b6e34df86bbb0cf9433dd48f15f105
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-01-05 14:24:15 +0100

    fix unit error

--- packages/eslint/test/rules/html/migrations/no-deprecated-css-classes.spec.ts
+++ packages/eslint/test/rules/html/migrations/no-deprecated-css-classes.spec.ts
@@ -13,26 +13,24 @@ htmlRuleTester.run(name, rule, {
       code: '<div>Click me</div>',
     },
   ],
-  invalid: [
-    classesMap.flatMap(classMap => [
-      // Only the deprecated class
-      {
-        code: `<div class="${classMap.old}"></div>`,
-        output: `<div></div>`,
-        errors: [{ messageId: classMap.old }],
-      },
-      // Deprecated class with other classes
-      {
-        code: `<div class="${classMap.old} extra-class"></div>`,
-        output: `<div class="extra-class"></div>`,
-        errors: [{ messageId: classMap.old }],
-      },
-      // Deprecated class in middle of other classes
-      {
-        code: `<div class="foo ${classMap.old} bar"></div>`,
-        output: `<div class="foo bar"></div>`,
-        errors: [{ messageId: classMap.old }],
-      },
-    ]),
-  ],
+  invalid: classesMap.flatMap(classMap => [
+    // Only the deprecated class
+    {
+      code: `<div class="${classMap.old}"></div>`,
+      output: `<div></div>`,
+      errors: [{ messageId: classMap.old }],
+    },
+    // Deprecated class with other classes
+    {
+      code: `<div class="${classMap.old} extra-class"></div>`,
+      output: `<div class="extra-class"></div>`,
+      errors: [{ messageId: classMap.old }],
+    },
+    // Deprecated class in middle of other classes
+    {
+      code: `<div class="foo ${classMap.old} bar"></div>`,
+      output: `<div class="foo bar"></div>`,
+      errors: [{ messageId: classMap.old }],
+    },
+  ]),
 });
