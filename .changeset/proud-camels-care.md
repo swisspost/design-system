5c7b2297429d698b9569da4aa24ab38ee4f763ff
Alizé Debray <alize.debray@post.ch>
2026-01-05 15:50:06 +0100

    feat(components): update post-closebutton

---
+++ .changeset/proud-camels-care.md
@@ -0,0 +1,6 @@
+---
+'@swisspost/design-system-components': patch
+---
+
+Updated the styles of the `post-closebutton` component.
+The close button is now intended to be absolutely positioned in the top-right corner of the element it closes.
--- packages/documentation/src/stories/components/button-close/button-close.stories.ts
+++ packages/documentation/src/stories/components/button-close/button-close.stories.ts
@@ -1,12 +1,14 @@
-import type { Args, StoryObj } from '@storybook/web-components-vite';
-import { html } from 'lit/static-html.js';
+import type { Args, StoryContext, StoryFn, StoryObj } from '@storybook/web-components-vite';
+import { html, nothing } from 'lit';
 import { MetaComponent } from '@root/types';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import './button-close.styles.scss';
 
 const meta: MetaComponent = {
   id: 'de313349-0c0b-4baf-adc6-cb8c2e36fc1a',
   title: 'Components/Button Close',
   component: 'post-closebutton',
+  render: getCloseButtonRenderer(),
   tags: ['package:WebComponents', 'status:Experimental'],
   parameters: {
     badges: [],
@@ -16,7 +18,7 @@ const meta: MetaComponent = {
     },
   },
   args: {
-    'slots-default': 'Close button',
+    'slots-default': 'Close',
     'buttonType': 'button',
   },
   argTypes: {
@@ -31,14 +33,32 @@ const meta: MetaComponent = {
 
 export default meta;
 
+function getCloseButtonRenderer(extraClasses?: string) {
+  return (args: Args) => html`
+    <post-closebutton
+      button-type=${args.buttonType !== 'button' ? args.buttonType : nothing}
+      class=${extraClasses ?? nothing}
+    >
+      ${unsafeHTML(args['slots-default'])}
+    </post-closebutton>
+  `;
+}
+
 type Story = StoryObj;
 
 export const Default: Story = {
+  decorators: [
+    (story: StoryFn, context: StoryContext) => {
+      return html`<div class="mock-element">${story(context.args, context)}</div>`;
+    },
+  ],
+};
+
+export const Positioning: Story = {
   render: (args: Args) => {
-    return args.buttonType !== 'button'
-      ? html`<post-closebutton button-type=${args.buttonType}
-          >${unsafeHTML(args['slots-default'])}</post-closebutton
-        > `
-      : html`<post-closebutton>${unsafeHTML(args['slots-default'])}</post-closebutton> `;
+    const renderCloseButton = getCloseButtonRenderer(
+      'position-absolute top-0 start-100 translate-middle',
+    );
+    return html` <div class="position-relative">Closable element ${renderCloseButton(args)}</div> `;
   },
 };
