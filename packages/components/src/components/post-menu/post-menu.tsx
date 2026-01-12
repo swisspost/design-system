1cf5795565233b844d88ca84dbfc71677bb09756
Myrta Sakellariou <66249294+myrta2302@users.noreply.github.com>
2026-01-12 14:40:58 +0100

    feat(components): enable open animation on `<post tooltip>` (#6821)
    ## 📄 Description
    
    This PR updates `<post-tooltip>` so that it now correctly uses the open
    animation provided by `<post-popovercontainer>`, which was previously
    not visible.
    
    Additionally, but not directly connected to the scope of the current
    ticket it:
    - Removes the animation cancellation logic from the popovercontainer,
    that seems to not work as expected.
    - Includes a small improvement on the `<post-popover>` component.
    (Popover animation has already been enabled in the #6538 )
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [ ] No design review needed
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes
    
    ---------
    
    Co-authored-by: Alizé Debray <33580481+alizedebray@users.noreply.github.com>

--- packages/components/src/components/post-menu/post-menu.tsx
+++ packages/components/src/components/post-menu/post-menu.tsx
@@ -167,7 +167,6 @@ export class PostMenu {
   private handlePostBeforeToggle(event: CustomEvent<{ willOpen: boolean }>) {
     this.isVisible = event.detail.willOpen;
     this.toggleMenu.emit(this.isVisible);
-
     if (this.isVisible) {
       this.lastFocusedElement = this.root?.activeElement as HTMLElement;
       requestAnimationFrame(() => {
@@ -180,6 +179,7 @@ export class PostMenu {
       this.lastFocusedElement.focus();
     }
   }
+
 
   private readonly handleClick = (e: MouseEvent) => {
     const target = e.target as HTMLElement;
