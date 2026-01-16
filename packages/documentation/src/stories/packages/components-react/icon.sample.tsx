864e9ada87101f399525615eb2ce16ecec418104
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2026-01-16 10:56:53 +0100

    chore(docs): update react documentation

---
+++ packages/documentation/src/stories/packages/components-react/icon.sample.tsx
@@ -0,0 +1,5 @@
+import { PostIconLetter } from '@swisspost/design-system-components-react/icons/index';
+
+export default function Page() {
+  return <PostIconLetter size="1.5em"></PostIconLetter>;
+}
