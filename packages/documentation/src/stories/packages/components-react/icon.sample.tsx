518d980e2b591bfd70f389a7fa85110c28c39f8d
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2026-01-21 14:06:56 +0100

    updage docs

--- packages/documentation/src/stories/packages/components-react/icon.sample.tsx
+++ packages/documentation/src/stories/packages/components-react/icon.sample.tsx
@@ -1,5 +1,31 @@
-import { PostIconLetter } from '@swisspost/design-system-components-react/icons';
+import {
+  PostIconLetter,
+  PostIconLetterSolid,
+} from '@swisspost/design-system-components-react/icons';
 
 export default function Page() {
-  return <PostIconLetter style={{ fontSize: '1.5em' }}></PostIconLetter>;
+  return (
+    <>
+      {/* Regular letter icon */}
+      <PostIconLetter></PostIconLetter>
+
+      {/* Solid letter icon */}
+      <PostIconLetterSolid></PostIconLetterSolid>
+
+      {/* Letter icon with custom styles, fontSize defines icon size */}
+      <PostIconLetter style={{ color: 'red', fontSize: '2em' }}></PostIconLetter>
+
+      {/* Flipped letter icon */}
+      <PostIconLetter flipH={true}></PostIconLetter>
+
+      {/* Rotated letter icon */}
+      <PostIconLetter rotate={45}></PostIconLetter>
+
+      {/* Scaled letter icon (applies a transform, does not change the used space of the icon) */}
+      <PostIconLetter scale={2}></PostIconLetter>
+
+      {/* Animated letter icon */}
+      <PostIconLetter animation="spin"></PostIconLetter>
+    </>
+  );
 }
