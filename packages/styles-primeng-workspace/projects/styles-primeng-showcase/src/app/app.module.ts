5e330bb0df9515974fc0c68065efce296c2907de
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-01-21 15:54:27 +0100

    remove deprecated animations module

--- packages/styles-primeng-workspace/projects/styles-primeng-showcase/src/app/app.module.ts
+++ packages/styles-primeng-workspace/projects/styles-primeng-showcase/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { TableModule } from 'primeng/table';
 import { providePrimeNG } from 'primeng/config';
@@ -11,8 +9,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, TableModule, FormsModule],
-  providers: [provideAnimationsAsync(), providePrimeNG(), provideZoneChangeDetection()],
+  imports: [BrowserModule, TableModule, FormsModule],
+  providers: [providePrimeNG(), provideZoneChangeDetection()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
