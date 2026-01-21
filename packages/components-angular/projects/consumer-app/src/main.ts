764db36b55d000e7ff978edbe90e06c2b0d83aa3
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-01-21 08:55:35 +0100

    revert components-angular main.ts

--- packages/components-angular/projects/consumer-app/src/main.ts
+++ packages/components-angular/projects/consumer-app/src/main.ts
@@ -1,8 +1,7 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
