a6c5ca1f9697929da89273f0fd740f6a71267336
Alizé Debray <33580481+alizedebray@users.noreply.github.com>
2026-01-23 10:34:34 +0100

    chore(docs): deprecate header properties (#6946)
    ## 📄 Description
    
    Deprecate header properties that are removed in v10.
    
    ## 🚀 Demo
    
    https://preview-6946--swisspost-design-system.netlify.app/?path=/docs/ebb11274-091b-4cb7-9a3f-3e0451c9a865--docs
    
    ---
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [x] No design review needed
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes

--- packages/documentation/src/stories/components/internet-header/header/overrides-stories/header-custom-config.stories.ts
+++ packages/documentation/src/stories/components/internet-header/header/overrides-stories/header-custom-config.stories.ts
@@ -3,7 +3,7 @@ import * as HeaderStories from '@/stories/components/internet-header/header/head
 export default {
   ...HeaderStories.default,
   id: 'bfdf4e7c-37d3-40f8-a5d0-734f3e6612b4',
-  title: 'Components/Internet Header/Header/Custom Config',
+  title: 'Components/Internet Header/Header/Custom Config (deprecated)',
 };
 
 export const Default = HeaderStories.CustomConfig;
