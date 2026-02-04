7afc18bdbed3c2306aa1afe745a3c16a2fc01004
alionazherdetska <alionazhv@gmail.com>
2026-02-04 17:00:16 +0100

    refactored the config

--- packages/components/playwright.config.ts
+++ packages/components/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : 8,
+  workers: process.env.CI ? 8 : undefined,
 
   reporter: [
     ['html', { outputFolder: 'test-results/html-report' }],
