a5d3161d4d17be17fde822cebace403bb0e80c20
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-02-05 11:52:46 +0100

    Revert "karma links update"
    This reverts commit 368ad0b67d8e945f3509873ddde69e12116842b7.

--- packages/intranet-header-workspace/projects/intranet-header-showcase/karma.conf.js
+++ packages/intranet-header-workspace/projects/intranet-header-showcase/karma.conf.js
@@ -4,13 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular/build/builders/karma'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       jasmine: {
--- packages/intranet-header-workspace/projects/intranet-header/karma.conf.js
+++ packages/intranet-header-workspace/projects/intranet-header/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/build'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
