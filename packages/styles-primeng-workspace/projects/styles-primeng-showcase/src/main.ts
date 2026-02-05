b387f569f5832fe6cefeca907d1041fe01c27449
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-02-05 16:35:30 +0100

    fix code smell

--- packages/styles-primeng-workspace/projects/styles-primeng-showcase/src/main.ts
+++ packages/styles-primeng-workspace/projects/styles-primeng-showcase/src/main.ts
@@ -1,8 +1,7 @@
-import { provideZoneChangeDetection } from "@angular/core";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowser()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
