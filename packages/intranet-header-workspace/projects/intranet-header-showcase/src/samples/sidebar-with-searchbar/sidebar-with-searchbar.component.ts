e2ebbd4158617281c116f93c4e990800c4aba4f6
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-02-05 15:18:03 +0100

    update intranet header to 19 no migrations

--- packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/sidebar-with-searchbar/sidebar-with-searchbar.component.ts
+++ packages/intranet-header-workspace/projects/intranet-header-showcase/src/samples/sidebar-with-searchbar/sidebar-with-searchbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'samples-sidebar-with-searchbar',
-  templateUrl: './sidebar-with-searchbar.component.html',
+    selector: 'samples-sidebar-with-searchbar',
+    templateUrl: './sidebar-with-searchbar.component.html',
+    standalone: false
 })
 export class SamplesSidebarWithSearchbarComponent {
   title = 'samples-sidebar-with-searchbar';
