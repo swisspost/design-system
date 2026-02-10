3149dbf77b37fb491e48c673f0a013f42094c3d4
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-02-10 18:03:03 +0100

    revert files

--- packages/documentation/.storybook/main.ts
+++ packages/documentation/.storybook/main.ts
@@ -80,7 +80,6 @@ const config: StorybookConfig = {
     STORYBOOK_GTM_PAGE_CONTEXT_ENVIRONMENT_PROD: 'design-system.post.ch',
     STORYBOOK_GTM_PAGE_CONTEXT_ENVIRONMENT_FALLBACK: 'dev',
     STORYBOOK_BASE_URL: 'https://design-system.post.ch',
-    STORYBOOK_DISABLE_TELEMETRY: '1',
   }),
 
   async viteFinal(config: InlineConfig) {
