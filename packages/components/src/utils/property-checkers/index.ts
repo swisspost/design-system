ed8385618bbb43a77216fe07309b0e55d632e024
Alizé Debray <33580481+alizedebray@users.noreply.github.com>
2026-02-16 14:15:55 +0100

    chore(component): update utils to prepare for slider (#6436)
    ## 📄 Description
    
    Update component utilities that will be used in the slider component.
    
    ---
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [x] No design review needed
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes

--- packages/components/src/utils/property-checkers/index.ts
+++ packages/components/src/utils/property-checkers/index.ts
@@ -4,13 +4,16 @@ import { checkPattern } from './check-pattern';
 import { checkType } from './check-type';
 import { checkUrl } from './check-url';
 import { emptyOr } from './empty-or';
+import { checkArrayOf } from '@/utils/property-checkers/check-array-of';
 
 export const checkEmptyOrOneOf = emptyOr(checkOneOf);
 export const checkEmptyOrPattern = emptyOr(checkPattern);
 export const checkEmptyOrType = emptyOr(checkType);
 export const checkEmptyOrUrl = emptyOr(checkUrl);
+export const checkEmptyOrArrayOf = emptyOr(checkArrayOf);
 
 export const checkRequiredAndOneOf = requiredAnd(checkOneOf);
 export const checkRequiredAndPattern = requiredAnd(checkPattern);
 export const checkRequiredAndType = requiredAnd(checkType);
 export const checkRequiredAndUrl = requiredAnd(checkUrl);
+export const checkRequiredAndArrayOf = requiredAnd(checkArrayOf);
