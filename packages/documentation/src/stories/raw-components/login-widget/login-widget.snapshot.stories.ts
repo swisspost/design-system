b6ef20fa9a0b97b01c07847c4fc2c48dffb927a8
alionazherdetska <alionazhv@gmail.com>
2026-03-02 17:18:05 +0100

    removed the redundant import

--- packages/documentation/src/stories/raw-components/login-widget/login-widget.snapshot.stories.ts
+++ packages/documentation/src/stories/raw-components/login-widget/login-widget.snapshot.stories.ts
@@ -1,4 +1,4 @@
-import type { Args, StoryContext, StoryObj } from '@storybook/web-components-vite';
+import type { Args, StoryObj } from '@storybook/web-components-vite';
 import meta from './login-widget.stories';
 import { html } from 'lit';
 import { schemes } from '@/shared/snapshots/schemes';
