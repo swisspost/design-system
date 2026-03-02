967f34f9ae69ef13af65f1c7a552fb969f735ee4
alionazherdetska <alionazhv@gmail.com>
2026-03-02 17:14:11 +0100

    added e2e tesst

---
+++ packages/documentation/src/stories/raw-components/login-widget/login-widget.snapshot.stories.ts
@@ -0,0 +1,28 @@
+import type { Args, StoryContext, StoryObj } from '@storybook/web-components-vite';
+import meta from './login-widget.stories';
+import { html } from 'lit';
+import { schemes } from '@/shared/snapshots/schemes';
+
+const { id, ...metaWithoutId } = meta;
+
+export default {
+  ...metaWithoutId,
+  title: 'Snapshots',
+};
+
+type Story = StoryObj<HTMLPostLoginWidgetElement>;
+
+export const PostLoginWidget: Story = {
+  render: (args: Args) => {
+    return schemes(() => html`
+      <post-login-widget
+        login-url=${args.loginUrl}
+        logout-url=${args.logoutUrl}
+        text-user-profile=${args.textUserProfile}
+        text-messages=${args.textMessages}
+        text-settings=${args.textSettings}
+        text-logout=${args.textLogout}
+      ></post-login-widget>
+    `);
+  },
+};
