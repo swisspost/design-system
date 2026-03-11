c0a01d226829d53f694b0babbe371d8111f4a0c8
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-03-11 14:16:31 +0100

    removed temp color scheme filter

--- packages/documentation/src/stories/components/datepicker/datepicker.snapshot.stories.ts
+++ packages/documentation/src/stories/components/datepicker/datepicker.snapshot.stories.ts
@@ -1,7 +1,7 @@
 import type { Args, StoryContext, StoryObj } from '@storybook/web-components-vite';
 import meta from './datepicker.stories';
 import { html } from 'lit';
-import { COLOR_SCHEMES, schemes } from '@/shared/snapshots/schemes';
+import { schemes } from '@/shared/snapshots/schemes';
 import { bombArgs } from '@/utils';
 
 const { id, ...metaWithoutId } = meta;
@@ -20,21 +20,20 @@ export const Datepicker: Story = {
         <div class="row">
           <h1>Datepicker</h1>
           ${bombArgs({
-            inline: [true,false],
-            range: [true, false]
-          })
-            .map((args: Args) => {
-              return html`
-                <div class="col-md-6 mb-32">
-                  <h2 class="h4">${args.range ? 'Range' : 'Simple'} ${args.inline ? 'inline' : 'input'} datepicker</h2>
-                  ${meta.render?.({ ...context.args, ...args }, context)}
-                </div>
-              `
-            })}
+            inline: [true, false],
+            range: [true, false],
+          }).map((args: Args) => {
+            return html`
+              <div class="col-md-6 mb-32">
+                <h2 class="h4">
+                  ${args.range ? 'Range' : 'Simple'} ${args.inline ? 'inline' : 'input'} datepicker
+                </h2>
+                ${meta.render?.({ ...context.args, ...args }, context)}
+              </div>
+            `;
+          })}
         </div>
       `,
-      // Remove this when styling is done
-      {filter: scheme => scheme === COLOR_SCHEMES.light,
-    });
+    );
   },
 };
