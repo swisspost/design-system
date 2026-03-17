2d824855098a40c6a47f9531f4997c5f20830c79
leagrdv <lea.gardavaud@post.ch>
2026-03-17 16:32:57 +0100

    address PR comments

--- packages/documentation/src/stories/getting-started/packages/components-angular/angular-app-standalone-main.sample.ts
+++
@@ -1,8 +0,0 @@
-// main.ts
-
-import { importProvidersFrom } from '@angular/core';
-import { PostComponentsModule } from '@swisspost/design-system-components-angular';
-
-providers: [
-  importProvidersFrom(PostComponentsModule),
-]
