fccdcae476a9b1b7d672cbb33f30955ef54990ff
Machado da Silva Hugo <hugomslv@users.noreply.github.com>
2026-03-17 14:11:51 +0100

    feat(inline-notification): add inline notification component with accessibility tests and documentation

---
+++ packages/documentation/cypress/e2e/components/inline-notification.cy.ts
@@ -0,0 +1,13 @@
+describe('Inline Notification', () => {
+  describe('Accessibility', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?id=snapshots--inline-notification');
+      cy.get('.inline-notification', { timeout: 30000 }).should('be.visible');
+      cy.injectAxe();
+    });
+
+    it('Has no detectable a11y violations on load for all variants', () => {
+      cy.checkA11y('#root-inner');
+    });
+  });
+});
