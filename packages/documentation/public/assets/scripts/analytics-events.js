b676055600a41923109fd61c1980d5aa658b3331
Alona Zherdetska <138328641+alionazherdetska@users.noreply.github.com>
2026-03-20 10:21:52 +0100

    chore(sonar): resolve SonarQubecode smells (#7381)
    ## 📄 Description
    
    This PR addresses all the SonarQube issues except for those in
    `klp-login-widget` and `internet-header` since both components are being
    replaced soon, so fixing them now would be redundant.
    
    The deprecated `platformBrowserDynamic` will be addressed in
    https://github.com/swisspost/design-system/pull/7371
    
    ## 🚀 Demo
    
    If applicable, please add a screenshot or video to illustrate the
    changes.
    
    ---
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [ ] No design review needed
    
    ## 🧪 Visual regression tests
    
    - [ ] Visual changes detected and approved _(Check this box if VRT fails
    and changes are intentional)_
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes

--- packages/documentation/public/assets/scripts/analytics-events.js
+++ packages/documentation/public/assets/scripts/analytics-events.js
@@ -1,15 +1,15 @@
-if (window.__GTM__) {
+if (globalThis.__GTM__) {
   // Page Context
-  window.addEventListener('storybook:ready', function () {
-    window.dataLayer.push({
+  globalThis.addEventListener('storybook:ready', function () {
+    globalThis.dataLayer.push({
       event: 'page_context', // constant, required
       spa: 'true', // constant, required
-      content_language: window.__GTM__.get('CONTENT_LANGUAGE'), // required
-      content_geo_region: window.__GTM__.get('GEO_REGION'), // required
-      source_code_version: window.__GTM__.get('SOURCE_CODE_VERSION'), // recommended
-      page_name_language_neutral: window.__GTM__.getPageTitle(), // required
-      page_url_language_neutral: window.location.href, // required
-      environment: window.__GTM__.getEnvironment(), // required
+      content_language: globalThis.__GTM__.get('CONTENT_LANGUAGE'), // required
+      content_geo_region: globalThis.__GTM__.get('GEO_REGION'), // required
+      source_code_version: globalThis.__GTM__.get('SOURCE_CODE_VERSION'), // recommended
+      page_name_language_neutral: globalThis.__GTM__.getPageTitle(), // required
+      page_url_language_neutral: globalThis.location.href, // required
+      environment: globalThis.__GTM__.getEnvironment(), // required
       login_status: 'false', // required
       platform_name: 'ms_design_system', // required
       department_name: 'IT16.12', // required
@@ -19,16 +19,16 @@ if (window.__GTM__) {
   });
 
   // Page Change
-  window.addEventListener('storybook:routeChange', function () {
-    window.dataLayer.push({
+  globalThis.addEventListener('storybook:routeChange', function () {
+    globalThis.dataLayer.push({
       event: 'page_change', // constant, required
-      content_language: window.__GTM__.get('CONTENT_LANGUAGE'), // required
-      content_geo_region: window.__GTM__.get('GEO_REGION'), // required
-      page_name_language_neutral: window.__GTM__.getPageTitle(), // required
-      page_url_language_neutral: window.location.href, // required
+      content_language: globalThis.__GTM__.get('CONTENT_LANGUAGE'), // required
+      content_geo_region: globalThis.__GTM__.get('GEO_REGION'), // required
+      page_name_language_neutral: globalThis.__GTM__.getPageTitle(), // required
+      page_url_language_neutral: globalThis.location.href, // required
       virtual_pageview: 'true', // SPA route change indicator, boolean true/false
-      virtual_page_name: window.__GTM__.getPageTitle(), // SPA route change/transition new page name/page title
-      virtual_url: window.location.href, // SPA route change new URL
+      virtual_page_name: globalThis.__GTM__.getPageTitle(), // SPA route change/transition new page name/page title
+      virtual_url: globalThis.location.href, // SPA route change new URL
     });
   });
 }
