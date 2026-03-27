44be244b6a8917f37e7fb836fffcdec20db199f7
alionazherdetska <alionazhv@gmail.com>
2026-03-27 19:15:33 +0100

    feat(components): add global styles to the components global styles entry

--- packages/documentation/.storybook/preview.ts
+++ packages/documentation/.storybook/preview.ts
@@ -14,6 +14,7 @@ import {
 import './helpers/register-web-components';
 import './addons/cypress-storybook/client';
 
+import '@swisspost/design-system-components/post-components.css';
 import './styles/preview.scss';
 
 import { SyntaxHighlighter } from 'storybook/internal/components';
