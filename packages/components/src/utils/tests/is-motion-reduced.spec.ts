4564b832856c62e0836dbda0fa7f288610c917a7
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-03-27 08:43:13 +0100

    review comment update

--- packages/components/src/utils/tests/is-motion-reduced.spec.ts
+++ packages/components/src/utils/tests/is-motion-reduced.spec.ts
@@ -1,23 +1,22 @@
 describe('isMotionReduced', () => {
   let matchMedia;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     jest.resetModules();
     matchMedia = window.matchMedia = jest.fn();
+    const { Build } = await import('@stencil/core');
+    Build.isBrowser = true; // Mocking the Browser, as Stencil's Build.isBrowser = false and Build.isServer = true in Jest.
   });
 
   it('should return true if reduced motion is requested', async () => {
     matchMedia.mockReturnValue({ matches: true });
-    const { Build } = await import('@stencil/core');
-    Build.isBrowser = true; // Mocking the Browser, as Stencil's Build.isBrowser = false and Build.isServer = true in Jest.
     const { isMotionReduced } = await import('../is-motion-reduced');
     expect(isMotionReduced()).toBe(true);
   });
 
   it('should return false if reduced motion is not requested', async () => {
     matchMedia.mockReturnValue({ matches: false });
-    const { Build } = await import('@stencil/core');
-    Build.isBrowser = true;
+
     const { isMotionReduced } = await import('../is-motion-reduced');
     expect(isMotionReduced()).toBe(false);
   });
