008b83479b70644575c6401b2e571a61fcf48767
Machado da Silva Hugo <hugomslv@users.noreply.github.com>
2026-03-31 15:40:58 +0200

    fix e2e

--- packages/documentation/cypress/e2e/components/inline-notification.cy.ts
+++ packages/documentation/cypress/e2e/components/inline-notification.cy.ts
@@ -5,12 +5,13 @@ describe('Inline Notification', () => {
       cy.get('.inline-notification', { timeout: 30000 }).should('be.visible');
       cy.injectAxe();
     });
-    
-  describe('Snapshots', () => {
-    it('renders all variants with and without title', () => {
-      cy.visit('/iframe.html?id=snapshots--inline-notification');
-      cy.get('.inline-notification', { timeout: 30000 }).should('be.visible');
-      cy.percySnapshot('Inline Notification - All Variants');
+
+    describe('Snapshots', () => {
+      it('renders all variants with and without title', () => {
+        cy.visit('/iframe.html?id=snapshots--inline-notification');
+        cy.get('.inline-notification', { timeout: 30000 }).should('be.visible');
+        cy.percySnapshot('Inline Notification - All Variants');
+      });
     });
   });
 });
