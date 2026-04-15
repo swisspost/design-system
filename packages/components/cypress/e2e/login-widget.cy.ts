8084051ac9ff1a4400914abea5a3e97f9e3ca5df
alionazherdetska <alionazhv@gmail.com>
2026-04-15 11:11:49 +0200

    fixed sonar

--- packages/components/cypress/e2e/login-widget.cy.ts
+++ packages/components/cypress/e2e/login-widget.cy.ts
@@ -57,7 +57,7 @@ describe('post-login-widget', { baseUrl: null }, () => {
     cy.get('[data-testid="login-link"]').should('be.visible');
 
     cy.intercept('GET', SESSION_URL, { body: AUTH_FIXTURE }).as('session2');
-    cy.get('post-login-widget').then(([el]) => (el as HTMLPostLoginWidgetElement).refresh());
+    cy.get('post-login-widget').then(([el]) => el.refresh());
     cy.wait('@session2');
 
     cy.get('[data-testid="user-menu"]').should('be.visible');
