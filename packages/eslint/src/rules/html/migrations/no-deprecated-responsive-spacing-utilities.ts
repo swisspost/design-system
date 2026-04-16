768785eca3ede6c3bb9693fe9b81841c07525a74
alionazherdetska <alionazhv@gmail.com>
2026-04-16 10:37:21 +0200

    aligned the spacing

--- packages/eslint/src/rules/html/migrations/no-deprecated-responsive-spacing-utilities.ts
+++ packages/eslint/src/rules/html/migrations/no-deprecated-responsive-spacing-utilities.ts
@@ -6,14 +6,14 @@ const prefixes = ['m', 'mt', 'mb', 'ml', 'mr', 'p', 'pt', 'pb', 'pl', 'pr', 'gap
 // Previous size tokens mapped to the new responsive pixel class suffixes.
 // Entries marked needsReview: true are not 1:1 migrations and must be manually checked.
 const sizeMap: Record<string, { suffixes: string[]; needsReview?: boolean }> = {
-  'tiny':       { suffixes: ['-12', '-md-16'] },
-  'small':      { suffixes: ['-12', '-sm-16'] },
-  'regular':    { suffixes: ['-16', '-md-24'] },
-  'large':      { suffixes: ['-16', '-md-24', '-lg-32'], needsReview: true },
-  'big':        { suffixes: ['-24', '-md-32', '-lg-40'], needsReview: true },
+  'tiny': { suffixes: ['-12', '-md-16'] },
+  'small': { suffixes: ['-12', '-sm-16'] },
+  'regular': { suffixes: ['-16', '-md-24'] },
+  'large': { suffixes: ['-16', '-md-24', '-lg-32'], needsReview: true },
+  'big': { suffixes: ['-24', '-md-32', '-lg-40'], needsReview: true },
   'bigger-big': { suffixes: ['-24', '-md-48'] },
-  'huge':       { suffixes: ['-32', '-md-40', '-lg-56'], needsReview: true },
-  'giant':      { suffixes: ['-56', '-md-80'] },
+  'huge': { suffixes: ['-32', '-md-40', '-lg-56'], needsReview: true },
+  'giant': { suffixes: ['-56', '-md-80'] },
 };
 
 const classesMap = prefixes.flatMap(prefix =>
