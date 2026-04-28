00d2fb71018837f502e512035023866fc5e70702
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-04-28 17:21:27 +0200

    cases excluded from sonar

--- apps/documentation/src/stories/packages/theme-ag-grid/theme-ag-grid-usage.sample.js
+++ apps/documentation/src/stories/packages/theme-ag-grid/theme-ag-grid-usage.sample.js
@@ -1,5 +1,5 @@
 import { swissPostTheme } from '@swisspost/design-system-theme-ag-grid';
-
-myOptions: GridOptions = {
+// prettier-ignore
+myOptions: GridOptions = { // NOSONAR javascript:S2703
   theme: swissPostTheme,
 };
