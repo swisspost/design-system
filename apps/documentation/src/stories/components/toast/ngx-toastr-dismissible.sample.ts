4335b3b63704f9472fba72fd4ee6eded0613cc51
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-04-29 08:59:22 +0200

    corrected nosonar HTML tag

--- apps/documentation/src/stories/components/toast/ngx-toastr-dismissible.sample.ts
+++ apps/documentation/src/stories/components/toast/ngx-toastr-dismissible.sample.ts
@@ -1,5 +1,5 @@
-this.toastr.success('The task has been completed.', 'Success!', {
-  // NOSONAR
+// prettier-ignore
+this.toastr.success('The task has been completed.', 'Success!', {// NOSONAR
   toastClass: 'toast toast-success toast-dismissible',
   closeButton: true,
 });
