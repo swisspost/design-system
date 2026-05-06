da310adb096f046ff78cd1b9767a07b4a56f2644
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-05-06 16:01:13 +0200

    fix code smell

--- packages/icons/tests/__mocks__/node-fetch.js
+++ packages/icons/tests/__mocks__/node-fetch.js
@@ -1,6 +1,5 @@
 const fetch = jest.fn();
 module.exports = fetch;
 module.exports.default = fetch;
-module.exports.Response = class Response {
-  /* Mock implementation for testing purposes */
-};
+module.exports.Response = class Response { // NOSONAR };
+}
