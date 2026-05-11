4929332c00a6e4032a627daadade180b1f710ef8
Myrta Sakellariou <myrta.sakellariou@gmail.com>
2026-05-11 11:35:52 +0200

    fix lint error

--- packages/icons/tests/__mocks__/node-fetch.js
+++ packages/icons/tests/__mocks__/node-fetch.js
@@ -1,4 +1,5 @@
 const fetch = jest.fn();
 module.exports = fetch;
 module.exports.default = fetch;
-module.exports.Response = class Response { // NOSONAR };
+module.exports.Response = class Response { // NOSONAR 
+};
