b31d16abf5fa7533d7ef9d24a6a463166ff89ea4
Alizé Debray <alize.debray@post.ch>
2026-05-25 16:23:32 +0200

    remove less than check

--- packages/components/src/utils/property-checkers/index.ts
+++ packages/components/src/utils/property-checkers/index.ts
@@ -2,7 +2,6 @@ import { checkArrayOf } from '@/utils/property-checkers/check-array-of';
 import { checkBetween } from './check-between';
 import { checkDate } from './check-date';
 import { checkGreaterThan } from './check-greater-than';
-import { checkLessThan } from './check-less-than';
 import { checkOneOf } from './check-one-of';
 import { checkPattern } from './check-pattern';
 import { checkType } from './check-type';
@@ -17,7 +16,6 @@ export const checkEmptyOrUrl = emptyOr(checkUrl);
 export const checkEmptyOrArrayOf = emptyOr(checkArrayOf);
 export const checkEmptyOrDate = emptyOr(checkDate);
 export const checkEmptyOrGreaterThan = emptyOr(checkGreaterThan);
-export const checkEmptyOrLessThan = emptyOr(checkLessThan);
 export const checkEmptyOrBetween = emptyOr(checkBetween);
 
 export const checkRequiredAndOneOf = requiredAnd(checkOneOf);
@@ -26,7 +24,6 @@ export const checkRequiredAndType = requiredAnd(checkType);
 export const checkRequiredAndUrl = requiredAnd(checkUrl);
 export const checkRequiredAndArrayOf = requiredAnd(checkArrayOf);
 export const checkRequiredAndGreaterThan = requiredAnd(checkGreaterThan);
-export const checkRequiredAndLessThan = requiredAnd(checkLessThan);
 export const checkRequiredAndBetween = requiredAnd(checkBetween);
 
 export { checkIsoDate } from './check-iso-date';
