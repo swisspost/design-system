54edcedc38499b759a9da025dbca8f426464d555
Oliver Schürch <oli@skaos.ch>
2026-05-29 14:43:09 +0200

    Reapply "fix(components): include support for the new `:open` selector in the popovercontainer e2e helper"
    This reverts commit 26fe706bc9fc2b5ef76b2a2a91662852eb0ebcfc.

--- packages/components/cypress/e2e/helper/popovercontainer.ts
+++ packages/components/cypress/e2e/helper/popovercontainer.ts
@@ -1,7 +1,11 @@
-export function isPopoverSupported() {
-  return (
-    typeof HTMLElement !== 'undefined' &&
-    typeof HTMLElement.prototype === 'object' &&
-    'popover' in HTMLElement.prototype
-  );
+export function getPopoverOpenSelector(): string {
+  if (typeof CSS !== 'undefined' && CSS.supports) {
+    if (CSS.supports('selector(:popover-open)')) {
+      return ':popover-open';
+    }
+    if (CSS.supports('selector(:open)')) {
+      return ':open';
+    }
+  }
+  return String.raw`.\:popover-open`;
 }
