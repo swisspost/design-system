6d7db69881b6e94b1dcbb8044049a8dbde7c12dc
Oliver Schürch <oli@skaos.ch>
2026-05-29 11:42:00 +0200

    chore(components): add nanoid utility comment

--- packages/components/src/utils/nanoid.ts
+++ packages/components/src/utils/nanoid.ts
@@ -1,3 +1,11 @@
+/**
+ * Initially copy'n'pasted from https://github.com/ai/nanoid/blob/main/non-secure/index.js, then adopted to our needs.
+ *
+ * Our custom implementation generates unique IDs for DOM elements efficiently without requiring cryptographic security (as nanoid).
+ * It eliminates the nanoid package dependency (as well as the transistive `node:crypto` dependency in node environments), meeting all our requirements.
+ * This approach optimizes performance and minimizes the codebase footprint.
+ */
+
 const ALPHABET = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict';
 
 export function nanoid(size = 21) {
