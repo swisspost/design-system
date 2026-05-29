70f23a2e8512cad5658fa0099e9d821dfa80150c
Oliver Schürch <oli@skaos.ch>
2026-05-29 12:59:54 +0200

    fix(components): lint error

--- packages/components/src/hydrate-stub/index.stub.js
+++ packages/components/src/hydrate-stub/index.stub.js
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/no-empty-file */
 /**
  * This is a stub file for stencils hydrate app (helps generating declarative shadow DOM (DSD) during server-side rendering).
  * Whenever the hydrate app is imported in a non-nodejs environment, this stub file will be returned instead.
