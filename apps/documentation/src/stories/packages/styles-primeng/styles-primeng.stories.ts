34579de439b0e3a5ec1f84bc88d00fb680165503
Lea <lea.gardavaud@post.ch>
2026-06-25 13:10:05 +0200

    feat(styles-primeng): add `design-system-styles-primeng` package back with preset (#7919)
    ## 📄 Description
    
    Please include a summary of the changes made in this PR.
    
    ## 🚀 Demo
    
    If applicable, please add a screenshot or video to illustrate the
    changes.
    
    ---
    
    ## 🔮 Design review
    
    - [ ] Design review done
    - [ ] No design review needed
    
    ## 🧪 Visual regression tests
    
    - [x] Visual changes detected and approved _(Check this box if VRT fails
    and changes are intentional)_
    
    ## 📝 Checklist
    
    - ✅ My code follows the style guidelines of this project
    - 🛠️ I have performed a self-review of my own code
    - 📄 I have made corresponding changes to the documentation
    - ⚠️ My changes generate no new warnings or errors
    - 🧪 I have added tests that prove my fix is effective or that my feature
    works
    - ✔️ New and existing unit tests pass locally with my changes
    
    ---------
    
    Co-authored-by: Alizé Debray <alize.debray@post.ch>

---
+++ apps/documentation/src/stories/packages/styles-primeng/styles-primeng.stories.ts
@@ -0,0 +1,16 @@
+import { StoryObj } from '@storybook/web-components-vite';
+import { MetaExtended } from '@root/types';
+
+const meta: MetaExtended = {
+  id: 'd2112bed-c611-4098-a1ad-e654f7d622e7',
+  title: 'Packages/Styles PrimeNg',
+  parameters: {
+    badges: [],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {};
