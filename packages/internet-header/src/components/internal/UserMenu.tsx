a095798efb64a488546feb291483682a644495cc
alionazherdetska <alionazhv@gmail.com>
2026-07-09 13:48:23 +0200

    revretd redundant changes

--- packages/internet-header/src/components/internal/UserMenu.tsx
+++ packages/internet-header/src/components/internal/UserMenu.tsx
@@ -21,7 +21,7 @@ export const UserMenu: FunctionalComponent<{ config: UserMenuConfig } & UserMenu
   const isB2C = config.user.userType === 'B2C';
   const isB2B = config.user.userType === 'B2B';
   const canChangeUserAndProfile = (isB2C || isB2B) && config.user.changeUserAndProfile === 'userAndProfile';
-  const canChangeCompany = isB2B && !!config.user.canChangeCompany && !!config.user.company;
+  const canChangeCompany = isB2B && config.user.canChangeCompany && config.user.company;
   const canSeeAccountSwitch = canChangeUserAndProfile || canChangeCompany;
 
   const userFullname = [config.user.name, config.user.surname].join(' ');
