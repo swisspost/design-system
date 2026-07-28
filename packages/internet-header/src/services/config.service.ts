595163f34b87f58c7897f5eac9399443a57602df
Philipp Gfeller <1659006+gfellerph@users.noreply.github.com>
2026-07-28 17:39:14 +0200

    update config URLs

--- packages/internet-header/src/services/config.service.ts
+++ packages/internet-header/src/services/config.service.ts
@@ -109,7 +109,7 @@ export const generateConfigUrl = (
   const parsedLang = lang.toLowerCase();
   const isProd = parsedEnvironment === 'PROD';
   // NOTE: use preview.post.ch for local testing
-  const host = `https://${isProd ? 'www' : 'int'}.post.ch`;
+  const host = `https://${isProd ? '' : 'int'}.preview.post.ch`;
 
   try {
     const query = new URLSearchParams({
