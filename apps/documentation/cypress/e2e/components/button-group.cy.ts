1775120075705829519f2badb2ae50567f655f9c
alionazherdetska <alionazhv@gmail.com>
2026-08-17 13:55:16 +0200

    reset the files

---
+++ apps/documentation/cypress/e2e/components/button-group.cy.ts
@@ -0,0 +1,15 @@
+describe('Button Group', () => {
+  describe('Accessibility', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?id=snapshots--button-group');
+      cy.get('.btn-group', { timeout: 30000 }).should('be.visible');
+      cy.injectAxe();
+    });
+
+    it('Has no detectable a11y violations on load for all variants', () => {
+      cy.checkA11y('#root-inner', undefined, (violations) => {
+        expect(violations).to.have.length(0);
+      });
+    });
+  });
+});
