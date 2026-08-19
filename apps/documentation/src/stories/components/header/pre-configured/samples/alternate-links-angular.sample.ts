95408fd631fc2a34086bb734f9191fdc92993d17
alionazherdetska <alionazhv@gmail.com>
2026-08-19 11:20:17 +0200

    refactored the examples

--- apps/documentation/src/stories/components/header/pre-configured/samples/alternate-links-angular.sample.ts
+++ apps/documentation/src/stories/components/header/pre-configured/samples/alternate-links-angular.sample.ts
@@ -2,20 +2,14 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
-const LANGUAGES = ['de', 'fr', 'it', 'en'];
-
 @Component({ selector: 'app-root', templateUrl: './app.component.html' })
 export class AppComponent {
   constructor(private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
-        const pathWithoutLang = event.urlAfterRedirects.replace(/^\/(de|fr|it|en)/, '');
-
-        for (const lang of LANGUAGES) {
-          const link = document.querySelector<HTMLLinkElement>(`link[hreflang="${lang}"]`);
-          if (link) link.href = `/${lang}${pathWithoutLang}`;
-        }
+        // Always update related <link rel="alternate" hreflang="..." href="..."> elements, after every route change.
+        // You can use the header config, to find the language codes applied to the language switch, if needed.
       });
   }
 }
--- apps/documentation/src/stories/components/header/pre-configured/samples/alternate-links-react.sample.ts
+++ apps/documentation/src/stories/components/header/pre-configured/samples/alternate-links-react.sample.ts
@@ -1,17 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const LANGUAGES = ['de', 'fr', 'it', 'en'];
-
 export function useAlternateLinks() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const pathWithoutLang = pathname.replace(/^\/(de|fr|it|en)/, '');
-
-    for (const lang of LANGUAGES) {
-      const link = document.querySelector<HTMLLinkElement>(`link[hreflang="${lang}"]`);
-      if (link) link.href = `/${lang}${pathWithoutLang}`;
-    }
+    // Always update related <link rel="alternate" hreflang="..." href="..."> elements, after every route change.
+    // You can use the header config, to find the language codes applied to the header switch, if needed.
   }, [pathname]);
 }
