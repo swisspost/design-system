6dcbccd2fb2d4843bb408ee81e66fabbc41de2a1
Alizé Debray <alize.debray@post.ch>
2026-08-20 10:17:39 +0200

    revert unnecessary changes

--- apps/documentation/src/stories/components/tabs/tabs.stories.ts
+++ apps/documentation/src/stories/components/tabs/tabs.stories.ts
@@ -1,10 +1,8 @@
-import { defaultNav } from '@root/src/stories/components/side-navigation/nav-content';
-import { MetaComponent } from '@root/types';
 import { StoryObj } from '@storybook/web-components-vite';
-import { html, nothing } from 'lit';
+import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
-import { PartialStoryFn } from 'storybook/internal/types';
+import { MetaComponent } from '@root/types';
 
 const meta: MetaComponent<
   HTMLPostTabsElement & {
@@ -336,93 +334,4 @@ export const ActivePagesItem: Story = {
       </post-tab-item>
     `,
   },
-};
-
-// Temporary
-function paragraphs(length: number = 6) {
-  return Array.from({ length }).map(() => html`<p aria-hidden="true" class="fake-content"></p>`);
-}
-
-function header(story: PartialStoryFn) {
-  return html`
-    <post-header text-menu="Menu">
-      <post-logo slot="post-logo" url="/">Homepage</post-logo>
-      <post-side-navigation-trigger slot="side-nav" for="sidenavigation">
-        <button>
-          <span>Menu</span>
-          <post-icon aria-hidden="true" name="burger"></post-icon>
-        </button>
-      </post-side-navigation-trigger>
-      <p slot="title">[Application Title]</p>
-      <a href="" slot="local-nav">
-        <span>Login</span>
-        <post-icon name="login"></post-icon>
-      </a>
-    </post-header>
-    ${story()}
-  `;
-}
-
-function sideNav(story: PartialStoryFn) {
-  return html`
-    <post-side-navigation size="large" id="sidenavigation" text-close="Close">
-      <nav aria-label="Main navigation">${unsafeHTML(defaultNav)}</nav>
-    </post-side-navigation>
-
-    <main class="main-container">${story()}</main>
-  `;
-}
-
-function container(story: PartialStoryFn) {
-  return html` <div class="container my-16">${story()}</div> `;
-}
-
-function containerFluid(story: PartialStoryFn) {
-  return html` <div class="container-fluid my-16">${story()}</div> `;
-}
-
-function section(story: PartialStoryFn) {
-  return html`
-    <section class="section palette palette-accent">
-      <div class="container py-64">${story()}</div>
-    </section>
-  `;
-}
-
-export const TestBase: StoryObj = {
-  args: {
-    variant: 'Page Tabs',
-    showTabs: true,
-  },
-  render: args => html`${args.showTabs ? renderTabs(args) : nothing}${paragraphs()}`,
-};
-
-export const Container: Story = {
-  ...TestBase,
-  decorators: [container, header],
-};
-
-export const ContainerFluid: Story = {
-  ...TestBase,
-  decorators: [containerFluid, header],
-};
-
-export const Section: Story = {
-  ...TestBase,
-  decorators: [section, header],
-};
-
-export const ContainerAndSidenav: Story = {
-  ...TestBase,
-  decorators: [container, sideNav, header],
-};
-
-export const ContainerFluidAndSidenav: Story = {
-  ...TestBase,
-  decorators: [containerFluid, sideNav, header],
-};
-
-export const SectionAndSidenav: Story = {
-  ...TestBase,
-  decorators: [section, sideNav, header],
 };
