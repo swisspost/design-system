f97991822663d3b5d9529615582ca0184e24901c
alionazherdetska <alionazhv@gmail.com>
2026-08-28 16:50:11 +0200

    fixed sonar

--- apps/documentation/cypress/snapshots/components/tabs.snapshot.ts
+++ apps/documentation/cypress/snapshots/components/tabs.snapshot.ts
@@ -1,19 +1,15 @@
 describe('Tabs', () => {
-  it('content tabs', () => {
-    cy.visit('/iframe.html?id=snapshots--content-tabs');
-    cy.get('post-tab-item[data-hydrated]', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Tabs', { widths: [320, 600, 1440] });
-  });
+  const stories = [
+    { name: 'content tabs', storyId: 'snapshots--content-tabs' },
+    { name: 'page tabs', storyId: 'snapshots--page-tabs' },
+    { name: 'page tabs with sidenav', storyId: 'snapshots--page-tabs-with-sidenav' },
+  ];
 
-  it('page tabs', () => {
-    cy.visit('/iframe.html?id=snapshots--page-tabs');
-    cy.get('post-tab-item[data-hydrated]', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Tabs', { widths: [320, 600, 1440] });
-  });
-
-  it('page tabs with sidenav', () => {
-    cy.visit('/iframe.html?id=snapshots--page-tabs-with-sidenav');
-    cy.get('post-tab-item[data-hydrated]', { timeout: 30000 }).should('be.visible');
-    cy.percySnapshot('Tabs', { widths: [320, 600, 1440] });
+  stories.forEach(({ name, storyId }) => {
+    it(name, () => {
+      cy.visit(`/iframe.html?id=${storyId}`);
+      cy.get('post-tab-item[data-hydrated]', { timeout: 30000 }).should('be.visible');
+      cy.percySnapshot('Tabs', { widths: [320, 600, 1440] });
+    });
   });
 });
