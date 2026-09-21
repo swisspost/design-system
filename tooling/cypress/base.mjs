56ac429792e3b0d19acfc1bcb34ee98dc77cfd78
yomonom <yomonom@digamma.dev>
2026-09-21 16:23:40 +0200

    refactor: create dedicated browser plugin

--- tooling/cypress/base.mjs
+++ tooling/cypress/base.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'cypress';
-import { installBrowser, BROWSER } from './browser.mjs';
-
-const UNSUPPORTED_BROWSERS = new Set(['chrome', 'electron']);
+import { browserPlugin } from './plugins/browser.mjs';
 
 export default defineConfig({
   includeShadowDom: true,
@@ -10,26 +8,11 @@ export default defineConfig({
   },
   video: false,
   e2e: {
-    defaultBrowser: BROWSER.name,
     includeShadowDom: true,
     viewportWidth: 1024,
     viewportHeight: 576,
     async setupNodeEvents(_on, config) {
-      const browser = await installBrowser();
-
-      config.browsers = [
-        ...config.browsers.filter(browser => !UNSUPPORTED_BROWSERS.has(browser.name)),
-        {
-          name: BROWSER.name,
-          family: BROWSER.family,
-          channel: BROWSER.channel,
-          displayName: BROWSER.displayName,
-          version: browser.buildId,
-          majorVersion: Number.parseInt(browser.buildId, 10),
-          path: browser.executablePath,
-        },
-      ];
-
+      await browserPlugin(config);
       return config;
     },
   },
